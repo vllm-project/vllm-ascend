98b01b8f27a5dd3bf727ce4c5708ffa7b8d44275
wangxiyuan <wangxiyuan@huawei.com>
2025-01-13 10:20:04 +0800

    Update vllm_ascend/attention.py
    Co-authored-by: Mengqing Cao <cmq0113@163.com>

--- vllm_ascend/attention.py
+++ vllm_ascend/attention.py
@@ -19,7 +19,7 @@ from vllm.attention.ops.paged_attn import (PagedAttention,
                                            PagedAttentionMetadata)
 
 if TYPE_CHECKING:
-    from vllm_ascend.worker import ModelInputForNPUBuilder
+    from vllm_ascend.model_runner import ModelInputForNPUBuilder
 
 SHARE_MASK_TRIL_PREFIX_CACHE = None
 SHARE_MASK_TRIL = None
