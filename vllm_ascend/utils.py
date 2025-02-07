c03527a376efe893e392564b33289fe266c4efcb
hw_whx <wanghexiang7@huawei.com>
2025-02-07 10:26:03 +0800

    fix ci problems
    Signed-off-by: hw_whx <wanghexiang7@huawei.com>

--- vllm_ascend/utils.py
+++ vllm_ascend/utils.py
@@ -22,13 +22,14 @@ from vllm.logger import init_logger
 logger = init_logger(__name__)
 
 
-def try_register_lib(lib_name: str, lib_info: str = None):
+def try_register_lib(lib_name: str, lib_info: str = ""):
     import importlib
+    import importlib.util
     try:
         module_spec = importlib.util.find_spec(lib_name)
         if module_spec is not None:
             importlib.import_module(lib_name)
-            if lib_info is not None:
+            if lib_info:
                 logger.info(lib_info)
     except Exception:
         pass
