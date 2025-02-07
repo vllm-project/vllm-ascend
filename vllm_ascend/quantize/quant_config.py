d3323515f4b069ea0a4e920285cf6e47d89d584f
angazenn <zengyanjia@huawei.com>
2025-02-07 10:01:23 +0800

    fix import errors
    Signed-off-by: angazenn <zengyanjia@huawei.com>

--- vllm_ascend/quantize/quant_config.py
+++ vllm_ascend/quantize/quant_config.py
@@ -18,15 +18,17 @@
 from typing import Any, Dict, List, Optional
 
 import torch
-import torch_npu
 
 from vllm.logger import init_logger
-from vllm.model_executor.layers.linear import (LinearBase, LinearMethodBase, RowParallelLinear)
+from vllm.model_executor.layers.linear import (LinearBase, LinearMethodBase, 
+                                               RowParallelLinear, UnquantizedLinearMethod)
 from vllm.model_executor.layers.quantization import (register_quantization_config)
 from vllm.model_executor.layers.quantization.base_config import (QuantizationConfig, QuantizeMethodBase)
 from vllm.model_executor.parameter import (BasevLLMParameter,
                                            ChannelQuantScaleParameter,
                                            ModelWeightParameter)
+from vllm.model_executor.layers.quantization.utils.quant_utils import (
+    FUSED_LAYER_NAME_MAPPING)
 from vllm.distributed import get_tensor_model_parallel_rank
 from .quantizer import AscendQuantizer
 
