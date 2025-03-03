871b301ef13b1a2b53e445f9a7a6f02387dcf5ed
angazenn <zengyanjia@huawei.com>
2025-03-03 10:05:56 +0800

    fix ruff checkout
    Signed-off-by: angazenn <zengyanjia@huawei.com>

--- vllm_ascend/patch/__init__.py
+++ vllm_ascend/patch/__init__.py
@@ -14,5 +14,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import vllm_ascend.patch.patch_minicpm  # noqa
 import vllm_ascend.patch.patch_cache_dtype  # noqa
+import vllm_ascend.patch.patch_minicpm  # noqa
