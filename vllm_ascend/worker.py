47f35cb35d5c759261cf1aea8c6d6632b72118b8
SidaoY <1024863041@qq.com>
2025-03-03 19:50:31 +0800

    add noqa
    Signed-off-by: SidaoY <1024863041@qq.com>

--- vllm_ascend/worker.py
+++ vllm_ascend/worker.py
@@ -72,7 +72,7 @@ class NPUWorker(LocalOrDistributedWorkerBase):
         from vllm_ascend import patch  # noqa: F401
 
         # TODO: Remove this line after fixing the hard-coding issue in VLLM later.
-        from torch_npu.contrib import transfer_to_npu
+        from torch_npu.contrib import transfer_to_npu # noqa: F401
 
         WorkerBase.__init__(self, vllm_config=vllm_config)
         # Try to import mindie_turbo to accelerate vLLM inference.
