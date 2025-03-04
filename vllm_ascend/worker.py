aa43ce1d3d41817d4d257d4d4bffbdc7cd383224
mengwei805 <mengwei25@huawei.com>
2025-03-04 16:43:16 +0800

    bugfix
    Signed-off-by: mengwei805 <mengwei25@huawei.com>

--- vllm_ascend/worker.py
+++ vllm_ascend/worker.py
@@ -93,10 +93,6 @@ class NPUWorker(LocalOrDistributedWorkerBase):
         # mlp_speculator
         speculative_config = self.speculative_config
         model_config = self.model_config
-
-        if speculative_config:
-            from vllm_ascend import spec_decode  # noqa: F401
-
         speculative_args = {} if speculative_config is None \
             or (speculative_config.draft_model_config.hf_config.model_type ==
                 model_config.hf_config.model_type) \
