8e66a7ddc7202db097a550e336c6af01fd536713
htwang <onehaitao@foxmail.com>
2025-03-08 11:35:22 +0800

    add all to all and reduce scatter
    Signed-off-by: htwang <onehaitao@foxmail.com>

--- vllm_ascend/communicator.py
+++ vllm_ascend/communicator.py
@@ -17,6 +17,7 @@
 from typing import Optional
 
 import torch
+import torch.distributed as dist
 from torch.distributed import ProcessGroup
 from vllm.distributed.device_communicators.base_device_communicator import \
     DeviceCommunicatorBase
@@ -32,3 +33,48 @@ class NPUCommunicator(DeviceCommunicatorBase):
         super().__init__(cpu_group, device, device_group, unique_name)
         # init device according to rank
         self.device = torch.npu.current_device()
+
+    def all_to_all(self,
+                   input_: torch.Tensor,
+                   scatter_dim: int = 0,
+                   gather_dim: int = -1) -> torch.Tensor:
+
+        if scatter_dim < 0:
+            scatter_dim += input_.dim()
+        if gather_dim < 0:
+            gather_dim += input_.dim()
+        if scatter_dim == gather_dim:
+            return input_
+
+        input_list = [
+            t.contiguous()
+            for t in torch.tensor_split(input_, self.world_size, scatter_dim)
+        ]
+        output_list = [
+            torch.empty_like(input_list[0]) for _ in range(self.world_size)
+        ]
+
+        dist.all_to_all(output_list, input_list, group=self.device_group)
+        output_tensor = torch.cat(output_list, dim=gather_dim).contiguous()
+        return output_tensor
+
+    def reduce_scatter(self,
+                       input_: torch.Tensor,
+                       scatter_dim: int = 0) -> torch.Tensor:
+
+        if scatter_dim < 0:
+            scatter_dim += input_.dim()
+        if scatter_dim != 0:
+            input_ = torch.transpose(input_, 0, scatter_dim)
+        dim_size = list(input_.size())
+        dim_size[0] = dim_size[0] // self.world_size
+        output_tensor = torch.empty(dim_size,
+                                    dtype=input_.dtype,
+                                    device=input_.device)
+        dist.reduce_scatter_tensor(output_tensor,
+                                   input_.contiguous(),
+                                   group=self.device_group)
+        if scatter_dim != 0:
+            output_tensor = torch.transpose(output_tensor, 0,
+                                            scatter_dim).contiguous()
+        return output_tensor
