13efed12ae10b1ffecf4933d3e2ab256e45ff5fa
new-TonyWang <wangtonyyu222@gmail.com>
2025-03-11 14:21:49 +0800

    support multistep decode, add test cases
    Signed-off-by: new-TonyWang <wangtonyyu222@gmail.com>

--- vllm_ascend/platform.py
+++ vllm_ascend/platform.py
@@ -111,7 +111,6 @@ class NPUPlatform(Platform):
                 parallel_config.worker_cls = "vllm_ascend.worker.worker.NPUWorker"
 
         cache_config = vllm_config.cache_config
-        vllm_config.scheduler_config.chunked_prefill_enabled = False
         if cache_config and cache_config.block_size is None:
             # TODO: Set block_size to 128 will lead unexpected accuracy issue in mla case.  Please set block_size to 128 back once the problem is fixed.
             cache_config.block_size = 16
