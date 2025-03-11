2b6fcc7509447a71b0cca65586d55d683a23edae
new-TonyWang <wangtonyyu222@gmail.com>
2025-03-11 15:12:31 +0800

    support multistep decode, add test cases
    Signed-off-by: new-TonyWang <wangtonyyu222@gmail.com>

--- vllm_ascend/platform.py
+++ vllm_ascend/platform.py
@@ -112,9 +112,9 @@ class NPUPlatform(Platform):
                 parallel_config.worker_cls = "vllm.spec_decode.spec_decode_worker.create_spec_worker"
                 parallel_config.sd_worker_cls = "vllm_ascend.worker.worker.NPUWorker"
             elif vllm_config.scheduler_config.is_multi_step:
-                parallel_config.worker_cls = "vllm_ascend.multi_step_worker.MultiStepWorker"
+                parallel_config.worker_cls = "vllm_ascend.worker.multi_step_worker.MultiStepWorker"
             else:
-                parallel_config.worker_cls = "vllm_ascend.worker.NPUWorker"
+                parallel_config.worker_cls = "vllm_ascend.worker.worker.NPUWorker"
 
         cache_config = vllm_config.cache_config
         vllm_config.scheduler_config.chunked_prefill_enabled = False
