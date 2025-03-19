08d356f775f28fb7af6555914912023605cbeee0
shen-shanshan <467638484@qq.com>
2025-03-19 06:31:33 +0000

    update
    Signed-off-by: shen-shanshan <467638484@qq.com>

--- vllm_ascend/patch/__init__.py
+++ vllm_ascend/patch/__init__.py
@@ -14,7 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import vllm_ascend.patch.patch_cache_dtype  # noqa
 import vllm_ascend.patch.patch_metrics  # noqa
 import vllm_ascend.patch.patch_minicpm  # noqa
 import vllm_ascend.patch.patch_rejection_sampler  # noqa
