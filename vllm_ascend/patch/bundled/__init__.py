c1fe715b9ae2516907116d4cded2b2037dc2e494
Shuqiao Li <celestialli@outlook.com>
2025-03-27 02:35:12 +0000

    sleep mode
    Signed-off-by: Shuqiao Li <celestialli@outlook.com>

--- vllm_ascend/patch/bundled/__init__.py
+++ vllm_ascend/patch/bundled/__init__.py
@@ -14,9 +14,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import vllm_ascend.patch.patch_cache_dtype  # noqa
-import vllm_ascend.patch.patch_metrics  # noqa
-import vllm_ascend.patch.patch_minicpm  # noqa
-import vllm_ascend.patch.patch_multi_step_worker  # noqa
-import vllm_ascend.patch.patch_rejection_sampler  # noqa
-import vllm_ascend.patch.patch_spec_decode_worker  # noqa
+import vllm_ascend.patch.bundled.patch_cache_dtype  # noqa
+import vllm_ascend.patch.bundled.patch_metrics  # noqa
+import vllm_ascend.patch.bundled.patch_minicpm  # noqa
+import vllm_ascend.patch.bundled.patch_multi_step_worker  # noqa
+import vllm_ascend.patch.bundled.patch_rejection_sampler  # noqa
+import vllm_ascend.patch.bundled.patch_spec_decode_worker  # noqa
--- vllm_ascend/platform.py
+++ vllm_ascend/platform.py
@@ -103,7 +103,7 @@ class NPUPlatform(Platform):
     @classmethod
     def check_and_update_config(cls, vllm_config: VllmConfig) -> None:
         # RayWorkerWrapper monkey patch when setup
-        from vllm_ascend.patch import ray_patch  # noqa: F401
+        from vllm_ascend.patch.bundled import ray_patch  # noqa: F401
 
         compilation_config = vllm_config.compilation_config
         if compilation_config.level != CompilationLevel.NO_COMPILATION:
