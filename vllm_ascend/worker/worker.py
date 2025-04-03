9186819a39eb0a9bd1315db4800d41a2a0cc5da8
ZihuiQian <qianzihui@huawei.com>
2025-04-03 10:56:40 +0800

    solve codecheck/ruff issue
    Signed-off-by: ZihuiQian <qianzihui@huawei.com>

--- vllm_ascend/worker/worker.py
+++ vllm_ascend/worker/worker.py
@@ -24,7 +24,7 @@ import torch
 import torch.distributed
 from torch import nn
 from vllm import envs
-from vllm.config import ParallelConfig, VllmConfig
+from vllm.config import VllmConfig
 from vllm.distributed import (ensure_kv_transfer_initialized,
                               ensure_model_parallel_initialized,
                               init_distributed_environment,
