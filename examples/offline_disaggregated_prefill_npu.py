686828591e0c712cca942c58c470c319536e5382
ZihuiQian <qianzihui@huawei.com>
2025-04-03 11:26:22 +0800

    fix import sorting issue
    Signed-off-by: ZihuiQian <qianzihui@huawei.com>

--- examples/offline_disaggregated_prefill_npu.py
+++ examples/offline_disaggregated_prefill_npu.py
@@ -16,11 +16,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import multiprocessing as mp
 import os
 import time
-
 from multiprocessing import Event, Process
-import multiprocessing as mp
 
 
 def clean_up():
