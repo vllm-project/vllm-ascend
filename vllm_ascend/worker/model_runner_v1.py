5697d695ca4ef08ae40f4ebde97db9892d4bb6c2
ttanzhiqiang <389825161@qq.com>
2025-04-25 01:10:50 +0800

    vllm ascend test
    Signed-off-by: ttanzhiqiang <tanzhiqiang.tzq@antgroup.com>

--- vllm_ascend/worker/model_runner_v1.py
+++ vllm_ascend/worker/model_runner_v1.py
@@ -750,7 +750,7 @@ class NPUModelRunner:
                         dtype=self.dtype,
                         device=self.device))
             intermediate_tensors = IntermediateTensors({
-                k : v[:self.max_num_tokens]
+                k: v[:self.max_num_tokens]
                 for k, v in self.intermediate_tensors.items()
             })
 
