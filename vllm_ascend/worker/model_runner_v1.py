a5e2010dd8933c9b0da103b9387af55d824065e0
Yikun Jiang <yikunkero@gmail.com>
2025-05-11 17:53:05 +0800

    Update model_runner_v1.py
    Signed-off-by: Yikun Jiang <yikunkero@gmail.com>

--- vllm_ascend/worker/model_runner_v1.py
+++ vllm_ascend/worker/model_runner_v1.py
@@ -18,7 +18,6 @@
 #
 
 import gc
-import inspect
 import math
 import os
 import time
@@ -56,6 +55,7 @@ from vllm.v1.worker.gpu_input_batch import CachedRequestState, InputBatch
 from vllm_ascend.attention.attention import AttentionMaskBuilder
 from vllm_ascend.attention.attention_v1 import AscendAttentionState
 from vllm_ascend.platform import NPUPlatform
+from vllm_ascend.utils import vllm_version_is
 
 if TYPE_CHECKING:
     import xgrammar as xgr  # type: ignore[import-untyped]
@@ -188,19 +188,26 @@ class NPUModelRunner:
         # Request states.
         self.requests: Dict[str, CachedRequestState] = {}
         # Persistent batch.
-        input_batch_kwargs = {
-            "max_num_reqs": self.max_num_reqs,
-            "max_model_len": self.model_config.max_model_len,
-            "max_num_blocks_per_req": self.max_num_blocks_per_req,
-            "device": self.device,
-            "pin_memory": True,
-            "vocab_size": self.model_config.get_vocab_size(),
-        }
-        # For version compatibility and consistency with the latest vllm code
-        if "max_num_batched_tokens" in inspect.signature(
-                InputBatch).parameters:
-            input_batch_kwargs["max_num_batched_tokens"] = self.max_num_tokens
-        self.input_batch = InputBatch(**input_batch_kwargs)
+        # Remove this after we drop 0.8.5 support
+        if vllm_version_is("0.8.5") or vllm_version_is("0.8.5.post1"):
+            self.input_batch = InputBatch(
+                max_num_reqs=self.max_num_reqs,
+                max_model_len=self.model_config.max_model_len,
+                max_num_blocks_per_req=self.max_num_blocks_per_req,
+                device=self.device,
+                pin_memory=True,
+                vocab_size=self.model_config.get_vocab_size(),
+            )
+        else:
+            self.input_batch = InputBatch(
+                max_num_reqs=self.max_num_reqs,
+                max_model_len=self.model_config.max_model_len,
+                max_num_blocks_per_req=self.max_num_blocks_per_req,
+                max_num_batched_tokens = self.max_num_tokens,
+                device=self.device,
+                pin_memory=True,
+                vocab_size=self.model_config.get_vocab_size(),
+            )
 
         self.input_ids = torch.zeros(self.max_num_tokens,
                                      dtype=torch.int32,
