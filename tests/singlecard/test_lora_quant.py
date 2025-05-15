e70bcb699772eb794b0ab029dde0cb8aee6e9f17
paulyu <paulyu0307@gmail.com>
2025-05-15 10:31:50 +0800

    [Bugfix] fix bug
    Signed-off-by: paulyu <paulyu0307@gmail.com>

--- tests/singlecard/test_lora_quant.py
+++ tests/singlecard/test_lora_quant.py
@@ -29,8 +29,8 @@ MODELS = [
     #     quantization="GPTQ"),
 ]
 
-os.environ["VLLM_USE_MODELSCOPE"] = True
 os.environ["PYTORCH_NPU_ALLOC_CONF"] = "max_split_size_mb:256"
+os.environ["VLLM_USE_MODELSCOPE"] = "True"
 
 
 def do_sample(llm: vllm.LLM,
