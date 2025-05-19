1dd4084a9aaae070b322d2b18d7c4c19938dbba3
paulyu <paulyu0307@gmail.com>
2025-05-19 20:26:38 +0800

    [Bugfix] fix bug
    Signed-off-by: paulyu <paulyu0307@gmail.com>

--- tests/multicard/test_ilama_lora_tp2.py
+++ tests/multicard/test_ilama_lora_tp2.py
@@ -1,8 +1,11 @@
+import pytest
+
 from tests.conftest import VllmRunner
 from tests.singlecard.test_ilama_lora import MODEL_PATH, do_sample
 
 
-def test_ilama_lora_tp2(ilama_lora_files):
+@pytest.mark.parametrize("distributed_executor_backend", ["mp"])
+def test_ilama_lora_tp2(distributed_executor_backend, ilama_lora_files):
     with VllmRunner(model_name=MODEL_PATH,
                     enable_lora=True,
                     max_loras=4,
@@ -15,7 +18,9 @@ def test_ilama_lora_tp2(ilama_lora_files):
                     max_loras=4,
                     max_model_len=1024,
                     max_num_seqs=16,
-                    tensor_parallel_size=2) as vllm_model:
+                    tensor_parallel_size=2,
+                    distributed_executor_backend=distributed_executor_backend
+                    ) as vllm_model:
         output2 = do_sample(vllm_model.model, ilama_lora_files, lora_id=2)
 
     for i in range(len(output1)):
