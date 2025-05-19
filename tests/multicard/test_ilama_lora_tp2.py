7125ff1f02cde4762a7fe0834ddd426c9b67354d
paulyu12 <507435917@qq.com>
2025-05-19 23:13:12 +0800

    [Bugfix] fig bug
    Signed-off-by: paulyu12 <507435917@qq.com>

--- tests/multicard/test_ilama_lora_tp2.py
+++ tests/multicard/test_ilama_lora_tp2.py
@@ -1,7 +1,8 @@
 import pytest
 
 from tests.conftest import VllmRunner
-from tests.singlecard.test_ilama_lora import MODEL_PATH, do_sample
+from tests.singlecard.test_ilama_lora import (EXPECTED_LORA_OUTPUT, MODEL_PATH,
+                                              do_sample)
 
 
 @pytest.mark.parametrize("distributed_executor_backend", ["mp"])
@@ -10,18 +11,11 @@ def test_ilama_lora_tp2(distributed_executor_backend, ilama_lora_files):
                     enable_lora=True,
                     max_loras=4,
                     max_model_len=1024,
-                    max_num_seqs=16) as vllm_model:
-        output1 = do_sample(vllm_model.model, ilama_lora_files, lora_id=1)
-
-    with VllmRunner(model_name=MODEL_PATH,
-                    enable_lora=True,
-                    max_loras=4,
-                    max_model_len=1024,
                     max_num_seqs=16,
                     tensor_parallel_size=2,
                     distributed_executor_backend=distributed_executor_backend
                     ) as vllm_model:
-        output2 = do_sample(vllm_model.model, ilama_lora_files, lora_id=2)
+        output = do_sample(vllm_model.model, ilama_lora_files, lora_id=2)
 
-    for i in range(len(output1)):
-        assert output1[i] == output2[i]
+    for i in range(len(EXPECTED_LORA_OUTPUT)):
+        assert output[i] == EXPECTED_LORA_OUTPUT[i]
