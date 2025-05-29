7244ebb147cf83aa968b0d736459bab2a4608942
Vincent Yuan <farawayboat@gmail.com>
2025-05-29 08:46:27 +0000

    feat: adapted to the latest patch structure
    Signed-off-by: Vincent Yuan <farawayboat@gmail.com>

--- vllm_ascend/platform.py
+++ vllm_ascend/platform.py
@@ -27,9 +27,7 @@ from vllm.platforms import Platform, PlatformEnum
 
 import vllm_ascend.envs as ascend_envs
 from vllm_ascend.ascend_config import check_ascend_config, init_ascend_config
-from vllm_ascend.utils import (ASCEND_QUATIZATION_METHOD,
-                               communication_adaptation_310p, is_310p,
-                               update_aclgraph_sizes)
+from vllm_ascend.utils import ASCEND_QUATIZATION_METHOD, update_aclgraph_sizes
 
 CUSTOM_OP_ENABLED = False
 try:
@@ -88,10 +86,6 @@ class NPUPlatform(Platform):
 
         from vllm_ascend.quantization.quant_config import \
             AscendQuantConfig  # noqa: F401
-
-        if is_310p():
-            logger.info("patch torch communication while using Ascend 310P")
-            communication_adaptation_310p()
 
     @classmethod
     def get_device_capability(cls, device_id: int = 0):
