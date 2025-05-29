5ff4578c6bf33665d5aa5add6e5ce7c77c46fb48
wangli <wangli858794774@gmail.com>
2025-05-29 09:21:24 +0000

    add pip installation test
    Signed-off-by: wangli <wangli858794774@gmail.com>

---
+++ tests/e2e/doctests/003-pip-source-installation-test.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+#
+# Copyright (c) 2025 Huawei Technologies Co., Ltd. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# This file is a part of the vllm-ascend project.
+#
+set -euo pipefail
+SOURCE_CODE_DIR=/tmp/vllm-source-code/
+
+function install_from_source() {
+    local ORIGINAL_DIR=$(pwd)
+    mkdir -p "$SOURCE_CODE_DIR" && cd "$SOURCE_CODE_DIR"
+    _info "====> Install vllm and vllm-ascend from ${VLLM_VERSION}"
+    if [[ "$VLLM_VERSION" == "main" ]]; then
+        git clone --depth 1 --branch v0.8.5.post1 https://github.com/vllm-project/vllm
+        cd vllm
+        VLLM_TARGET_DEVICE=empty pip install -v -e .
+        cd ..
+
+        git clone  --depth 1 --branch v0.8.5rc1 https://github.com/vllm-project/vllm-ascend.git
+        cd vllm-ascend
+        pip install -v -e .
+        cd ..
+    elif [[ "$VLLM_VERSION" == "v0.7.3" ]]; then
+        git clone --depth 1 --branch v0.7.3 https://github.com/vllm-project/vllm
+        cd vllm
+        VLLM_TARGET_DEVICE=empty pip install . --extra-index https://download.pytorch.org/whl/cpu/
+        cd ..
+
+        git clone  --depth 1 --branch v0.7.3 https://github.com/vllm-project/vllm-ascend.git
+        cd vllm-ascend
+        pip install -e . --extra-index https://download.pytorch.org/whl/cpu/
+        cd ..
+    else
+        echo "[ERROR] Unsupported VLLM_VERSION: $VLLM_VERSION"
+        exit 1
+    fi
+    cd "$ORIGINAL_DIR"
+}
+
+
+function clear_installation() {
+    _info "====> Clear previous vllm and vllm-ascend installation"
+    rm -rf "${SOURCE_CODE_DIR}"
+}
+
+trap 'clean_venv; clear_installation' EXIT
+check_npus
+create_vllm_venv
+install_from_source
+
+pip list | grep vllm
+
+# Verify the installation
+_info "====> Run offline example test"
+python3 "${SCRIPT_DIR}/../../examples/offline_inference_npu.py"
