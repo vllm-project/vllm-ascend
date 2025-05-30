b8b6175f13e0e888f4d035297bde2abd01f05907
raindaywhu <raindaywhu@163.com>
2025-05-30 10:18:43 +0800

    init matrix core acclib dir struct

---
+++ vllm_ascend/eplb/core/policy/policy_factory.py
@@ -0,0 +1,14 @@
+# Copyright Huawei Technologies Co., Ltd. 2023-2024. All rights reserved.
+from .eplb_policy import EplbPolicy, DynamicConfig
+from .mock_load_balance import MockLoadBalance
+from .dynamic_ep import DynamicEP
+
+
+class PolicyFactory:
+    @staticmethod
+    def generate_policy(policy_type: int, config: DynamicConfig) -> EplbPolicy:
+        policy = {
+            0: MockLoadBalance,
+            1: DynamicEP,
+        }
+        return policy.get(policy_type, MockLoadBalance)(config)
