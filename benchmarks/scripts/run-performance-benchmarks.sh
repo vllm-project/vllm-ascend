4a0e0326e2ac1b9d7acab55c4a05088a4f5613b4
wangli <wangli858794774@gmail.com>
2025-05-30 18:28:56 +0800

    add more endpoint
    Signed-off-by: wangli <wangli858794774@gmail.com>

--- benchmarks/scripts/run-performance-benchmarks.sh
+++ benchmarks/scripts/run-performance-benchmarks.sh
@@ -49,7 +49,7 @@ wait_for_server() {
   # wait for vllm server to start
   # return 1 if vllm server crashes
   timeout 1200 bash -c '
-    until curl -X POST localhost:8000/v1/completions; do
+    until curl -s -X POST localhost:8000/v1/completions || curl -s -X POST localhost:8000/v1/chat/completions; do
       sleep 1
     done' && return 0 || return 1
 }
