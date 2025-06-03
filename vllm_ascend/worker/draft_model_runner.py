7c78f8b2e05b893f87823cfde29702361fb7e3f1
shen-shanshan <467638484@qq.com>
2025-06-03 02:09:43 +0000

    fix
    Signed-off-by: shen-shanshan <467638484@qq.com>

--- vllm_ascend/worker/draft_model_runner.py
+++ vllm_ascend/worker/draft_model_runner.py
@@ -276,7 +276,7 @@ class TP1DraftModelRunner(ModelRunnerWrapperBase):
                     intermediate_tensors=intermediate_tensors,
                     **MultiModalKwargs.as_kwargs(
                         multi_modal_kwargs,
-                        dtype=self.model_runner.model_config.dtype,
+                        dtype=self.model_config.dtype,
                         device=self.device,
                     ),
                     **model_execute_kwargs,
--- vllm_ascend/worker/model_runner.py
+++ vllm_ascend/worker/model_runner.py
@@ -1445,7 +1445,7 @@ class NPUModelRunner(NPUModelRunnerBase[ModelInputForNPUWithSamplingMetadata]):
                     intermediate_tensors=intermediate_tensors,
                     **MultiModalKwargs.as_kwargs(
                         multi_modal_kwargs,
-                        dtype=self.model_runner.model_config.dtype,
+                        dtype=self.model_config.dtype,
                         device=self.device,
                     ),
                     **seqlen_agnostic_kwargs,
