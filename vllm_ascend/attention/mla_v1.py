8160ea7e343e4683bc7ff783cd2c14a63dcdefd8
chenwaner <48718746+chenwaner@users.noreply.github.com>
2025-06-05 11:54:09 +0800

    Update mla_v1.py
    return to use layout=BNSD in npu_fused_infer_attention_score when KV_NZ disabled

--- vllm_ascend/attention/mla_v1.py
+++ vllm_ascend/attention/mla_v1.py
@@ -691,20 +691,29 @@ class AscendMLAImpl(MLAAttentionImpl):
             dtype=q.dtype,
             device=q.device)
         if self.running_in_graph:
-            # TorchAir's shape is [bs, num_heads_per_rank, seq_len, dim]
-            q_nope = q_nope.view(num_tokens, 1, self.num_heads, -1)
-            q_pe = q_pe.view(num_tokens, 1, self.num_heads, -1)
-            # shape of knope/k_pe for npu graph mode should be:
-            # [num_blocks, num_kv_heads, block_size, self.kv_lora_rank/self.qk_rope_head_dim]
             block_size = kv_c_and_k_pe_cache[0].shape[1]
             if self.enable_kv_nz:
-                block_num = k_nope.size()[0]
-                k_nope = k_nope.view(block_num, self.num_kv_heads, 
+                # TorchAir's shape is [bs, num_heads_per_rank, seq_len, dim]
+                q_nope = q_nope.view(num_tokens, 1, self.num_heads, -1)
+                q_pe = q_pe.view(num_tokens, 1, self.num_heads, -1)
+                # shape of knope/k_pe for npu graph mode should be:
+                # [num_blocks, num_kv_heads, block_size, self.kv_lora_rank/self.qk_rope_head_dim]
+                k_nope = k_nope.view(-1, self.num_kv_heads, 
                                  self.kv_lora_rank // 16, block_size, 16)
-                k_pe = k_pe.view(block_num, self.num_kv_heads, 
+                k_pe = k_pe.view(-1, self.num_kv_heads, 
                                  self.qk_rope_head_dim // 16, block_size, 16)
+                input_layout="BSND"
             else:
-                k_nope, k_pe = k_nope.squeeze(2), k_pe.squeeze(2)
+                # TorchAir's shape is [bs, num_heads_per_rank, seq_len, dim]
+                q_nope = q_nope.view(num_tokens, self.num_heads, 1, -1)
+                q_pe = q_pe.view(num_tokens, self.num_heads, 1, -1)
+                # shape of knope/k_pe for npu graph mode should be:
+                # [num_blocks, num_kv_heads, block_size, self.kv_lora_rank/self.qk_rope_head_dim]
+                k_nope = k_nope.view(-1, self.num_kv_heads, block_size,
+                                    self.kv_lora_rank)
+                k_pe = k_pe.view(-1, self.num_kv_heads, block_size,
+                                self.qk_rope_head_dim)
+                input_layout="BNSD"
 
             attn_output, _ = torch_npu.npu_fused_infer_attention_score(
                 q_nope,
@@ -714,7 +723,7 @@ class AscendMLAImpl(MLAAttentionImpl):
                 key_rope=k_pe,
                 num_heads=self.num_heads,
                 num_key_value_heads=self.num_kv_heads,
-                input_layout="BSND",
+                input_layout=input_layout,
                 atten_mask=attn_metadata.attn_mask,
                 scale=self.scale,
                 antiquant_mode=0,
