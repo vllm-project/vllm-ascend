fba30809411fc9f338b330a35f327eeb1dd4659f
ganyi <pleaplusone.gy@gmail.com>
2025-06-09 14:53:58 +0800

    fix dp issues in new vllm
    Signed-off-by: ganyi <pleaplusone.gy@gmail.com>

--- vllm_ascend/worker/model_runner_v1.py
+++ vllm_ascend/worker/model_runner_v1.py
@@ -23,6 +23,7 @@ import time
 import types
 import weakref
 import copy
+import math
 from contextlib import contextmanager, nullcontext
 from dataclasses import dataclass
 from typing import TYPE_CHECKING, Dict, List, Optional, Union, cast
@@ -1622,8 +1623,6 @@ class NPUModelRunner(LoRAModelRunnerMixin):
             # For the case of no forward caused by receiving remote kv,
             # one round of dummy inference is necessary
             # to prevent hang over the collective calls.
-            if finished_recving is not None and len(finished_recving) > 0:
-                self._dummy_run(1)
         if not finsihed_sending and not finished_recving:
             return EMPTY_MODEL_RUNNER_OUTPUT
         
@@ -2134,9 +2133,12 @@ class NPUModelRunner(LoRAModelRunnerMixin):
                         kv_cache_list = []
                         for i in range(num_caches):
                             cache_shape = kv_cache_shape[1:]
-                            kv_cache_for_compute = torch.zeros(cache_shape, dtype=dtype, device=self.device)
+                            cache_size = math.prod(cache_shape)
+                            cache_size_aligned = cache_size + alignment
+                            kv_cache = torch.zeros(cache_size_aligned, dtype=dtype, device=self.device)
+                            kv_cache = align_memory(kv_cache, alignment)[:cache_size].view(cache_shape)
                             kv_cache_for_compute = torch_npu.npu_format_cast(kv_cache_for_compute, acl_format)
-                            kv_cache_list.append(kv_cache_for_compute)
+                            kv_cache_list.append(kv_cache)
                         kv_caches[layer_name] = kv_cache_list
                 else:
                     # TODO: add new branches when introducing more types of
