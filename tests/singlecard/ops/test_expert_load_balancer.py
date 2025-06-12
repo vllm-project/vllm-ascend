d5edb8e795bda690b2dd0b7a3cf6da4073b973a6
songshanhu07 <1763685535@qq.com>
2025-06-12 15:09:55 +0800

    add static EPLB unit test
    Signed-off-by: songshanhu07 <1763685535@qq.com>

---
+++ tests/singlecard/ops/test_expert_load_balancer.py
@@ -0,0 +1,93 @@
+import pytest
+import torch
+import json
+from vllm_ascend.ops.expert_load_balancer import ExpertLoadBalancer
+
+
+MOCK_DATA = {
+    "moe_layer_count": 1,
+    "layer_list": [
+        {
+            "layer_id": 0,
+            "device_count": 2,
+            "device_list": [
+                {
+                    "device_id": 0,
+                    "device_expert": [7, 2, 0, 3, 5]
+                },
+                {
+                    "device_id": 1,
+                    "device_expert": [6, 1, 4, 7, 2]
+                }
+            ]
+        }
+    ]
+}
+
+
+@pytest.fixture
+def mock_expert_load_balancer(tmp_path):
+    # 创建临时JSON文件
+    json_file = tmp_path / "expert_map.json"
+    with open(json_file, 'w') as f:
+        json.dump(MOCK_DATA, f)
+
+    # 初始化 ExpertLoadBalancer 实例
+    return ExpertLoadBalancer(str(json_file), global_expert_num=8)
+
+
+def test_init(mock_expert_load_balancer):
+    assert isinstance(mock_expert_load_balancer.expert_map_tensor, torch.Tensor)
+    assert mock_expert_load_balancer.layers_num == MOCK_DATA["moe_layer_count"]
+    assert mock_expert_load_balancer.ranks_num == MOCK_DATA["layer_list"][0]["device_count"]
+
+
+def test_generate_index_dicts(mock_expert_load_balancer):
+    tensor_2d = torch.tensor([[7, 2, 0, 3, 5], [6, 1, 4, 7, 2]])
+    result = mock_expert_load_balancer.generate_index_dicts(tensor_2d)
+    expected_result = [{7: 0, 2: 1, 0: 2, 3: 3, 5:4}, {6: 5, 1: 6, 4: 7, 7: 8, 2: 9}]
+    assert result == expected_result
+
+
+def test_generate_expert_placement_map(mock_expert_load_balancer):
+    expert_placement_map = mock_expert_load_balancer.generate_expert_placement_map()
+    print(expert_placement_map)
+    assert expert_placement_map.shape == (mock_expert_load_balancer.layers_num,
+                                          mock_expert_load_balancer.ranks_num,
+                                          8)
+    assert torch.all(expert_placement_map >= -1)
+
+
+def test_generate_log2phy_expert_map(mock_expert_load_balancer):
+    layer_id = 0
+    log2phy_map = mock_expert_load_balancer.generate_log2phy_expert_map(layer_id)
+    assert log2phy_map.shape == (mock_expert_load_balancer.ranks_num, 8)
+    assert torch.all(log2phy_map >= -1)
+
+
+def test_get_rank_placement_map(mock_expert_load_balancer):
+    layer_id = 0
+    rank_id = 0
+    rank_local_expert_num, rank_expert_map = mock_expert_load_balancer.get_rank_placement_map(layer_id, rank_id)
+    assert rank_local_expert_num == 5
+    assert rank_expert_map.equal(torch.tensor([ 2, -1,  1,  3, -1,  4, -1,  0], dtype=torch.int32))
+    rank_id = 1
+    rank_local_expert_num, rank_expert_map = mock_expert_load_balancer.get_rank_placement_map(layer_id, rank_id)
+    assert rank_expert_map.equal(torch.tensor([-1,  1,  4, -1,  2, -1,  0,  3], dtype=torch.int32))
+
+
+def test_get_rank_log2phy_map(mock_expert_load_balancer):
+    layer_id = 0
+    rank_id = 0
+    log2phy_map = mock_expert_load_balancer.get_rank_log2phy_map(layer_id, rank_id)
+    assert log2phy_map.equal(torch.tensor([2, 6, 1, 3, 7, 4, 5, 0], dtype=torch.int32))
+    rank_id = 1
+    log2phy_map = mock_expert_load_balancer.get_rank_log2phy_map(layer_id, rank_id)
+    assert log2phy_map.equal(torch.tensor([2, 6, 9, 3, 7, 4, 5, 8], dtype=torch.int32))
+
+
+def test_get_global_redundant_expert_num(mock_expert_load_balancer):
+    redundant_expert_num = mock_expert_load_balancer.get_global_redundant_expert_num()
+    expected_redundant_expert_num = len(MOCK_DATA["layer_list"][0]["device_list"][0]["device_expert"]) * \
+                                    MOCK_DATA["layer_list"][0]["device_count"] - 8
+    assert redundant_expert_num == expected_redundant_expert_num
