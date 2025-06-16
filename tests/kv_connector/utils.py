d69338ba54b6e2809d5dcedb18459a7fdcf339e4
zouyida2052 <zouyida@huawei.com>
2025-06-16 21:45:55 +0800

    update ut
    Signed-off-by: zouyida2052 <zouyida@huawei.com>

--- tests/kv_connector/utils.py
+++ tests/kv_connector/utils.py
@@ -1,5 +1,8 @@
 # SPDX-License-Identifier: Apache-2.0
+# This code is from: https://github.com/vllm-project/vllm/tests/v1/kv_connector/unit/utils.py
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
+# Copyright (c) 2025 Huawei Technologies Co., Ltd. All Rights Reserved.
+
 from typing import Any, Optional
 
 import torch
