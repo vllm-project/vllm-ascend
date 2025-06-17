1d57355a0574beb73ac219c6d28d35aac4d9c6e1
shen-shanshan <467638484@qq.com>
2025-06-17 09:13:52 +0000

    update
    Signed-off-by: shen-shanshan <467638484@qq.com>

--- vllm_ascend/platform.py
+++ vllm_ascend/platform.py
@@ -188,8 +188,7 @@ class NPUPlatform(Platform):
             elif vllm_config.speculative_config:
                 # NOTE: We set this var to `1` in vllm-ascend to avoid segment
                 # fault when using spec decode with V0 engine.
-                if not envs.VLLM_USE_V1:
-                    os.environ["ACL_OP_INIT_MODE"] = "1"
+                os.environ["ACL_OP_INIT_MODE"] = "1"
                 parallel_config.worker_cls = "vllm.spec_decode.spec_decode_worker.create_spec_worker"
                 parallel_config.sd_worker_cls = "vllm_ascend.worker.worker.NPUWorker"
             elif vllm_config.scheduler_config.is_multi_step:
