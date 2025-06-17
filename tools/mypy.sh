409e271b5052c39495442521e47671fd0c60e846
wangli <wangli858794774@gmail.com>
2025-06-17 17:08:27 +0800

    fix and add
    Signed-off-by: wangli <wangli858794774@gmail.com>

--- tools/mypy.sh
+++ tools/mypy.sh
@@ -20,14 +20,22 @@
 #
 
 CI=${1:-0}
-PYTHON_VERSION=${2:-3.9}
+PYTHON_VERSION=${2:-local}
 
 if [ "$CI" -eq 1 ]; then
     set -e
 fi
 
+if [ $PYTHON_VERSION == "local" ]; then
+    PYTHON_VERSION=$(python -c 'import sys; print(f"{sys.version_info.major}.{sys.version_info.minor}")')
+fi
+
 run_mypy() {
     echo "Running mypy on $1"
+    if [ "$CI" -eq 1 ] && [ -z "$1" ]; then
+        mypy --python-version "${PYTHON_VERSION}" "$@"
+        return
+    fi
     mypy --check-untyped-defs --follow-imports skip --python-version "${PYTHON_VERSION}" "$@"
 }
 
