fdd665b6a80cfdbbb020041acf469e43c92a5da0
David9857 <985700846@qq.com>
2025-06-17 19:42:39 +0800

    optimize rope in qwen3
    Signed-off-by: David9857 <985700846@qq.com>

--- vllm_ascend/models/qwen3.py
+++ vllm_ascend/models/qwen3.py
@@ -24,6 +24,60 @@ from vllm.model_executor.models.utils import AutoWeightsLoader, PPMissingLayer, 
 
 from vllm_ascend.ops.layernorm import AddRMSNormQuant
 
+class CustomQwen3Attention(Qwen3Attention):
+
+    def __init__(self,
+                 hidden_size: int,
+                 num_heads: int,
+                 num_kv_heads: int,
+                 max_position: int = 4096 * 32,
+                 head_dim: Optional[int] = None,
+                 rms_norm_eps: float = 1e-06,
+                 qkv_bias: bool = False,
+                 rope_theta: float = 10000,
+                 cache_config: Optional[CacheConfig] = None,
+                 quant_config: Optional[QuantizationConfig] = None,
+                 rope_scaling: Optional[tuple] = None,
+                 prefix: str = "",
+                 attn_type: str = AttentionType.DECODER) -> None:
+        super().__init__(hidden_size=hidden_size,
+                        num_heads=num_heads,
+                        num_kv_heads=num_kv_heads,
+                        max_position=max_position,
+                        head_dim=head_dim,
+                        rms_norm_eps=rms_norm_eps,
+                        qkv_bias=qkv_bias,
+                        rope_theta=rope_theta,
+                        cache_config=cache_config,
+                        quant_config=quant_config,
+                        rope_scaling=rope_scaling,
+                        prefix=prefix,
+                        attn_type=attn_type)
+
+    def forward(
+        self,
+        positions: torch.Tensor,
+        cos: torch.Tensor,
+        sin: torch.Tensor,
+        hidden_states: torch.Tensor,
+    ) -> torch.Tensor:
+        qkv, _ = self.qkv_proj(hidden_states)
+        q, k, v = qkv.split([self.q_size, self.kv_size, self.kv_size], dim=-1)
+        # Add qk-norm
+        q_by_head = q.view(*q.shape[:-1], q.shape[-1] // self.head_dim,
+                           self.head_dim)
+        q_by_head = self.q_norm(q_by_head)
+        q = q_by_head.view(q.shape)
+        k_by_head = k.view(*k.shape[:-1], k.shape[-1] // self.head_dim,
+                           self.head_dim)
+        k_by_head = self.k_norm(k_by_head)
+        k = k_by_head.view(k.shape)
+        q, k = self.rotary_emb(positions, cos, sin, q, k)
+        attn_output = self.attn(q, k, v)
+        output, _ = self.o_proj(attn_output)
+        return output
+
+
 class CustomQwen3DecoderLayer(nn.Module):
 
     def __init__(
@@ -48,7 +102,7 @@ class CustomQwen3DecoderLayer(nn.Module):
         else:
             attn_type = AttentionType.ENCODER_ONLY
 
-        self.self_attn = Qwen3Attention(
+        self.self_attn = CustomQwen3Attention(
             hidden_size=self.hidden_size,
             num_heads=config.num_attention_heads,
             max_position=config.max_position_embeddings,
@@ -88,6 +142,8 @@ class CustomQwen3DecoderLayer(nn.Module):
     def forward(
         self,
         positions: torch.Tensor,
+        cos: torch.Tensor,
+        sin: torch.Tensor,
         hidden_states: torch.Tensor,
         residual: Optional[torch.Tensor],
     ) -> tuple[torch.Tensor, torch.Tensor]:
@@ -100,6 +156,8 @@ class CustomQwen3DecoderLayer(nn.Module):
                 hidden_states, residual)
         hidden_states = self.self_attn(
             positions=positions,
+            cos=cos,
+            sin=sin,
             hidden_states=hidden_states,
         )
         hidden_states, residual = self.post_attention_layernorm(
@@ -128,6 +186,46 @@ class CustomQwen3Model(Qwen2Model):
         super().__init__(vllm_config=vllm_config,
                          prefix=prefix,
                          decoder_layer_type=CustomQwen3DecoderLayer)
+        self.cos_sin_cache = self.layers[0].self_attn.rotary_emb.cos_sin_cache
+        
+    def forward(
+        self,
+        input_ids: torch.Tensor,
+        positions: torch.Tensor,
+        intermediate_tensors: Optional[IntermediateTensors] = None,
+        inputs_embeds: Optional[torch.Tensor] = None,
+    ) -> Union[torch.Tensor, IntermediateTensors]:
+        if get_pp_group().is_first_rank:
+            if inputs_embeds is not None:
+                hidden_states = inputs_embeds
+            else:
+                hidden_states = self.get_input_embeddings(input_ids)
+            residual = None
+        else:
+            assert intermediate_tensors is not None
+            hidden_states = intermediate_tensors["hidden_states"]
+            residual = intermediate_tensors["residual"]
+
+        cos_sin = self.cos_sin_cache.index_select(0, positions)
+        last_dim = cos_sin.size()[-1]
+        cos, sin = cos_sin.reshape(-1, 2, last_dim//2).repeat(1, 1, 2).chunk(2, dim=-2)
+        cos, sin = cos.view(-1, last_dim).contiguous(), sin.view(-1, last_dim).contiguous()
+
+        for layer in self.layers[self.start_layer:self.end_layer]:
+            hidden_states, residual = layer(
+                positions,
+                cos,
+                sin,
+                hidden_states,
+                residual,
+            )
+        if not get_pp_group().is_last_rank:
+            return IntermediateTensors({
+                "hidden_states": hidden_states,
+                "residual": residual
+            })
+        hidden_states, _ = self.norm(hidden_states, residual)
+        return hidden_states
 
 
 class CustomQwen3ForCausalLM(nn.Module, SupportsLoRA, SupportsPP):
