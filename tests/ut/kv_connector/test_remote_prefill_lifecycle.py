c2eedd4f6918332bb79c399a8dd29a8662bd0e34
ganyi <pleaplusone.gy@gmail.com>
2025-06-18 14:23:28 +0800

    fix isort
    Signed-off-by: ganyi <pleaplusone.gy@gmail.com>

--- tests/ut/kv_connector/test_remote_prefill_lifecycle.py
+++ tests/ut/kv_connector/test_remote_prefill_lifecycle.py
@@ -18,13 +18,12 @@
 #
 import copy
 
+from tests.ut.kv_connector.utils import (assert_scheduler_empty,
+                                         create_model_runner_output,
+                                         create_request, create_scheduler,
+                                         create_vllm_config)
 from vllm.v1.outputs import EMPTY_MODEL_RUNNER_OUTPUT
 from vllm.v1.request import FinishReason, RequestStatus
-
-from tests.ut.kv_connector.utils import (assert_scheduler_empty,
-                                      create_model_runner_output,
-                                      create_request, create_scheduler,
-                                      create_vllm_config)
 
 
 def test_basic_lifecycle():
