f6835f8b564e4d09a83146f4138f98987c596a82
liziyu <liziyu16@huawei.com>
2025-06-18 18:18:31 +0800

    update gen_ranktable
    Signed-off-by: liziyu <liziyu16@huawei.com>

--- examples/disaggregate_prefill_v1/gen_ranktable.sh
+++ examples/disaggregate_prefill_v1/gen_ranktable.sh
@@ -3,6 +3,7 @@
 source /usr/local/Ascend/ascend-toolkit/set_env.sh
 export LD_LIBRARY_PATH=/usr/local/Ascend/ascend-toolkit/latest/opp/vendors/customize/op_api/lib/:${LD_LIBRARY_PATH}
 
+NPUS_PER_NODE=8
 while [[ $# -gt 0 ]]; do
     case "$1" in
         --ips)
@@ -12,6 +13,11 @@ while [[ $# -gt 0 ]]; do
                 IPs+=("$1")
                 shift
             done
+            ;;
+        --npus-per-node)
+            shift
+            NPUS_PER_NODE="$1"
+            shift
             ;;
         --network-card-name)
             shift
@@ -30,30 +36,33 @@ while [[ $# -gt 0 ]]; do
             ;;
     esac
 done
-LOCAL_HOST=`hostname -I|awk -F " " '{print$1}'`
-GPUS_PER_NODE=8
+LOCAL_HOSTS=($(hostname -I))
+LOCAL_HOST="127.0.0.1"
 MASTER_ADDR=${IPs[0]}
 MASTER_PORT=6657
 NNODES=${#IPs[@]}
-NODE_RANK="2"
-for i in "${!IPs[@]}";
-do
-    echo "${IPs[$i]}"
-    if [ "$LOCAL_HOST" == "${IPs[$i]}" ];
-    then
-        NODE_RANK=$i
-        break
-    fi
+NODE_RANK="8"
+for i in "${!IPs[@]}"; do
+    ip="${IPs[$i]}"
+    for local_host in "${LOCAL_HOSTS[@]}"; do
+        if [[ "$local_host" == "$ip" ]]; then
+            LOCAL_HOST=$local_host
+            NODE_RANK=$i
+            break 2
+        fi
+    done
 done
+
 if [[ $NODE_RANK == "" ]];then
-    echo "[Error] para \"NODE_RANK\" must be config"
+    echo "[Error] para \"NODE_RANK\" must be defined"
     exit 1
 fi
 
-WORLD_SIZE=$(($GPUS_PER_NODE * $NNODES))
-RANKSTART=`expr $GPUS_PER_NODE \* $NODE_RANK`
+WORLD_SIZE=$(($NPUS_PER_NODE * $NNODES))
+RANKSTART=`expr $NPUS_PER_NODE \* $NODE_RANK`
 
 echo "========>param:"
+echo "LOCAL_HOST": $LOCAL_HOST
 echo "WORLD_SIZE: " $WORLD_SIZE
 echo "RANKSTART": $RANKSTART
 echo "NNODES": $NNODES
@@ -67,5 +76,5 @@ if [[ -n "${GEN_RANKTABLE}" || ! -e ${PWD}/ranktable.json ]]; then
         --node_rank ${NODE_RANK} \
         --master_addr ${MASTER_ADDR} \
         --master_port ${MASTER_PORT} \
-        gen_ranktable.py --prefill-device-cnt $PREFILL_DEVICE_CNT --decode-device-cnt $DECODE_DEVICE_CNT
+        gen_ranktable.py --local-host $LOCAL_HOST --prefill-device-cnt $PREFILL_DEVICE_CNT --decode-device-cnt $DECODE_DEVICE_CNT
 fi
