52d7fa0301be737f0e514109088c7f400fe2c99f
underfituu <hzhucong@163.com>
2025-06-18 14:12:03 +0800

    fix kv_connector ut
    Signed-off-by: underfituu <hzhucong@163.com>

--- tests/ut/kv_connector/test_remote_decode_lifecycle.py
+++ tests/ut/kv_connector/test_remote_decode_lifecycle.py
@@ -20,7 +20,7 @@
 from vllm.v1.outputs import EMPTY_MODEL_RUNNER_OUTPUT
 from vllm.v1.request import FinishReason, RequestStatus
 
-from tests.kv_connector.utils import (assert_scheduler_empty,
+from tests.ut.kv_connector.utils import (assert_scheduler_empty,
                                       create_model_runner_output,
                                       create_request, create_scheduler,
                                       create_vllm_config)
