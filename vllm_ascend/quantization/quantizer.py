253c889edbe3561bbf7d84a97d2cf9c16c116983
pichangping <1337510399@qq.com>
2025-06-19 18:43:46 +0800

    support w4a8 quantization
    Signed-off-by: pichangping <1337510399@qq.com>

--- vllm_ascend/quantization/quantizer.py
+++ vllm_ascend/quantization/quantizer.py
@@ -281,11 +281,13 @@ class W8A8DYNAMICQuantizer(VLLMAscendQuantizer):
     def build_moe_method():
         return AscendW8A8DynamicFusedMoEMethod()
 
+
 class W4A8DYNAMICQuantizer(VLLMAscendQuantizer):
 
     @staticmethod
     def build_moe_method():
         return AscendW4A8DynamicFusedMoEMethod()
+
 
 SUPPORT_ASCEND_QUANTIZER_TYPE = {
     "W8A8": W8A8Quantizer,
