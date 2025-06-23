2043e5b3cf9a752465cdb0b347ff750a05236796
rika <35144242+nakairika@users.noreply.github.com>
2025-06-23 20:40:06 +0800

    Update layernorm.py

--- vllm_ascend/ops/layernorm.py
+++ vllm_ascend/ops/layernorm.py
@@ -19,17 +19,27 @@ from typing import Optional, Tuple, Union
 
 import torch
 from vllm.model_executor.layers.layernorm import RMSNorm
-
+from vllm.distributed import (get_tensor_model_parallel_rank,
+                              get_tensor_model_parallel_world_size,
+                              split_tensor_along_last_dim,
+                              split_tensor_along_first_dim)
 
 def forward_oot(
     self,
     x: torch.Tensor,
     residual: Optional[torch.Tensor] = None,
+    is_fc3=False
 ) -> Union[torch.Tensor, Tuple[torch.Tensor, torch.Tensor]]:
     import torch_npu
 
+    if is_fc3:
+        tp_size = get_tensor_model_parallel_world_size()
+        tp_rank = get_tensor_model_parallel_rank()
+        if residual is not None:
+            residual_split = split_tensor_along_first_dim(residual, num_partitions=tp_size)[tp_rank].contiguous()
+
     if residual is not None:
-        x, _, residual = torch_npu.npu_add_rms_norm(x, residual, self.weight,
+        x, _, residual = torch_npu.npu_add_rms_norm(x, residual_split if is_fc3 else residual, self.weight,
                                                     self.variance_epsilon)
         return x, residual
 
