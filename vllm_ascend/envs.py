0bbdc30ae522c1f036a38b2a2a57a475d67e51c7
leichao.lc <leichao.lc@antgroup.com>
2025-06-27 10:40:46 +0800

    Implement Mooncake Connector

--- vllm_ascend/envs.py
+++ vllm_ascend/envs.py
@@ -121,6 +121,10 @@ env_variables: Dict[str, Callable[[], Any]] = {
     # value to False to disable the optimized model.
     "USE_OPTIMIZED_MODEL":
     lambda: bool(int(os.getenv('USE_OPTIMIZED_MODEL', '1'))),
+    "VLLM_BASE_PORT":
+    lambda: int(os.getenv("VLLM_BASE_PORT", 8790)),
+    "MOONCAKE_CONNECTOR_PROTOCOL":
+    lambda: os.getenv("MOONCAKE_CONNECTOR_PROTOCOL", "ascend"),
 }
 
 # end-env-vars-definition
