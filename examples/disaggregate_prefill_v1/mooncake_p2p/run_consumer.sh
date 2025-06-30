41becab918f63f126d4340abac9429dfed6723eb
fjw <2270923832@qq.com>
2025-06-30 14:31:29 +0800

    Mooncake's p2p scenario implementation
    Signed-off-by: fjw <2270923832@qq.com>

---
+++ examples/disaggregate_prefill_v1/mooncake_p2p/run_consumer.sh
@@ -0,0 +1,51 @@
+set +x
+export HCCL_EXEC_TIMEOUT=204
+export HCCL_CONNECT_TIMEOUT=120
+export HCCL_IF_IP=xx.xx.xx.xx
+export GLOO_SOCKET_IFNAME="enp189s0f0"
+export TP_SOCKET_IFNAME="enp189s0f0"
+export HCCL_SOCKET_IFNAME="enp189s0f0"
+export ASCEND_RT_VISIBLE_DEVICES=4,5,6,7
+export OMP_PROC_BIND=false
+export OMP_NUM_THREADS=100
+export MOONCAKE_CONFIG_PATH="/home/xxx/json/mooncake_barebone.json"
+export VLLM_USE_V1=1
+export VLLM_BASE_PORT=9800
+
+vllm serve "model_path" \
+  --host xx.xx.xx.xx \
+  --port 8200 \
+  --tensor-parallel-size 2\
+  --seed 1024 \
+  --max-model-len 2000  \
+  --max-num-batched-tokens 2000 \
+  --enforce-eager \
+  --data-parallel-size 2 \
+  --data-parallel-size-local 2 \
+  --data-parallel-address xx.xx.xx.xx \
+  --data-parallel-rpc-port 9200 \
+  --data-parallel-start-rank 4 \
+  --trust-remote-code \
+  --gpu-memory-utilization 0.8  \
+  --kv-transfer-config  \
+  '{"kv_connector": "MooncakeConnectorV1_barebone",
+  "kv_buffer_device": "npu",
+  "kv_role": "kv_consumer",
+  "kv_parallel_size": 1,
+  "kv_port": "20002",
+  "engine_id": "1",
+  "kv_rank": 1,
+  "kv_connector_module_path": "vllm_ascend.distributed.mooncake_connector_v1_barebone",
+  "kv_connector_extra_config": {
+            "prefill": {
+                    "dp_size": 2,
+                    "tp_size": 2
+             },
+             "decode": {
+                    "dp_size": 2,
+                    "tp_size": 2
+             }
+      }
+  }'  \
+  --additional-config \
+  '{}'\
