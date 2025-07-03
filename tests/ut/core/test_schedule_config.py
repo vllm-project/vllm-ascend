e285145e97481c54a110ad92a91e95135859d6a2
zeshengzong <zesheng.zong@outlook.com>
2025-07-03 11:45:05 +0000

    Update

--- tests/ut/core/test_schedule_config.py
+++ tests/ut/core/test_schedule_config.py
@@ -15,21 +15,46 @@
 # This file is a part of the vllm-ascend project.
 #
 
-import unittest
-from vllm_ascend.core.schedule_config import AscendSchedulerConfig
 from vllm.config import SchedulerConfig
 
-class TestAscendSchedulerConfig(unittest.TestCase):
+from tests.ut.base import TestBase
+from vllm_ascend.core.schedule_config import AscendSchedulerConfig
+
+
+class TestAscendSchedulerConfig(TestBase):
+
     def test_initialize_from_config(self):
-        max_num_batched_tokens=128
-        vllm_scheduler_config = SchedulerConfig(max_num_batched_tokens=max_num_batched_tokens, max_model_len=max_num_batched_tokens, chunked_prefill_enabled=True)
-        
+        max_num_batched_tokens = 128
+        vllm_scheduler_config = SchedulerConfig(
+            max_num_batched_tokens=max_num_batched_tokens,
+            max_model_len=max_num_batched_tokens,
+            chunked_prefill_enabled=True)
+
         conf = AscendSchedulerConfig.initialize_from_config(
             vllm_scheduler_config, ascend_scheduler_config=None)
 
         self.assertEqual(conf.enable_chunked_prefill, False)
         self.assertEqual(conf.policy, "fcfs")
         self.assertEqual(conf.num_scheduler_steps, 1)
-        self.assertEqual(conf.scheduler_cls, "vllm_ascend.core.scheduler.AscendScheduler")
-        self.assertEqual(conf.max_num_encoder_input_tokens, max_num_batched_tokens)
+        self.assertEqual(conf.scheduler_cls,
+                         "vllm_ascend.core.scheduler.AscendScheduler")
+        self.assertEqual(conf.max_num_encoder_input_tokens,
+                         max_num_batched_tokens)
         self.assertEqual(conf.encoder_cache_size, max_num_batched_tokens)
+
+    def test_initialize_from_config_errors(self):
+        with self.assertRaisesRegex(NotImplementedError,
+                                    "only supports LLM models"):
+            AscendSchedulerConfig.initialize_from_config(
+                SchedulerConfig(is_multimodal_model=True, ),
+                ascend_scheduler_config=None)
+        with self.assertRaisesRegex(NotImplementedError,
+                                    "doesn't support send_delta_data"):
+            AscendSchedulerConfig.initialize_from_config(
+                SchedulerConfig(send_delta_data=True, ),
+                ascend_scheduler_config=None)
+        with self.assertRaisesRegex(NotImplementedError,
+                                    "doesn't support scheduler_delay_factor"):
+            AscendSchedulerConfig.initialize_from_config(
+                SchedulerConfig(delay_factor=2, ),
+                ascend_scheduler_config=None)
