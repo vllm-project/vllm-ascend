466df2995f51c49cf59485e52e2e5980ad3de0f8
shen-shanshan <467638484@qq.com>
2025-07-04 03:11:49 +0000

    modify doc format
    Signed-off-by: shen-shanshan <467638484@qq.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -116,6 +116,10 @@ html_theme_options = {
     'use_repository_button': True,
     'use_edit_page_button': True,
 }
+
+singlehtml_sidebars = {
+    'user_guide/environment_variables': [],  # disable the side bar
+}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
