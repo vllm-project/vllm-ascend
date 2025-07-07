17f4400756d3d5e3a413b0157af464a91edae5dc
MengqingCao <cmq0113@163.com>
2025-07-07 04:55:25 +0000

    [CI/UT][Refactor] refactor multi-card CI
    Signed-off-by: MengqingCao <cmq0113@163.com>

--- tests/e2e/singlecard/core/test_prefix_caching.py
+++ tests/e2e/singlecard/core/test_prefix_caching.py
@@ -68,7 +68,7 @@ def test_prefix_cache_with_v1_scheduler(model: str, max_tokens: int) -> None:
     with VllmRunner(model,
                     enforce_eager=True,
                     max_model_len=2048,
-                    tensor_parallel_size=2,
+                    tensor_parallel_size=1,
                     gpu_memory_utilization=0.7) as vllm_model:
         prefix_cache_output = vllm_model.generate_greedy(
             INPUT_PROMPTS, max_tokens)
@@ -77,7 +77,7 @@ def test_prefix_cache_with_v1_scheduler(model: str, max_tokens: int) -> None:
                     enable_prefix_caching=False,
                     enforce_eager=True,
                     max_model_len=2048,
-                    tensor_parallel_size=2,
+                    tensor_parallel_size=1,
                     gpu_memory_utilization=0.7) as vllm_model:
         vllm_output = vllm_model.generate_greedy(INPUT_PROMPTS, max_tokens)
 
@@ -104,7 +104,7 @@ def test_prefix_cache_with_ascend_scheduler(model: str,
                     },
                     enforce_eager=True,
                     max_model_len=2048,
-                    tensor_parallel_size=2,
+                    tensor_parallel_size=1,
                     gpu_memory_utilization=0.7) as vllm_model:
         vllm_output = vllm_model.generate_greedy(INPUT_PROMPTS, max_tokens)
 
@@ -117,7 +117,7 @@ def test_prefix_cache_with_ascend_scheduler(model: str,
                     },
                     enforce_eager=True,
                     max_model_len=2048,
-                    tensor_parallel_size=2,
+                    tensor_parallel_size=1,
                     gpu_memory_utilization=0.7) as vllm_model:
         prefix_cache_output = vllm_model.generate_greedy(
             INPUT_PROMPTS, max_tokens)
@@ -132,7 +132,7 @@ def test_prefix_cache_with_ascend_scheduler(model: str,
                     },
                     enforce_eager=True,
                     max_model_len=2048,
-                    tensor_parallel_size=2,
+                    tensor_parallel_size=1,
                     gpu_memory_utilization=0.7) as vllm_model:
         chunk_prefill_prefix_cache_output = vllm_model.generate_greedy(
             INPUT_PROMPTS, max_tokens)
