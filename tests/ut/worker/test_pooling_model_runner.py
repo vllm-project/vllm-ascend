9917da7756b96a0b950478067a19d4532ff2cb0c
wangyanhui-cmss <wangyanhui_yewu@cmss.chinamobile.com>
2025-07-07 14:22:42 +0800

    [CI/UT] modify the model path
    ### What this PR does / why we need it?
     Add ut for test_pooling_model_runner.py
    
    ### Does this PR introduce _any_ user-facing change?
    N/A
    
    ### How was this patch tested?
     python -m unittest  test_pooling_model_runner.py
    
    ---------
    
    Signed-off-by: wangyanhui-cmss <wangyanhui_yewu@cmss.chinamobile.com>

--- tests/ut/worker/test_pooling_model_runner.py
+++ tests/ut/worker/test_pooling_model_runner.py
@@ -26,7 +26,7 @@ class TestPoolingModelRunner(unittest.TestCase):
         self.attn_backend = "npu"
 
         model_runner = self._create_model_runner(
-            "test/ut/fake_weight",
+            "tests/ut/fake_weight",
             trust_remote_code=True,
             enable_chunked_prefill=False,
         )
