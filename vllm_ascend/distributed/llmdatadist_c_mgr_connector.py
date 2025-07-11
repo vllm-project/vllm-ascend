d8b5cc7a2f00e4b3955c1a78db64fc9dcb94e2a6
underfituu <hzhucong@163.com>
2025-07-11 18:26:29 +0800

    fix lint
    Signed-off-by: underfituu <hzhucong@163.com>

--- vllm_ascend/distributed/llmdatadist_c_mgr_connector.py
+++ vllm_ascend/distributed/llmdatadist_c_mgr_connector.py
@@ -384,7 +384,8 @@ class LLMDataDistCMgrConnectorWorker():
                                 f"LLMDataDistCMgrConnectorWorker: Receiving request {finished_req_id} finished"
                             )
                             self.finished_reqs.add(finished_req_id)
-                    sock.send_multipart((identity, b"", b"receiving decode finished"))
+                    sock.send_multipart(
+                        (identity, b"", b"receiving decode finished"))
                 else:
                     raise RuntimeError(
                         f"LLMDataDistCMgrConnectorWorker: Receiving unexpected request event {event_msg} from remote !"
