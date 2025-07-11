1e435e610b0b4bb1242c7e3f286f3cad21cfc327
weijinqian <weijinqian@huawei.com>
2025-07-12 00:33:33 +0800

    handle clean code
    Signed-off-by: weijinqian_v1 <weijinqian@huawei.com>

--- vllm_ascend/multistream/ms_split.py
+++ vllm_ascend/multistream/ms_split.py
@@ -304,25 +304,24 @@ def model_input_split_v1_attn(
         # the attn_mla kernel in torch npu only accept 128*128 attn mask
         attn_mask_pre = attn_mask_post = attn_metadata.attn_mask
         attn_state_pre = attn_state_post = attn_metadata.attn_state
-
     elif attn_metadata.attn_state == AscendAttentionState.DecodeOnly:
         # should be none in decode only state
         attn_mask_pre = attn_mask_post = attn_metadata.attn_mask
-        attn_state_pre = attn_state_post = AscendAttentionState.DecodeOnly  # noqa
+        attn_state_pre = attn_state_post = AscendAttentionState.DecodeOnly  # type: ignore
     else:
         # chunked prefill
         assert attn_metadata.attn_mask is not None
         if has_prefill_pre:
-            attn_state_pre = attn_state_post = AscendAttentionState.ChunkedPrefill  # noqa
+            attn_state_pre = attn_state_post = AscendAttentionState.ChunkedPrefill  # type: ignore
             attn_mask_pre = attn_metadata.attn_mask[:token_index, :max(
                 seq_lens_pre)].contiguous()
-            attn_state_post = AscendAttentionState.ChunkedPrefill  # noqa
+            attn_state_post = AscendAttentionState.ChunkedPrefill  # type: ignore
             attn_mask_post = attn_metadata.attn_mask[
                 token_index:, :max(seq_lens_post)].contiguous()
         else:
-            attn_state_pre = AscendAttentionState.DecodeOnly  # noqa
+            attn_state_pre = AscendAttentionState.DecodeOnly  # type: ignore
             attn_mask_pre = None
-            attn_state_post = AscendAttentionState.ChunkedPrefill  # noqa
+            attn_state_post = AscendAttentionState.ChunkedPrefill  # type: ignore
             attn_mask_post = attn_metadata.attn_mask[
                 token_index:, :max(seq_lens_post)].contiguous()
 
