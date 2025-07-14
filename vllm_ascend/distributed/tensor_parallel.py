c2f670df7cb86572d7a242c7c43b181a0ffda2d2
duyangkai <duyangkai@huawei.com>
2025-07-14 09:52:24 +0800

    fix header
    Signed-off-by: duyangkai <duyangkai@huawei.com>

--- vllm_ascend/distributed/tensor_parallel.py
+++ vllm_ascend/distributed/tensor_parallel.py
@@ -13,6 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# Adapts from: Megatron/megatron/core/tensor_parallel/mappings.py.
 # This file is a part of the vllm-ascend project.
 import torch
 
