c15449ba0ed7caf1652a3b97dc8b1fbdbe27da0e
rjg-lyh <1318825571@qq.com>
2025-07-15 15:19:04 +0800

    [V0.9.1] Patch compilation.decorator to support flashcomm_v1 in aclgraph
    Signed-off-by: rjg-lyh <1318825571@qq.com>

--- vllm_ascend/models/qwen2.py
+++ vllm_ascend/models/qwen2.py
@@ -1,34 +1,30 @@
 from collections.abc import Iterable
-from typing import Any, Optional, Union
+from typing import Optional, Union
 
 import torch
-from torch import nn
 import torch.nn.functional as F
+from torch import nn
 from transformers import Qwen2Config
-
 from vllm.compilation.decorators import support_torch_compile
 from vllm.config import CacheConfig, VllmConfig
-from vllm.distributed import get_pp_group, get_tensor_model_parallel_world_size
+from vllm.distributed import (get_pp_group, get_tensor_model_parallel_rank,
+                              get_tensor_model_parallel_world_size,
+                              tensor_model_parallel_all_gather,
+                              tensor_model_parallel_all_reduce,
+                              tensor_model_parallel_reduce_scatter)
+from vllm.forward_context import get_forward_context
 from vllm.model_executor.layers.logits_processor import LogitsProcessor
 from vllm.model_executor.layers.quantization import QuantizationConfig
 from vllm.model_executor.layers.vocab_parallel_embedding import ParallelLMHead
+from vllm.model_executor.models.interfaces import SupportsLoRA, SupportsPP
+from vllm.model_executor.models.qwen2 import Qwen2DecoderLayer, Qwen2Model
+from vllm.model_executor.models.utils import (AutoWeightsLoader,
+                                              PPMissingLayer, maybe_prefix)
 from vllm.model_executor.sampling_metadata import SamplingMetadata
 from vllm.sequence import IntermediateTensors
 
-from vllm.model_executor.models.interfaces import SupportsLoRA, SupportsPP
-from vllm.model_executor.models.utils import (AutoWeightsLoader, PPMissingLayer, maybe_prefix)
-
-from vllm.model_executor.models.qwen2 import Qwen2Model, Qwen2DecoderLayer
-from vllm.distributed import (
-                            get_pp_group,
-                            get_tensor_model_parallel_world_size,
-                            get_tensor_model_parallel_rank,
-                            tensor_model_parallel_all_gather,
-                            tensor_model_parallel_all_reduce,
-                            tensor_model_parallel_reduce_scatter)
-from vllm_ascend.attention.attention_v1 import AscendAttentionState
-from vllm.forward_context import get_forward_context
 import vllm_ascend.envs as ascend_envs
+from vllm_ascend.attention.attention_v1 import AscendAttentionState
 
 
 def all_gather_and_maybe_unpad(
@@ -40,6 +36,7 @@ def all_gather_and_maybe_unpad(
         return hidden_states[:-pad_size, :]
     return hidden_states
 
+
 def maybe_pad_and_reduce_scatter(
     hidden_states: torch.Tensor,
     pad_size: int,
@@ -48,6 +45,7 @@ def maybe_pad_and_reduce_scatter(
         hidden_states = F.pad(hidden_states, (0, 0, 0, pad_size))
     hidden_states = tensor_model_parallel_reduce_scatter(hidden_states, 0)
     return hidden_states
+
 
 class CustomQwen2DecoderLayer(Qwen2DecoderLayer):
 
@@ -64,9 +62,9 @@ class CustomQwen2DecoderLayer(Qwen2DecoderLayer):
                          prefix=prefix)
         self.tp_rank = get_tensor_model_parallel_rank()
         self.tp_size = get_tensor_model_parallel_world_size()
-        self.self_attn.o_proj.reduce_results=False
-        self.mlp.down_proj.reduce_results=False
-        
+        self.self_attn.o_proj.reduce_results = False
+        self.mlp.down_proj.reduce_results = False
+
     def forward(
         self,
         positions: torch.Tensor,
@@ -81,19 +79,22 @@ class CustomQwen2DecoderLayer(Qwen2DecoderLayer):
             if flashcomm_v1_enabled:
                 if pad_size > 0:
                     residual = F.pad(residual, (0, 0, 0, pad_size))
-                residual = torch.chunk(residual, self.tp_size, dim=0)[self.tp_rank]
+                residual = torch.chunk(residual, self.tp_size,
+                                       dim=0)[self.tp_rank]
             hidden_states = self.input_layernorm(hidden_states)
         else:
             hidden_states, residual = self.input_layernorm(
                 hidden_states, residual)
             if flashcomm_v1_enabled:
-                hidden_states = all_gather_and_maybe_unpad(hidden_states, pad_size)
+                hidden_states = all_gather_and_maybe_unpad(
+                    hidden_states, pad_size)
         hidden_states = self.self_attn(
             positions=positions,
             hidden_states=hidden_states,
         )
         if flashcomm_v1_enabled:
-            hidden_states = maybe_pad_and_reduce_scatter(hidden_states, pad_size)
+            hidden_states = maybe_pad_and_reduce_scatter(
+                hidden_states, pad_size)
         else:
             hidden_states = tensor_model_parallel_all_reduce(hidden_states)
         # Fully Connected
@@ -103,7 +104,8 @@ class CustomQwen2DecoderLayer(Qwen2DecoderLayer):
             hidden_states = all_gather_and_maybe_unpad(hidden_states, pad_size)
         hidden_states = self.mlp(hidden_states)
         if flashcomm_v1_enabled:
-            hidden_states = maybe_pad_and_reduce_scatter(hidden_states, pad_size)
+            hidden_states = maybe_pad_and_reduce_scatter(
+                hidden_states, pad_size)
         else:
             hidden_states = tensor_model_parallel_all_reduce(hidden_states)
         return hidden_states, residual
@@ -120,11 +122,12 @@ class CustomQwen2DecoderLayer(Qwen2DecoderLayer):
     })
 class CustomQwen2Model(Qwen2Model):
 
-    def __init__(self,
-                 *,
-                 vllm_config: VllmConfig,
-                 prefix: str = "",
-                 decoder_layer_type: type[nn.Module] = CustomQwen2DecoderLayer):
+    def __init__(
+            self,
+            *,
+            vllm_config: VllmConfig,
+            prefix: str = "",
+            decoder_layer_type: type[nn.Module] = CustomQwen2DecoderLayer):
         super().__init__(vllm_config=vllm_config,
                          prefix=prefix,
                          decoder_layer_type=decoder_layer_type)
@@ -156,7 +159,8 @@ class CustomQwen2Model(Qwen2Model):
             flashcomm_v1_enabled = True
         if flashcomm_v1_enabled:
             num_tokens = hidden_states.size(0)
-            pad_size = (self.tp_size - (num_tokens % self.tp_size)) % self.tp_size
+            pad_size = (self.tp_size -
+                        (num_tokens % self.tp_size)) % self.tp_size
         for layer in self.layers[self.start_layer:self.end_layer]:
             hidden_states, residual = layer(
                 positions,
@@ -201,7 +205,7 @@ class CustomQwen2ForCausalLM(nn.Module, SupportsLoRA, SupportsPP):
 
         self.quant_config = quant_config
         self.model = CustomQwen2Model(vllm_config=vllm_config,
-                                prefix=maybe_prefix(prefix, "model"))
+                                      prefix=maybe_prefix(prefix, "model"))
 
         if get_pp_group().is_last_rank:
             if config.tie_word_embeddings:
