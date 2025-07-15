562980cf53515096699a8feca3c7cc869b8b17b4
SunnyLee151064 <83354342+SunnyLee151064@users.noreply.github.com>
2025-07-15 15:03:20 +0800

    Update test_expert_load_balancer.py

--- tests/ut/ops/test_expert_load_balancer.py
+++ tests/ut/ops/test_expert_load_balancer.py
@@ -41,6 +41,7 @@ class MockData(TypedDict):
 
 
 class TestExpertLoadBalancer(TestBase):
+
     def setUp(self):
         _TEST_DIR = os.path.dirname(__file__)
         json_file = _TEST_DIR + "/expert_map.json"
