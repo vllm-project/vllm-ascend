f202bb69c489aabc6ea933cbb9c6aca5a9cfea61
zhoux77899 <zhouxiang100@huawei.com>
2025-07-16 17:56:38 +0800

    models(qwen3_moe): fix lint
    Signed-off-by: ZhouXiang <zhouxiang100@huawei.com>

--- vllm_ascend/models/qwen3_moe.py
+++ vllm_ascend/models/qwen3_moe.py
@@ -41,7 +41,7 @@ class CustomQwen3MoeForCausalLM(Qwen3MoeForCausalLM):
             "up_proj",
         ],
         "experts":
-            ["experts.0.gate_proj", "experts.0.up_proj", "experts.0.down_proj"],
+        ["experts.0.gate_proj", "experts.0.up_proj", "experts.0.down_proj"],
     }
     qwen3.Qwen3MoeSparseMoeBlock = AscendSparseMoeBlock
 
