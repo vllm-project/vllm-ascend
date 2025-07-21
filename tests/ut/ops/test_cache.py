83df950af3d694575ec23f033a7e6e21688cf7fd
SunnyLee151064 <83354342+SunnyLee151064@users.noreply.github.com>
2025-07-21 16:17:51 +0800

    Update test_cache.py

--- tests/ut/ops/test_cache.py
+++ tests/ut/ops/test_cache.py
@@ -1,23 +1,19 @@
-import unittest
-
 import torch
 
+from tests.ut.base import TestBase
 from vllm_ascend.ops.cache import concat_and_cache_mla
 
 
-class TestCache(unittest.TestCase):
+class TestCache(TestBase):
 
     def setUp(self):
         self.num_tokens = 4
-        self.num_kv_head = 2
+        self.num_kv_head = 1
         self.nope = 3
         self.rope = 4
         self.num_blocks = 2
         self.block_size = 2
-
-        # 随机生成输入张量
-        self.kv_c_normed = torch.randn(self.num_tokens, self.num_kv_head,
-                                       self.nope)
+        self.kv_c_normed = torch.randn(self.num_tokens, self.nope)
         self.k_pe = torch.randn(self.num_tokens, self.num_kv_head, self.rope)
         self.kv_cache = torch.zeros(self.num_blocks, self.block_size,
                                     self.num_kv_head, self.nope + self.rope)
@@ -28,7 +24,9 @@ class TestCache(unittest.TestCase):
                              self.slot_mapping)
         kv_cache_reshaped = self.kv_cache.view(
             self.num_blocks * self.block_size, self.num_kv_head, -1)
-        expected = torch.cat([self.kv_c_normed, self.k_pe], dim=-1).squeeze(1)
+        expected = torch.cat([self.kv_c_normed.unsqueeze(1), self.k_pe],
+                             dim=-1).squeeze(1)
         for i in range(self.num_tokens):
             idx = self.slot_mapping[i].item()
-            assert torch.allclose(kv_cache_reshaped[idx], expected[i])
+            self.assertEqual(
+                torch.allclose(kv_cache_reshaped[idx], expected[i]))
