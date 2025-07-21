3b62391379caa943c96fbed3a1829a3539789437
wangxiaoxin-sherie <wangxiaoxin7@huawei.com>
2025-07-21 21:33:59 +0800

    fix logits

--- vllm_ascend/ops/logits_processor.py
+++ vllm_ascend/ops/logits_processor.py
@@ -3,9 +3,11 @@ from vllm.model_executor.layers.logits_processor import \
 import torch
 from typing import Optional
 from vllm_ascend.distributed.parallel_state import get_lm_tp_group
+from vllm.distributed.parallel_state import get_dp_group
 from vllm.model_executor.layers.vocab_parallel_embedding import (
     VocabParallelEmbedding)
 from vllm.model_executor.sampling_metadata import SamplingMetadata
+from vllm.forward_context import ForwardContext, get_forward_context
 
 class CustomLogitsProcessor(LogitsProcessor):
     
@@ -68,13 +70,8 @@ class CustomLogitsProcessor(LogitsProcessor):
 
         # # Gather logits for TP
         # logits = self._gather_logits(logits)
+        # cu_tokens_across_dp_cpu = get_forward_context().dp_metadata.cu_tokens_across_dp_cpu
         
-        lm_tp_size = lm_head.tp_size
-        global_dp_batch_size = torch.diff(cu_tokens_across_dp_cpu, prepend=cu_tokens_across_dp_cpu.new_zeros(1))
-        local_batch_size = hidden_states.size(0)
-        lm_group_batch_size = [global_dp_batch_size[x%lm_tp_size] for x in get_lm_tp_group().ranks]
-        lm_total_batchsize = sum(lm_group_batch_size)
-        local_output_dim = lm_head.num_embeddings_per_partition
         
         #adapt: 8DP to 8TP
         hidden_states = get_lm_tp_group().all_gather(hidden_states, dim=0)
@@ -83,18 +80,16 @@ class CustomLogitsProcessor(LogitsProcessor):
                                                 hidden_states,
                                                 bias=embedding_bias)
         
-        input_splits = lm_group_batch_size
-        output_splits = [local_batch_size * local_output_dim for _ in lm_group_batch_size]
-        output_ = torch.empty(local_batch_size * local_output_dim * lm_tp_size,
-                              dtype=hidden_states.dtype,
-                              device=hidden_states.device)
+        world_size = get_lm_tp_group().world_size
+        vocab_size = logits.shape[-1] * world_size
+        logits = logits.view(-1)
+
+        output_ = torch.zeros_like(logits).view(-1)
         torch.distributed.all_to_all_single(output_,
                                             logits,
-                                            input_splits,
-                                            output_splits,
                                             group=get_lm_tp_group().device_group)
-        output_ = output_.view(lm_head.lm_tp_size, -1, local_output_dim).transpose(0, 1)
-        logits = output_.reshape(local_output_dim, -1)
+        output_ = output_.view(world_size, -1,  vocab_size // world_size).transpose(0, 1)
+        logits = output_.reshape(-1, vocab_size)
 
         # Remove paddings in vocab (if any).
         if logits is not None:
