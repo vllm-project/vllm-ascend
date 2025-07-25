925e8a6d53428f04abe87221d86d67a2999a0f66
zhanghaiwen <zhanghaiwen@cmss.chinamobile.com>
2025-07-25 17:46:58 +0800

    delete dispose_tensor
    Signed-off-by: zhanghaiwen <zhanghaiwen@cmss.chinamobile.com>

--- vllm_ascend/ops/fused_moe.py
+++ vllm_ascend/ops/fused_moe.py
@@ -1471,7 +1471,6 @@ class AscendFusedMoE(FusedMoE):
                 dispose_tensor(e_hidden_states)
             else:
                 final_hidden_states = e_hidden_states
-                dispose_tensor(e_hidden_states)
         else:
             final_hidden_states = e_hidden_states
 
