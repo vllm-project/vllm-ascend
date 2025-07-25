a670e7f282484c31092389ef2ec4a807b526fdc1
lwq <liwenquan5@huawei.com>
2025-07-25 14:39:23 +0800

    Add some uts for piece backend
    Signed-off-by: lwq <liwenquan5@huawei.com>

---
+++ tests/ut/compilation/registe_fusion_op.py
@@ -0,0 +1,15 @@
+from vllm_ascend import ops
+
+FUSION_OP_REGISTERED = False
+
+
+
+try:
+
+    ops.register_dummy_fusion_op()
+
+    FUSION_OP_REGISTERED = True
+
+except Exception as e:
+
+    print("Regist dummy fusion op failed!")
