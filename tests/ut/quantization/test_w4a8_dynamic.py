1cc3eee82b0137203199ba27ecac5ce695af8d13
zhoux77899 <zhouxiang100@huawei.com>
2025-07-28 15:51:40 +0800

    test(ci): trigger ci
    Signed-off-by: ZhouXiang <zhouxiang100@huawei.com>

--- tests/ut/quantization/test_w4a8_dynamic.py
+++ tests/ut/quantization/test_w4a8_dynamic.py
@@ -11,17 +11,17 @@ class TestAscendW4A8DynamicLinearMethod(TestBase):
         self.method.group_size = 8
 
     def test_get_weight(self):
-        weight = self.method.get_weight(8, 16, torch.bfloat16)
+        weight = self.method.get_weight(8, 32, torch.bfloat16)
         self.assertEqual(weight["weight"].dtype, torch.int8)
-        self.assertEqual(weight["weight"].shape, (16, 8))
+        self.assertEqual(weight["weight"].shape, (32, 8))
 
     def test_get_pergroup_param(self):
-        params = self.method.get_pergroup_param(8, 16, torch.bfloat16)
+        params = self.method.get_pergroup_param(8, 32, torch.bfloat16)
         self.assertEqual(params["weight_scale"].dtype, torch.bfloat16)
         self.assertEqual(params["weight_scale"].shape, (1, ))
         self.assertEqual(params["weight_offset"].dtype, torch.bfloat16)
         self.assertEqual(params["weight_offset"].shape, (1, ))
         self.assertEqual(params["weight_scale_second"].dtype, torch.bfloat16)
-        self.assertEqual(params["weight_scale_second"].shape, (16, 1))
+        self.assertEqual(params["weight_scale_second"].shape, (32, 1))
         self.assertEqual(params["weight_offset_second"].dtype, torch.bfloat16)
-        self.assertEqual(params["weight_offset_second"].shape, (16, 1))
+        self.assertEqual(params["weight_offset_second"].shape, (32, 1))
