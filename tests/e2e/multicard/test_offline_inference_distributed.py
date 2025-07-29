d72aad364cfddb4cc6294165586440d57280da49
zhoux77899 <zhouxiang100@huawei.com>
2025-07-29 20:01:04 +0800

    fix(test): change e2e test tp_size
    Signed-off-by: ZhouXiang <zhouxiang100@huawei.com>

--- tests/e2e/multicard/test_offline_inference_distributed.py
+++ tests/e2e/multicard/test_offline_inference_distributed.py
@@ -202,7 +202,7 @@ def test_models_distributed_Qwen3_W4A8DYNAMIC():
             snapshot_download("vllm-ascend/Qwen3-8B-W4A8"),
             max_model_len=8192,
             dtype="auto",
-            tensor_parallel_size=4,
+            tensor_parallel_size=2,
             quantization="ascend",
     ) as vllm_model:
         vllm_model.generate_greedy(example_prompts, max_tokens)
