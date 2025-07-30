8363a8fb162e51930a7c45009e5e5d2af2adce21
weijinqian_v1 <weijinqian@huawei.com>
2025-07-30 10:39:50 +0800

    [v0.9.1][Feature] add moe alltoallv.
    Signed-off-by: weijinqian_v1 <weijinqian@huawei.com>

--- vllm_ascend/attention/attention_v1.py
+++ vllm_ascend/attention/attention_v1.py
@@ -28,7 +28,6 @@ from vllm.forward_context import ForwardContext, get_forward_context
 from vllm.utils import direct_register_custom_op
 from vllm.v1.core.sched.output import SchedulerOutput
 
-from vllm_ascend.multistream.base import MSAttentionMetadataSplitConfig
 from vllm_ascend.ops.attention import vanilla_chunked_prefill
 from vllm_ascend.utils import (ACL_FORMAT_FRACTAL_NZ, aligned_16, is_310p,
                                nd_to_nz_2d, nd_to_nz_spec)
