56af380bc1b5532a846547aafa58ba92c3ca2c3e
ganyi <pleaplusone.gy@gmail.com>
2025-07-31 11:29:57 +0800

    fix format issue
    Signed-off-by: ganyi <pleaplusone.gy@gmail.com>

--- vllm_ascend/patch/platform/patch_0_9_1/patch_core.py
+++ vllm_ascend/patch/platform/patch_0_9_1/patch_core.py
@@ -1,79 +1,81 @@
 import os
 import signal
 from typing import Optional
-from vllm.config import VllmConfig, ParallelConfig
-from vllm.v1.engine.core import EngineCoreProc, DPEngineCoreProc
+
+from vllm.config import ParallelConfig, VllmConfig
+from vllm.transformers_utils.config import \
+    maybe_register_config_serialize_by_value
 from vllm.utils import logger
-from vllm.transformers_utils.config import (
-    maybe_register_config_serialize_by_value)
+from vllm.v1.engine.core import DPEngineCoreProc, EngineCoreProc
+
 import vllm_ascend.envs as vllm_ascend_envs
 
+
 class ExternealDPEngineCoreProc(DPEngineCoreProc):
-  def __init__(self, *args, **kwargs):
-      # Use the external data parallelism master port from envs
-      super().__init__(*args, **kwargs)
-      self.engines_running = True
-  
-  def _has_global_unfinished_reqs(self, local_unfinished):
-      return True
 
-  def _init_data_parallel(self, vllm_config: VllmConfig):
+    def __init__(self, *args, **kwargs):
+        # Use the external data parallelism master port from envs
+        super().__init__(*args, **kwargs)
+        self.engines_running = True
 
-      # Configure GPUs and stateless process group for data parallel.
-      dp_rank = vllm_config.parallel_config.data_parallel_rank
-      dp_size = vllm_config.parallel_config.data_parallel_size
-      local_dp_rank = vllm_config.parallel_config.data_parallel_rank_local
+    def _has_global_unfinished_reqs(self, local_unfinished):
+        return True
 
-      assert dp_size > 1
-      assert 0 <= local_dp_rank <= dp_rank < dp_size
+    def _init_data_parallel(self, vllm_config: VllmConfig):
 
-      if vllm_config.kv_transfer_config is not None:
-          # modify the engine_id and append the local_dp_rank to it to ensure
-          # that the kv_transfer_config is unique for each DP rank.
-          vllm_config.kv_transfer_config.engine_id = (
-              f"{vllm_config.kv_transfer_config.engine_id}_dp{local_dp_rank}"
-          )
-          logger.debug("Setting kv_transfer_config.engine_id to %s",
-                        vllm_config.kv_transfer_config.engine_id)
+        # Configure GPUs and stateless process group for data parallel.
+        dp_rank = vllm_config.parallel_config.data_parallel_rank
+        dp_size = vllm_config.parallel_config.data_parallel_size
+        local_dp_rank = vllm_config.parallel_config.data_parallel_rank_local
 
-      from vllm.platforms import current_platform
-      device_control_env_var = current_platform.device_control_env_var
-      world_size = vllm_config.parallel_config.world_size
-      os.environ[device_control_env_var] = ",".join(
-          str(current_platform.device_id_to_physical_device_id(i))
-          for i in range(local_dp_rank * world_size, (local_dp_rank + 1) *
-                          world_size))
+        assert dp_size > 1
+        assert 0 <= local_dp_rank <= dp_rank < dp_size
 
-      self.dp_rank = dp_rank
+        if vllm_config.kv_transfer_config is not None:
+            # modify the engine_id and append the local_dp_rank to it to ensure
+            # that the kv_transfer_config is unique for each DP rank.
+            vllm_config.kv_transfer_config.engine_id = (
+                f"{vllm_config.kv_transfer_config.engine_id}_dp{local_dp_rank}"
+            )
+            logger.debug("Setting kv_transfer_config.engine_id to %s",
+                         vllm_config.kv_transfer_config.engine_id)
 
-  def run_busy_loop(self):
-      """Core busy loop of the EngineCore for data parallel case."""
-      # Note: In customized DPEngineCoreProc, no idle time will exist. We assume the another dp groups are always 
-      # running.
+        from vllm.platforms import current_platform
+        device_control_env_var = current_platform.device_control_env_var
+        world_size = vllm_config.parallel_config.world_size
+        os.environ[device_control_env_var] = ",".join(
+            str(current_platform.device_id_to_physical_device_id(i))
+            for i in range(local_dp_rank * world_size, (local_dp_rank + 1) *
+                           world_size))
 
-      # Loop until process is sent a SIGINT or SIGTERM
-      while True:
-          # 1) Poll the input queue until there is work to do.
-          self._process_input_queue()
+        self.dp_rank = dp_rank
 
-          # 2) Step the engine core.
-          executed = self._process_engine_step()
-          self._maybe_publish_request_counts()
+    def run_busy_loop(self):
+        """Core busy loop of the EngineCore for data parallel case."""
+        # Note: In customized DPEngineCoreProc, no idle time will exist. We assume the another dp groups are always
+        # running.
 
-          local_unfinished_reqs = self.scheduler.has_unfinished_requests()
-          if not executed:
-              if not local_unfinished_reqs and not self.engines_running:
-                  # All engines are idle.
-                  continue
+        # Loop until process is sent a SIGINT or SIGTERM
+        while True:
+            # 1) Poll the input queue until there is work to do.
+            self._process_input_queue()
 
-              # We are in a running state and so must execute a dummy pass
-              # if the model didn't execute any ready requests.
-              self.execute_dummy_batch()
+            # 2) Step the engine core.
+            executed = self._process_engine_step()
+            self._maybe_publish_request_counts()
 
-def run_engine_core(*args,
-                    dp_rank: int = 0,
-                    local_dp_rank: int = 0,
-                    **kwargs):
+            local_unfinished_reqs = self.scheduler.has_unfinished_requests()
+            if not executed:
+                if not local_unfinished_reqs and not self.engines_running:
+                    # All engines are idle.
+                    continue
+
+                # We are in a running state and so must execute a dummy pass
+                # if the model didn't execute any ready requests.
+                self.execute_dummy_batch()
+
+
+def run_engine_core(*args, dp_rank: int = 0, local_dp_rank: int = 0, **kwargs):
     """Launch EngineCore busy loop in background process."""
 
     # Signal handler used for graceful termination.
@@ -96,8 +98,7 @@ def run_engine_core(*args,
 
     engine_core: Optional[EngineCoreProc] = None
     try:
-        parallel_config: ParallelConfig = kwargs[
-            "vllm_config"].parallel_config
+        parallel_config: ParallelConfig = kwargs["vllm_config"].parallel_config
         if parallel_config.data_parallel_size > 1 or dp_rank > 0:
             # Set data parallel rank for this engine process.
             parallel_config.data_parallel_rank = dp_rank
@@ -122,7 +123,8 @@ def run_engine_core(*args,
         if engine_core is not None:
             engine_core.shutdown()
 
+
 # Apply this patch only if the external data parallelism is enabled
 if vllm_ascend_envs.VLLM_ASCEND_EXTERNAL_DP_LB_ENABLED:
-  # Patch the EngineCoreClient to use the custom make_async_mp_client
-  EngineCoreProc.run_engine_core = run_engine_core  # type: ignore[attr-defined]
+    # Patch the EngineCoreClient to use the custom make_async_mp_client
+    EngineCoreProc.run_engine_core = run_engine_core  # type: ignore[attr-defined]
