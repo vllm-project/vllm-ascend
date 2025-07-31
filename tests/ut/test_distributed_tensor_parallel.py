9130e589bb12ab8cd4f79eb5b5315ca361787f0e
weijinqian_v1 <weijinqian@huawei.com>
2025-07-31 11:05:47 +0800

    [v0.9.1][Feature] add moe alltoallv.
    Signed-off-by: weijinqian_v1 <weijinqian@huawei.com>

--- tests/ut/test_distributed_tensor_parallel.py
+++ tests/ut/test_distributed_tensor_parallel.py
@@ -32,29 +32,21 @@ class TestDistributedCommunication(PytestBase):
 
     @pytest.fixture(autouse=True)
     def context(self, mocker: MockerFixture):
-        mocker.patch(
-            "torch.npu.current_device",
-            return_value="cpu")
-        mocker.patch(
-            "torch.distributed.get_world_size",
-            return_value=4)
+        mocker.patch("torch.npu.current_device", return_value="cpu")
+        mocker.patch("torch.distributed.get_world_size", return_value=4)
 
-        mocker.patch(
-            "torch.distributed.get_rank",
-            return_value=0)
+        mocker.patch("torch.distributed.get_rank", return_value=0)
 
-    @pytest.mark.parametrize("world_size, test_tensor, expected", [
-        (1, torch.randn(8, 16), (8, 16)),
-        (4, torch.randn(8, 16), (32, 16))
-    ])
-    def test_gather_along_first_dim(self, test_tensor, expected,
-                                    world_size, mocker: MockerFixture):
+    @pytest.mark.parametrize("world_size, test_tensor, expected",
+                             [(1, torch.randn(8, 16), (8, 16)),
+                              (4, torch.randn(8, 16), (32, 16))])
+    def test_gather_along_first_dim(self, test_tensor, expected, world_size,
+                                    mocker: MockerFixture):
         """test _gather_along_first_dim"""
-        mocker.patch(
-            "torch.distributed.get_world_size",
-            return_value=world_size)
+        mocker.patch("torch.distributed.get_world_size",
+                     return_value=world_size)
 
-        result = _gather_along_first_dim(test_tensor, mocker.MagicMock() )
+        result = _gather_along_first_dim(test_tensor, mocker.MagicMock())
 
         assert result.shape == expected
 
@@ -62,25 +54,25 @@ class TestDistributedCommunication(PytestBase):
         (torch.randn(8, 16), [5, 10, 15, 2], (32, 16)),
     ])
     def test_gather_along_first_dim_unequal_split(self, test_tensor, expected,
-                                                  output_split_sizes, mocker: MockerFixture):
+                                                  output_split_sizes,
+                                                  mocker: MockerFixture):
         """test _gather_along_first_dim"""
 
-        result = _gather_along_first_dim(test_tensor, mocker.MagicMock() , output_split_sizes)
+        result = _gather_along_first_dim(test_tensor, mocker.MagicMock(),
+                                         output_split_sizes)
 
         assert result.shape == expected
 
-    @pytest.mark.parametrize("world_size, test_tensor, expected", [
-        (1, torch.randn(8, 16, 32), (8, 16, 32)),
-        (4, torch.randn(8, 16, 32), (8, 16, 32 * 4))
-    ])
-    def test_gather_along_last_dim(self, test_tensor, expected,
-                                   world_size, mocker: MockerFixture):
+    @pytest.mark.parametrize("world_size, test_tensor, expected",
+                             [(1, torch.randn(8, 16, 32), (8, 16, 32)),
+                              (4, torch.randn(8, 16, 32), (8, 16, 32 * 4))])
+    def test_gather_along_last_dim(self, test_tensor, expected, world_size,
+                                   mocker: MockerFixture):
         """test _gather_along_last_dim"""
-        mocker.patch(
-            "torch.distributed.get_world_size",
-            return_value=world_size)
+        mocker.patch("torch.distributed.get_world_size",
+                     return_value=world_size)
 
-        result = _gather_along_last_dim(test_tensor, mocker.MagicMock() )
+        result = _gather_along_last_dim(test_tensor, mocker.MagicMock())
 
         assert result.shape == expected
 
@@ -88,19 +80,21 @@ class TestDistributedCommunication(PytestBase):
         ((32, 16), (8, 16)),
         ((40, 10), (10, 10)),
     ])
-    def test_reduce_scatter_along_first_dim(self, input_shape,
-                                            expected_shape, mocker: MockerFixture):
+    def test_reduce_scatter_along_first_dim(self, input_shape, expected_shape,
+                                            mocker: MockerFixture):
         input_tensor = torch.randn(*input_shape)
-        result = _reduce_scatter_along_first_dim(input_tensor, mocker.MagicMock() )
+        result = _reduce_scatter_along_first_dim(input_tensor,
+                                                 mocker.MagicMock())
         assert result.shape == expected_shape
 
     @pytest.mark.parametrize("input_shape,expected_shape", [
         ((8, 16, 32), (8, 16, 8)),
     ])
-    def test_reduce_scatter_along_last_dim(self, input_shape,
-                                           expected_shape, mocker: MockerFixture):
+    def test_reduce_scatter_along_last_dim(self, input_shape, expected_shape,
+                                           mocker: MockerFixture):
         input_tensor = torch.randn(*input_shape)
-        result = _reduce_scatter_along_last_dim(input_tensor, mocker.MagicMock() )
+        result = _reduce_scatter_along_last_dim(input_tensor,
+                                                mocker.MagicMock())
         assert result.shape == expected_shape
 
     @pytest.mark.parametrize("func,input_shape,expected_shape", [
@@ -111,15 +105,15 @@ class TestDistributedCommunication(PytestBase):
          (8, 16, 8)),
         ("gather_from_sequence_parallel_region", (8, 16), (32, 16)),
     ])
-    def test_wrapper_functions(self, func, input_shape,
-                               expected_shape, mocker: MockerFixture):
+    def test_wrapper_functions(self, func, input_shape, expected_shape,
+                               mocker: MockerFixture):
         """test wrapper funcs"""
         mod = importlib.import_module(
             'vllm_ascend.distributed.tensor_parallel')
         globals = mod.__dict__
         test_func = globals[func]
         input_tensor = torch.randn(*input_shape)
-        result = test_func(input_tensor, mocker.MagicMock() )
+        result = test_func(input_tensor, mocker.MagicMock())
         assert result.shape == expected_shape
 
     @pytest.mark.parametrize(
@@ -127,9 +121,10 @@ class TestDistributedCommunication(PytestBase):
         [
             ((8, 16), (32, 4)),  # [num_tokens/TP, H] -> [num_tokens, H/TP]
         ])
-    def test_all_to_all_sp2hp(self, input_shape, output_shape, mocker: MockerFixture):
+    def test_all_to_all_sp2hp(self, input_shape, output_shape,
+                              mocker: MockerFixture):
         input_tensor = torch.randn(*input_shape)
-        result = all_to_all_sp2hp(input_tensor, mocker.MagicMock() )
+        result = all_to_all_sp2hp(input_tensor, mocker.MagicMock())
         assert result.shape == output_shape
 
     @pytest.mark.parametrize(
@@ -137,7 +132,8 @@ class TestDistributedCommunication(PytestBase):
         [
             ((32, 4), (8, 16)),  # [num_tokens, H/TP] -> [num_tokens/TP, H]
         ])
-    def test_all_to_all_hp2sp(self, input_shape, output_shape, mocker: MockerFixture):
+    def test_all_to_all_hp2sp(self, input_shape, output_shape,
+                              mocker: MockerFixture):
         input_tensor = torch.randn(*input_shape)
-        result = all_to_all_hp2sp(input_tensor, mocker.MagicMock() )
+        result = all_to_all_hp2sp(input_tensor, mocker.MagicMock())
         assert result.shape == output_shape
