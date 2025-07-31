248218cf34700ea7903f5b51dff0ffaf806ae9a5
wangxiaoxin-sherie <wangxiaoxin7@huawei.com>
2025-07-31 09:34:30 +0800

    add attntp optimze
    Signed-off-by: wangxiaoxin-sherie <wangxiaoxin7@huawei.com>

--- tests/e2e/multicard/test_data_parallel.py
+++ tests/e2e/multicard/test_data_parallel.py
@@ -27,6 +27,7 @@ from unittest.mock import patch
 
 import pytest
 
+DENSE_MODELS = ["Qwen/Qwen2.5-0.5B-Instruct"]
 MODELS = ["Qwen/Qwen2.5-0.5B-Instruct", "Qwen/Qwen3-30B-A3B"]
 
 
@@ -56,6 +57,48 @@ def test_data_parallel_inference(model, max_tokens):
     ]
     if model == "Qwen/Qwen3-30B-A3B":
         cmd.append("--enable-expert-parallel")
+
+    print(f"Running subprocess: {' '.join(cmd)}")
+    proc = subprocess.run(cmd,
+                          env=env,
+                          stdout=subprocess.PIPE,
+                          stderr=subprocess.STDOUT,
+                          timeout=600)
+    output = proc.stdout.decode()
+
+    print(output)
+
+    assert "DP rank 0 needs to process" in output
+    assert "DP rank 1 needs to process" in output
+    assert "Generated text:" in output
+    assert proc.returncode == 0
+
+
+@pytest.mark.parametrize("model", DENSE_MODELS)
+@pytest.mark.parametrize("max_tokens", [32])
+@patch.dict(os.environ, {"ASCEND_RT_VISIBLE_DEVICES": "0,1,2,3"})
+@patch.dict(os.environ, {"VLLM_ASCEND_ENABLE_MLP_OPTIMIZE": "1"})
+def test_mlp_optimize(model, max_tokens):
+    script = "examples/offline_data_parallel.py"
+
+    env = os.environ.copy()
+
+    cmd = [
+        sys.executable,
+        script,
+        "--model",
+        model,
+        "--dp-size",
+        "2",
+        "--tp-size",
+        "2",
+        "--node-size",
+        "1",
+        "--node-rank",
+        "0",
+        "--trust-remote-code",
+        "--enforce-eager",
+    ]
 
     print(f"Running subprocess: {' '.join(cmd)}")
     proc = subprocess.run(cmd,
