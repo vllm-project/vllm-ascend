e60a221d9d1f9ae69679ebc0419ffcf498684b35
ganyi <pleaplusone.gy@gmail.com>
2025-07-31 11:29:57 +0800

    fix format issue
    Signed-off-by: ganyi <pleaplusone.gy@gmail.com>

--- vllm_ascend/patch/platform/patch_0_9_1/patch_core_client.py
+++ vllm_ascend/patch/platform/patch_0_9_1/patch_core_client.py
@@ -1,8 +1,12 @@
 from typing import Optional
-from vllm.v1.engine.core_client import EngineCoreClient, MPClient, AsyncMPClient
+
 from vllm.config import VllmConfig
+from vllm.v1.engine.core_client import (AsyncMPClient, EngineCoreClient,
+                                        MPClient)
 from vllm.v1.executor.abstract import Executor
+
 import vllm_ascend.envs as vllm_ascend_envs
+
 
 def make_async_mp_client(
     vllm_config: VllmConfig,
@@ -13,9 +17,10 @@ def make_async_mp_client(
 ) -> "MPClient":
     # Use only AsyncMPClient here for dp scenario and use nginx for the dp request routering
     return AsyncMPClient(vllm_config, executor_class, log_stats,
-                          client_addresses, client_index)
+                         client_addresses, client_index)
+
 
 # Apply this patch only if the external data parallelism is enabled
 if vllm_ascend_envs.VLLM_ASCEND_EXTERNAL_DP_LB_ENABLED:
-  # Patch the EngineCoreClient to use the custom make_async_mp_client
-  EngineCoreClient.make_async_mp_client = make_async_mp_client  # type: ignore[attr-defined]
+    # Patch the EngineCoreClient to use the custom make_async_mp_client
+    EngineCoreClient.make_async_mp_client = make_async_mp_client  # type: ignore[attr-defined]
