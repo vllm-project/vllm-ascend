e438c614c52f9f4489972319c8da63193ed82989
wangxiaoxin-sherie <wangxiaoxin7@huawei.com>
2025-08-01 10:29:14 +0800

    xx

--- vllm_ascend/ascend_forward_context.py
+++ vllm_ascend/ascend_forward_context.py
@@ -71,6 +71,7 @@ def set_ascend_forward_context(
         forward_context.fused_moe_state = fused_moe_state
 
         forward_context.in_profile_run = in_profile_run
+        forward_context.num_tokens_across_dp = num_tokens_across_dp
 
         # NOTE: This cannot be set using set_forward_context
         # due to multiple warmups before actual capturing
