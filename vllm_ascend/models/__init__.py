a228863beab6e2de1591f39b58f85236e4710424
wangxiaoxin-sherie <wangxiaoxin7@huawei.com>
2025-08-01 10:06:33 +0800

    xx

--- vllm_ascend/models/__init__.py
+++ vllm_ascend/models/__init__.py
@@ -12,7 +12,7 @@ def register_model():
         AscendQwen2_5_VLForConditionalGeneration  # noqa: F401
     from .qwen2_vl import AscendQwen2VLForConditionalGeneration  # noqa: F401
     from .qwen3 import CustomQwen3ForCausalLM  # noqa: F401
-    from .qwen2 import CustomQwen3ForCausalLM  # noqa: F401
+    from .qwen2 import CustomQwen2ForCausalLM  # noqa: F401
 
     ModelRegistry.register_model(
         "DeepSeekMTPModel",
