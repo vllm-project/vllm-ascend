b1659457e261014a357cc57d62ed05341eb5a47b
Wang Kunpeng <1289706727@qq.com>
2025-08-04 22:07:57 +0800

    [main][Prefill Perf] add doc
    Signed-off-by: Wang Kunpeng <1289706727@qq.com>

--- vllm_ascend/ascend_config.py
+++ vllm_ascend/ascend_config.py
@@ -48,7 +48,7 @@ class AscendConfig:
         self.chunked_prefill_for_mla = additional_config.get(
             "chunked_prefill_for_mla", False)
         self.enable_shared_expert_dp = additional_config.get(
-            "enable_shared_expert_dp", False)
+            "enable_shared_expert_dp", True)
 
 
 class TorchairGraphConfig:
