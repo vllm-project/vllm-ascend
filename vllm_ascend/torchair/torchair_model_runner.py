3136f4e9ddbfaed3884b42bda71004cccc0f1ce8
wangxiyuan <wangxiyuan1007@gmail.com>
2025-08-05 18:59:55 +0800

    [5/N][Refactor] torchair model runner refactor
    Signed-off-by: wangxiyuan <wangxiyuan1007@gmail.com>

--- vllm_ascend/torchair/torchair_model_runner.py
+++ vllm_ascend/torchair/torchair_model_runner.py
@@ -23,8 +23,12 @@ import torch
 import torch_npu
 from vllm.config import VllmConfig
 from vllm.forward_context import get_forward_context
+from vllm.logger import logger
 
 import vllm_ascend.envs as envs_ascend
+from vllm_ascend.platform import NPUPlatform
+from vllm_ascend.torchair.utils import (check_torchair_cache_exist,
+                                        write_kv_cache_bytes_to_file)
 from vllm_ascend.utils import (ACL_FORMAT_FRACTAL_ND, ACL_FORMAT_FRACTAL_NZ,
                                maybe_converting_weight_acl_format)
 from vllm_ascend.worker.model_runner_v1 import NPUModelRunner
@@ -38,6 +42,7 @@ class NPUTorchairModelRunner(NPUModelRunner):
     def _get_forward_metadata_across_dp_and_pad(
             self, num_tokens: int, with_prefill: bool, enable_dbo: bool
     ) -> tuple[int, Optional[torch.Tensor], bool, bool]:
+        """Override from NPUModelRunner to pad num_tokens"""
         if self.dp_size == 1:
             return num_tokens, None, with_prefill, enable_dbo
 
@@ -73,7 +78,7 @@ class NPUTorchairModelRunner(NPUModelRunner):
         return maybe_padded_num_tokens, num_tokens_across_dp, with_prefill, enable_dbo
 
     def _build_attention_metadata(self, with_prefill, num_tokens, skip_attn):
-        """Build attention metadata for dummy run."""
+        """Override from NPUModelRunner to build attention metadata."""
         if not with_prefill:
             attn_metadata = self.attn_metadata_builder.build_torchair_graph_dummy(
                 num_reqs=num_tokens, num_actual_tokens=1)
@@ -86,7 +91,7 @@ class NPUTorchairModelRunner(NPUModelRunner):
                                           is_torchair_compile, input_ids,
                                           positions, attn_metadata, num_tokens,
                                           intermediate_tensors, inputs_embeds):
-        """Generate hidden states for dummy run."""
+        """Override from NPUModelRunner to use compiled_model"""
         model_kwargs = {}
         if with_prefill:
             # Only mark static while compiling
@@ -126,5 +131,52 @@ class NPUTorchairModelRunner(NPUModelRunner):
         return hidden_states
 
     def _convert_torch_foramt(self, kv_cache):
+        """Override from NPUModelRunner to use torchair format."""
         kv_cache = torch_npu.npu_format_cast(kv_cache, ACL_FORMAT_FRACTAL_ND)
         return kv_cache
+
+    def _compile_torchair_graph(self, torchair_graph_batch_sizes) -> None:
+        # Trigger torchair graph capture for specific shapes.
+        # Capture the large shapes first so that the smaller shapes
+        # can reuse the memory pool allocated for the large shapes.
+        for idx, num_tokens in enumerate(reversed(torchair_graph_batch_sizes)):
+            for _ in range(self.vllm_config.compilation_config.
+                           cudagraph_num_of_warmups):
+                self._dummy_run(num_tokens, is_torchair_compile=True)
+            self._dummy_run(num_tokens, is_torchair_compile=True)
+            logger.info("Batchsize %d is compiled successfully: %d/%d.",
+                        num_tokens, idx + 1, len(torchair_graph_batch_sizes))
+
+    def _capture_model(self):
+        """Override from NPUModelRunner to use torchair graph capture."""
+        # TODO(NeverRaR): Calling graph_capture(device=self.device) in
+        # torchair graph capture can cause some issues, so now we just
+        # temporarily split the codepath for the two different graph patterns.
+        torchair_graph_batch_sizes = self.torchair_graph_batch_sizes
+        graph_num = len(torchair_graph_batch_sizes)
+
+        if self.use_cached_npu_graph and not check_torchair_cache_exist():
+            # If caching is enabled but does not exist, we will compile the model twice. The first
+            # time is used to generate the cache, and the second time is used to load the cache to
+            # skip the overhead caused by Dynamo guard mechanism.
+            logger.info(
+                "Use cached npu graph but cache doesn't exist! Now we compile graph to genetate torchair cache, this usually takes %.1f~%.1f mins.",
+                0.5 * graph_num, 1.5 * graph_num)
+            self._compile_torchair_graph(torchair_graph_batch_sizes)
+            NPUPlatform.synchronize()
+            torch._dynamo.reset()
+            self.torchair_compiled_models.clear()
+        if self.use_cached_npu_graph:
+            logger.info(
+                "Loading torchair graph cache, this usually takes %.1f~%.1f mins.",
+                0.3 * graph_num, 0.5 * graph_num)
+            self._compile_torchair_graph(torchair_graph_batch_sizes)
+        else:
+            logger.info(
+                "Capturing torchair graph, this usually takes %.1f~%.1f mins.",
+                0.5 * graph_num, 1.5 * graph_num)
+            self._compile_torchair_graph(torchair_graph_batch_sizes)
+
+        if self.new_kv_cache_bytes > 0:
+            write_kv_cache_bytes_to_file(torch.distributed.get_rank(),
+                                         self.new_kv_cache_bytes)
