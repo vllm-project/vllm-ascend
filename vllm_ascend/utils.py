dd4dc4dd4c755a1b27f06a4e51451c61c6226f2e
Icey <1790571317@qq.com>
2025-08-14 12:16:39 +0000

    [CustomOp] Register FusedMoe
    Signed-off-by: Icey <1790571317@qq.com>

--- vllm_ascend/utils.py
+++ vllm_ascend/utils.py
@@ -499,6 +499,8 @@ def register_ascend_customop():
 
     from vllm_ascend.ops.layernorm import AscendRMSNorm
     CustomOp.register_oot(_decorated_op_cls=AscendRMSNorm, name="RMSNorm")
+    from vllm_ascend.ops.fused_moe import AscendFusedMoE
+    CustomOp.register_oot(_decorated_op_cls=AscendFusedMoE, name="FusedMoE")
 
     # NOTE: Keep this at last to ensure all custom actions are registered
     _ASCEND_CUSTOMOP_IS_REIGISTERED = True
