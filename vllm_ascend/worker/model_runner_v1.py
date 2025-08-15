3c8d8388ef7d414bd5d69daaef99aa11e51d882a
CaveNightingale <cavenightingale@foxmail.com>
2025-08-15 11:32:33 +0800

    fix D hanging after ~17 requests
    Signed-off-by: CaveNightingale <cavenightingale@foxmail.com>

--- vllm_ascend/worker/model_runner_v1.py
+++ vllm_ascend/worker/model_runner_v1.py
@@ -1636,7 +1636,7 @@ class NPUModelRunner(LoRAModelRunnerMixin):
              finished_recving) = (self._process_reqs(scheduler_output,
                                                      intermediate_tensors))
         kv_connector_output = None
-        if finished_sending is not None and finished_recving is not None:
+        if finished_sending is not None or finished_recving is not None:
             kv_connector_output = KVConnectorOutput(
                 finished_sending=finished_sending,
                 finished_recving=finished_recving)
