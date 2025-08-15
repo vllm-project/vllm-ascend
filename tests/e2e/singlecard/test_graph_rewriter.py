3214dd726715931fdda166c98748f44bc7d630f5
ganyi <pleaplusone.gy@gmail.com>
2025-08-15 12:14:14 +0800

    add comments, license, and remove some non necessary code
    Signed-off-by: ganyi <pleaplusone.gy@gmail.com>

--- tests/e2e/singlecard/test_graph_rewriter.py
+++ tests/e2e/singlecard/test_graph_rewriter.py
@@ -1,3 +1,21 @@
+#
+# Copyright (c) 2025 Huawei Technologies Co., Ltd. All Rights Reserved.
+# This file is a part of the vllm-ascend project.
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 import torch
 import torch.nn as nn
 import torch_npu
@@ -24,9 +42,6 @@ class ModelWithRMSNormQuant(nn.Module):
         hidden_states = self.former_linear(x)
         x, residual = torch_npu.npu_add_rms_norm(hidden_states, x, self.weight, self.eps)
         quantized_output = quant_per_tensor(x, self.quant_scale, self.quant_offset)
-        # output = torch_npu.npu_quant_matmul(
-        #     quantized_output, self.post_linear.weight.transpose(1, 0), self.post_linear.bias,
-        #     self.deq_scale, None, torch.int8)
         return quantized_output, residual
 
 
