1bfa56008b40834b3d135b911e328cd3e0153f24
wangxiaoteng666 <wangxiaoteng@huawei.com>
2025-08-18 16:30:54 +0800

    bugfix proxy
    Signed-off-by: wangxiaoteng666 <wangxiaoteng@huawei.com>

--- examples/disaggregate_prefill_v1/load_balance_proxy_server_example.py
+++ examples/disaggregate_prefill_v1/load_balance_proxy_server_example.py
@@ -4,6 +4,7 @@
 
 import argparse
 import asyncio
+import functools
 import heapq
 import os
 import sys
@@ -231,6 +232,29 @@ async def lifespan(app: FastAPI):
         await d.client.aclose()
 
 
+async def listen_for_disconnect(request: Request) -> None:
+    """Return if a disconnect message is received"""
+    while True:
+        message = await request.receive()
+        if message["type"] == "http.disconnect":
+            break
+
+def with_cancellation(handler_func):
+    @functools.wraps(handler_func)
+    async def wrapper(*args, **kwargs):
+        request = kwargs["request"]
+        handler_task = asyncio.create_task(handler_func(*args, **kwargs))
+        cancellation_task = asyncio.create_task(listen_for_disconnect(request))
+        done, pending = await asyncio.wait([handler_task, cancellation_task],
+                                           return_when=asyncio.FIRST_COMPLETED)
+        for task in pending:
+            task.cancel()
+        if handler_task in done:
+            return handler_task.result
+        return None
+    return wrapper
+
+
 app = FastAPI(lifespan=lifespan)
 
 
@@ -411,11 +435,13 @@ async def _handle_completions(api: str, request: Request):
 
 
 @app.post("/v1/completions")
+@with_cancellation
 async def handle_completions(request: Request):
     return await _handle_completions("/completions", request)
 
 
 @app.post("/v1/chat/completions")
+@with_cancellation
 async def handle_chat_completions(request: Request):
     return await _handle_completions("/chat/completions", request)
 
