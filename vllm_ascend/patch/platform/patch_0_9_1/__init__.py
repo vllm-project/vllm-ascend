22a735d81e12e96257b5d59c715c7e4d578703fd
wangxiaoteng666 <wangxiaoteng@huawei.com>
2025-08-20 17:23:04 +0800

    bugfix_proxy
    Signed-off-by: wangxiaoteng666 <wangxiaoteng@huawei.com>

--- vllm_ascend/patch/platform/patch_0_9_1/__init__.py
+++ vllm_ascend/patch/platform/patch_0_9_1/__init__.py
@@ -23,3 +23,4 @@ import vllm_ascend.patch.platform.patch_0_9_1.patch_configs  # noqa
 import vllm_ascend.patch.platform.patch_0_9_1.patch_core  # noqa
 import vllm_ascend.patch.platform.patch_0_9_1.patch_core_client  # noqa
 import vllm_ascend.patch.platform.patch_0_9_1.patch_decorator  # noqa
+import vllm_ascend.patch.platform.patch_0_9_1.patch_scheduler  # noqa
