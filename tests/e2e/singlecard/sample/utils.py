adfd48d03923d10cb5b8385a059f9678268eb338
wangli <wangli858794774@gmail.com>
2025-08-27 11:30:31 +0800

    add header
    Signed-off-by: wangli <wangli858794774@gmail.com>

--- tests/e2e/singlecard/sample/utils.py
+++ tests/e2e/singlecard/sample/utils.py
@@ -1,12 +1,26 @@
-# SPDX-License-Identifier: Apache-2.0
-# SPDX-FileCopyrightText: Copyright contributors to the vLLM project
-
+#
+# Copyright (c) 2025 Huawei Technologies Co., Ltd. All Rights Reserved.
+# Copyright 2023 The vLLM team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# This file is a part of the vllm-ascend project.
+# Adapted from vllm/tests/basic_correctness/test_basic_correctness.py
+#
 import types
 from enum import Enum, auto
 from typing import Optional
 
 import torch
-
 from vllm.config import VllmConfig
 from vllm.sampling_params import SamplingParams
 from vllm.v1.sample.logits_processor import (LOGITSPROCS_GROUP, BatchUpdate,
