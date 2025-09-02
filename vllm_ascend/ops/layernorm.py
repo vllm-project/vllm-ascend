7eb392ecd0714a5e2f7600f948149725886ed005
MengqingCao <cmq0113@163.com>
2025-09-02 06:33:04 +0000

    [Perf][CustomOP] Recover performance through override init
    Signed-off-by: MengqingCao <cmq0113@163.com>

--- vllm_ascend/ops/layernorm.py
+++ vllm_ascend/ops/layernorm.py
@@ -60,6 +60,16 @@ class AddRMSNormW8A8Quant(RMSNorm):
 
 class AscendRMSNorm(RMSNorm):
 
+    def __init__(
+        self,
+        hidden_size: int,
+        eps: float = 1e-6,
+        var_hidden_size: Optional[int] = None,
+        has_weight: bool = True,
+        dtype: Optional[torch.dtype] = None,
+    ) -> None:
+        super().__init__(hidden_size, eps, var_hidden_size, has_weight, dtype)
+
     def forward_oot(
         self,
         x: torch.Tensor,
