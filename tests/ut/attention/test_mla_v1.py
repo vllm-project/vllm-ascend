91c3d2a4a38c2911614915df15d3ba09e76feb01
lidenghui <lidenghui1110@gmail.com>
2025-09-02 17:39:43 +0800

    fix ut
    Signed-off-by: lidenghui <lidenghui1110@gmail.com>

--- tests/ut/attention/test_mla_v1.py
+++ tests/ut/attention/test_mla_v1.py
@@ -521,8 +521,12 @@ class TestAscendMLAImpl(TestBase):
         ]
         self.impl.num_kv_heads = self.impl.num_heads
 
-        decode_res, prefill_res = self.impl._mla_preprocess("mock-layer",
-            hidden_states, kv_cache, attn_metadata, need_gather_q_kv=False)
+        decode_res, prefill_res = self.impl._mla_preprocess(
+            "mock-layer",
+            hidden_states,
+            kv_cache,
+            attn_metadata,
+            need_gather_q_kv=False)
 
         self.assertIsNotNone(decode_res)
         self.assertIsNotNone(prefill_res)
