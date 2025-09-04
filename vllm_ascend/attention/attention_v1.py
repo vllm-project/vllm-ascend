0878bc4c51c073c3bf7ab0c4cb63b3ce0009b2d7
nsdie <yeyifan@huawei.com>
2025-09-04 20:02:29 +0800

    fix
    Signed-off-by: nsdie <yeyifan@huawei.com>

--- vllm_ascend/attention/attention_v1.py
+++ vllm_ascend/attention/attention_v1.py
@@ -265,20 +265,6 @@ class AscendAttentionBackendImpl(AttentionImpl):
         self.key_cache = None
         self.value_cache = None
 
-    def _repeat_kv(self, hidden_states: torch.Tensor,
-                   n_rep: int) -> torch.Tensor:
-        """
-        This is the equivalent of torch.repeat_interleave(x, dim=1, repeats=n_rep). The hidden states go from (batch,
-        num_key_value_heads, seqlen, head_dim) to (batch, num_attention_heads, seqlen, head_dim)
-        """
-        slen, num_key_value_heads, head_dim = hidden_states.shape
-        if n_rep == 1:
-            return hidden_states
-        hidden_states = hidden_states[:, None, :, :].expand(
-            slen, n_rep, num_key_value_heads, head_dim)
-        return hidden_states.reshape(slen, num_key_value_heads * n_rep,
-                                     head_dim)
-
     def _forward_prefill_no_cache(
         self,
         query: torch.Tensor,
