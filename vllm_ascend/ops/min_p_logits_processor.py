6dbc1728b422550bd9b81f8aa469fc3daa47e5cf
CaranLic <740821011@qq.com>
2025-09-05 14:50:35 +0800

    change decode bs
    Signed-off-by: CaranLic <740821011@qq.com>

--- vllm_ascend/ops/min_p_logits_processor.py
+++ vllm_ascend/ops/min_p_logits_processor.py
@@ -1,20 +1,20 @@
 import torch
 from vllm.config import get_current_vllm_config
-from vllm.v1.sample.logits_processor impot MinPLogitsProcessor
+from vllm.v1.sample.logits_processor import MinPLogitsProcessor
 
 original_min_p_logits_processor_init_func = MinPLogitsProcessor.__init__
 
 def min_p_logits_processor_init_func(self, *args, **kwargs):
     original_min_p_logits_processor_init_func(self, *args, **kwargs)
 
-    vllml_config = get_current_vllm_config()
-    decode_max_num_seqs = getattr(vllml_config.scheduler_config,
+    vllm_config = get_current_vllm_config()
+    decode_max_num_seqs = getattr(vllm_config.scheduler_config,
                                   'decode_max_num_seqs', 0)
     # reinit MinPLogitsProcessor if decode_max_num_seqs configured
     if decode_max_num_seqs != 0:
         device = args[1]
         is_pin_memory = args[2]
-        max_num_reqs = decode_max_num_seqs
+        max_num_reqs = max(vllm_config.scheduler_config.max_num_seqs, decode_max_num_seqs)
 
         self.min_p_cpu_tensor = torch.zeros((max_num_reqs, ),
                                             dtype=torch.float32,
