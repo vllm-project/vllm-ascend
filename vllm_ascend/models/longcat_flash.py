d198afb330e0f0e2fe65980c060bfacefe4b7569
zhanghaiwen <zhanghaiwen@cmss.chinamobile.com>
2025-09-05 17:13:11 +0800

    add load_weights method
    Signed-off-by: zhanghaiwen <zhanghaiwen@cmss.chinamobile.com>

--- vllm_ascend/models/longcat_flash.py
+++ vllm_ascend/models/longcat_flash.py
@@ -279,3 +279,131 @@ class CustomLongcatFlashForCausalLM(LongcatFlashForCausalLM):
         self.logits_processor = LogitsProcessor(config.vocab_size)
         self.make_empty_intermediate_tensors = (
             self.model.make_empty_intermediate_tensors)
+
+    def load_weights(self, weights: Iterable[tuple[str,
+                                                   torch.Tensor]]) -> set[str]:
+
+        stacked_params_mapping = [
+            #("fused_qkv_a_proj", "q_a_proj", 0),
+            #("fused_qkv_a_proj", "kv_a_proj_with_mqa", 1),
+            (".gate_up_proj", ".gate_proj", 0),
+            (".gate_up_proj", ".up_proj", 1),
+        ]
+
+        expert_params_mapping = self.get_expert_mapping()
+        loaded_params: set[str] = set()
+
+        params_dict = dict(self.named_parameters())
+        for name, loaded_weight in weights:
+            if "rotary_emb.inv_freq" in name:
+                continue
+            for param_name, weight_name, shard_id in stacked_params_mapping:
+                if weight_name not in name:
+                    continue
+                if "mlp" in name and "mlps" not in name:
+                    continue
+                name = name.replace(weight_name, param_name)
+                # Skip loading extra bias for GPTQ models.
+                if (name.endswith(".bias")
+                        or name.endswith("_bias")) and name not in params_dict:
+                    continue
+                # Skip mtp
+                if ".mtp." in name:
+                    continue
+                if is_pp_missing_parameter(name, self):
+                    continue
+                param = params_dict[name]
+                weight_loader = param.weight_loader
+                weight_loader(param, loaded_weight, shard_id)
+                break
+            else:
+                is_expert_weight = False
+                for mapping in expert_params_mapping:
+                    param_name, weight_name, expert_id, shard_id = mapping
+                    if weight_name not in name:
+                        continue
+                    is_expert_weight = True
+                    name_mapped = name.replace(weight_name, param_name)
+                    # Skip mtp
+                    if ".mtp." in name_mapped:
+                        continue
+                    if (name_mapped.endswith(".bias")
+                            or name_mapped.endswith("_bias")
+                        ) and name not in params_dict:
+                        continue
+                    if is_pp_missing_parameter(name, self):
+                        continue
+                    param = params_dict[name_mapped]
+                    weight_loader = param.weight_loader
+                    weight_loader = typing.cast(Callable[..., bool],
+                                                param.weight_loader)
+                    success = weight_loader(param,
+                                            loaded_weight,
+                                            name_mapped,
+                                            shard_id=shard_id,
+                                            expert_id=expert_id,
+                                            return_success=True)
+                    if success:
+                        name = name_mapped
+                        break
+                else:
+                    if is_expert_weight:
+                        # We've checked that this is an expert weight
+                        # However it's not mapped locally to this rank
+                        # So we simply skip it
+                        continue
+                    # Skip loading extra bias for GPTQ models.
+                    if name.endswith(".bias") and name not in params_dict:
+                        continue
+                    # Skip loading kv_scale from ckpts towards new design.
+                    if name.endswith(".kv_scale") and name not in params_dict:
+                        continue
+                    # Skip mtp
+                    if ".mtp." in name:
+                        continue
+                    if name is None:
+                        continue
+                    if is_pp_missing_parameter(name, self):
+                        continue
+                    param = params_dict[name]
+                    weight_loader = getattr(param, "weight_loader",
+                                            default_weight_loader)
+                    weight_loader(param, loaded_weight)
+            loaded_params.add(name)
+        for layer_id in range(self.config.num_hidden_layers):
+            for i in range(2):
+                if isinstance(self.model.layers[layer_id], PPMissingLayer):
+                    continue
+                self_attn = self.model.layers[layer_id].self_attn[i]
+                if hasattr(self.quant_config, "weight_block_size"
+                           ) and self_attn.kv_b_proj.weight.dtype in (
+                               torch.float8_e4m3fn,
+                               torch.float8_e4m3fnuz,
+                           ):
+                    weight_block_size = self.quant_config.weight_block_size
+                    if weight_block_size is not None:
+                        assert hasattr(self_attn.kv_b_proj, "weight_scale_inv")
+                        dtype = torch.get_default_dtype()
+                        w = block_dequant(self_attn.kv_b_proj.weight,
+                                          self_attn.kv_b_proj.weight_scale_inv,
+                                          weight_block_size).to(dtype)
+                else:
+                    w = self_attn.kv_b_proj.weight
+
+                w_kc, w_vc = w.unflatten(
+                    0,
+                    (-1,
+                     self_attn.qk_nope_head_dim + self_attn.v_head_dim)).split(
+                         [self_attn.qk_nope_head_dim, self_attn.v_head_dim],
+                         dim=1)
+                self_attn.w_kc = w_kc.transpose(1, 2).contiguous().transpose(
+                    1, 2)
+                self_attn.w_vc = w_vc.contiguous().transpose(1, 2)
+                if self.config.mla_scale_q_lora:
+                    self_attn.q_a_layernorm.weight.data *= (
+                        self.config.hidden_size / self.config.q_lora_rank)**0.5
+                if self.config.mla_scale_kv_lora:
+                    self_attn.kv_a_layernorm.weight.data *= (
+                        self.config.hidden_size /
+                        self.config.kv_lora_rank)**0.5
+        return loaded_params
