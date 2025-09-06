48711af95e795b4db52ffd797a1c5a6e807779bc
Pr0Wh1teGivee <calvin_zhu0210@outlook.com>
2025-09-06 12:44:59 +0800

    Update moe_comm_method.py

--- vllm_ascend/ops/moe/moe_comm_method.py
+++ vllm_ascend/ops/moe/moe_comm_method.py
@@ -73,6 +73,7 @@ class MoECommMethod(ABC):
         # For load balance
         log2phy: torch.Tensor = None,
         global_redundant_expert_num: int = 0,
+        need_trans: bool = False
     ) -> torch.Tensor:
         # Check constraints
         assert hidden_states.shape[1] == w1.shape[1], (
@@ -119,7 +120,7 @@ class MoECommMethod(ABC):
                                        w2_scale_bias=w2_scale_bias,
                                        with_quant=use_int8_w8a8
                                        or use_int4_w4a8,
-                                       need_trans=False)
+                                       need_trans=need_trans)
 
         hidden_states[:] = self.token_dispatcher.token_combine(
             hidden_states=mlp_output)
