913b609c4ed1d43f809fabb6b4fdaeb20c86735a
jesse <szxfml@gmail.com>
2025-09-08 09:00:26 +0800

    [BugFix] Async scheduling and PP compatibility with DP
    Signed-off-by: jesse <szxfml@gmail.com>

--- vllm_ascend/worker/worker_v1.py
+++ vllm_ascend/worker/worker_v1.py
@@ -197,34 +197,33 @@ class NPUWorker(WorkerBase):
         scheduler_output: "SchedulerOutput",
     ) -> Optional[ModelRunnerOutput]:
         intermediate_tensors = None
-        if not get_pp_group().is_first_rank:
+        forward_pass = scheduler_output.total_num_scheduled_tokens > 0
+        if forward_pass and not get_pp_group().is_first_rank:
             intermediate_tensors = IntermediateTensors(
                 get_pp_group().recv_tensor_dict(
                     all_gather_group=get_tp_group()))
 
         output = self.model_runner.execute_model(scheduler_output,
                                                  intermediate_tensors)
+        if isinstance(output, ModelRunnerOutput):
+            return output
+        assert isinstance(output, IntermediateTensors)
         parallel_config = self.vllm_config.parallel_config
-        if parallel_config.distributed_executor_backend != "external_launcher" \
-            and not get_pp_group().is_last_rank:
-            assert isinstance(output, IntermediateTensors)
-            get_pp_group().send_tensor_dict(output.tensors,
-                                            all_gather_group=get_tp_group())
-            if not has_kv_transfer_group():
-                return None
+        assert parallel_config.distributed_executor_backend != (
+            "external_launcher") and not get_pp_group().is_last_rank
 
-            kv_connector_output = output.kv_connector_output
-            finished_sending = kv_connector_output.finished_sending
-            finished_recving = kv_connector_output.finished_recving
+        get_pp_group().send_tensor_dict(output.tensors,
+                                        all_gather_group=get_tp_group())
 
-            if not finished_sending and not finished_recving:
-                return EMPTY_MODEL_RUNNER_OUTPUT
+        kv_connector_output = output.kv_connector_output
+        if not kv_connector_output:
+            return None
 
-            new_output = copy.copy(EMPTY_MODEL_RUNNER_OUTPUT)
-            new_output.kv_connector_output = kv_connector_output
-            return new_output
-
-        assert isinstance(output, ModelRunnerOutput)
+        # In case of PP with kv transfer, we need to pass through the
+        # kv_connector_output
+        if (not kv_connector_output.finished_sending
+                and not kv_connector_output.finished_recving):
+            return EMPTY_MODEL_RUNNER_OUTPUT
         return output
 
     def load_model(self) -> None:
