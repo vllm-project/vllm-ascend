f2a4a0fcfb94ee95d39bd56108ee6b56e920d941
Icey <1790571317@qq.com>
2025-09-12 07:37:11 +0000

    resolve conflicts
    Signed-off-by: Icey <1790571317@qq.com>

--- vllm_ascend/utils.py
+++ vllm_ascend/utils.py
@@ -496,7 +496,8 @@ def register_ascend_customop(vllm_config: Optional[VllmConfig] = None):
 
     from vllm_ascend.models.layers.mla import AscendMultiHeadLatentAttention
     from vllm_ascend.ops.activation import AscendQuickGELU, AscendSiluAndMul
-    from vllm_ascend.ops.common_fused_moe import AscendFusedMoE
+    from vllm_ascend.ops.common_fused_moe import (
+        AscendFusedMoE, AscendUnquantizedFusedMoEMethod)
     from vllm_ascend.ops.layernorm import AscendQuantRMSNorm, AscendRMSNorm
     from vllm_ascend.ops.linear import (AscendColumnParallelLinear,
                                         AscendMergedColumnParallelLinear,
@@ -524,6 +525,7 @@ def register_ascend_customop(vllm_config: Optional[VllmConfig] = None):
         "RMSNorm": AscendRMSNorm,
         "FusedMoE": AscendFusedMoE,
         "MultiHeadLatentAttention": AscendMultiHeadLatentAttention,
+        "UnquantizedFusedMoEMethod": AscendUnquantizedFusedMoEMethod,
     }
 
     if vllm_config is not None and \
