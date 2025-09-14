1b103cb31706f523de171c4c60e46d569372c842
wangxiyuan <wangxiyuan1007@gmail.com>
2025-09-14 18:27:11 +0800

    fix lint
    Signed-off-by: wangxiyuan <wangxiyuan1007@gmail.com>

--- vllm_ascend/models/qwen3_next.py
+++ vllm_ascend/models/qwen3_next.py
@@ -1,7 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 # mypy: ignore-errors
-
 """Inference-only Qwen3Next model."""
 from collections.abc import Iterable
 from typing import Optional
