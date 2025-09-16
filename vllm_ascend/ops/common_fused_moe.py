134f4ec8599671fce9f4428c2bf46ab18698b1a2
offline0806 <z00858301@china.huawei.com>
2025-09-16 15:17:48 +0800

    [EPLB]Fix ci.
    Signed-off-by: offline0806 <z00858301@china.huawei.com>

--- vllm_ascend/ops/common_fused_moe.py
+++ vllm_ascend/ops/common_fused_moe.py
@@ -245,7 +245,6 @@ class AscendFusedMoE(FusedMoE):
         if self.dynamic_eplb:
             self.expert_map_path = ascend_config.expert_map_path
             self.global_redundant_expert_num = ascend_config.init_redundancy_expert
-            self.global_num_experts = self.global_num_experts + self.global_redundant_expert_num
             if self.expert_map_path and os.path.exists(self.expert_map_path) and os.access(self.expert_map_path, os.R_OK):
                 self.expert_load_balancer = ExpertLoadBalancer(self.expert_map_path, self.global_num_experts)
                 self.local_num_experts, self.expert_map = (self.expert_load_balancer.get_rank_placement_map(self.moe_instance_id, self.ep_rank))
