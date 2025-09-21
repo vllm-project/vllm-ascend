5fc59c02f9896198d9ff6e19ad0d337b3f4d1be0
booker123456 <945658361@qq.com>
2025-09-21 14:17:57 +0800

    [Patch]Ascend affinity multimodal embeddings merge
    Signed-off-by: booker123456 <945658361@qq.com>

--- vllm_ascend/patch/platform/patch_common/patch_multimodal_merge.py
+++ vllm_ascend/patch/platform/patch_common/patch_multimodal_merge.py
@@ -14,7 +14,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# Adapted from vllm/model_executor/models/qwen2_vl.py
 # This file is a part of the vllm-ascend project.
 
 import torch
