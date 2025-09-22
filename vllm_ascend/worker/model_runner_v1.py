33b059eaac88aafae72911f4366a224c3b0b27a9
fems14 <1804143737@qq.com>
2025-09-22 14:49:09 +0800

    mix bugfix
    Signed-off-by: fems14 <1804143737@qq.com>

--- vllm_ascend/worker/model_runner_v1.py
+++ vllm_ascend/worker/model_runner_v1.py
@@ -2289,7 +2289,7 @@ class NPUModelRunner(LoRAModelRunnerMixin):
                                         dtype=np.int32)
 
         # Force dummy run on prefill stage when this node is deemed as kv producer.
-        if self.is_kv_producer:
+        if self.is_kv_producer and not self.is_kv_consumer:
             with_prefill = True
 
         attn_metadata = self._build_attention_metadata(with_prefill,
