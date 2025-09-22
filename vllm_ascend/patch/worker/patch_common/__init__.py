d160509fae7d82d81fc64a6c89a57449727811f8
Icey <1790571317@qq.com>
2025-09-22 12:20:57 +0000

    change ops patch to patch_common dir
    Signed-off-by: Icey <1790571317@qq.com>

--- vllm_ascend/patch/worker/patch_common/__init__.py
+++ vllm_ascend/patch/worker/patch_common/__init__.py
@@ -18,8 +18,7 @@
 from vllm.triton_utils import HAS_TRITON
 
 if HAS_TRITON:
-    import vllm_ascend.ops.casual_conv1d
-    import vllm_ascend.ops.sigmoid_gating
+    import vllm_ascend.patch.worker.patch_common.patch_triton
 
 import vllm_ascend.patch.worker.patch_common.patch_distributed  # noqa
 import vllm_ascend.patch.worker.patch_common.patch_logits  # noqa
