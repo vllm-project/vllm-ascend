280579759b391458bbea26def92927f9fdd79490
Icey <1790571317@qq.com>
2025-09-22 07:00:44 +0000

    patch fused_recurrent_gated_delta_rule_fwd_kernel
    Signed-off-by: Icey <1790571317@qq.com>

--- vllm_ascend/patch/worker/patch_common/__init__.py
+++ vllm_ascend/patch/worker/patch_common/__init__.py
@@ -17,6 +17,6 @@
 
 import vllm_ascend.patch.worker.patch_common.patch_distributed  # noqa
 import vllm_ascend.patch.worker.patch_common.patch_logits  # noqa
-
+import vllm_ascend.ops.sigmoid_gating
 # TODO: revert me when triton import is fixed
 # import vllm_ascend.patch.worker.patch_common.patch_minicpm  # noqa
