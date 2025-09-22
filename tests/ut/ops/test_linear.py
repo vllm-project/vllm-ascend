a7bc7a95bc2e9a64be8539bb75bd69dbcd7e2203
funanyang <985619145@qq.com>
2025-09-22 19:45:17 +0800

    fix ut Signed-off-by: funanyang <985619145@qq.com> Co-authored-by: funanyang <funanyang@huawei.com>

--- tests/ut/ops/test_linear.py
+++ tests/ut/ops/test_linear.py
@@ -94,8 +94,7 @@ class TestAscendRowParallelLinear(BaseLinearTest):
                         output_size=8,
                         prefix="o_proj",
                     )
-                    self.assertEqual(linear.comm_group, parallel_state._OTP)
-                    self.assertEqual(linear.forward_type, "oproj_tp")
+                    self.assertEqual(linear.custom_op.comm_group, parallel_state._OTP)
                     output = linear(input_tensor)
                     outputs.append(output[0])
                     self.assertEqual(output[0].shape[0],
