a9b7a53cdc0f1c43742b0a03c48cb91d9db184eb
Pr0Wh1teGivee <calvin_zhu0210@outlook.com>
2025-09-24 14:49:07 +0800

    fix fused_moe memory leak

--- vllm_ascend/ops/moe/fused_moe_prepare_and_finalize.py
+++ vllm_ascend/ops/moe/fused_moe_prepare_and_finalize.py
@@ -162,7 +162,6 @@ class FusedMoEPrepareAndFinalizeWithMC2(FusedMoEPrepareAndFinalize):
                                                          dim=0)
                 hidden_states = split_hidden_states[self.tp_rank]
                 router_logits = split_router_logits[self.tp_rank]
-                self.split_hidden_states = split_hidden_states  # Save for finalize
 
         return hidden_states, router_logits, mc2_mask
 
@@ -179,9 +178,7 @@ class FusedMoEPrepareAndFinalizeWithMC2(FusedMoEPrepareAndFinalize):
         if not (self.enable_shared_expert_dp or self.replace_allreduce):
             if self.tp_size > 1:
                 # All-gather across TP group
-                dist.all_gather(list(self.split_hidden_states), hidden_states,
-                                self.moe_config.tp_group.device_group)
-                hidden_states = torch.cat(self.split_hidden_states, dim=0)
+                hidden_states = self.moe_config.tp_group.all_gather(hidden_states, 0)
 
             # Unpad if necessary
             if self.num_tokens < hidden_states.shape[0]:
@@ -244,7 +241,6 @@ class FusedMoEPrepareAndFinalizeWithAll2All(FusedMoEPrepareAndFinalize):
                 split_router_logits = torch.tensor_split(router_logits,
                                                          self.tp_size,
                                                          dim=0)
-                self.split_hidden_states = split_hidden_states
 
                 hidden_states = split_hidden_states[self.tp_rank]
                 router_logits = split_router_logits[self.tp_rank]
@@ -263,9 +259,7 @@ class FusedMoEPrepareAndFinalizeWithAll2All(FusedMoEPrepareAndFinalize):
         """
         if not (self.enable_shared_expert_dp or self.replace_allreduce):
             if self.tp_size > 1:
-                dist.all_gather(list(self.split_hidden_states), hidden_states,
-                                self.moe_config.tp_group.device_group)
-                hidden_states = torch.cat(self.split_hidden_states, dim=0)
+                hidden_states = self.moe_config.tp_group.all_gather(hidden_states, 0)
 
             if self.num_tokens < hidden_states.shape[0]:
                 hidden_states = hidden_states[:self.num_tokens]
