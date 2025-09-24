0a0a218d8db1e3748a2ee7d30a2fff557a23f7e0
socrahow <suzihao4@h-partners.com>
2025-09-24 17:18:17 +0800

    adjust code format
    Signed-off-by: socrahow <suzihao4@h-partners.com>

--- vllm_ascend/utils.py
+++ vllm_ascend/utils.py
@@ -500,7 +500,8 @@ def register_ascend_customop(vllm_config: Optional[VllmConfig] = None):
     from vllm_ascend.ops.activation import AscendQuickGELU, AscendSiluAndMul
     from vllm_ascend.ops.common_fused_moe import (AscendFusedMoE,
                                                   AscendSharedFusedMoE)
-    from vllm_ascend.ops.layernorm import AscendGemmaRMSNorm, AscendQuantRMSNorm, AscendRMSNorm
+    from vllm_ascend.ops.layernorm import (AscendGemmaRMSNorm,
+                                           AscendQuantRMSNorm, AscendRMSNorm)
     from vllm_ascend.ops.linear import (AscendColumnParallelLinear,
                                         AscendMergedColumnParallelLinear,
                                         AscendQKVParallelLinear,
