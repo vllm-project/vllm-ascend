97e8ab0b050204181b17ddcd6e6d3f6f46594ae9
weijinqian_v1 <weijinqian@huawei.com>
2025-09-28 19:05:57 +0800

    [bugfix] Fix warning bug: model config is None.
    Signed-off-by: weijinqian_v1 <weijinqian@huawei.com>

--- vllm_ascend/utils.py
+++ vllm_ascend/utils.py
@@ -591,14 +591,11 @@ def dense_optim_enable() -> bool:
 
 
 def enable_sp(vllm_config=None) -> bool:
-    from vllm.config import get_current_vllm_config
-    enable_sequence_parallelism = False
     if vllm_config is None:
+        from vllm.config import get_current_vllm_config
         vllm_config = get_current_vllm_config()
-        enable_sequence_parallelism = vllm_config.compilation_config.pass_config.enable_sequence_parallelism
-
     return (
-        enable_sequence_parallelism
+        vllm_config.compilation_config.pass_config.enable_sequence_parallelism
         or envs_ascend.VLLM_ASCEND_ENABLE_FLASHCOMM)
 
 
