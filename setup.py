9ca3f3b9e2319c3954b93da38f828e744adb321c
fems14 <1804143737@qq.com>
2025-10-09 15:54:32 +0800

    fix connector register failed.
    Signed-off-by: fems14 <1804143737@qq.com>

--- setup.py
+++ setup.py
@@ -392,6 +392,6 @@ setup(
     entry_points={
         "vllm.platform_plugins": ["ascend = vllm_ascend:register"],
         "vllm.general_plugins":
-        ["ascend_enhanced_model = vllm_ascend:register_model"],
+        ["ascend_enhanced_model = vllm_ascend:register_model", "ascend_enhanced_connector = vllm_ascend:register_connector"],
     },
 )
