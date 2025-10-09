dad1da3688a5606200362916f97c76ddcc364759
fems14 <1804143737@qq.com>
2025-10-09 16:15:45 +0800

    fix lint
    Signed-off-by: fems14 <1804143737@qq.com>

--- setup.py
+++ setup.py
@@ -392,6 +392,9 @@ setup(
     entry_points={
         "vllm.platform_plugins": ["ascend = vllm_ascend:register"],
         "vllm.general_plugins":
-        ["ascend_enhanced_model = vllm_ascend:register_model", "ascend_enhanced_connector = vllm_ascend:register_connector"],
+        "vllm.general_plugins": [
+            "ascend_enhanced_model = vllm_ascend:register_model",
+            "ascend_enhanced_connector = vllm_ascend:register_connector"
+        ],
     },
 )
