39611e9566aa3d45acb6af4c1f03b74faca32cb4
Che Ruan <cr623@ic.ac.uk>
2025-10-09 16:07:06 +0800

    bugfix
    Signed-off-by: Che Ruan <cr623@ic.ac.uk>

--- vllm_ascend/ascend_config.py
+++ vllm_ascend/ascend_config.py
@@ -47,6 +47,7 @@ class AscendConfig:
             ascend_scheduler_config)
         # Todo: Once https://github.com/vllm-project/vllm/issues/22246 is merged in vllm. Remove this config
         self.expert_map_path = additional_config.get("expert_map_path", None)
+        self.eplb_policy_type = additional_config.get("eplb_policy_type", 0)
         self.expert_map_record_path = additional_config.get(
             "expert_map_record_path",
             None)  # Provide path to export expert map
