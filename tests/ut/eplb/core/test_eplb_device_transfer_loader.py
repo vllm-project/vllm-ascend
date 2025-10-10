976ba590cb3c6866c43ce82c632a543681f221c3
offline0806 <3337230449@qq.com>
2025-10-10 19:00:30 +0800

    [[ut]fix ut.
    Signed-off-by: offline0806 <3337230449@qq.com>

--- tests/ut/eplb/core/test_eplb_device_transfer_loader.py
+++ tests/ut/eplb/core/test_eplb_device_transfer_loader.py
@@ -49,8 +49,6 @@ def test_generate_task_and_state_flow(mock_adaptor):
         loader_obj.generate_expert_d2d_transfer_task([], [], {}, 0)
         assert loader_obj.comm_op_list is None
         assert loader_obj.state == loader.ExpertWeightUpdateState.WAITING
-        assert loader_obj.comm_op_list
-        assert loader_obj.recv_expert_list
 
 
 def test_asyn_transfer_and_update(mock_adaptor):
