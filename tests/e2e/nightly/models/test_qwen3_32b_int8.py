9ef536be2e4b9ccfc1b1923e07bea7836157cad8
wangli <wangli858794774@gmail.com>
2025-10-11 16:10:03 +0800

    rebase
    Signed-off-by: wangli <wangli858794774@gmail.com>

--- tests/e2e/nightly/models/test_qwen3_32b_int8.py
+++ tests/e2e/nightly/models/test_qwen3_32b_int8.py
@@ -48,21 +48,20 @@ async def test_models(model: str, tp_size: int) -> None:
         "PAGED_ATTENTION_MASK_LEN": "5500"
     }
     server_args = [
-        "--port", "20002",
-        "--no-enable-prefix-caching",
-        "--tensor-parallel-size", str(tp_size),
-        "--max-model-len", "36864",
-        "--max-num-batched-tokens", "36864",
-        "--block-size", "128",
-        "--trust-remote-code",
-        "--quantization", "ascend",
-        "--gpu-memory-utilization", "0.9",
+        "--port", "20002", "--no-enable-prefix-caching",
+        "--tensor-parallel-size",
+        str(tp_size), "--max-model-len", "36864", "--max-num-batched-tokens",
+        "36864", "--block-size", "128", "--trust-remote-code",
+        "--quantization", "ascend", "--gpu-memory-utilization", "0.9",
         "--additional-config", '{"enable_weight_nz_layout":true}'
     ]
     request_keyword_args: dict[str, Any] = {
         **api_keyword_args,
     }
-    with RemoteOpenAIServer(model, server_args, env_dict=env_dict, auto_port=False) as server:
+    with RemoteOpenAIServer(model,
+                            server_args,
+                            env_dict=env_dict,
+                            auto_port=False) as server:
         client = server.get_async_client()
         batch = await client.completions.create(
             model=model,
@@ -71,4 +70,3 @@ async def test_models(model: str, tp_size: int) -> None:
         )
         choices: list[openai.types.CompletionChoice] = batch.choices
         assert choices[0].text, "empty response"
-
