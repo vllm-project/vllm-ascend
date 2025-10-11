5639acc4e844a6b85c5d063122045462f25da22a
realliujiaxu <realliujiaxu@163.com>
2025-10-11 19:29:53 +0800

    delete cu_tokens_across_dp_cpu in forward_context
    Signed-off-by: realliujiaxu <realliujiaxu@163.com>

--- vllm_ascend/ascend_forward_context.py
+++ vllm_ascend/ascend_forward_context.py
@@ -174,9 +174,6 @@ def set_ascend_forward_context(
 
         dp_world_size = get_dp_group().world_size
         if dp_world_size > 1 and forward_context.dp_metadata is not None:
-            # FIXME cu_tokens_across_dp_cpu and max_tokens_across_dp_cpu should be cpu tensor
-            forward_context.dp_metadata.cu_tokens_across_dp_cpu = \
-                forward_context.dp_metadata.cu_tokens_across_sp(1)
             max_tokens_across_dp = \
                 forward_context.dp_metadata.max_tokens_across_dp_cpu.item()
             if sp_enabled:
