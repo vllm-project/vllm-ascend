c0aa3eedc20373ef3f29ce97b28e53eab1512dfb
realliujiaxu <realliujiaxu@163.com>
2025-10-11 11:15:23 +0800

    add log
    Signed-off-by: realliujiaxu <realliujiaxu@163.com>

--- vllm_ascend/ops/common_fused_moe.py
+++ vllm_ascend/ops/common_fused_moe.py
@@ -37,7 +37,7 @@ from vllm_ascend.eplb.core.eplb_utils import (determine_default_expert_map,
 from vllm_ascend.ops.expert_load_balancer import ExpertLoadBalancer
 from vllm_ascend.ops.moe.experts_selector import select_experts
 from vllm_ascend.ops.moe.moe_comm_method import setup_moe_comm_method
-from vllm_ascend.utils import (ACL_FORMAT_FRACTAL_NZ, is_310p,
+from vllm_ascend.utils import (ACL_FORMAT_FRACTAL_NZ, enable_sp, is_310p,
                                npu_stream_switch, shared_expert_dp_enabled)
 
 
@@ -399,6 +399,10 @@ class AscendSharedFusedMoE(SharedFusedMoE, AscendFusedMoE):
         self.multistream_overlap_shared_expert = ascend_config.multistream_overlap_shared_expert
         if self.multistream_overlap_shared_expert:
             self.shared_expert_stream = torch.npu.Stream()
+        if enable_sp():
+            logger.info_once(
+                "Sequence parallelism is enabled, shared experts are replicated for best performance."
+            )
 
     def forward(
         self,
