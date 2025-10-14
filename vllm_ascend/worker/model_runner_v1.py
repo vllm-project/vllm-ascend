8d9fc07f7721e3a8ca5a16075b576b23a079cae7
MengqingCao <cmq0113@163.com>
2025-10-14 03:43:52 +0000

    tiny fix
    Signed-off-by: MengqingCao <cmq0113@163.com>

--- vllm_ascend/worker/model_runner_v1.py
+++ vllm_ascend/worker/model_runner_v1.py
@@ -3225,9 +3225,7 @@ class NPUModelRunner(LoRAModelRunnerMixin):
                         block_size=block_size,
                         num_kv_heads=attn_module.num_kv_heads,
                         head_size=attn_module.head_size,
-                        dtype=self.kv_cache_dtype,
-                        use_mla=use_mla,
-                        use_sfa=use_sfa)
+                        dtype=self.kv_cache_dtype)
             elif attn_module.attn_type in (AttentionType.ENCODER,
                                            AttentionType.ENCODER_ONLY):
                 # encoder-only attention does not need KV cache.
