3b8b8ca50581c3327a7250cc2170c0f60b326313
Icey <1790571317@qq.com>
2025-10-16 04:42:24 +0000

    tiny fix
    Signed-off-by: Icey <1790571317@qq.com>

--- vllm_ascend/ops/common_fused_moe.py
+++ vllm_ascend/ops/common_fused_moe.py
@@ -43,7 +43,8 @@ from vllm_ascend.utils import (ACL_FORMAT_FRACTAL_NZ, enable_sp, is_310p,
 
 if vllm_version_is("0.11.0"):
     from vllm.config import CompilationLevel
-    from vllm.model_executor.layers.shared_fused_moe import SharedFusedMoE
+    from vllm.model_executor.layers.shared_fused_moe import \
+        SharedFusedMoE  # type: ignore
 else:
     from vllm.config import CompilationMode
     from vllm.model_executor.layers.fused_moe.shared_fused_moe import \
