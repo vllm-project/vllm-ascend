fb3c9da104d733a7425bbc995407ab2a3dddf43a
wangli <wangli858794774@gmail.com>
2025-10-16 16:52:28 +0800

    add doc
    Signed-off-by: wangli <wangli858794774@gmail.com>

--- tests/e2e/nightly/multi_node/test_multi_node.py
+++ tests/e2e/nightly/multi_node/test_multi_node.py
@@ -1,6 +1,6 @@
 from tests.e2e.conftest import RemoteOpenAIServer
-from tests.e2e.nightly.multi_node.config.multi_node_config import \
-    MultiNodeConfig
+from tests.e2e.nightly.multi_node.config.multi_node_config import (
+    DISAGGREGATED_PREFILL_PROXY_SCRIPT, MultiNodeConfig)
 
 
 def test_multi_node() -> None:
@@ -8,22 +8,21 @@ def test_multi_node() -> None:
     env_dict = config.envs
     perf_cmd = config.perf_cmd
     acc_cmd = config.acc_cmd
-    if config.disaggregated_prefill:
-        config.launch_server_proxy()
 
-    with RemoteOpenAIServer(
-            model=config.model,
-            vllm_serve_args=config.server_cmd,
-            server_port=config.server_port,
-            env_dict=env_dict,
-            auto_port=False,
-            max_wait_seconds=1000,
-    ) as remote_server:
-        base_url = remote_server.url_root
-        if config.is_master:
-            pass
-            # subprocess.run(perf_cmd, check=True)
-            # subprocess.run(acc_cmd, check=True)
-        else:
-            remote_server.hang_until_terminated()
-            # TODO: enable perf and acc test
+    with config.launch_server_proxy(DISAGGREGATED_PREFILL_PROXY_SCRIPT):
+        with RemoteOpenAIServer(
+                model=config.model,
+                vllm_serve_args=config.server_cmd,
+                server_port=config.server_port,
+                env_dict=env_dict,
+                auto_port=False,
+                max_wait_seconds=1000,
+        ) as remote_server:
+            base_url = remote_server.url_root
+            if config.is_master:
+                pass
+                # TODO: enable perf and acc test
+                # subprocess.run(perf_cmd, check=True)
+                # subprocess.run(acc_cmd, check=True)
+            else:
+                remote_server.hang_until_terminated()
