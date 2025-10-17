a3c46ae0a42b54b7449c3ab3e98d6ff0d91bb396
offline0806 <3337230449@qq.com>
2025-10-17 11:25:30 +0800

    [log]add log when patch v1.
    Signed-off-by: offline0806 <3337230449@qq.com>

--- vllm_ascend/utils.py
+++ vllm_ascend/utils.py
@@ -259,10 +259,13 @@ def prefetch_stream() -> torch.npu.Stream:
 
 
 def adapt_patch(is_global_patch: bool = False):
-    ascend_config = get_ascend_config()
-    dynamic_eplb = ascend_config.dynamic_eplb or ascend_config.expert_map_record_path
-    if dynamic_eplb:
-        from vllm_ascend.patch import v1  # noqa: F401
+    try:
+        ascend_config = get_ascend_config()
+        dynamic_eplb = ascend_config.dynamic_eplb or ascend_config.expert_map_record_path
+        if dynamic_eplb:
+            from vllm_ascend.patch import v1  # noqa: F401
+    except RuntimeError as e:
+        logger.warning(f"Fail to patch v1 : {e}")
     if is_global_patch:
         from vllm_ascend.patch import platform  # noqa: F401
     else:
