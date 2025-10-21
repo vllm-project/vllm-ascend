2906b10875b104b50b8ddec7762d2bb744b182fd
shaopeng666 <shaopeng666@noreply.gitcode.com>
2025-10-21 20:56:45 +0800

    add NZ enbale judge for vl model weght load
    Signed-off-by: shaopeng666 <shaopeng666@noreply.gitcode.com>

--- vllm_ascend/models/qwen2_5_vl.py
+++ vllm_ascend/models/qwen2_5_vl.py
@@ -42,7 +42,7 @@ from vllm.model_executor.models.qwen2_5_vl import (
 from vllm.model_executor.models.utils import maybe_prefix
 from vllm.multimodal import MULTIMODAL_REGISTRY
 
-from vllm_ascend.utils import ACL_FORMAT_FRACTAL_ND
+from vllm_ascend.utils import (ACL_FORMAT_FRACTAL_ND, is_enable_nz)
 
 MIN_PAD_SIZE = 64  # min_size to pad weight
 MAX_PAD_SIZE = 128  # max_size to pad weight
@@ -284,11 +284,14 @@ class AscendQwen2_5_VisionTransformer(Qwen2_5_VisionTransformer):
             dim=2)
         qkv_weight_final = qkv_weight_padded.reshape(-1, self.hidden_size)
 
-        qkv_weight_final_copy = torch.empty_like(qkv_weight_final).copy_(
-            qkv_weight_final)
-        qkv_weight_final_copy = torch_npu.npu_format_cast(
-            qkv_weight_final_copy, ACL_FORMAT_FRACTAL_ND)
-        return qkv_weight_final_copy
+        if is_enable_nz():
+            qkv_weight_final_copy = torch.empty_like(qkv_weight_final).copy_(
+                qkv_weight_final)
+            qkv_weight_final_copy = torch_npu.npu_format_cast(
+                qkv_weight_final_copy, ACL_FORMAT_FRACTAL_ND)
+            return qkv_weight_final_copy
+        
+        return qkv_weight_final
 
     def pad_proj_weight(self, data):
         out_weight = torch.nn.functional.pad(
@@ -297,10 +300,13 @@ class AscendQwen2_5_VisionTransformer(Qwen2_5_VisionTransformer):
             (0, self.half_pad_hidden_size_per_attention_head, 0, 0)).reshape(
                 self.hidden_size, -1)
 
-        out_weight_copy = torch.empty_like(out_weight).copy_(out_weight)
-        out_weight_copy = torch_npu.npu_format_cast(out_weight_copy,
-                                                    ACL_FORMAT_FRACTAL_ND)
-        return out_weight_copy
+        if is_enable_nz():
+            out_weight_copy = torch.empty_like(out_weight).copy_(out_weight)
+            out_weight_copy = torch_npu.npu_format_cast(out_weight_copy,
+                                                        ACL_FORMAT_FRACTAL_ND)
+            return out_weight_copy
+        
+        return out_weight
 
     def pad_qkv_weight_scale_offset(self, data):
         reshaped_data = data.reshape(
