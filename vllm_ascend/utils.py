f0aa47ea75df9bcf4bd2a99ecb93b3e01ed86283
Zetong Li <slippersss@126.com>
2025-10-21 21:02:23 +0800

    [Bugfix][MTP] Fix performance degradation when mtp>1
    Signed-off-by: Zetong Li <slippersss@126.com>

--- vllm_ascend/utils.py
+++ vllm_ascend/utils.py
@@ -410,6 +410,21 @@ def update_aclgraph_sizes(vllm_config: VllmConfig) -> None:
             vllm_config.model_config.architectures[0], num_hidden_layers,
             len(original_sizes))
 
+    # default or defined cudagraph_capture_sizes may not consider num_speculative_tokens>1 scenario
+    # the maximum size cudagraph_capture_sizes[0] should be greater or equal than
+    # max_num_seqs*(num_speculative_tokens+1), otherwise draft model will run in eager mode
+    if vllm_config.speculative_config is not None:
+        original_sizes = compilation_config.cudagraph_capture_sizes
+        required_size = (vllm_config.scheduler_config.max_num_seqs *
+                         (vllm_config.speculative_config.num_speculative_tokens + 1))
+        assert len(original_sizes) > 0
+        if original_sizes[0] < required_size:
+            original_sizes[0] = required_size
+            compilation_config.init_with_cudagraph_sizes(original_sizes)
+            logger.info(
+                "Adjusted ACL graph maximum batch size to: %d for speculative decoding",
+                original_sizes[0])
+
 
 # TODO(wxy): Move to ops module
 def dispose_tensor(x: torch.Tensor):
