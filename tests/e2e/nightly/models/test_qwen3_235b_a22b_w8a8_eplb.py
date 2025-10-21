d0485436aca024c6fbfdd231684f830355bfdb22
offline0806 <3337230449@qq.com>
2025-10-21 14:04:06 +0800

    [CI]fix pre commit.
    Signed-off-by: offline0806 <3337230449@qq.com>

--- tests/e2e/nightly/models/test_qwen3_235b_a22b_w8a8_eplb.py
+++ tests/e2e/nightly/models/test_qwen3_235b_a22b_w8a8_eplb.py
@@ -73,12 +73,10 @@ async def test_models(model: str, tp_size: int) -> None:
         "DYNAMIC_EPLB": "true"
     }
     server_args = [
-        "--no-enable-prefix-caching",
-        "--enable-expert-parallel",
+        "--no-enable-prefix-caching", "--enable-expert-parallel",
         "--tensor-parallel-size",
-        str(tp_size), "--port", 
-        str(port), "--max-model-len", "36864",
-        "--max-num-batched-tokens",
+        str(tp_size), "--port",
+        str(port), "--max-model-len", "36864", "--max-num-batched-tokens",
         "36864", "--block-size", "128", "--trust-remote-code", "quantization",
         "ascend", "--gpu-memory-utilization", "0.9", "--additional-config",
         '{"enable_weight_nz_layout":true, "dynamic_eplb": true, '
