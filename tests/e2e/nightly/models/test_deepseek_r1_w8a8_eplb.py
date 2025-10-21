16581b37f64f2e578bb5b13ba925245fa0a72436
offline0806 <3337230449@qq.com>
2025-10-21 14:36:00 +0800

    [CI]fix pre commit.
    Signed-off-by: offline0806 <3337230449@qq.com>

--- tests/e2e/nightly/models/test_deepseek_r1_w8a8_eplb.py
+++ tests/e2e/nightly/models/test_deepseek_r1_w8a8_eplb.py
@@ -83,7 +83,7 @@ async def test_models(model: str, tp_size: int, dp_size: int) -> None:
         "36864", "--block-size", "128", "--trust-remote-code", "quantization",
         "ascend", "--gpu-memory-utilization", "0.9", "--additional-config",
         '{"enable_weight_nz_layout":true, '
-        '"torch_air_graph_config":{"enabled": true, "enable_multistream_mla": true, "graph_batch_size": [16], "use_cached_graph": ture},'
+        '"torch_air_graph_config":{"enabled": true, "enable_multistream_mla": true, "graph_batch_size": [16], "use_cached_graph": true},'
         '"dynamic_eplb": true, "num_iterations_eplb_update": 200, "num_wait_worker_iterations": 100, "init_redundancy_expert": 16}'
     ]
     request_keyword_args: dict[str, Any] = {
