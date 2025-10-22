ebcd99952bdd70690c8f33c8255acdd0c683e672
whx-sjtu <2952154980@qq.com>
2025-10-22 14:45:25 +0800

    patch to fix mtp quant bug
    Signed-off-by: whx-sjtu <2952154980@qq.com>

--- vllm_ascend/patch/platform/__init__.py
+++ vllm_ascend/patch/platform/__init__.py
@@ -17,6 +17,7 @@
 import os
 
 import vllm_ascend.patch.platform.patch_config  # noqa
+import vllm_ascend.patch.platform.patch_deepseek_mtp  # noqa
 import vllm_ascend.patch.platform.patch_distributed  # noqa
 import vllm_ascend.patch.platform.patch_mamba_config  # noqa
 
