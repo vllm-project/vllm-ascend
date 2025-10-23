7ea5f3e0df84fcf8099911a42ed5c189585d6b60
fems14 <1804143737@qq.com>
2025-10-23 20:22:58 +0800

    fix lint
    Signed-off-by: fems14 <1804143737@qq.com>

--- vllm_ascend/distributed/mooncake/mooncake_engine.py
+++ vllm_ascend/distributed/mooncake/mooncake_engine.py
@@ -479,7 +479,8 @@ class MooncakeEngine:
                         tokens):
                     keys.append(key.to_string())
                     starts.append(start)
-                res = self.m_store.batch_exists(keys)  # type: ignore[assignment]
+                res = self.m_store.batch_exists(
+                    keys)  # type: ignore[assignment]
                 for index, value in enumerate(res):  # type: ignore[arg-type]
                     if value != 1:
                         return starts[index]
@@ -528,12 +529,15 @@ class MooncakeEngine:
                 multi_tp_keys = keys[:]
                 for i in range(1, self.tp_size):
                     for item in keys:
-                        new_str = item.replace("@0", f"@{i}", 1)  # type: ignore[attr-type]
+                        new_str = item.replace("@0", f"@{i}",
+                                                1)  # type: ignore[attr-type]
                         multi_tp_keys.append(new_str)
-                res = self.m_store.batch_exists(multi_tp_keys)  # type: ignore[assignment]
+                res = self.m_store.batch_exists(
+                    multi_tp_keys)  # type: ignore[assignment]
                 num_block = len(keys)
                 multi_tp_values = [
-                    res[i * num_block:(i + 1) * num_block]  # type: ignore[index]
+                    res[i * num_block:(i + 1) *
+                        num_block]  # type: ignore[index]
                     for i in range(self.tp_size)
                 ]
                 index = self.find_min_first_non_one_index(multi_tp_values)
