ee5b910f4b011cc50805ca071cf6e066337bd0e9
fems14 <1804143737@qq.com>
2025-10-23 20:17:09 +0800

    add judge machine
    Signed-off-by: fems14 <1804143737@qq.com>

--- vllm_ascend/patch/platform/patch_sched_yield.py
+++ vllm_ascend/patch/platform/patch_sched_yield.py
@@ -1,3 +1,12 @@
+import sys
+from vllm.platforms import Platform, CpuArchEnum
 import vllm.distributed.utils
 
-vllm.distributed.utils.USE_SCHED_YIELD = False
+is_arm = (Platform.get_cpu_architecture() == CpuArchEnum.ARM)
+
+USE_SCHED_YIELD = (((sys.version_info[:3] >= (3, 11, 1))
+                   or (sys.version_info[:2] == (3, 10)
+                       and sys.version_info[2] >= 8))
+                       and not is_arm)
+
+vllm.distributed.utils.USE_SCHED_YIELD = USE_SCHED_YIELD
