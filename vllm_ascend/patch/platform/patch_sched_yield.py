983ff9e32ca193a3cc4d76a7d540d143f5de63e2
fems14 <1804143737@qq.com>
2025-10-23 20:39:14 +0800

    fix lint
    Signed-off-by: fems14 <1804143737@qq.com>

--- vllm_ascend/patch/platform/patch_sched_yield.py
+++ vllm_ascend/patch/platform/patch_sched_yield.py
@@ -1,12 +1,12 @@
 import sys
-from vllm.platforms import Platform, CpuArchEnum
 import vllm.distributed.utils
+from vllm.platforms import Platform, CpuArchEnum
 
 is_arm = (Platform.get_cpu_architecture() == CpuArchEnum.ARM)
 
-USE_SCHED_YIELD = (((sys.version_info[:3] >= (3, 11, 1))
-                   or (sys.version_info[:2] == (3, 10)
-                       and sys.version_info[2] >= 8))
-                       and not is_arm)
+USE_SCHED_YIELD = (
+    ((sys.version_info[:3] >= (3, 11, 1)) or
+     (sys.version_info[:2] == (3, 10) and sys.version_info[2] >= 8))
+    and not is_arm)
 
 vllm.distributed.utils.USE_SCHED_YIELD = USE_SCHED_YIELD
