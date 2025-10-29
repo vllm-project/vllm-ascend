92dd765af728ee02744bd991d9532d2796c0c5de
baxingpiaochong <771405853@qq.com>
2025-10-29 14:17:25 +0800

    fix
    Signed-off-by: baxingpiaochong <771405853@qq.com>

--- vllm_ascend/distributed/mooncake_connector.py
+++ vllm_ascend/distributed/mooncake_connector.py
@@ -665,7 +665,6 @@ class MooncakeConnector(KVConnectorBase_V1):
         assert self.connector_scheduler is not None
         return self.connector_scheduler.request_finished(request, block_ids)
 
-
     ############################################################
     # Worker Side Methods
     ############################################################
