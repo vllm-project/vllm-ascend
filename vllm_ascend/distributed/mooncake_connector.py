bcb21bb326fc436195170c68543b26b93a1ff7a1
chenxiao <Jaychou1620@Gmail.com>
2025-11-03 18:53:53 +0800

    bugfix: decode pp size assert error
    Signed-off-by: chenxiao <Jaychou1620@Gmail.com>

--- vllm_ascend/distributed/mooncake_connector.py
+++ vllm_ascend/distributed/mooncake_connector.py
@@ -1105,7 +1105,7 @@ class MooncakeConnectorWorker:
         self._decode_dp_size = decode_parallel_config["dp_size"]
          # get prefill pp size from extra config
         self._decode_pp_size = prefill_parallel_config.get("pp_size", 1)
-        assert self._decode_pp_size == 1, "decode pp size must be 1"
+        assert self._decode_pp_size != 1, "decode pp size must be 1"
 
     def register_kv_caches(self, kv_caches: dict[str, torch.Tensor]):
         """Register the KV Cache data."""
