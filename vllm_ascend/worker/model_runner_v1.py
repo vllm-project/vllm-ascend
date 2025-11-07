f031015739c8777ba610408439df6fbab77cdab5
zss <3265779424@qq.com>
2025-11-07 15:32:08 +0800

    suport AscendScheduer and fix A2 MC2 and pp bug
    Signed-off-by: chenxiao <Jaychou1620@Gmail.com>

--- vllm_ascend/worker/model_runner_v1.py
+++ vllm_ascend/worker/model_runner_v1.py
@@ -2386,7 +2386,7 @@ class NPUModelRunner(LoRAModelRunnerMixin):
             moe_comm_type = MoECommType.ALLGATHER
         elif soc_version in {AscendSocVersion.A2}:
             if (num_tokens <= self.mc2_tokens_capacity
-                    and self.parallel_config.world_size_across_dp >= 16):
+                    and self.parallel_config.world_size_across_dp/self.parallel_config.pipeline_parallel_size >= 16):
                 moe_comm_type = MoECommType.MC2
             else:
                 # Currently, w4a8_dynamic does not support allgatherep
