7cf62b7907b101fcffed6fa3de1b75a344911865
wxsIcey <1790571317@qq.com>
2025-11-10 03:28:40 +0000

    fix
    Signed-off-by: wxsIcey <1790571317@qq.com>

--- vllm_ascend/models/qwen3_next.py
+++ vllm_ascend/models/qwen3_next.py
@@ -297,8 +297,7 @@ class CustomQwen3NextGatedDeltaNet(Qwen3NextGatedDeltaNet, MambaBase):
             mixed_qkv_non_spec = causal_conv1d_update(
                 mixed_qkv_non_spec,
                 conv_state,
-                # qwen3-next enable nz has accuracy problems, so disable it here
-                torch_npu.npu_format_cast(conv_weights, 2),
+                conv_weights,
                 self.conv1d.bias,
                 self.activation,
                 conv_state_indices=non_spec_state_indices_tensor[:attn_metadata
