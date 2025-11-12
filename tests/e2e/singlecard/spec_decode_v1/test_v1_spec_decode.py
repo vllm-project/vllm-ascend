6957ce506a3b82818b0e170c391b64a09b230edc
Cyclone-07 <hongchfan@sjtu.edu.cn>
2025-11-12 14:40:26 +0800

    fix:add requirement and suffix test
    Signed-off-by: Cyclone-07 <hongchfan@sjtu.edu.cn>

--- tests/e2e/singlecard/spec_decode_v1/test_v1_spec_decode.py
+++ tests/e2e/singlecard/spec_decode_v1/test_v1_spec_decode.py
@@ -98,7 +98,40 @@ def test_ngram_correctness(
     # Upon failure, inspect the outputs to check for inaccuracy.
     assert matches > int(0.7 * len(ref_outputs))
 
+def test_suffix_correctness(
+    test_prompts: list[list[dict[str, Any]]],
+    sampling_config: SamplingParams,
+    model_name: str,
+):
+    '''
+    Compare the outputs of a original LLM and a speculative LLM
+    should be the same when using suffix speculative decoding.
+    '''
+    pytest.skip("Not current support for the test.")
+    ref_llm = LLM(model=model_name, max_model_len=1024, enforce_eager=False)
+    ref_outputs = ref_llm.chat(test_prompts, sampling_config)
+    del ref_llm
+    with VllmRunner(model_name,
+                    speculative_config={
+                        "method": "suffix",
+                        "num_speculative_tokens": 4,
+                    },
+                    max_model_len=1024,
+                    enforce_eager=False) as runner:
+        spec_outputs = runner.model.chat(test_prompts, sampling_config)
+    matches = 0
+    misses = 0
+    for ref_output, spec_output in zip(ref_outputs, spec_outputs):
+        if ref_output.outputs[0].text == spec_output.outputs[0].text:
+            matches += 1
+        else:
+            misses += 1
+            print(f"ref_output: {ref_output.outputs[0].text}")
+            print(f"spec_output: {spec_output.outputs[0].text}")
 
+    # Heuristic: expect at least 70% of the prompts to match exactly
+    # Upon failure, inspect the outputs to check for inaccuracy.
+    assert matches > int(0.7 * len(ref_outputs))
 @pytest.mark.parametrize("use_eagle3", [False, True], ids=["eagle", "eagle3"])
 def test_eagle_correctness(
     test_prompts: list[list[dict[str, Any]]],
