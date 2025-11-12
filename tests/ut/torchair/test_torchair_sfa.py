86de6c64058b3f84e50ce3f0ae4f248a0fab71d3
CodeNine-CJ <chenjian343@huawei.com>
2025-11-12 17:06:56 +0800

    ut test
    Signed-off-by: CodeNine-CJ <chenjian343@huawei.com>

--- tests/ut/torchair/test_torchair_sfa.py
+++ tests/ut/torchair/test_torchair_sfa.py
@@ -1,8 +1,6 @@
 from unittest.mock import MagicMock, patch
 
 import torch
-from vllm.distributed.parallel_state import GroupCoordinator
-from vllm.model_executor.layers.linear import LinearBase
 
 from tests.ut.base import TestBase
 from vllm_ascend.attention.attention_v1 import AscendAttentionState
