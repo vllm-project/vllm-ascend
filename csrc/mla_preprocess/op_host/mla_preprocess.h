a74cbe1cb91ea9e08b3f17782050fda3fcb20533
chenjunyi <isjunyi.chen@gmail.com>
2025-11-13 19:44:02 +0800

    support cachemode int8_nzcache with bf16 in mlapo
    Signed-off-by: chenjunyi <isjunyi.chen@gmail.com>

--- csrc/mla_preprocess/op_host/mla_preprocess.h
+++ csrc/mla_preprocess/op_host/mla_preprocess.h
@@ -62,6 +62,7 @@ constexpr uint32_t INDEX_WUQ = 18;
 constexpr uint32_t INDEX_WUK = 20;
 
 constexpr uint32_t MAX_SUPPORT_TOKEN_NUMS = 1024;
+constexpr uint32_t MAX_CACHE_MODE_NUMS = 3;
 
 inline uint32_t CeilDiv(const uint32_t dividend, const uint32_t divisor)
 {
@@ -677,18 +678,19 @@ std::tuple<at::Tensor, at::Tensor, uint32_t> mla_preprocess_tiling(
     // tiling
     int32_t bIndex = N - 1;
     uint32_t tilingSize = sizeof(MlaTilingData);
+    int32_t tilingOffset = tilingSize * MAX_SUPPORT_TOKEN_NUMS * (opParam.cacheMode - 1) + (tilingSize * bIndex);
     static auto global_tiling_data =
-        at::empty({tilingSize * MAX_SUPPORT_TOKEN_NUMS},
+        at::empty({tilingSize * MAX_SUPPORT_TOKEN_NUMS * MAX_CACHE_MODE_NUMS},
                   at::TensorOptions().dtype(at::kByte).device(hiddenState.options().device()));
     if (bIndex >= 0 && bIndex < MAX_SUPPORT_TOKEN_NUMS) {
-        aclrtMemcpy(global_tiling_data.data_ptr<uint8_t>() + (tilingSize * bIndex), tilingSize, &tilingData, tilingSize,
+        aclrtMemcpy(global_tiling_data.data_ptr<uint8_t>() + tilingOffset, tilingSize, &tilingData, tilingSize,
                     ACL_MEMCPY_HOST_TO_DEVICE);
     } else {
         // Handle the case where bIndex is out of range
         TORCH_CHECK(false, "bIndex is out of range: ", bIndex);
     }
     at::Tensor tiling = at::from_blob(
-        global_tiling_data.data_ptr<uint8_t>() + (tilingSize * bIndex),
+        global_tiling_data.data_ptr<uint8_t>() + tilingOffset,
         tilingSize,
         at::kByte);
 
