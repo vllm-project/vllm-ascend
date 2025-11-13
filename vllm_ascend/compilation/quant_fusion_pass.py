d15fed56eabb0a056e35d0a0c5c7f838d5723297
wxsIcey <1790571317@qq.com>
2025-11-13 07:05:28 +0000

    fix
    Signed-off-by: wxsIcey <1790571317@qq.com>

--- vllm_ascend/compilation/quant_fusion_pass.py
+++ vllm_ascend/compilation/quant_fusion_pass.py
@@ -16,16 +16,16 @@
 # limitations under the License.
 #
 import torch
-from vllm.compilation.vllm_inductor_pass import VllmInductorPass
-from torch._inductor.pattern_matcher import PatternMatcherPass
 import torch._inductor.pattern_matcher as pm
+from torch._inductor.pattern_matcher import PatternMatcherPass
+from vllm.compilation.vllm_inductor_pass import VllmInductorPass
 
 
 class AddRMSNormQuantPattern:
 
     def __init__(self, vllm_config):
         self.vllm_config = vllm_config
-        
+
     def get_inputs(self):
         """
         Generate example inputs for the AddRMSNormQuant fusion pattern.
@@ -38,7 +38,7 @@ class AddRMSNormQuantPattern:
         return [rms_norm_input, residual, rms_norm_weight, scale, offset]
 
     def register(self, pm_pass: PatternMatcherPass):
-        
+
         def pattern(rms_norm_input, residual, rms_norm_weight, scale, offset):
             """
           Pattern for AddRMSNormQuant fusion.
@@ -51,7 +51,8 @@ class AddRMSNormQuantPattern:
                 out0, scale, offset, torch.qint8, -1, False)
             return quantized_output, out1
 
-        def replacement(rms_norm_input, residual, rms_norm_weight, scale, offset):
+        def replacement(rms_norm_input, residual, rms_norm_weight, scale,
+                        offset):
             """
           Replacement for the AddRMSNormQuant fusion.
           """
@@ -66,8 +67,9 @@ class AddRMSNormQuantPattern:
             quantized_output = output[0]
             out1 = output[2]
             return quantized_output, out1
-        
-        pm.register_replacement(pattern, replacement, self.get_inputs(), pm.fwd_only, pm_pass)
+
+        pm.register_replacement(pattern, replacement, self.get_inputs(),
+                                pm.fwd_only, pm_pass)
 
 
 class AscendQuantFusionPass(VllmInductorPass):
@@ -78,9 +80,7 @@ class AscendQuantFusionPass(VllmInductorPass):
     def __init__(self, vllm_config):
         super().__init__(vllm_config)
         self.patterns: PatternMatcherPass = PatternMatcherPass(
-            pass_name="rmsnorm_quant_fusion_pass"
-        )
-        # Register the AddRMSNormQuant fusion pattern into the graph rewriter pattern list
+            pass_name="rmsnorm_quant_fusion_pass")
         AddRMSNormQuantPattern(vllm_config).register(self.patterns)
 
     def __call__(self, graph: torch.fx.Graph):
