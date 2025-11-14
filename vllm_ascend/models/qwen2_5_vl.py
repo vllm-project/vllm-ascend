f85fba238c68fcc745e858e4879080ae6aa6105c
shen-shanshan <467638484@qq.com>
2025-11-14 06:48:47 +0000

    replace VisionPatchEmbed for better performance
    Signed-off-by: shen-shanshan <467638484@qq.com>

--- vllm_ascend/models/qwen2_5_vl.py
+++ vllm_ascend/models/qwen2_5_vl.py
@@ -47,7 +47,7 @@ from vllm_ascend.utils import (ACL_FORMAT_FRACTAL_ND, is_enable_nz,
                                vllm_version_is)
 
 if not vllm_version_is("0.11.0"):
-    from vllm.model_executor.models.vision import conv3d_to_linear_weight
+    pass
 
 MIN_PAD_SIZE = 64  # min_size to pad weight
 MAX_PAD_SIZE = 128  # max_size to pad weight
@@ -164,14 +164,6 @@ class AscendQwen2_5_VisionBlock(Qwen2_5_VisionBlock):
         return x
 
 
-class AscendQwen2_5_VisionPatchEmbed(Qwen2_5_VisionPatchEmbed):
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = x.matmul(
-            self.proj.weight.data.view(self.hidden_size, -1).transpose(0, 1))
-        return x
-
-
 class AscendQwen2_5_VisionRotaryEmbedding(Qwen2_5_VisionRotaryEmbedding):
 
     def __init__(self, dim: int, theta: float = 10000.0) -> None:
@@ -198,7 +190,7 @@ class AscendQwen2_5_VisionTransformer(Qwen2_5_VisionTransformer):
         head_dim = self.hidden_size // self.num_heads
         self.rotary_pos_emb = AscendQwen2_5_VisionRotaryEmbedding(head_dim //
                                                                   2)
-        self.patch_embed = AscendQwen2_5_VisionPatchEmbed(
+        self.patch_embed = Qwen2_5_VisionPatchEmbed(
             patch_size=vision_config.patch_size,
             temporal_patch_size=vision_config.temporal_patch_size,
             in_channels=vision_config.in_channels,
@@ -360,9 +352,6 @@ class AscendQwen2_5_VisionTransformer(Qwen2_5_VisionTransformer):
         params_dict = dict(self.named_parameters(remove_duplicate=False))
         loaded_params: Set[str] = set()
         for name, loaded_weight in weights:
-            if not vllm_version_is("0.11.0"):
-                if name.endswith("patch_embed.proj.weight"):
-                    loaded_weight = conv3d_to_linear_weight(loaded_weight)
             for (param_name, weight_name, shard_id) in stacked_params_mapping:
                 if weight_name not in name:
                     continue
