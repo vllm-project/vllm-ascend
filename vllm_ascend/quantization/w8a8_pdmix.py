06eb388d0cc08782b15d45127fc8b7d0a6569578
SlightwindSec <slightwindsec@gmail.com>
2025-11-17 22:03:53 +0800

    fix lint
    Signed-off-by: SlightwindSec <slightwindsec@gmail.com>

--- vllm_ascend/quantization/w8a8_pdmix.py
+++ vllm_ascend/quantization/w8a8_pdmix.py
@@ -1,20 +1,23 @@
-from typing import Dict, Any
+from typing import Any, Dict
+
 import torch
 import torch_npu
 
+from vllm_ascend.ascend_config import PDScenarioState, get_ascend_config
 from vllm_ascend.utils import ACL_FORMAT_FRACTAL_NZ, is_enable_nz
-from vllm_ascend.ascend_config import get_ascend_config, PDScenarioState
 
-from .w8a8_dynamic import AscendW8A8DynamicLinearMethod, AscendW8A8DynamicFusedMoEMethod
 from .w8a8 import AscendW8A8LinearMethod
+from .w8a8_dynamic import (AscendW8A8DynamicFusedMoEMethod,
+                           AscendW8A8DynamicLinearMethod)
 
 
 class AscendW8A8PDMixLinearMethod(AscendW8A8DynamicLinearMethod):
+
     def __init__(self):
         super().__init__()
 
     @staticmethod
-    def apply(layer, x, bias = None, tp_rank = 0):
+    def apply(layer, x, bias=None, tp_rank=0):
         if get_ascend_config().pd_scenario_state == PDScenarioState.DNode:
             return AscendW8A8LinearMethod.apply(layer, x, bias, tp_rank)
         else:
@@ -29,7 +32,8 @@ class AscendW8A8PDMixLinearMethod(AscendW8A8DynamicLinearMethod):
         output_size: int,
         params_dtype: torch.dtype,
     ) -> Dict[str, Any]:
-        return AscendW8A8LinearMethod.get_perchannel_param(output_size, params_dtype)
+        return AscendW8A8LinearMethod.get_perchannel_param(
+            output_size, params_dtype)
 
     def process_weights_after_loading(self, layer):
         expanding_factor = layer.weight.data.shape[1]
@@ -53,6 +57,7 @@ class AscendW8A8PDMixLinearMethod(AscendW8A8DynamicLinearMethod):
 
 
 class AscendW8A8PDMixFusedMoeMethod(AscendW8A8DynamicFusedMoEMethod):
+
     def __init__(self):
         super().__init__()
 
