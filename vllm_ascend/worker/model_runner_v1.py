c38c6bb7041f8e76451cc2c4e61a27f8dc3c2b41
Jade Zheng <zheng.shoujian@outlook.com>
2025-11-18 18:17:08 +0800

    update
    Signed-off-by: Jade Zheng <zheng.shoujian@outlook.com>

--- vllm_ascend/worker/model_runner_v1.py
+++ vllm_ascend/worker/model_runner_v1.py
@@ -3087,7 +3087,7 @@ class NPUModelRunner(LoRAModelRunnerMixin):
                         hidden_states[dummy_indices])
 
                 def dummy_drafter_compute_logits(hidden_states):
-                    return self.drafter.compute_logits(
+                    return self.drafter.model.compute_logits(
                         hidden_states[dummy_indices])
 
             else:
