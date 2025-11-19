942e0d2b0393c97a3c5230f1aa61b4dbb41a5776
leo-pony <nengjunma@outlook.com>
2025-11-19 13:33:03 +0000

    fix lint error
    Signed-off-by: leo-pony <nengjunma@outlook.com>

--- tests/e2e/multicard/test_offline_inference_distributed.py
+++ tests/e2e/multicard/test_offline_inference_distributed.py
@@ -177,20 +177,22 @@ def test_sp_for_qwen3_moe() -> None:
                                      top_p=0.9)
 
     splitting_ops: List[str] = []
-    with VllmRunner(snapshot_download("Qwen/Qwen3-30B-A3B"),
-                    dtype="auto",
-                    tensor_parallel_size=2,
-                    distributed_executor_backend="mp",
-                    compilation_config={
-                        "pass_config": {
-                            "enable_sequence_parallelism": True,
-                        },
-                        # FIXME: When check the splitting_ops list is empyt should first check it is not none
-                        #  issue has been fixed which imported in PR:https://github.com/vllm-project/vllm/pull/27126
-                        "splitting_ops": splitting_ops
-                    },
-                    enable_expert_parallel=True,
-                    enforce_eager=True) as vllm_model:
+    with VllmRunner(
+            snapshot_download("Qwen/Qwen3-30B-A3B"),
+            dtype="auto",
+            tensor_parallel_size=4,
+            distributed_executor_backend="mp",
+            compilation_config=
+        {
+            "pass_config": {
+                "enable_sequence_parallelism": True,
+            },
+            # FIXME: When check the splitting_ops list is empyt should first check it is not none
+            #  issue has been fixed which imported in PR:https://github.com/vllm-project/vllm/pull/27126
+            "splitting_ops": splitting_ops
+        },
+            enable_expert_parallel=True,
+            enforce_eager=True) as vllm_model:
         vllm_model.generate(example_prompts, sampling_params)
 
 
