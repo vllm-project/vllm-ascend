c563446f594b03e39252ffd967cb47abe6e1e522
李少鹏 <lishaopeng21@huawei.com>
2025-11-19 14:38:36 +0800

    avoid mrope fusion op when running qwen2.5-vl on a+x machine
    Signed-off-by: 李少鹏 <lishaopeng21@huawei.com>

--- vllm_ascend/ops/rotary_embedding.py
+++ vllm_ascend/ops/rotary_embedding.py
@@ -406,7 +406,9 @@ class AscendMRotaryEmbedding(MRotaryEmbedding):
         key: torch.Tensor,
     ):
         from vllm.platforms import CpuArchEnum, Platform
-        if self.mrope_section != [16, 24, 24] or Platform.get_cpu_architecture() == CpuArchEnum.X86:
+        if self.mrope_section != [
+            16, 24, 24
+        ] or Platform.get_cpu_architecture() == CpuArchEnum.X86:
             return super().forward_oot(positions, query, key)
 
         import torch_npu
