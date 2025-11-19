92d0146e56fcb84b856c01455859b7c95145b273
shenchuxiaofugui <1311027364@qq.com>
2025-11-19 10:07:41 +0800

    cc
    Signed-off-by: shenchuxiaofugui <1311027364@qq.com>

--- vllm_ascend/ops/fused_moe/fused_moe.py
+++ vllm_ascend/ops/fused_moe/fused_moe.py
@@ -243,8 +243,8 @@ class AscendFusedMoE(FusedMoE):
                 self.log2phy = determine_default_log2phy_map(
                     self.global_num_experts, self.ep_size, self.ep_rank).npu()
                 
-        if self.expert_map is not None and isinstance(
-                self.expert_map, torch.Tensor):
+        if self.expert_map is not None and isinstance(self.expert_map,
+                                                      torch.Tensor):
             logger.info_once(
                 "[EP Rank %s/%s] Expert parallelism is enabled. Local/global"
                 " number of experts: %s/%s. Experts local to global index map:"
