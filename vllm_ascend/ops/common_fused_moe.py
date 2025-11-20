7f16de5bcbabf9b139a44e3b37dd908659dcd603
shenchuxiaofugui <1311027364@qq.com>
2025-11-20 21:40:25 +0800

    fix
    Signed-off-by: shenchuxiaofugui <1311027364@qq.com>

--- vllm_ascend/ops/common_fused_moe.py
+++ vllm_ascend/ops/common_fused_moe.py
@@ -38,12 +38,12 @@ from vllm_ascend.eplb.core.eplb_utils import (determine_default_expert_map,
 from vllm_ascend.ops.expert_load_balancer import ExpertLoadBalancer
 from vllm_ascend.ops.moe.experts_selector import select_experts
 from vllm_ascend.ops.moe.moe_comm_method import setup_moe_comm_method
+from vllm_ascend.quantization.w8a8_dynamic import \
+    AscendW8A8DynamicFusedMoEMethod
 from vllm_ascend.utils import (ACL_FORMAT_FRACTAL_NZ, enable_sp, is_310p,
                                is_enable_nz, npu_stream_switch,
                                shared_expert_dp_enabled,
                                shared_experts_compute_stream)
-from vllm_ascend.quantization.w8a8_dynamic import \
-    AscendW8A8DynamicFusedMoEMethod
 
 
 class AscendUnquantizedFusedMoEMethod(UnquantizedFusedMoEMethod):
