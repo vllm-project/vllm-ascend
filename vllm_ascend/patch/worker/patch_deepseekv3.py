precision fix
Signed-off-by: Che Ruan <cr623@ic.ac.uk>

vllm_ascend/patch/worker/patch_deepseekv3.py
@@ -1,36 +1,40 @@
 import typing
-from typing import Iterable,Union
-from collections.abc import Callable, Iterable
+from typing import Iterable
 
 import torch
 import vllm
 from torch import nn
 from transformers import DeepseekV2Config, DeepseekV3Config
-from vllm.config import ParallelConfig, get_current_vllm_config
-from vllm.distributed import (get_ep_group, get_tensor_model_parallel_rank,
-                              get_tensor_model_parallel_world_size,
-                              tensor_model_parallel_all_gather)
-from vllm.model_executor.layers.linear import ReplicatedLinear
-from vllm.model_executor.layers.quantization import QuantizationConfig
-from vllm.model_executor.layers.shared_fused_moe import SharedFusedMoE
+import torch.distributed as dist
+from vllm.distributed import (
+    get_ep_group,
+    get_tensor_model_parallel_rank,
+    get_tensor_model_parallel_world_size,
+    tensor_model_parallel_all_gather,
+)
+
 from vllm.model_executor.model_loader.weight_utils import (
     default_weight_loader, maybe_remap_kv_scale_name)
-from vllm.model_executor.models.deepseek_v2 import (
-    DeepseekV2ForCausalLM, DeepseekV2MLP, DeepseekV2MoE,
-    get_spec_layer_idx_from_weight_name)
-from vllm.model_executor.models.utils import (is_pp_missing_parameter,
-                                              sequence_parallel_chunk)
-
+from collections.abc import Callable, Iterable
+# from vllm.model_executor.layers.fused_moe.shared_fused_moe import SharedFusedMoE
+from vllm.model_executor.layers.shared_fused_moe import SharedFusedMoE
+from vllm.model_executor.models.deepseek_v2 import DeepseekV2ForCausalLM, get_spec_layer_idx_from_weight_name, DeepseekV2MLP, DeepseekV2MoE
+from vllm.model_executor.models.utils import is_pp_missing_parameter
+from vllm.config import ParallelConfig
+from vllm.config import get_current_vllm_config
 from vllm_ascend.ascend_config import get_ascend_config
-
+from vllm.model_executor.layers.quantization import QuantizationConfig
+from vllm.model_executor.layers.linear import ReplicatedLinear
+from vllm.model_executor.models.utils import sequence_parallel_chunk
+from vllm_ascend.ops.common_fused_moe import (AscendFusedMoE,
+                                                AscendSharedFusedMoE)
 
 class AscendDeepseekV2MoE(DeepseekV2MoE,nn.Module):
-
     def __init__(
         self,
-        config: Union[DeepseekV2Config, DeepseekV3Config],
+        config: DeepseekV2Config | DeepseekV3Config,
         parallel_config: ParallelConfig,
-        quant_config: Union[QuantizationConfig, None] = None,
+        quant_config: QuantizationConfig | None = None,
         prefix: str = "",
     ):
         nn.Module.__init__(self)
@@ -48,8 +52,10 @@ class AscendDeepseekV2MoE(DeepseekV2MoE,nn.Module):
         self.is_sequence_parallel = parallel_config.use_sequence_parallel_moe
 
         if config.hidden_act != "silu":
-            raise ValueError(f"Unsupported activation: {config.hidden_act}. "
-                             "Only silu is supported for now.")
+            raise ValueError(
+                f"Unsupported activation: {config.hidden_act}. "
+                "Only silu is supported for now."
+            )
 
         self.gate = ReplicatedLinear(
             config.hidden_size,
@@ -60,7 +66,8 @@ class AscendDeepseekV2MoE(DeepseekV2MoE,nn.Module):
         )
         if config.topk_method == "noaux_tc":
             self.gate.e_score_correction_bias = nn.Parameter(
-                torch.empty(config.n_routed_experts, dtype=torch.float32))
+                torch.empty(config.n_routed_experts, dtype=torch.float32)
+            )
         else:
             self.gate.e_score_correction_bias = None
 
@@ -74,12 +81,16 @@ class AscendDeepseekV2MoE(DeepseekV2MoE,nn.Module):
         self.n_local_physical_experts = self.n_physical_experts // self.ep_size
 
         self.physical_expert_start = self.ep_rank * self.n_local_physical_experts
-        self.physical_expert_end = (self.physical_expert_start +
-                                    self.n_local_physical_experts)
-
+        self.physical_expert_end = (
+            self.physical_expert_start + self.n_local_physical_experts
+        )
+        
         ascend_config = get_ascend_config()
-        mix_placement = getattr(ascend_config, "mix_placement", False)
-        if (config.n_shared_experts is None or mix_placement):
+        mix_placement = getattr(ascend_config,"mix_placement",False)
+        if (
+            config.n_shared_experts is None
+            or mix_placement
+        ):
             self.shared_experts = None
         else:
             intermediate_size = config.moe_intermediate_size * config.n_shared_experts
@@ -111,7 +122,8 @@ class AscendDeepseekV2MoE(DeepseekV2MoE,nn.Module):
             # we do scaling outside, set factor to 1.0 to avoid double mul
             # aiter applies routed_scaling_factor internally
             routed_scaling_factor=1.0
-            if not mix_placement else self.routed_scaling_factor,
+            if not mix_placement
+            else self.routed_scaling_factor,
             e_score_correction_bias=self.gate.e_score_correction_bias,
             enable_eplb=self.enable_eplb,
             num_redundant_experts=self.n_redundant_experts,
@@ -130,8 +142,9 @@ class AscendDeepseekV2MoE(DeepseekV2MoE,nn.Module):
             hidden_states = sequence_parallel_chunk(hidden_states)
 
         router_logits, _ = self.gate(hidden_states)
-        fused_moe_out = self.experts(hidden_states=hidden_states,
-                                     router_logits=router_logits)
+        fused_moe_out = self.experts(
+            hidden_states=hidden_states, router_logits=router_logits
+        )
         ascend_config = get_ascend_config()
         shared_output, final_hidden_states = fused_moe_out
         if self.shared_experts is None:
@@ -140,32 +153,29 @@ class AscendDeepseekV2MoE(DeepseekV2MoE,nn.Module):
         # Fix FP16 overflow
         # See DeepseekV2DecoderLayer for more details.
         if hidden_states.dtype != torch.float16:
-            if not getattr(ascend_config, "mix_placement", False):
-                final_hidden_states *= self.routed_scaling_factor
+            final_hidden_states *= self.routed_scaling_factor
         elif self.shared_experts is not None:
             assert shared_output is not None
             shared_output *= 1.0 / self.routed_scaling_factor
-
+        
         if self.shared_experts is not None:
             assert shared_output is not None
             final_hidden_states += shared_output
 
         if self.is_sequence_parallel:
             final_hidden_states = tensor_model_parallel_all_gather(
-                final_hidden_states, 0)
+                final_hidden_states, 0
+            )
             final_hidden_states = final_hidden_states[:num_tokens]
         elif self.tp_size > 1:
             final_hidden_states = self.experts.maybe_all_reduce_tensor_model_parallel(
-                final_hidden_states)
-
+                final_hidden_states
+            )
         return final_hidden_states.view(num_tokens, hidden_dim)
 
-
 class CustomDeepseekV2ForCausalLM(DeepseekV2ForCausalLM):
-
-    def load_weights(self, weights: Iterable[tuple[str,
-                                                   torch.Tensor]]) -> set[str]:
-        self.vllm_config = get_current_vllm_config()
+    def load_weights(self, weights: Iterable[tuple[str, torch.Tensor]]) -> set[str]:
+        self.vllm_config=get_current_vllm_config()
         ascend_config = get_ascend_config()
         stacked_params_mapping = [
             ("gate_up_proj", "gate_proj", 0),
@@ -181,10 +191,12 @@ class CustomDeepseekV2ForCausalLM(DeepseekV2ForCausalLM):
             ckpt_gate_proj_name="gate_proj",
             ckpt_down_proj_name="down_proj",
             ckpt_up_proj_name="up_proj",
-            num_experts=self.config.n_routed_experts + (
+            num_experts=self.config.n_routed_experts
+            + (
                 self.config.n_shared_experts
                 if mix_placement
-                else 0),
+                else 0
+            ),
             num_redundant_experts=self.num_redundant_experts,
         )
 
@@ -198,8 +210,10 @@ class CustomDeepseekV2ForCausalLM(DeepseekV2ForCausalLM):
             if spec_layer is not None:
                 continue
 
-            is_fuse_shared_experts_layer = (mix_placement
-                                            and ("mlp.shared_experts" in name))
+            is_fuse_shared_experts_layer = (
+                mix_placement
+                and ("mlp.shared_experts" in name)
+            )
 
             for param_name, weight_name, shard_id in stacked_params_mapping:
                 if weight_name not in name:
@@ -210,8 +224,7 @@ class CustomDeepseekV2ForCausalLM(DeepseekV2ForCausalLM):
                     continue
                 name_mapped = name.replace(weight_name, param_name)
 
-                if (param_name == "fused_qkv_a_proj"
-                    ) and name_mapped not in params_dict:
+                if (param_name == "fused_qkv_a_proj") and name_mapped not in params_dict:
                     continue
                 else:
                     name = name_mapped
@@ -230,8 +243,7 @@ class CustomDeepseekV2ForCausalLM(DeepseekV2ForCausalLM):
                 is_expert_weight = False
                 num_chunks = 1
                 if is_fuse_shared_experts_layer:
-                    num_chunks = getattr(self.config, "n_shared_experts",
-                                         1) or 1
+                    num_chunks = getattr(self.config, "n_shared_experts", 1) or 1
                     split_dim = 1 if "down_proj.weight" in name else 0
                     total = loaded_weight.shape[split_dim]
                     assert total % num_chunks == 0, (
@@ -245,18 +257,13 @@ class CustomDeepseekV2ForCausalLM(DeepseekV2ForCausalLM):
 
                     if is_fuse_shared_experts_layer:
                         if split_dim == 0:
-                            weight_to_load = loaded_weight[j *
-                                                           chunk_size:(j + 1) *
-                                                           chunk_size, :]
+                            weight_to_load = loaded_weight[j * chunk_size : (j + 1) * chunk_size, :]
                         else:
-                            weight_to_load = loaded_weight[:, j *
-                                                           chunk_size:(j + 1) *
-                                                           chunk_size]
+                            weight_to_load = loaded_weight[:, j * chunk_size : (j + 1) * chunk_size]
                         chunk_name = name.replace(
                             "mlp.shared_experts",
                             f"mlp.experts.{self.config.n_routed_experts + j}",
                         )
-
 
                     for mapping in expert_params_mapping:
                         param_name, weight_name, expert_id, shard_id = mapping
@@ -264,16 +271,14 @@ class CustomDeepseekV2ForCausalLM(DeepseekV2ForCausalLM):
                             continue
 
                         is_expert_weight = True
-                        name_mapped = chunk_name.replace(
-                            weight_name, param_name)
+                        name_mapped = chunk_name.replace(weight_name, param_name)
 
                         if is_pp_missing_parameter(name_mapped, self):
                             continue
                         if name_mapped not in params_dict.keys():
                             continue
                         param = params_dict[name_mapped]
-                        weight_loader = typing.cast(Callable[..., bool],
-                                                    param.weight_loader)
+                        weight_loader = typing.cast(Callable[..., bool], param.weight_loader)
                         success = weight_loader(
                             param,
                             weight_to_load,
@@ -286,7 +291,7 @@ class CustomDeepseekV2ForCausalLM(DeepseekV2ForCausalLM):
                             if not is_fuse_shared_experts_layer:
                                 name = name_mapped
                             else:
-                                loaded_params.add(name_mapped)
+                                loaded_params.add(name_mapped)    
                             break
                     else:
                         if is_expert_weight:
@@ -311,5 +316,4 @@ class CustomDeepseekV2ForCausalLM(DeepseekV2ForCausalLM):
 
 
 vllm.model_executor.models.deepseek_v2.DeepseekV2MoE = AscendDeepseekV2MoE
-
 DeepseekV2ForCausalLM.load_weights = CustomDeepseekV2ForCausalLM.load_weights