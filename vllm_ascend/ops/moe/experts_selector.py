precision fix

vllm_ascend/ops/moe/experts_selector.py
@@ -99,7 +99,7 @@ def select_experts(hidden_states: torch.Tensor,
                                            device=topk_ids.device)
 
         pad_shared_expert_weights = torch.full((topk_weights.shape[0], 1),
-                                               1.0 / 2.5,
+                                               0.4,
                                                dtype=topk_weights.dtype,
                                                device=topk_weights.device)
         topk_ids = torch.cat([topk_ids, pad_shared_expert_ids], dim=1)