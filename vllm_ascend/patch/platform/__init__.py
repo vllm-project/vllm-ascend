fcdc7bcf4e8ac930e376f85e2b4fb31628a6f5ac
白永斌 <baiyongbin3@h-partners.com>
2025-11-21 17:46:25 +0800

    fix typo
    Signed-off-by: 白永斌 <baiyongbin3@h-partners.com>

--- vllm_ascend/patch/platform/__init__.py
+++ vllm_ascend/patch/platform/__init__.py
@@ -22,6 +22,6 @@ import vllm_ascend.patch.platform.patch_distributed  # noqa
 import vllm_ascend.patch.platform.patch_mamba_config  # noqa
 import vllm_ascend.patch.platform.patch_sched_yield  # noqa
 
-if envs_ascend.DYNAMIC_EPLB not in ("true", "1") or os.getenv(
+if envs_ascend.DYNAMIC_EPLB in ("true", "1") or os.getenv(
         "EXPERT_MAP_RECORD", "false") == "true":
     import vllm_ascend.patch.platform.patch_multiproc_executor  # noqa
