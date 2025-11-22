4bbfbd701efee735f8c6cf57f9ab1ac4e64633b0
pz1116 <zpbzpb123123@gmail.com>
2025-11-22 16:00:42 +0800

    clean code
    Signed-off-by: pz1116 <zpbzpb123123@gmail.com>

--- vllm_ascend/distributed/mooncake/mooncake_store.py
+++ vllm_ascend/distributed/mooncake/mooncake_store.py
@@ -11,17 +11,15 @@ from vllm_ascend.distributed.mooncake.config_data import MooncakeEngineKey
 from vllm_ascend.distributed.mooncake.transfer_engine import get_global_te
 
 from .config_data import MooncakeStoreConfig
-
-METADATA_BYTES_LEN = 24
-BASE_PORT = int(os.getenv("VLLM_BASE_PORT", "8790"))
-
 from vllm_ascend.utils import vllm_version_is
 
 if vllm_version_is("0.11.0"):
     from vllm.utils import get_ip
 else:
     from vllm.utils.network_utils import get_ip
-
+    
+METADATA_BYTES_LEN = 24
+BASE_PORT = int(os.getenv("VLLM_BASE_PORT", "8790"))
 
 class Mooncakestore():
 
