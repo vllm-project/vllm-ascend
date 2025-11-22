852e05cec34f3745e349fae4e38493d6ffb6084d
fems14 <74094523+fems14@users.noreply.github.com>
2025-11-22 19:14:25 +0800

    Update config_data.py

--- vllm_ascend/distributed/kvpool/config_data.py
+++ vllm_ascend/distributed/kvpool/config_data.py
@@ -1,47 +1,48 @@
-import array
-import hashlib
 from dataclasses import dataclass
 from typing import Iterable, List, Optional, Tuple, Union
 
-import torch
 from vllm.distributed.kv_transfer.kv_connector.v1.base import \
     KVConnectorMetadata
-from vllm.utils import cdiv, logger
+from vllm.utils import logger
+
+from vllm_ascend.utils import vllm_version_is
+
+if vllm_version_is("0.11.0"):
+    from vllm.utils import cdiv
+else:
+    from vllm.utils.math_utils import cdiv
+
+from vllm.v1.core.kv_cache_utils import BlockHash
 from vllm.v1.core.sched.output import NewRequestData
 
 
+#Parameters related to the key
 @dataclass
 class KeyMetadata:
     """name of the LLM model"""
 
     model_name: str
-    """ world size when running under a distributed setting """
-    world_size: int
     """ worker id when running under a distributed setting """
-    worker_id: int
-
-    block_size: int = 128
+    head_or_tp_rank: int
 
 
 @dataclass(order=True)
 class PoolKey:
-    # model_name: str
-    # world_size: int
-    # worker_id: int
     key_metadata: KeyMetadata
     chunk_hash: str
 
     def __hash__(self):
         return hash((
             self.key_metadata.model_name,
-            self.key_metadata.world_size,
-            self.key_metadata.worker_id,
+            self.key_metadata.head_or_tp_rank,
             self.chunk_hash,
         ))
 
     def to_string(self):
-        return (f"{self.key_metadata.model_name}@{self.key_metadata.world_size}"
-                f"@{self.key_metadata.worker_id}@{self.chunk_hash}")
+        return (
+            f"{self.key_metadata.model_name}"
+            f"@head_or_tp_rank:{self.key_metadata.head_or_tp_rank}@{self.chunk_hash}"
+        )
 
     def split_layers(self, num_layers: int) -> List["LayerPoolKey"]:
         """Split the key into multiple keys for each layer"""
@@ -60,8 +61,7 @@ class PoolKey:
         return {
             "__type__": "CacheEngineKey",
             "model_name": self.key_metadata.model_name,
-            "world_size": self.key_metadata.world_size,
-            "worker_id": self.key_metadata.worker_id,
+            "head_or_tp_rank": self.key_metadata.head_or_tp_rank,
             "chunk_hash": self.chunk_hash,
         }
 
@@ -69,8 +69,7 @@ class PoolKey:
     def from_dict(d):
         return PoolKey(
             model_name=d["model_name"],
-            world_size=d["world_size"],
-            worker_id=d["worker_id"],
+            head_or_tp_rank=d["head_or_tp_rank"],
             chunk_hash=d["chunk_hash"],
         )
 
@@ -84,79 +83,82 @@ class LayerPoolKey(PoolKey):
     def __hash__(self):
         return hash((
             self.key_metadata.model_name,
-            self.key_metadata.world_size,
-            self.key_metadata.worker_id,
+            self.key_metadata.head_or_tp_rank,
             self.chunk_hash,
             self.layer_id,
         ))
 
     def to_string(self):
-        return (f"{self.key_metadata.model_name}@{self.key_metadata.world_size}"
-                f"@{self.key_metadata.worker_id}@{self.chunk_hash}@{self.layer_id}")
+        return (
+            f"{self.key_metadata.model_name}"
+            f"@head_or_tp_rank:{self.key_metadata.head_or_tp_rank}@{self.chunk_hash}@{self.layer_id}"
+        )
 
 
 class ChunkedTokenDatabase():
 
-    def __init__(
-        self,
-        metadata: KeyMetadata,
-    ):
+    def __init__(self, metadata: KeyMetadata, block_size: int, use_mla: bool):
         self.metadata = metadata
+        self.block_size = block_size
+        self.use_mla = use_mla
+        self.kv_caches_base_addr: list[int] = []
+        self.block_len: list[int] = []
 
     def _make_key_by_hash(self,
                           chunk_hash: str,
                           layer_id: Optional[int] = None):
         assert self.metadata is not None
         return PoolKey(
-            # self.metadata.model_name,
-            # self.metadata.world_size,
-            # self.metadata.worker_id,
             self.metadata,
             chunk_hash,
         )
 
-    def _hash(
-        self,
-        tokens: Union[torch.Tensor, List[int]],
-        prefix_hash: str,
-    ) -> str:
-        # TODO: change it to a more efficient hash function
-        if isinstance(tokens, torch.Tensor):
-            tokens_bytes = tokens.cpu().to(torch.uint32).numpy().tobytes()
-        elif isinstance(tokens, list):
-            tokens_bytes = array.array("I", tokens).tobytes()
-        return hashlib.sha256(prefix_hash.encode("ascii") +
-                              tokens_bytes).hexdigest()
+    def set_kv_caches_base_addr(self, kv_caches_base_addr: list[int]):
+        self.kv_caches_base_addr = kv_caches_base_addr
 
-    def _chunk_tokens(
-        self,
-        tokens: Union[torch.Tensor, List[int]],
-    ) -> Iterable[Union[torch.Tensor, List[int]]]:
-        """
-        Chunk the tokens into chunks of size self.metadata.block_size.
+    def set_block_len(self, block_len: list[int]):
+        self.block_len = block_len
 
-        :param tokens: the input tokens, with shape [seq_len]
-            device: the target device after chunking
+    def prepare_value(self, start: int, end: int, block_ids: list[int]):
+        addr_list = []
+        size_list = []
+        block_id = block_ids[start // self.block_size]
+        for index, base_addr in enumerate(self.kv_caches_base_addr):
+            block_len = (self.block_len[index % 2]
+                         if self.use_mla else self.block_len[0])
 
-        :return: a generator of chunks of tokens, each with
-                shape [metadata.block_size]
-        """
-        for i in range(0, len(tokens), self.metadata.block_size):
-            yield tokens[i:i + self.metadata.block_size]
+            addr = base_addr + block_id * block_len
+            length = int(block_len / self.block_size * (end - start))
+            addr_list.append(addr)
+            size_list.append(length)
+        return addr_list, size_list, block_id
 
-    def _prefix_hash(
-        self,
-        token_chunks: Iterable[Union[torch.Tensor, List[int]]],
-    ) -> Iterable[str]:
-        prefix_hash = ''
-        for token_chunk in token_chunks:
-            prefix_hash = self._hash(token_chunk, prefix_hash)
-            yield prefix_hash
+    def prepare_value_layer(self, start: int, end: int, block_ids: list[int],
+                            layer_id: int):
+        block_id = block_ids[start // self.block_size]
+        if self.use_mla:
+            addr_k = self.kv_caches_base_addr[layer_id *
+                                              2] + block_id * self.block_len[0]
+            addr_v = self.kv_caches_base_addr[layer_id * 2 +
+                                              1] + block_id * self.block_len[1]
+            length_k = int(self.block_len[0] / self.block_size * (end - start))
+            length_v = int(self.block_len[1] / self.block_size * (end - start))
+            size_list = [length_k, length_v]
+        else:
+            addr_k = self.kv_caches_base_addr[layer_id *
+                                              2] + block_id * self.block_len[0]
+            addr_v = self.kv_caches_base_addr[layer_id * 2 +
+                                              1] + block_id * self.block_len[0]
+            length = int(self.block_len[0] / self.block_size * (end - start))
+            size_list = [length, length]
+        addr_list = [addr_k, addr_v]
+        return addr_list, size_list
 
     def process_tokens(
         self,
-        tokens: Union[torch.Tensor, List[int]],
-        mask: Optional[torch.Tensor] = None,
+        token_len: int,
+        block_hashes: Union[list[BlockHash], list[str]],
+        mask_num: int = 0,
     ) -> Iterable[Tuple[int, int, PoolKey]]:
         """Process the tokens and return the corresponding cache engine keys.
 
@@ -178,30 +180,23 @@ class ChunkedTokenDatabase():
         :raises: ValueError if the number of Falses in the mask is not a
             multiple of the chunk size.
         """
-        if mask is not None:
-            num_falses = mask.numel() - mask.long().sum().item()
-        else:
-            num_falses = 0
-
-        if num_falses % self.metadata.block_size != 0:
-            raise ValueError(
-                "The number of Falses in the mask is not a multiple of the chunk size."
-            )
-        total_len = len(tokens)
-
-        token_chunks = self._chunk_tokens(tokens)
-        prefix_hashes = self._prefix_hash(token_chunks)
-
+        if not block_hashes:
+            return
+        if not isinstance(block_hashes[0], str):
+            block_hashes = [h.hex() for h in block_hashes]
         start_idx = 0
-        for chunk_id, hash_val in enumerate(prefix_hashes):
-            start_idx = chunk_id * self.metadata.block_size
-            end_idx = min(start_idx + self.metadata.block_size, total_len)
-            if start_idx < num_falses:
+        for chunk_id, hash_val in enumerate(block_hashes):
+            start_idx = chunk_id * self.block_size
+            if start_idx >= token_len:
+                break
+            end_idx = min(start_idx + self.block_size, token_len)
+            if start_idx < mask_num:
                 continue
             else:
                 yield start_idx, end_idx, self._make_key_by_hash(hash_val)
 
 
+#Parameters related to the connector metadata
 @dataclass
 class LoadSpec:
     # Number of tokens cached in vLLM
@@ -213,20 +208,12 @@ class LoadSpec:
 
 
 @dataclass
-class SaveSpec:
-    # Skip already saved tokens
-    skip_leading_tokens: int
-    # Whether the scheduler allow us to save the tokens
-    can_save: bool
-
-
-@dataclass
 class RequestTracker:
     # Request id
     req_id: str
 
     # The token ids that has been scheduled so far
-    token_ids: list[int]
+    token_len: int
 
     # The block ids that has been allocated so far
     # NOTE: allocated blocks could be more than the number of tokens
@@ -251,10 +238,6 @@ class RequestTracker:
                 local cache hit) and new tokens that will be scheduled.
 
         """
-        # vLLM 0.9.0 update: request.block_ids changed from list[int] to
-        # list[list[int]]
-        # Need to check the type of request.block_ids
-
         unfolded_block_ids = []
 
         if not isinstance(new_request.block_ids[0], list):
@@ -264,8 +247,7 @@ class RequestTracker:
 
         return RequestTracker(
             req_id=new_request.req_id,
-            token_ids=new_request.prompt_token_ids[:num_tokens_to_compute].
-            copy(),
+            token_len=num_tokens_to_compute,
             allocated_block_ids=unfolded_block_ids,
             num_saved_tokens=0,
         )
@@ -279,7 +261,7 @@ class RequestTracker:
         scheduled again
         """
 
-        self.token_ids.extend(new_token_ids)
+        self.token_len = self.token_len + len(new_token_ids)
 
         if len(new_block_ids) == 0:
             new_block_ids = []
@@ -298,13 +280,13 @@ class ReqMeta:
     # Request id
     req_id: str
     # Request tokens
-    token_ids: torch.Tensor
+    token_len_chunk: int
 
     block_ids: list[int]
-    # # Slot mapping if exchange for block_id
-    # slot_mapping: torch.Tensor
-    # Skip save or not
-    save_spec: Optional[SaveSpec] = None
+
+    block_hashes: list[BlockHash]
+
+    can_save: Optional[bool] = None
     # load_spec
     load_spec: Optional[LoadSpec] = None
 
@@ -316,6 +298,7 @@ class ReqMeta:
         block_size: int,
         load_spec: Optional[LoadSpec] = None,
         skip_save: Optional[bool] = False,
+        block_hashes: list[BlockHash] = [],
         is_last_chunk: Optional[bool] = None,
         discard_partial_chunks: bool = True,
     ) -> Optional["ReqMeta"]:
@@ -332,13 +315,11 @@ class ReqMeta:
             the request metadata if we need to perform load/save
             operations, None otherwise.
         """
-        input_token_ids = tracker.token_ids
-        input_token_len = len(input_token_ids)
+        input_token_len = tracker.token_len
 
         # For save operation: do not save if the following condition is met
         # 1. has already been saved before (num_saved_tokens > 0)
         # 2. number of unsaved tokens is not reached the chunk boundary
-        skip_leading_tokens = tracker.num_saved_tokens
         chunk_boundary = (cdiv(tracker.num_saved_tokens + 1, block_size) *
                           block_size if discard_partial_chunks else 0)
         # Calculate number of tokens to save based on discard_partial_chunks
@@ -353,11 +334,6 @@ class ReqMeta:
         # If we need to save, update the number of saved tokens
         if not skip_save:
             tracker.num_saved_tokens = num_tokens_to_save
-        save_spec = SaveSpec(skip_leading_tokens, not skip_save)
-
-        # Calculate the token ids and slot mappings for load and save
-        # OPTIMIZATION: pre-allocate the buffer for token ids and block ids
-        token_ids = torch.tensor(input_token_ids)[:num_tokens_to_save]
 
         # # For load operation: check whether the request is scheduled to load
         if load_spec is not None and load_spec.can_load:
@@ -370,14 +346,15 @@ class ReqMeta:
             # Do not load if not in `can_load` state
             load_spec = None
         logger.debug(
-            f"request:{tracker.req_id}, meta save spec:{save_spec}, meta load spec:{load_spec}"
+            f"request:{tracker.req_id}, meta save spec:{not skip_save}, meta load spec:{load_spec}"
         )
         return ReqMeta(
             req_id=tracker.req_id,
-            token_ids=token_ids,
+            token_len_chunk=num_tokens_to_save,
             block_ids=tracker.allocated_block_ids,
-            save_spec=save_spec,
+            can_save=not skip_save,
             load_spec=load_spec,
+            block_hashes=block_hashes,
             is_last_chunk=is_last_chunk,
         )
 
