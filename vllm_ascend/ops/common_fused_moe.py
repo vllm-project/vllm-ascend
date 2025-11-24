478d1f1674b81e94711f7bfa6e0026c9b76df20a
offline0806 <3337230449@qq.com>
2025-11-24 10:32:39 +0800

    [logical_to_physical_mcal_mapto resolve eplb preformance problem.apBugFix]fix log2phy param to logical
    Signed-off-by: offline0806 <3337230449@qq.com>

--- vllm_ascend/ops/common_fused_moe.py
+++ vllm_ascend/ops/common_fused_moe.py
@@ -202,7 +202,7 @@ class AscendFusedMoE(FusedMoE):
                 self.local_num_experts, self.expert_map = (
                     self.expert_load_balancer.get_rank_placement_map(
                         self.moe_instance_id, self.ep_rank))
-                self.log2phy = self.expert_load_balancer.get_rank_log2phy_map(
+                self.logical_to_physical_map = self.expert_load_balancer.get_rank_log2phy_map(
                     self.moe_instance_id, self.ep_rank).npu()
             except Exception as e:
                 logger.warning(
@@ -210,9 +210,9 @@ class AscendFusedMoE(FusedMoE):
                 self.local_num_experts, self.expert_map = determine_default_expert_map(
                     self.global_num_experts, self.ep_size, self.ep_rank,
                     self.global_redundant_expert_num)
-                self.log2phy = determine_default_log2phy_map(
+                self.logical_to_physical_map = determine_default_log2phy_map(
                     self.global_num_experts, self.ep_size, self.ep_rank,
-                    self.global_redundant_expert_num).npu()
+                    self.global_redundant_expert_num)
             if self.expert_map is not None and isinstance(
                     self.expert_map, torch.Tensor):
                 logger.info_once(
@@ -231,7 +231,7 @@ class AscendFusedMoE(FusedMoE):
                 self.local_num_experts, self.expert_map = determine_default_expert_map(
                     self.global_num_experts, self.ep_size, self.ep_rank,
                     self.global_redundant_expert_num)
-                self.log2phy = determine_default_log2phy_map(
+                self.logical_to_physical_map = determine_default_log2phy_map(
                     self.global_num_experts, self.ep_size, self.ep_rank,
                     self.global_redundant_expert_num).npu()
             if self.expert_map is not None and isinstance(
@@ -342,7 +342,7 @@ class AscendFusedMoE(FusedMoE):
             dynamic_scale_for_share=dynamic_scale_for_share,
             shared_experts=None,
             enable_force_load_balance=enable_force_load_balance,
-            log2phy=self.log2phy,
+            log2phy=self.logical_to_physical_map,
             global_redundant_expert_num=self.global_redundant_expert_num)
 
         if isinstance(final_hidden_states, tuple):
