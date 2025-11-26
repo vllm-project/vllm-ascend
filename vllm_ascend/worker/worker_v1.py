545d41d46740b870f42612a17edec7018ee7034f
Kurumi5210 <Jaychou1620@Gmail.com>
2025-11-26 15:53:09 +0800

    format code
    Signed-off-by: Kurumi5210 <Jaychou1620@Gmail.com>

--- vllm_ascend/worker/worker_v1.py
+++ vllm_ascend/worker/worker_v1.py
@@ -50,8 +50,9 @@ from vllm_ascend.cpu_binding import bind_cpus
 from vllm_ascend.device_allocator.camem import CaMemAllocator
 from vllm_ascend.distributed.parallel_state import init_ascend_model_parallel
 from vllm_ascend.platform import NPUPlatform
-from vllm_ascend.utils import (enable_sp, init_ascend_soc_version,
-                               is_enable_nz, prefill_context_parallel_enable,
+from vllm_ascend.utils import (check_ascend_device_type, enable_sp,
+                               init_ascend_soc_version, is_enable_nz,
+                               prefill_context_parallel_enable,
                                register_ascend_customop, sleep_mode_enabled,
                                try_register_lib, vllm_version_is,
                                check_ascend_device_type)
