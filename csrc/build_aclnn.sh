49203aef57c396da4722f7070393e32139af4864
QianChenxi <chenxi.qian.cq@outlook.com>
2025-11-26 18:03:54 +0800

    adapt new soc_version env
    Signed-off-by: QianChenxi <chenxi.qian.cq@outlook.com>

--- csrc/build_aclnn.sh
+++ csrc/build_aclnn.sh
@@ -3,15 +3,17 @@
 ROOT_DIR=$1
 SOC_VERSION=$2
 
-if [[ "$SOC_VERSION" =~ ^[Aa][Ss][Cc][Ee][Nn][Dd]310 ]]; then
+if [[ "$SOC_VERSION" =~ ^ascend310 ]]; then
     # ASCEND310P series
     # currently, no custom aclnn ops for ASCEND310 series
+    # CUSTOM_OPS=""
+    # SOC_ARG="ascend310p"
     exit 0
-elif [[ "$SOC_VERSION" =~ ^[Aa][Ss][Cc][Ee][Nn][Dd]910[Bb][0-9]$ ]]; then
+elif [[ "$SOC_VERSION" =~ ^ascend910b ]]; then
     # ASCEND910B (A2) series
     CUSTOM_OPS="grouped_matmul_swiglu_quant_weight_nz_tensor_list"
     SOC_ARG="ascend910b"
-elif [[ "$SOC_VERSION" =~ ^[Aa][Ss][Cc][Ee][Nn][Dd]910_93[0-9]{2}$ ]]; then
+elif [[ "$SOC_VERSION" =~ ^ascend910_93 ]]; then
     # ASCEND910C (A3) series
     CUSTOM_OPS="grouped_matmul_swiglu_quant_weight_nz_tensor_list"
     SOC_ARG="ascend910_93"
