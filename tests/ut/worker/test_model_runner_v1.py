c706545abe69b2508f1e0dea7c1843223d6647b2
ivyilike <pww123@cmbchina.com>
2025-11-26 14:34:25 +0800

    [Feat] Add worker interface "update_config" fix ut test
    Signed-off-by: ivyilike <pww123@cmbchina.com>

--- tests/ut/worker/test_model_runner_v1.py
+++ tests/ut/worker/test_model_runner_v1.py
@@ -114,7 +114,7 @@ def test_select_moe_comm_method_unsupported_soc():
                   return_value=True), \
          pytest.raises(ValueError, match=f"Unsupported soc_version: {unsupported_soc}"):
 
-        NPUModelRunner._select_moe_comm_method(mock_runner, 100, False)
+        NPUModelRunner._select_moe_comm_method(mock_runner, 100)
 
 
 def get_vllm_config():
