a70e857d810bb045f061b2481ac7671009d465ec
shen-shanshan <467638484@qq.com>
2025-11-27 07:39:29 +0000

    add patch for rope
    Signed-off-by: shen-shanshan <467638484@qq.com>

--- vllm_ascend/models/__init__.py
+++ vllm_ascend/models/__init__.py
@@ -8,13 +8,11 @@ def register_model():
 
     ModelRegistry.register_model(
         "Qwen3VLMoeForConditionalGeneration",
-        "vllm_ascend.models.qwen2_5_vl_without_padding:AscendQwen3VLMoeForConditionalGeneration"
-    )
+        "vllm_ascend.models.qwen3_vl:AscendQwen3VLMoeForConditionalGeneration")
 
     ModelRegistry.register_model(
         "Qwen3VLForConditionalGeneration",
-        "vllm_ascend.models.qwen2_5_vl_without_padding:AscendQwen3VLForConditionalGeneration"
-    )
+        "vllm_ascend.models.qwen3_vl:AscendQwen3VLForConditionalGeneration")
 
     # There is no PanguProMoEForCausalLM in vLLM, so we should register it before vLLM config initialization
     # to make sure the model can be loaded correctly. This register step can be removed once vLLM support PanguProMoEForCausalLM.
