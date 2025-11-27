4718c7cb5fb0a9609f523f3587f26cf60de0918e
chenxiao <Jaychou1620@Gmail.com>
2025-11-27 17:36:30 +0800

    format code
    Signed-off-by: chenxiao <Jaychou1620@Gmail.com>

--- vllm_ascend/distributed/mooncake_connector.py
+++ vllm_ascend/distributed/mooncake_connector.py
@@ -44,7 +44,7 @@ from vllm.v1.request import RequestStatus
 from vllm_ascend.ascend_config import get_ascend_config, init_ascend_config
 from vllm_ascend.distributed.mooncake_transfer_engine import global_te
 from vllm_ascend.distributed.utils import get_transfer_timeout_value
-from vllm_ascend.utils import prefill_context_parallel_enable, vllm_version_is
+from vllm_ascend.utils import prefill_context_parallel_enable
 
 # isort: off
 if prefill_context_parallel_enable():
