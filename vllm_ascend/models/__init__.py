e9fc2367e254710803f4fb374cde31bdfdff70eb
shen-shanshan <467638484@qq.com>
2025-11-28 07:37:48 +0000

    remove qwen2-vl
    Signed-off-by: shen-shanshan <467638484@qq.com>

--- vllm_ascend/models/__init__.py
+++ vllm_ascend/models/__init__.py
@@ -2,9 +2,9 @@ from vllm import ModelRegistry
 
 
 def register_model():
-    ModelRegistry.register_model(
-        "Qwen2VLForConditionalGeneration",
-        "vllm_ascend.models.qwen2_vl:AscendQwen2VLForConditionalGeneration")
+    # ModelRegistry.register_model(
+    #     "Qwen2VLForConditionalGeneration",
+    #     "vllm_ascend.models.qwen2_vl:AscendQwen2VLForConditionalGeneration")
 
     ModelRegistry.register_model(
         "Qwen3VLMoeForConditionalGeneration",
