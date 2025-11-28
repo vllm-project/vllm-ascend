18cd2f89226772f1dfd8254ea9d5a1200080998f
Ronald1995 <ronaldautomobile@163.com>
2025-11-28 20:16:30 +0800

    fix yapf error
    Signed-off-by: Ronald1995 <ronaldautomobile@163.com>

--- vllm_ascend/worker/npu_input_batch.py
+++ vllm_ascend/worker/npu_input_batch.py
@@ -69,6 +69,7 @@ class CachedRequestState:
     prompt_embeds: Optional[torch.Tensor] = None
 
     prev_num_draft_len: int = 0  # previous number of draft tokens
+
     def __post_init__(self):
         self.num_prompt_tokens = length_from_prompt_token_ids_or_embeds(
             self.prompt_token_ids, self.prompt_embeds)
