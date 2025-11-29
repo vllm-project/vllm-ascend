eed957775e147824a5ba5b2f230fb93d0d73ff07
SlightwindSec <slightwindsec@gmail.com>
2025-11-29 15:00:41 +0800

    clean code
    Signed-off-by: SlightwindSec <slightwindsec@gmail.com>

--- vllm_ascend/quantization/w8a8_pdmix.py
+++ vllm_ascend/quantization/w8a8_pdmix.py
@@ -37,24 +37,8 @@ class AscendW8A8PDMixLinearMethod(AscendW8A8DynamicLinearMethod):
             output_size, params_dtype)
 
     def process_weights_after_loading(self, layer):
-        expanding_factor = layer.weight.data.shape[1]
-        layer.aclnn_input_scale = torch.nn.Parameter(
-            layer.input_scale.data.repeat(expanding_factor),
-            requires_grad=False)
-        layer.aclnn_input_scale_reciprocal = torch.nn.Parameter(
-            1.0 / layer.aclnn_input_scale.data, requires_grad=False)
-        layer.aclnn_input_offset = torch.nn.Parameter(
-            layer.input_offset.data.repeat(expanding_factor),
-            requires_grad=False).to(layer.aclnn_input_scale.dtype)
-        if self.transpose_weight:
-            layer.weight.data = layer.weight.data.transpose(0, 1).contiguous()
-        if is_enable_nz():
-            layer.weight.data = torch_npu.npu_format_cast(
-                layer.weight.data, ACL_FORMAT_FRACTAL_NZ)
-        layer.weight_scale.data = torch.flatten(layer.weight_scale.data)
-        layer.weight_offset.data = torch.flatten(layer.weight_offset.data)
+        AscendW8A8LinearMethod.process_weights_after_loading(self, layer)
         layer.weight_scale_fp32 = layer.weight_scale.data.to(torch.float32)
-        layer.bias.data = layer.bias.data.to(layer.weight_scale.data.dtype)
         layer.is_kv_consumer = self.kv_transfer_config is not None and self.kv_transfer_config.is_kv_consumer
 
 
@@ -68,25 +52,7 @@ class AscendW8A8PDMixFusedMoeMethod(AscendW8A8DynamicFusedMoEMethod):
                                 intermediate_size_per_partition: int,
                                 hidden_sizes: int,
                                 params_dtype: torch.dtype) -> Dict[str, Any]:
-        param_dict = {}
-        param_dict["w13_weight_scale"] = torch.empty(
-            num_experts,
-            2 * intermediate_size_per_partition,
-            1,
-            dtype=params_dtype)
-        param_dict["w13_weight_offset"] = torch.empty(
-            num_experts,
-            2 * intermediate_size_per_partition,
-            1,
-            dtype=params_dtype)
-        param_dict["w2_weight_scale"] = torch.empty(num_experts,
-                                                    hidden_sizes,
-                                                    1,
-                                                    dtype=params_dtype)
-        param_dict["w2_weight_offset"] = torch.empty(num_experts,
-                                                     hidden_sizes,
-                                                     1,
-                                                     dtype=params_dtype)
+        param_dict = AscendW8A8DynamicFusedMoEMethod.get_dynamic_quant_param(num_experts, intermediate_size_per_partition, hidden_sizes, params_dtype)
         param_dict["w2_deq_scale"] = torch.empty(num_experts,
                                                  hidden_sizes,
                                                  dtype=torch.float32)
