b0704164d9dd33691261a1bfe990821fca89b587
Zetong Li <slippersss@126.com>
2025-12-01 18:37:56 +0800

    preserve env but set it as true by default
    Signed-off-by: Zetong Li <slippersss@126.com>

--- vllm_ascend/patch/platform/__init__.py
+++ vllm_ascend/patch/platform/__init__.py
@@ -17,12 +17,14 @@
 import os
 
 import vllm_ascend.patch.platform.patch_config  # noqa
-import vllm_ascend.patch.platform.patch_core  # noqa
 import vllm_ascend.patch.platform.patch_distributed  # noqa
 import vllm_ascend.patch.platform.patch_mamba_config  # noqa
-import vllm_ascend.patch.platform.patch_message_queue  # noqa
 import vllm_ascend.patch.platform.patch_sched_yield  # noqa
 
 if os.getenv("DYNAMIC_EPLB", "false") == "true" or os.getenv(
         "EXPERT_MAP_RECORD", "false") == "true":
     import vllm_ascend.patch.platform.patch_multiproc_executor  # noqa
+
+if os.getenv("SHM_BARRIER", "true") == "true":
+    import vllm_ascend.patch.platform.patch_core  # noqa
+    import vllm_ascend.patch.platform.patch_message_queue  # noqa
