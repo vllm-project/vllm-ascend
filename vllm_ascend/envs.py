9fafd98dddef32242db99703ed5c307ac1b1558c
YuhanBai <yuhan.bai0830@gmail.com>
2025-12-02 09:44:48 +0800

    Default VLLM_ASCEND_ENABLE_ASYNC_EXPONENTIAL switch to OFF
    Signed-off-by: YuhanBai <yuhan.bai0830@gmail.com>

--- vllm_ascend/envs.py
+++ vllm_ascend/envs.py
@@ -176,8 +176,9 @@ env_variables: Dict[str, Callable[[], Any]] = {
     # Whether to anbale dynamic EPLB
     "DYNAMIC_EPLB":
     lambda: os.getenv("DYNAMIC_EPLB", "false").lower(),
+    # Whether to enable exponential overlap with model executing.
     "VLLM_ASCEND_ENABLE_ASYNC_EXPONENTIAL":
-    lambda: int(os.getenv("VLLM_ASCEND_ENABLE_ASYNC_EXPONENTIAL", 1)),
+    lambda: bool(int(os.getenv("VLLM_ASCEND_ENABLE_ASYNC_EXPONENTIAL", '0'))),
 }
 
 # end-env-vars-definition
