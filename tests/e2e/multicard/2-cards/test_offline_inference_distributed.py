0b9098d81373b06650613a20a99534dd35e57f96
hfadzxy <starmoon_zhang@163.com>
2025-12-02 17:10:03 +0800

    [Test] DeepSeek-V3.2 Use AscendC operators
    Signed-off-by: hfadzxy <starmoon_zhang@163.com>

--- tests/e2e/multicard/2-cards/test_offline_inference_distributed.py
+++ tests/e2e/multicard/2-cards/test_offline_inference_distributed.py
@@ -212,3 +212,21 @@ def test_qwen3_dense_prefetch_mlp_weight_tp2(model):
             quantization="ascend",
     ) as vllm_model:
         vllm_model.generate_greedy(example_prompts, max_tokens)
+
+
+@patch.dict(os.environ, {"HCCL_OP_EXPANSION_MODE": "AIV"})
+@patch.dict(os.environ, {"VLLM_ASCEND_ENABLE_FLASHCOMM1": "1"})
+@patch.dict(os.environ, {"ASCEND_AGGREGATE_ENABLE": "1"})
+@patch.dict(os.environ, {"HCCL_BUFFSIZE": "2048"})
+def test_deepseek3_2_w8a8_pruning_tp2_ep():
+    example_prompts = [
+        "Hello, my name is",
+    ]
+    max_tokens = 5
+    with VllmRunner(
+            "vllm-ascend/DeepSeek-V3.2-W8A8-Pruning",
+            tensor_parallel_size=2,
+            quantization="ascend",
+            enable_expert_parallel=True,
+    ) as vllm_model:
+        vllm_model.generate_greedy(example_prompts, max_tokens)
