fef462ffbf9e4f20baf867f8df3510039c8ee054
wxsIcey <1790571317@qq.com>
2025-12-08 09:18:55 +0000

    fix
    Signed-off-by: wxsIcey <1790571317@qq.com>

--- vllm_ascend/worker/model_runner_v1.py
+++ vllm_ascend/worker/model_runner_v1.py
@@ -1445,8 +1445,8 @@ class NPUModelRunner(GPUModelRunner):
                     prefetch_stream=self.prefetch_stream,
                     model_instance=self.model,
                     weight_prefetch_method=self.weight_prefetch_method,
-                    cos=self.cos[:, :maybe_padded_num_tokens],
-                    sin=self.sin[:, :maybe_padded_num_tokens]):
+                    cos=self.cos[:, :maybe_padded_num_tokens] if self.cos is not None else None,
+                    sin=self.sin[:, :maybe_padded_num_tokens] if self.sin is not None else None):
                 self.maybe_setup_kv_connector(scheduler_output)
 
                 hidden_states = self._generate_process_reqs_hidden_states(
@@ -2172,8 +2172,8 @@ class NPUModelRunner(GPUModelRunner):
                     prefetch_stream=self.prefetch_stream,
                     model_instance=self.model,
                     weight_prefetch_method=self.weight_prefetch_method,
-                    cos=self.cos[:, :num_tokens],
-                    sin=self.sin[:, :num_tokens]):
+                    cos=self.cos[:, :num_tokens] if self.cos is not None else None,
+                    sin=self.sin[:, :num_tokens] if self.sin is not None else None):
                 hidden_states = self._generate_dummy_run_hidden_states(
                     input_ids, positions, num_tokens_padded,
                     intermediate_tensors, inputs_embeds)
