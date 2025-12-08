cda19649645a63969b4b8011b599f02d743191b9
whx-sjtu <2952154980@qq.com>
2025-12-08 11:55:47 +0800

    fix lint
    Signed-off-by: whx-sjtu <2952154980@qq.com>

--- vllm_ascend/compilation/acl_graph.py
+++ vllm_ascend/compilation/acl_graph.py
@@ -512,7 +512,7 @@ def set_graph_params(aclgraph_capture_sizes: list[int]):
     )
 
 
-def update_graph_params_workspaces(num_tokens: int, workspace: int):
+def update_graph_params_workspaces(num_tokens: int, workspace: torch.Tensor):
     global _graph_params
     if _graph_params is not None:
         _graph_params.workspaces[num_tokens] = weak_ref_tensors(workspace)
