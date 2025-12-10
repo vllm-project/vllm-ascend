17067ab3ca7001e0105ae9bd446bf4a764391a77
menogrey <1299267905@qq.com>
2025-12-10 16:17:17 +0800

    Fix lint.
    Signed-off-by: menogrey <1299267905@qq.com>

--- vllm_ascend/platform.py
+++ vllm_ascend/platform.py
@@ -30,12 +30,11 @@ from vllm_ascend.ascend_config import init_ascend_config
 from vllm_ascend.utils import refresh_block_size
 
 # isort: off
-from vllm_ascend.utils import (ASCEND_QUANTIZATION_METHOD,
-                               COMPRESSED_TENSORS_METHOD, AWQ_QUANTIZATION_METHOD, AscendDeviceType,
-                               enable_sp, get_ascend_device_type, is_vl_model,
-                               update_aclgraph_sizes,
-                               update_cudagraph_capture_sizes,
-                               update_default_aclgraph_sizes)
+from vllm_ascend.utils import (
+    ASCEND_QUANTIZATION_METHOD, COMPRESSED_TENSORS_METHOD,
+    AWQ_QUANTIZATION_METHOD, AscendDeviceType, enable_sp,
+    get_ascend_device_type, is_vl_model, update_aclgraph_sizes,
+    update_cudagraph_capture_sizes, update_default_aclgraph_sizes)
 
 # isort: on
 
