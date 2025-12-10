80777664494f284e2dda5987d28877cb2da9ac44
funanyang <985619145@qq.com>
2025-12-10 10:20:20 +0800

    fix yapf
    Signed-off-by: funanyang <985619145@qq.com>

--- vllm_ascend/sample/rejection_sampler.py
+++ vllm_ascend/sample/rejection_sampler.py
@@ -159,11 +159,12 @@ def apply_sampling_constraints(
             cu_num_draft_tokens,
             num_tokens,
         )
-    
+
     if get_ascend_device_type(
-        ) != AscendDeviceType._310P and top_p is not None and top_k is not None and 1 <= int(
-                top_k.max()) <= 1024:
-        return torch_npu.npu_top_k_top_p(logits, top_p.to(torch.bfloat16), top_k)
+    ) != AscendDeviceType._310P and top_p is not None and top_k is not None and 1 <= int(
+            top_k.max()) <= 1024:
+        return torch_npu.npu_top_k_top_p(logits, top_p.to(torch.bfloat16),
+                                         top_k)
     else:
         # NOTE(woosuk): `apply_top_k_top_p` uses sorting to calculate the mask,
         # which is slow for large vocab sizes. This may cause performance issues.
