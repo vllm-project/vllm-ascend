f39b31917dc3c31a629f29e33a5c183e87715e98
fems14 <74094523+fems14@users.noreply.github.com>
2025-12-11 21:30:07 +0800

    Update kv_transfer.py

--- vllm_ascend/distributed/kvpool/kv_transfer.py
+++ vllm_ascend/distributed/kvpool/kv_transfer.py
@@ -11,19 +11,20 @@ from vllm_ascend.distributed.kvpool.backend.backend import Backend
 
 # isort: off
 from vllm_ascend.distributed.kvpool.config_data import (ChunkedTokenDatabase,
-                                                        LasyerMultiBlockReqMeta
+                                                        LasyerMultiBlockReqMeta,
+                                                        PoolKey
                                                         )
 # isort: on
 
 
 class KVTransferThread(threading.Thread):
 
-    def __init__(self, m_store: Backend, token_database: ChunkedTokenDatabase,
-                 tp_rank: int, dcp_size: int, ready_event: threading.Event,
-                 name: str):
+    def __init__(self, m_store: Backend, token_database: ChunkedTokenDatabase, block_size: int,
+                 tp_rank: int, dcp_size: int, ready_event: threading.Event, name: str):
         super().__init__(daemon=True, name=name)
         self.m_store = m_store
         self.ready_event = ready_event
+        self.block_size = block_size
         self.tp_rank = tp_rank
         self.dcp_size = dcp_size
         self.token_database = token_database
@@ -40,7 +41,6 @@ class KVTransferThread(threading.Thread):
         block_ids: list[int],
         block_hashes: list[BlockHash],
         mask_num: int = 0,
-        is_last_chunk: Optional[bool] = None,
     ) -> torch.Tensor:
         req = ({
             "req_id": req_id,
@@ -48,7 +48,6 @@ class KVTransferThread(threading.Thread):
             "block_ids": block_ids,
             "block_hashes": block_hashes,
             "mask_num": mask_num,
-            "is_last_chunk": is_last_chunk,
         })
         self.request_queue.put(req)
 
@@ -85,47 +84,130 @@ class KVTransferThread(threading.Thread):
     def _handle_request(self, req_meta: dict[str, Any]):
         pass
 
+    def lookup(
+        self,
+        keys: list[PoolKey],
+        use_layerwise: bool,
+    ) -> int:
+        """
+        Checks the existence of KV cache of the tokens from the cache engine.
+        :param tokens: the input tokens, with shape [seq_len]
+        :return: An int indicating how many prefix tokens are cached.
+        """
+        keys_new = []
+        try:
+            if use_layerwise:
+                for key in keys:  
+                    keys_multi_layer = key.split_layers(self.num_layers)
+                    for item in keys_multi_layer:
+                        keys_new.append(item.to_string())
+            else:
+                keys_new = keys
+
+            res = self.m_store.exists(keys_new)  # type: ignore[assignment]
+
+            if use_layerwise:
+                res = self.check_all_layers_exists(res, self.num_layers)
+            for index, value in enumerate(res):  # type: ignore[arg-type]
+                if value != 1:
+                    return index
+            # all tokens where found, return the maximal end
+        except Exception as e:
+            logger.error(f"Remote connection failed in contains: {e}")
+            return 0
+        return len(keys)
+
+
 
 class KVCacheStoreSendingThread(KVTransferThread):
 
-    def __init__(self, m_store: Backend, token_database: ChunkedTokenDatabase,
-                 tp_rank: int, dcp_size: int, put_step: int,
-                 ready_event: threading.Event):
+    def __init__(self, m_store: Backend, token_database: ChunkedTokenDatabase, block_size: int,
+                 tp_rank: int, dcp_size: int, put_step: int, ready_event: threading.Event):
         super().__init__(m_store,
                          token_database,
+                         block_size,
                          tp_rank,
                          dcp_size,
                          ready_event,
                          name="KVCacheSendingThread")
         self.put_step = put_step
+    
+    def add_request(
+        self,
+        req_id: str,
+        token_len: int,
+        block_ids: list[int],
+        block_hashes: list[BlockHash],
+        is_last_chunk: Optional[bool] = None,
+    ) -> torch.Tensor:
+        req = ({
+            "req_id": req_id,
+            "token_len": token_len,
+            "block_ids": block_ids,
+            "block_hashes": block_hashes,
+            "is_last_chunk": is_last_chunk,
+        })
+        self.request_queue.put(req)
 
     def _handle_request(self, req_meta: dict[str, Any]):
         token_len = req_meta["token_len"]
-        mask_num = req_meta["mask_num"]
         block_ids = req_meta["block_ids"]
         block_hashes = req_meta["block_hashes"]
         req_id = req_meta["req_id"]
         is_last_chunk = req_meta["is_last_chunk"]
-        addr_list = []
-        size_list = []
-        key_list = []
+        starts = []
+        ends = []
+        keys = []
         for start, end, key in self.token_database.process_tokens(
-                token_len, block_hashes, mask_num):
+                token_len, block_hashes):
+            starts.append(start)
+            ends.append(end)
+            keys.append(key.to_string())
+
+
+        if not self.dcp_size > 1 :
+            starts = starts[self.tp_rank % self.put_step::self.put_step]
+            ends = ends[self.tp_rank % self.put_step::self.put_step]
+            keys = keys[self.tp_rank % self.put_step::self.put_step]
+
+          
+        if not keys:
+            if is_last_chunk:
+                self.set_finished_request(req_id)
+            return
+        
+        skip_block_num = self.lookup(keys, False)
+        logger.info(f"skip_leading_tokens:{skip_block_num}")
+
+        if skip_block_num == len(keys):
+            if is_last_chunk:
+                self.set_finished_request(req_id)
+            return
+
+        starts = starts[skip_block_num:]
+        ends = ends[skip_block_num:]
+        keys = keys[skip_block_num:]
+
+        logger.info(
+            "Storing KV cache for %d out of %d blocks "
+            "(skip_block_num=%d) for request %s",
+            len(keys),
+            token_len // self.block_size,
+            skip_block_num,
+            req_id,
+        )
+
+
+        addrs = []
+        sizes = []
+        for index, start in enumerate(starts):
             addr, size, _ = self.token_database.prepare_value(
-                start, end, block_ids)
-            key_list.append(key.to_string())
-            addr_list.append(addr)
-            size_list.append(size)
-        if self.dcp_size > 1:
-            self.m_store.put(key_list, addr_list, size_list)
-        else:
-            key_list_tp = key_list[self.tp_rank % self.put_step::self.put_step]
-            addr_list_tp = addr_list[self.tp_rank %
-                                     self.put_step::self.put_step]
-            size_list_tp = size_list[self.tp_rank %
-                                     self.put_step::self.put_step]
-            if key_list_tp:
-                self.m_store.put(key_list_tp, addr_list_tp, size_list_tp)
+                start, ends[index], block_ids)
+            addrs.append(addr)
+            sizes.append(size)
+        if keys:
+            self.m_store.put(keys, addrs, sizes)
+        
         if is_last_chunk:
             self.set_finished_request(req_id)
         self.request_queue.task_done()
@@ -133,10 +215,11 @@ class KVCacheStoreSendingThread(KVTransferThread):
 
 class KVCacheStoreRecvingThread(KVTransferThread):
 
-    def __init__(self, m_store: Backend, token_database: ChunkedTokenDatabase,
+    def __init__(self, m_store: Backend, token_database: ChunkedTokenDatabase, block_size: int,
                  tp_rank: int, dcp_size: int, ready_event: threading.Event):
         super().__init__(m_store,
                          token_database,
+                         block_size,
                          tp_rank,
                          dcp_size,
                          ready_event,
@@ -174,11 +257,12 @@ class KVCacheStoreRecvingThread(KVTransferThread):
 
 class KVCacheStoreLayerSendingThread(KVTransferThread):
 
-    def __init__(self, m_store: Backend, token_database: ChunkedTokenDatabase,
-                 tp_rank: int, dcp_size: int, put_step: int,
-                 ready_event: threading.Event, num_layers: int):
+    def __init__(self, m_store: Backend, token_database: ChunkedTokenDatabase, block_size: int,
+                 tp_rank: int, dcp_size: int, put_step: int, ready_event: threading.Event,
+                 num_layers: int):
         super().__init__(m_store,
                          token_database,
+                         block_size,
                          tp_rank,
                          dcp_size,
                          ready_event,
@@ -219,11 +303,12 @@ class KVCacheStoreLayerSendingThread(KVTransferThread):
 
 class KVCacheStoreLayerRecvingThread(KVTransferThread):
 
-    def __init__(self, m_store: Backend, token_database: ChunkedTokenDatabase,
+    def __init__(self, m_store: Backend, token_database: ChunkedTokenDatabase, block_size: int,
                  tp_rank: int, dcp_size: int, ready_event: threading.Event,
                  get_event: threading.Event):
         super().__init__(m_store,
                          token_database,
+                         block_size,
                          tp_rank,
                          dcp_size,
                          ready_event,
