afad9f7efeaf59138adaadbdef8f8745640e6c2b
zhangshushun <3265779424@qq.com>
2025-12-11 14:39:16 +0800

    Improve Code Standards
    Signed-off-by: zhangshushun <3265779424@qq.com>

--- vllm_ascend/utils.py
+++ vllm_ascend/utils.py
@@ -31,10 +31,10 @@ import torch_npu  # noqa: F401
 from packaging.version import InvalidVersion, Version
 from torch_npu.npu.streams import Event
 from vllm.logger import logger
+from vllm.sequence import IntermediateTensors
 
 import vllm_ascend.envs as envs_ascend
 from vllm_ascend.ascend_config import get_ascend_config
-from vllm.sequence import IntermediateTensors
 
 if TYPE_CHECKING:
     from vllm.config import VllmConfig
@@ -852,7 +852,8 @@ def weak_ref_tensors(
     # For IntermediateTensors used in pipeline parallelism
     if isinstance(tensors, IntermediateTensors):
         ret = IntermediateTensors({
-            key: weak_ref_tensor(val) for key, val in tensors.tensors.items()
+            key: weak_ref_tensor(val)
+            for key, val in tensors.tensors.items()
         })
         return ret
     raise ValueError("Invalid type for tensors")
