c05c18e436158d12fd6c8956cfbc296ca2b8de04
hust17yixuan <303660421@qq.com>
2025-12-16 21:24:54 +0800

    fix
    Signed-off-by: hust17yixuan <303660421@qq.com>

--- vllm_ascend/spec_decode/mtp_proposer.py
+++ vllm_ascend/spec_decode/mtp_proposer.py
@@ -778,8 +778,7 @@ class MtpProposer(Proposer):
                         hidden_states = torch.ops.vllm.maybe_pad_and_reduce(
                             hidden_states)
 
-                    if self.use_async_scheduling and attn_metadata[
-                            layer_name].decode is not None:
+                    if attn_metadata[layer_name].decode is not None:
                         for layer_name in self.attn_layer_name:
                             actual_size = len(attn_metadata[layer_name].decode.
                                               actual_seq_lengths_q)
