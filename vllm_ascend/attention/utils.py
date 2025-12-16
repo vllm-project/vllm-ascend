6a661f80ca2a461ee29dc059435a6d306fb72651
weijinqian_v1 <weijinqian@huawei.com>
2025-12-16 21:54:26 +0800

    [Refactor] unified attention pa and fia operator.
    Signed-off-by: weijinqian_v1 <weijinqian@huawei.com>

--- vllm_ascend/attention/utils.py
+++ vllm_ascend/attention/utils.py
@@ -23,7 +23,7 @@ def using_paged_attention(runtime_shape: int) -> bool:
         return False
     from vllm.config.compilation import CUDAGraphMode
     cudagraph_mode = vllm_config.compilation_config.cudagraph_mode
-    if cudagraph_mode != CUDAGraphMode.FULL_DECODE_ONLY and cudagraph_mode != CUDAGraphMode.NONE:
+    if cudagraph_mode != CUDAGraphMode.FULL_DECODE_ONLY and cudagraph_mode != CUDAGraphMode.NONE and cudagraph_mode != CUDAGraphMode.PIECEWISE:
         return False
 
     return runtime_shape in get_ascend_config().pa_shape_list
