a37724f03aa3ad9da1e3e43d4df6265046fdd4ea
hust17yixuan <303660421@qq.com>
2025-12-17 12:55:30 +0800

    test ci
    Signed-off-by: hust17yixuan <303660421@qq.com>

--- vllm_ascend/compilation/acl_graph.py
+++ vllm_ascend/compilation/acl_graph.py
@@ -341,8 +341,8 @@ def update_mla_attn_params(update_stream, forward_context, runtime_shape,
                 block_table = forward_context.attn_metadata[
                     key].decode.block_table
                 # TODO: This is a hack and should be fixed in the future.
-                # if speculative_config.disable_padded_drafter_batch:
-                #     block_table = block_table[:len(actual_seq_lengths)]
+                if speculative_config.disable_padded_drafter_batch:
+                    block_table = block_table[:len(actual_seq_lengths)]
                 seq_lens_list = seq_lens_list + [0] * (
                     len(actual_seq_lengths) - len(seq_lens_list))
             else:
