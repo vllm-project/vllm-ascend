18beee40d153fe20682621e50dcaa47324d0292f
Ascendyh <hw7osiris@outlook.com>
2025-12-19 18:05:18 +0800

    fix(test): clean code
    Signed-off-by: Ascendyh <hw7osiris@outlook.com>

--- tests/e2e/multicard/test_qwen3_next.py
+++ tests/e2e/multicard/test_qwen3_next.py
@@ -22,9 +22,9 @@ Run `pytest tests/e2e/multicard/test_qwen3_next.py`.
 """
 
 import os
-import pytest
 from unittest.mock import patch
 
+import pytest
 from modelscope import snapshot_download  # type: ignore
 
 from tests.e2e.conftest import VllmRunner
@@ -62,7 +62,7 @@ def test_qwen3_next_distributed_mp_full_decode_only_tp4():
         del vllm_model
 
 
-@pytest.mark.skip(reason="Until the precision issue is resolved.")
+@pytest.mark.skip(reason="Skip until the precision issue is resolved.")
 def test_qwen3_next_distributed_mp_eager_mtp_similarity_tp4():
     example_prompts = [
         "Hello, my name is",
