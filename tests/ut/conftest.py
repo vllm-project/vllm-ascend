6e0cdd2a87debead5cfc346195f4f998ad39a190
Meihan-chen <jcccx.cmh@gmail.com>
2025-12-22 11:46:04 +0800

    fix ut
    Signed-off-by: Meihan-chen <jcccx.cmh@gmail.com>

--- tests/ut/conftest.py
+++ tests/ut/conftest.py
@@ -1,26 +1,21 @@
-#
-# Copyright (c) 2025 Huawei Technologies Co., Ltd. All Rights Reserved.
-# Copyright 2023 The vLLM team.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
 # limitations under the License.
 # This file is a part of the vllm-ascend project.
 #
+import sys
+from unittest.mock import MagicMock
 
 from vllm_ascend.utils import adapt_patch  # noqa E402
-from vllm_ascend.utils import register_ascend_customop
+
+sys.modules['torch_npu'] = MagicMock(npu=MagicMock(current_device=MagicMock(
+    return_value=0)))
+sys.modules['torch_npu._inductor'] = MagicMock()
+
+triton_runtime = MagicMock()
+triton_runtime.driver.active.utils.get_device_properties.return_value = {
+    'num_aic': 8,
+    'num_vectorcore': 8,
+}
+sys.modules['triton.runtime'] = triton_runtime
 
 adapt_patch()
 adapt_patch(True)
-
-# register Ascend CustomOp here because uts will use this
-register_ascend_customop()
