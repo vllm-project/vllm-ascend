2bb8a676a4da1b74e0c8fcc530078839b94670a0
IncSec <1790766300@qq.com>
2025-12-24 02:21:57 +0800

    [Ops][Triton]Add qkv_gated_rmsnorm_rope triton operation for qwen3-next
    Signed-off-by: IncSec <1790766300@qq.com>

--- vllm_ascend/compilation/passes/qknorm_rope_fusion_pass.py
+++ vllm_ascend/compilation/passes/qknorm_rope_fusion_pass.py
@@ -224,6 +224,105 @@ class QKNormRopeFusionPatternWithBias:
                                 pm.fwd_only, pm_pass)
 
 
+class QKNormRopeFusionPatternWithGate:
+
+    def __init__(self,
+                 vllm_config,
+                 head_dim,
+                 rope_dim,
+                 num_heads,
+                 num_kv_heads,
+                 eps=1e-6):
+        self.vllm_config = vllm_config
+        self.head_dim = head_dim
+        self.rope_dim = rope_dim
+        self.num_heads = num_heads
+        self.num_kv_heads = num_kv_heads
+        self.q_size = self.num_heads * self.head_dim
+        self.kv_size = self.num_kv_heads * self.head_dim
+        self.eps = eps
+        self.device = vllm_config.device_config.device if vllm_config.device_config else None
+
+    def get_inputs(self):
+        T = 5
+        qkv = torch.empty(T,
+                          2 * self.q_size + 2 * self.kv_size,
+                          dtype=torch.bfloat16,
+                          device="npu")
+        q_weight = torch.empty(self.head_dim,
+                               dtype=torch.bfloat16,
+                               device="npu")
+        k_weight = torch.empty(self.head_dim,
+                               dtype=torch.bfloat16,
+                               device="npu")
+        cos = torch.empty(1,
+                          T,
+                          1,
+                          self.rope_dim,
+                          dtype=torch.bfloat16,
+                          device="npu")
+        sin = torch.empty(1,
+                          T,
+                          1,
+                          self.rope_dim,
+                          dtype=torch.bfloat16,
+                          device="npu")
+        return [qkv, q_weight, k_weight, cos, sin]
+
+    def register(self, pm_pass: PatternMatcherPass):
+
+        def pattern(qkv: torch.Tensor, q_weight: torch.Tensor,
+                    k_weight: torch.Tensor, cos: torch.Tensor,
+                    sin: torch.Tensor):
+            # split q gate k v
+            q_gate, k, v = qkv.split([2 * self.q_size, self.kv_size, self.kv_size],
+                                dim=-1)
+            orig_shape = q_gate.shape[:-1]
+            q_gate = q_gate.view(*orig_shape, self.num_heads, -1)
+            q, gate = torch.chunk(q_gate, 2, dim=-1)
+            q = q.reshape(*orig_shape, -1)
+            gate = gate.reshape(*orig_shape, -1)
+
+            # q norm & k norm
+            q_norm, _ = torch.ops.npu.npu_rms_norm(q.view(-1, self.num_heads, self.head_dim), 1.0 + q_weight, self.eps)
+            q_norm_flat = q_norm.view(-1, self.num_heads * self.head_dim)
+            k_norm, _ = torch.ops.npu.npu_rms_norm(k.view(-1, self.num_kv_heads, self.head_dim), 1.0 + k_weight, self.eps)
+            k_norm_flat = k_norm.view(-1, self.num_kv_heads * self.head_dim)
+
+            # q rope & k rope
+            q_shape, k_shape = q_norm_flat.shape, k_norm_flat.shape
+            q_rope_in = q_norm_flat.view(q_norm_flat.shape[0], -1, self.head_dim)
+            k_rope_in = k_norm_flat.view(k_norm_flat.shape[0], -1, self.head_dim)
+            q_rope, k_rope = torch.ops.vllm.rope_forward(
+                q_rope_in, k_rope_in, cos, sin, self.rope_dim, True)
+            
+            q_rope = q_rope.view(q_shape)
+            k_rope = k_rope.view(k_shape)
+
+            return q_rope, gate, k_rope, v
+
+
+        def replacement(qkv: torch.Tensor, q_weight: torch.Tensor,
+                        k_weight: torch.Tensor, cos: torch.Tensor,
+                        sin: torch.Tensor):
+            results = torch.ops.vllm.qkv_gated_rmsnorm_rope(
+                input=qkv,
+                sin=sin,
+                cos=cos,
+                q_weight=q_weight,
+                k_weight=k_weight,
+                q_hidden_size=self.q_size,
+                kv_hidden_size=self.kv_size,
+                head_dim=self.head_dim,
+                rope_dim=self.rope_dim,
+                eps=self.eps)
+
+            return results
+
+        pm.register_replacement(pattern, replacement, self.get_inputs(),
+                                pm.fwd_only, pm_pass)
+
+
 class QKNormRopeFusionPass(VllmInductorPass):
     """
     A pass for fusing QKV split and RMSNorm operations into a single qk_rmsnorm operator.
@@ -250,25 +349,35 @@ class QKNormRopeFusionPass(VllmInductorPass):
             )
             return
         layer = next(iter(attn_layers.values()))
+        rope_dim = int(layer.head_size * getattr(vllm_config.model_config.hf_text_config, "partial_rotary_factor", 1.0))
         for epsilon in [1e-6, 1e-5]:
-            if layer.head_size != 128:
-                logger.debug(
-                    "QKNorm and Rope fusion not enabled: head_dim %d is not equal of 128",
-                    layer.head_size)
-                continue
-            QKNormRopeFusionPattern(vllm_config=vllm_config,
-                                    head_dim=layer.head_size,
-                                    num_heads=layer.num_heads,
-                                    num_kv_heads=layer.num_kv_heads,
-                                    eps=epsilon).register(
-                                        self.pattern_match_passes)
+            # if layer.head_size != 128:
+            #     logger.debug(
+            #         "QKNorm and Rope fusion not enabled: head_dim %d is not equal of 128",
+            #         layer.head_size)
+            #     continue
+            if layer.head_size == 128 and rope_dim == 128:
+                QKNormRopeFusionPattern(vllm_config=vllm_config,
+                                        head_dim=layer.head_size,
+                                        num_heads=layer.num_heads,
+                                        num_kv_heads=layer.num_kv_heads,
+                                        eps=epsilon).register(
+                                            self.pattern_match_passes)
 
-            QKNormRopeFusionPatternWithBias(vllm_config=vllm_config,
-                                            head_dim=layer.head_size,
-                                            num_heads=layer.num_heads,
-                                            num_kv_heads=layer.num_kv_heads,
-                                            eps=epsilon).register(
-                                                self.pattern_match_passes)
+                QKNormRopeFusionPatternWithBias(vllm_config=vllm_config,
+                                                head_dim=layer.head_size,
+                                                num_heads=layer.num_heads,
+                                                num_kv_heads=layer.num_kv_heads,
+                                                eps=epsilon).register(
+                                                    self.pattern_match_passes)
+            elif layer.head_size == 256:
+                QKNormRopeFusionPatternWithGate(vllm_config=vllm_config,
+                                                head_dim=layer.head_size,
+                                                rope_dim=rope_dim,
+                                                num_heads=layer.num_heads,
+                                                num_kv_heads=layer.num_kv_heads,
+                                                eps=epsilon).register(
+                                                    self.pattern_match_passes)
 
     def __call__(self, graph: torch.fx.Graph):
         self.begin()
