8f8f63113bdab4d10314b39e98fae40842a33c1b
nsdie <yeyifan@huawei.com>
2025-12-25 18:05:15 +0800

    cleancode
    Signed-off-by: nsdie <yeyifan@huawei.com>

--- vllm_ascend/attention/attention_v1.py
+++ vllm_ascend/attention/attention_v1.py
@@ -43,9 +43,9 @@ from vllm_ascend.compilation.acl_graph import (get_graph_params,
 from vllm_ascend.utils import (AscendDeviceType, get_ascend_device_type,
                                weak_ref_tensors)
 
-
 # default max value of sliding window size
 SWA_INT_MAX = 2147483647
+
 
 @register_backend(AttentionBackendEnum.CUSTOM, "ASCEND")
 class AscendAttentionBackend(AttentionBackend):
