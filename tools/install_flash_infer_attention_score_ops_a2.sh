3dc446d8b64fb8e59aec4604be42497f323e6520
MengqingCao <cmq0113@163.com>
2025-12-26 13:15:49 +0000

    [Build] Add fused_infer_attention_score with flash decoding
    Signed-off-by: MengqingCao <cmq0113@163.com>

---
+++ tools/install_flash_infer_attention_score_ops_a2.sh
@@ -0,0 +1,13 @@
+cd /vllm-workspace
+# download fused_infer_attention_score related source files
+wget fused_infer_attention_score_a2_$(uname -i).tar
+tar -xvf a.tar ./fia_fd
+
+# replace fused_infer_attention_score operation files
+cd $ASCEND_TOOLKIT_HOME/opp/built-in/op_impl/ai_core/tbe/kernel/ascend910b
+rm -rf fused_infer_attention_score
+cp /vllm-workspace/fia_fd/fused_infer_attention_score .
+
+# replace related so
+cd $ASCEND_TOOLKIT_HOME/opp/built-in/op_impl/ai_core/tbe/op_tiling/lib/linux/$(uname -i)
+cp /vllm-workspace/fia_fd/*.so .
