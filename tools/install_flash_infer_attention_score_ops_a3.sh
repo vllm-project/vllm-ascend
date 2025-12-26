747f7afc137f935451cf4ea5f60a4d81ae213c7f
MengqingCao <cmq0113@163.com>
2025-12-26 13:33:27 +0000

    rm original so
    Signed-off-by: MengqingCao <cmq0113@163.com>

--- tools/install_flash_infer_attention_score_ops_a3.sh
+++ tools/install_flash_infer_attention_score_ops_a3.sh
@@ -1,3 +1,23 @@
+#!/bin/bash
+
+#
+# Copyright (c) 2025 Huawei Technologies Co., Ltd. All Rights Reserved.
+# Copyright 2023 The vLLM team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# This file is a part of the vllm-ascend project.
+#
+
 cd /vllm-workspace
 # download fused_infer_attention_score related source files
 wget fused_infer_attention_score_a3_$(uname -i).tar
@@ -10,4 +30,5 @@ cp -r /vllm-workspace/fused_infer_attention_score_a3_$(uname -i)/fused_infer_att
 
 # replace related so
 cd $ASCEND_TOOLKIT_HOME/opp/built-in/op_impl/ai_core/tbe/op_tiling/lib/linux/$(uname -i)
+rm libopmaster_ct.so libopmaster_rt2.0.so liboptiling.so
 cp /vllm-workspace/fused_infer_attention_score_a3_$(uname -i)/*.so .
