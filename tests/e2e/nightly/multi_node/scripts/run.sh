180a428d6859f4f895bc181f30d2ad2ce9b38693
MrZ20 <2609716663@qq.com>
2025-12-29 15:29:29 +0800

    add nightly test
    Signed-off-by: MrZ20 <2609716663@qq.com>

--- tests/e2e/nightly/multi_node/scripts/run.sh
+++ tests/e2e/nightly/multi_node/scripts/run.sh
@@ -157,6 +157,18 @@ kill_npu_processes() {
   sleep 4
 }
 
+upgrade_vllm_ascend_scr() {
+    # Fix me(Potabk): Remove this once our image build use 
+    # The separate architecture build process currently suffers from errors during cross-compilation
+    # causing the image to fail to build correctly. 
+    # This results in the nightly test code not being the latest version.
+    cd "$WORKSPACE/vllm-ascend"
+    # git pull origin main
+    git fetch origin pull/5386/head:pr-5386
+    git checkout pr-5386
+    
+}
+
 run_tests_with_log() {
     set +e
     kill_npu_processes
@@ -181,6 +193,7 @@ main() {
     if [[ "$CONFIG_YAML_PATH" == *"DeepSeek-V3_2-Exp-bf16.yaml" ]]; then
         install_extra_components
     fi
+    upgrade_vllm_ascend_scr
     cd "$WORKSPACE/vllm-ascend"
     run_tests_with_log
 }
