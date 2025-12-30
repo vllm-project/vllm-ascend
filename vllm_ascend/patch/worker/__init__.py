96be8ce57470ae25cee1fe8869c590b33e04b4dd
wxsIcey <1790571317@qq.com>
2025-12-30 13:03:44 +0000

    add patch
    Signed-off-by: wxsIcey <1790571317@qq.com>

--- vllm_ascend/patch/worker/__init__.py
+++ vllm_ascend/patch/worker/__init__.py
@@ -21,7 +21,9 @@ if HAS_TRITON:
     import vllm_ascend.patch.worker.patch_triton
 
 # isort: off
+import vllm_ascend.ops.register_custom_ops  # noqa
 import vllm_ascend.patch.platform.patch_sched_yield  # noqa
+import vllm_ascend.patch.worker.patch_unquantized_gemm  # noqa
 import vllm_ascend.patch.worker.patch_bert  # noqa
 import vllm_ascend.patch.worker.patch_distributed  # noqa
 import vllm_ascend.patch.worker.patch_deepseek  # noqa
