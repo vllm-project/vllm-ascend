0f31b2b79bd81e918bb519c86f29bdf26d1664e9
Zetong Li <slippersss@126.com>
2026-01-08 22:34:28 +0800

    fix ut
    Signed-off-by: Zetong Li <slippersss@126.com>

--- tests/ut/spec_decode/test_eagle_proposer.py
+++ tests/ut/spec_decode/test_eagle_proposer.py
@@ -107,6 +107,7 @@ class TestEagleProposerLoadModel(TestBase):
         self.vllm_config.speculative_config.method = "eagle"
         self.device = torch.device("cpu")
         self.runner = MagicMock()
+        self.runner._sync_metadata_across_dp.return_value = (8, 8, False)
 
         self.vllm_config.cache_config.block_size = 16
         self.vllm_config.scheduler_config.max_num_batched_tokens = 1024
@@ -165,7 +166,7 @@ class TestEagleProposerLoadModel(TestBase):
 
         self.proposer.load_model(mock_model)
         mock_get_model.assert_called_once()
-        self.assertEqual(self.proposer.attn_layer_name, ["layer3"])
+        self.assertEqual(self.proposer.attn_layer_names, ["layer3"])
         self.assertIs(self.proposer.model.model.embed_tokens,
                       mock_model.model.embed_tokens)
 
@@ -196,7 +197,7 @@ class TestEagleProposerLoadModel(TestBase):
 
         self.assertIsNot(self.proposer.model.model.embed_tokens,
                          mock_model.model.embed_tokens)
-        self.assertEqual(self.proposer.attn_layer_name, ["layer2"])
+        self.assertEqual(self.proposer.attn_layer_names, ["layer2"])
 
     @patch(
         "vllm_ascend.spec_decode.eagle_proposer.get_layers_from_vllm_config")
@@ -239,6 +240,7 @@ class TestEagleProposerDummyRun(TestBase):
         self.vllm_config.speculative_config.num_speculative_tokens = 4
         self.device = torch.device("cpu")
         self.runner = MagicMock()
+        self.runner._sync_metadata_across_dp.return_value = (8, 8, False)
 
         self.vllm_config.cache_config.block_size = 16
         self.vllm_config.scheduler_config.max_num_batched_tokens = 1024
@@ -246,6 +248,7 @@ class TestEagleProposerDummyRun(TestBase):
         self.vllm_config.model_config.dtype = torch.float16
         self.vllm_config.model_config.max_model_len = 2048
         self.vllm_config.model_config.uses_mrope = False
+        self.vllm_config.model_config.use_mla = False
         self.vllm_config.speculative_config.speculative_token_tree = str([
             (i + 1) * (0, ) for i in range(4)
         ])
@@ -264,6 +267,9 @@ class TestEagleProposerDummyRun(TestBase):
                                       runner=self.runner)
         self.proposer.model = MagicMock()
         self.proposer.update_stream = MagicMock()
+        self.proposer.use_sparse = False
+        self.proposer.pcp_size = 1
+        self.proposer.dcp_size = 1
 
     def tearDown(self):
         self.mock_cpugpubuffer.stop()
@@ -308,7 +314,6 @@ class TestEagleProposerDummyRun(TestBase):
                                 in_graph_capturing=True,
                                 aclgraph_runtime_mode=CUDAGraphMode.FULL)
         self.assertTrue(self.proposer.model.call_count == 4)
-        mock_update_attn_params.assert_not_called()
         self.proposer.use_cuda_graph = last_use_cuda_graph
 
     @patch("vllm_ascend.spec_decode.eagle_proposer.update_attn_params")
@@ -345,6 +350,7 @@ class TestEagleProposerHelperMethods(TestBase):
         self.runner.input_batch.req_ids = [0, 1, 2]
         self.runner.arange_np = np.arange(10)
         self.runner.input_batch.num_reqs = 3
+        self.runner._sync_metadata_across_dp.return_value = (8, 8, False)
 
         self.vllm_config.cache_config.block_size = 16
         self.vllm_config.scheduler_config.max_num_batched_tokens = 1024
