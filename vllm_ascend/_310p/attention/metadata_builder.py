115fdb7490e947f61687dde2ef691dff3a42b667
Tflowers-0129 <2906339855@qq.com>
2026-01-10 16:45:17 +0800

    fix mypy test for runner/attnmask/metadatabuilder
    Signed-off-by: Tflowers-0129 <2906339855@qq.com>

--- vllm_ascend/_310p/attention/metadata_builder.py
+++ vllm_ascend/_310p/attention/metadata_builder.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import torch
+from typing import Any
 from vllm.config import VllmConfig
 from vllm.v1.kv_cache_interface import AttentionSpec
 
@@ -17,5 +18,5 @@ class AscendAttentionMetadataBuilder310P(_BaseBuilder):
         device: torch.device,
     ):
         super().__init__(kv_cache_spec, layer_names, vllm_config, device)
-
-        self.attn_mask_builder = AttentionMaskBuilder(self.device)
+        
+        self.attn_mask_builder: Any = AttentionMaskBuilder(self.device)
