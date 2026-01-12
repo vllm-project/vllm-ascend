9fbdc435e24111a2aab361c51c6115cf47a94de6
ZT-AIA <1028681969@qq.com>
2026-01-12 20:22:58 +0800

    repair
    Signed-off-by: ZT-AIA <1028681969@qq.com>

--- vllm_ascend/patch/platform/patch_lora_model_manager.py
+++ vllm_ascend/patch/platform/patch_lora_model_manager.py
@@ -7,7 +7,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0 
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,24 +17,21 @@
 # Adapted from vllm/model_executor/models/qwen2_vl.py
 # This file is a part of the vllm-ascend project.
 
-import torch
 import math
 
+import torch
 from vllm.config.lora import LoRAConfig
 from vllm.lora.layers import BaseLayerWithLoRA, LoRAMapping
 from vllm.lora.lora_model import LoRAModel
 from vllm.lora.model_manager import LoRAModelManager
 from vllm.lora.punica_wrapper import get_punica_wrapper
-from vllm.lora.utils import (
-    get_supported_lora_modules,
-    is_moe_model,
-    process_packed_modules_mapping,
-)
+from vllm.lora.utils import (get_supported_lora_modules, is_moe_model,
+                             process_packed_modules_mapping)
 from vllm.model_executor.models import SupportsLoRA, supports_multimodal
 from vllm.model_executor.models.interfaces import is_pooling_model
 
 
-class  AscendLoRAModelManager:
+class AscendLoRAModelManager:
     """A manager that manages multiple LoRA-fine-tuned models."""
 
     def __init__(
@@ -65,9 +62,11 @@ class  AscendLoRAModelManager:
         self.lora_config = lora_config
         self.device = device
         self.max_num_seqs = max_num_seqs
-        assert self.capacity >= self.lora_slots # type:ignore
+        assert self.capacity >= self.lora_slots  # type:ignore
         self.max_num_batched_tokens = math.ceil(max_num_batched_tokens / 8) * 8
-        self.lora_index_to_id: list[int | None] = [None] * self.lora_slots # type:ignore
+        self.lora_index_to_id: list[int | None] = [
+            None
+        ] * self.lora_slots  # type:ignore
         self.vocab_size = vocab_size
         self.punica_wrapper = get_punica_wrapper(
             max_num_batched_tokens,
@@ -80,21 +79,22 @@ class  AscendLoRAModelManager:
         assert self.supported_lora_modules, "No supported LoRA modules found in"
         f" {self.model.__class__.__name__}."
 
-        self.packed_modules_mapping = process_packed_modules_mapping(self.model)
+        self.packed_modules_mapping = process_packed_modules_mapping(
+            self.model)
         # Used to indicate whether the model is a multimodal model
         self.supports_mm: bool = (
             supports_multimodal(self.model)
             # In case the model only supports LoRA for
             # text modules (e.g. ChatGLM)
-            and hasattr(self.model, "get_mm_mapping")
-        )
+            and hasattr(self.model, "get_mm_mapping"))
         self.is_pooling_model = is_pooling_model(self.model)
         self.packed_modules: dict[str, list[str]] = {}
         self.modules: dict[str, BaseLayerWithLoRA] = {}
         # Dict instead of a set for compatibility with LRUCache.
         self._last_mapping: LoRAMapping | None = None
-        self._is_3d_moe_model = is_moe_model(self.model) and self.model.is_3d_moe_weight
-        self._create_lora_modules() # type:ignore
+        self._is_3d_moe_model = is_moe_model(
+            self.model) and self.model.is_3d_moe_weight
+        self._create_lora_modules()  # type:ignore
 
         self.model.lora_manager = self
 
