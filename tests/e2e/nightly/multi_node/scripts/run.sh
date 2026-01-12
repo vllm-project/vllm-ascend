51fdfecc0c38f549c09c8146c26a960bb332408d
Meihan-chen <jcccx.cmh@gmail.com>
2026-01-13 00:36:33 +0800

    update triton ver in 0113
    Signed-off-by: Meihan-chen <jcccx.cmh@gmail.com>

--- tests/e2e/nightly/multi_node/scripts/run.sh
+++ tests/e2e/nightly/multi_node/scripts/run.sh
@@ -150,7 +150,7 @@ install_triton_ascend() {
 
     export PATH=/usr/local/Ascend/tools/bishengir/bin:$PATH
     which bishengir-compile
-    python3 -m pip install -i https://test.pypi.org/simple/ triton-ascend==3.2.0.dev20260112
+    python3 -m pip install -i https://test.pypi.org/simple/ triton-ascend==3.2.0.dev20260113
     echo "====> Triton ascend installation completed"
 }
 
