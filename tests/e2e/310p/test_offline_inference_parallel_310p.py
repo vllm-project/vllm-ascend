2ff97a1d4a665b20a762197e4a3fdd6c9fbc72bb
Tflowers-0129 <2906339855@qq.com>
2026-01-16 14:48:59 +0800

    fix 310p e2e tp test bugs
    Signed-off-by: Tflowers-0129 <2906339855@qq.com>

--- tests/e2e/310p/test_offline_inference_parallel_310p.py
+++ tests/e2e/310p/test_offline_inference_parallel_310p.py
@@ -5,7 +5,6 @@ from tests.e2e.conftest import VllmRunner
 
 @pytest.mark.parametrize("dtype", ["float16"])
 @pytest.mark.parametrize("max_tokens", [5])
-@pytest.skip("310p does not support parallel inference now. Fix me")
 def test_models(dtype: str, max_tokens: int) -> None:
     example_prompts = [
         "Hello, my name is",
