b87fb18c07266f1355985eb3b456d7dcd9ff0b2a
lilinsiman <lilinsiman@gmail.com>
2026-01-19 14:54:55 +0800

    Suit for incoming changes
    positions to _get_positions
    
    Signed-off-by: lilinsiman <lilinsiman@gmail.com>

--- vllm_ascend/spec_decode/eagle_proposer.py
+++ vllm_ascend/spec_decode/eagle_proposer.py
@@ -550,7 +550,7 @@ class EagleProposer(VllmEagleProposer):
             common_attn_metadata.seq_lens_cpu[:batch_size].masked_fill_(
                 exceeds_mask, 1)
             common_attn_metadata.num_computed_tokens_cpu[:batch_size] += 1
-            common_attn_metadata.positions[:batch_size].copy_(clamped_positions)
+            common_attn_metadata._get_positions[:batch_size].copy_(clamped_positions)
 
             if self.attn_metadata_builder is None:
                 attn_metadata_builder = self._get_attention_metadata_builder()
