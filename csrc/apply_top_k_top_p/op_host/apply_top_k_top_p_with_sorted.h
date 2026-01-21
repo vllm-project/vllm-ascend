d37913c998862a665c9bb24ee6eeedd7d122d089
linfeng-yuan <1102311262@qq.com>
2026-01-21 18:31:34 +0800

    support apply_top_k_top_p op
    Signed-off-by: linfeng-yuan <1102311262@qq.com>

---
+++ csrc/apply_top_k_top_p/op_host/apply_top_k_top_p_with_sorted.h
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file apply_top_k_top_p_with_sorted.h
+ * \brief
+ */
+#ifndef OP_API_INC_LEVEL0_OP_APPLY_TOP_K_TOP_P_WITH_SORTED_OP_H_
+#define OP_API_INC_LEVEL0_OP_APPLY_TOP_K_TOP_P_WITH_SORTED_OP_H_
+
+#include "opdev/op_executor.h"
+
+namespace l0op {
+const aclTensor* ApplyTopKTopPWithSorted(const aclTensor* sortedValue, const aclTensor* sortedIndices,
+                                         const aclTensor* p, const aclTensor* k, aclOpExecutor* executor);
+}
+#endif // OP_API_INC_LEVEL0_OP_APPLY_TOP_K_TOP_P_WITH_SORTED_OP_H_
+
