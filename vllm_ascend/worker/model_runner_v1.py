4f2edf4d28c06720c870cf4753e47d896f6e45e6
Meihan-chen <jcccx.cmh@gmail.com>
2026-01-22 20:35:38 +0800

    fix FusedMoEParallelConfig
    Signed-off-by: Meihan-chen <jcccx.cmh@gmail.com>

--- vllm_ascend/worker/model_runner_v1.py
+++ vllm_ascend/worker/model_runner_v1.py
@@ -103,7 +103,7 @@ from vllm_ascend.utils import (AscendDeviceType, ProfileExecuteDuration,
                                enable_sp, get_ascend_device_type,
                                is_drafter_moe_model, is_moe_model,
                                lmhead_tp_enable, maybe_trans_nz,
-                               set_weight_prefetch_method)
+                               set_weight_prefetch_method, vllm_version_is)
 from vllm_ascend.worker.npu_input_batch import NPUInputBatch
 from vllm_ascend.worker.pcp_utils import PCPManager
 
