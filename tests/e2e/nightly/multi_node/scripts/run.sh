9f00bbead24a5a4ff4de442f714c747d4407c1d0
wxsIcey <1790571317@qq.com>
2026-01-22 09:34:52 +0000

    fix
    Signed-off-by: wxsIcey <1790571317@qq.com>

--- tests/e2e/nightly/multi_node/scripts/run.sh
+++ tests/e2e/nightly/multi_node/scripts/run.sh
@@ -196,6 +196,7 @@ main() {
     check_npu_info
     check_and_config
     show_vllm_info
+    install_pypto
     install_triton_ascend
     if [[ "$CONFIG_YAML_PATH" == *"DeepSeek-V3_2-Exp-bf16.yaml" ]]; then
         install_extra_components
