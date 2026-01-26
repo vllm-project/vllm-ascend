aced535f4e2b00f8155f44bd8340e65d41c3d133
chencangtao <chencangtao@huawei.com>
2026-01-26 11:00:47 +0800

    make switch open test
    Signed-off-by: chencangtao <chencangtao@huawei.com>

--- vllm_ascend/ascend_config.py
+++ vllm_ascend/ascend_config.py
@@ -21,7 +21,6 @@ from vllm.utils.math_utils import cdiv
 
 if TYPE_CHECKING:
     from vllm.config import VllmConfig
-    from vllm.config.compilation import CUDAGraphMode
 
 
 class AscendConfig:
@@ -256,6 +255,8 @@ class NpugraphExConfig:
         """
         self.enable = enable
         self.enable_static_kernel = enable_static_kernel
+
+        from vllm.config.compilation import CUDAGraphMode
         compilation_config = vllm_config.compilation_config
         if compilation_config.cudagraph_mode in (CUDAGraphMode.FULL_DECODE_ONLY, CUDAGraphMode.FULL):
             self.enable = self.enable and True
