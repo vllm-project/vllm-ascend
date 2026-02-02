0222290621b262e96bc2f505a7444cd2fc37f482
lidenghui <lidenghui1110@gmail.com>
2026-02-02 12:46:40 +0800

    rerun ci
    Signed-off-by: lidenghui <lidenghui1110@gmail.com>

--- csrc/transpose_kv_cache_by_block/op_kernel/common.h
+++ csrc/transpose_kv_cache_by_block/op_kernel/common.h
@@ -7,7 +7,8 @@ using namespace AscendC;
 template <typename T>
 __aicore__ inline __gm__ T* GetTensorAddr(uint16_t index, GM_ADDR tensorPtr) {
     __gm__ uint64_t* dataAddr = reinterpret_cast<__gm__ uint64_t*>(tensorPtr);
-    uint64_t tensorPtrOffset = *dataAddr;  // The offset of the data address from the first address.
+    // The offset of the data address from the first address.
+    uint64_t tensorPtrOffset = *dataAddr;
     // Moving 3 bits to the right means dividing by sizeof(uint64 t).
     __gm__ uint64_t* retPtr = dataAddr + (tensorPtrOffset >> 3);
     return reinterpret_cast<__gm__ T*>(*(retPtr + index));
