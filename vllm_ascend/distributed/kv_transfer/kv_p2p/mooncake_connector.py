818bd4bb51cb130511aec822dc4d88dd266b4a92
liziyu <liziyu16@huawei.com>
2026-02-02 09:22:28 +0800

    fix lint
    Signed-off-by: liziyu <liziyu16@huawei.com>

--- vllm_ascend/distributed/kv_transfer/kv_p2p/mooncake_connector.py
+++ vllm_ascend/distributed/kv_transfer/kv_p2p/mooncake_connector.py
@@ -49,7 +49,6 @@ from vllm.v1.request import RequestStatus
 from vllm_ascend.ascend_config import get_ascend_config, init_ascend_config
 from vllm_ascend.distributed.kv_transfer.utils.mooncake_transfer_engine import global_te
 from vllm_ascend.distributed.kv_transfer.utils.utils import get_transfer_timeout_value
-from vllm_ascend.utils import is_vl_model
 
 # isort: off
 if TYPE_CHECKING:
