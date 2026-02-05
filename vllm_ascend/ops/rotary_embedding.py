c15f63cdb54fff877f7ad6e2ff08b7c4eabc51d6
GoCHug <93277779+GoCHug@users.noreply.github.com>
2026-02-05 23:17:01 +0800

    Add support for rotary_dim in rotary_embedding
    Signed-off-by: GoCHug <93277779+GoCHug@users.noreply.github.com>

--- vllm_ascend/ops/rotary_embedding.py
+++ vllm_ascend/ops/rotary_embedding.py
@@ -90,6 +90,8 @@ def set_cos_and_sin(vllm_config, max_num_reqs, decode_token_per_req, dtype,
         if hasattr(model_config.hf_text_config, "partial_rotary_factor"):
             rope_dim = int(rope_dim *
                            model_config.hf_text_config.partial_rotary_factor)
+        elif hasattr(model_config.hf_text_config, "rotary_dim"):
+            rope_dim = int(model_config.hf_text_config.rotary_dim)
         _cos = torch.ones(1,
                           max_num_batched_tokens,
                           1,
