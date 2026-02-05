a0335f3e6f37ca7519b94c5a262d8159c476f705
pu-zhe <zpuaa@outlook.com>
2026-02-05 22:35:28 +0800

    cleancode
    Signed-off-by: pu-zhe <zpuaa@outlook.com>

--- vllm_ascend/_310p/ops/activation.py
+++ vllm_ascend/_310p/ops/activation.py
@@ -23,8 +23,6 @@ from vllm_ascend.ops.activation import AscendSiluAndMul
 
 class AscendSiluAndMul310(AscendSiluAndMul):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        torch.ops.vllm.maybe_prefetch_mlp_down_proj(x)
         h = x.shape[-1] // 2
         out = F.silu(x[..., :h]) * x[..., h:]
-        torch.ops.vllm.maybe_wait_prefetch_done(out)
         return out
