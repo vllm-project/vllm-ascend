05d8e67d2a11feb9d17765ec0ab7e851191f9b8a
Angazenn <supperccell@163.com>
2026-02-05 17:18:46 +0800

    bugfix
    Signed-off-by: Angazenn <supperccell@163.com>

--- vllm_ascend/compilation/passes/qknorm_rope_fusion_pass.py
+++ vllm_ascend/compilation/passes/qknorm_rope_fusion_pass.py
@@ -76,7 +76,7 @@ class QKNormRopeFusionPattern:
 
             q_flat = q_norm_out.view(q.shape)
             k_flat = k_norm_out.view(k.shape)
-            q_rope, k_rope = torch.ops.vllm.rope_forward_oot(
+            q_rope, k_rope = torch.ops.vllm.rope_forward(
                 cos_sin_cache,
                 positions,
                 q_flat,
@@ -166,7 +166,7 @@ class QKNormRopeFusionPatternWithBias:
 
             q_flat = q_normed.view(q.shape)
             k_flat = k_normed.view(k.shape)
-            q_rope, k_rope = torch.ops.vllm.rope_forward_oot(
+            q_rope, k_rope = torch.ops.vllm.rope_forward(
                 cos_sin_cache,
                 positions,
                 q_flat,
