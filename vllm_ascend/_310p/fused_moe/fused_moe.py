0b447aab300f1dd5c43763eb8f5af6cfb02ef1ee
pu-zhe <zpuaa@outlook.com>
2026-02-06 15:21:37 +0800

    submit
    Signed-off-by: pu-zhe <zpuaa@outlook.com>

--- vllm_ascend/_310p/fused_moe/fused_moe.py
+++ vllm_ascend/_310p/fused_moe/fused_moe.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-from collections.abc import Callable, Optional
+from typing import Callable, Optional
 
 import torch
 from vllm.distributed import get_dp_group, get_ep_group, get_tp_group
