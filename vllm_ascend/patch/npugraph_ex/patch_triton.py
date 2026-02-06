291414b2573131faf49f520ee5d0c92a9eda871f
chencangtao <chencangtao@huawei.com>
2026-02-06 14:27:54 +0800

    test patch work Signed-off-by: chencangtao <chencangtao@huawei.com>
    Signed-off-by: chencangtao <chencangtao@huawei.com>

--- vllm_ascend/patch/npugraph_ex/patch_triton.py
+++ vllm_ascend/patch/npugraph_ex/patch_triton.py
@@ -15,9 +15,44 @@
 # limitations under the License.
 #
 
+import torch
+from torch._subclasses.fake_tensor import FakeTensor
 import torchair
-from torchair.core._concrete_graph import ValuePack, _is_symlist
+from torchair.core._concrete_graph import _is_symlist
 from torchair.npu_fx_compiler import _unpack_meta_list
+
+
+class ValuePack:
+    def __init__(self, meta, npu_meta=None) -> None:
+        self._meta = meta
+        self._npu_meta = meta if npu_meta is None else npu_meta
+
+    @property
+    def meta(self):
+        return self._meta
+
+    @property
+    def npu(self):
+        return self._npu_meta
+
+    def __getitem__(self, key):
+        if isinstance(self._meta, dict):
+            return self._meta.get(key)
+        raise ValueError(f"Unsupported meta type for ValuePack __getitem__, key:{key}, type: {type(self._meta)}")
+
+    def __repr__(self) -> str:
+        if isinstance(self._meta, FakeTensor):
+            meta_str = f"FakeTensor(dtype={self._meta.dtype}, size={list(self._meta.size())}"
+        elif isinstance(self._meta, torch.Tensor):
+            meta_str = f"torch.Tensor(dtype={self._meta.dtype}, size={list(self._meta.size())}"
+        elif isinstance(self._meta, torch.SymInt):
+            meta_str = f"torch.SymInt({self._meta})"
+        else:
+            try:
+                meta_str = f"{type(self._meta)}({self._meta})"
+            except Exception:
+                meta_str = f"{type(self._meta)}"
+        return f'Pack(meta:{meta_str} npu:{self._npu_meta})'
 
 
 def _unpack_meta(args, kwargs):
@@ -70,12 +105,6 @@ def _unpack_npu(self, args, kwargs):
     return unpacked, unpacked_kwargs
 
 
-def __getitem__(self, key):
-    if isinstance(self._meta, dict):
-        return self._meta.get(key)
-    raise ValueError(f"Unsupported meta type for ValuePack __getitem__, key:{key}, type: {type(self._meta)}")
-
-
-torchair.core._concrete_graph.ValuePack.__getitem__ = __getitem__
+torchair.core._concrete_graph.ValuePack = ValuePack
 torchair.npu_fx_compiler._unpack_meta = _unpack_meta
 torchair.npu_fx_compiler._NpuGraphConverter._unpack_npu = _unpack_npu
