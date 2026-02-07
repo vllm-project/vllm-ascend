5d88450de8a56fdc8e2eae279516a21bcca07a37
Ronald1995 <ronaldautomobile@163.com>
2026-02-07 11:33:49 +0800

    fix add_request error
    Signed-off-by: Ronald1995 <ronaldautomobile@163.com>

--- vllm_ascend/worker/v2/states.py
+++ vllm_ascend/worker/v2/states.py
@@ -62,8 +62,6 @@ class AscendRequestState(RequestState):
         prompt_len,
         prefill_token_ids,
         num_computed_tokens,
-        sampling_params,
-        lora_request,
     ):
 
         super().add_request(
@@ -71,8 +69,6 @@ class AscendRequestState(RequestState):
             prompt_len,
             prefill_token_ids,
             num_computed_tokens,
-            sampling_params,
-            lora_request,
         )
         req_idx = self.req_id_to_index[req_id]
         self.num_computed_tokens_cpu[req_idx] = num_computed_tokens
