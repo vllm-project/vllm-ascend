0f80f1a7150ea448ad3608b296a1cc365261947e
Angazenn <supperccell@163.com>
2026-02-09 00:32:23 +0800

    bugfix
    Signed-off-by: Angazenn <supperccell@163.com>

--- vllm_ascend/patch/worker/__init__.py
+++ vllm_ascend/patch/worker/__init__.py
@@ -29,6 +29,7 @@ import vllm_ascend.patch.worker.patch_distributed  # noqa
 import vllm_ascend.patch.worker.patch_deepseek  # noqa
 import vllm_ascend.patch.worker.patch_multimodal_merge  # noqa
 import vllm_ascend.patch.worker.patch_minicpm  # noqa
+import vllm_ascend.patch.worker.patch_model_runner  # noqa
 import vllm_ascend.patch.worker.patch_rope  # noqa
 import vllm_ascend.patch.worker.patch_qwen3_next  # noqa
 import vllm_ascend.patch.worker.patch_qwen3_next_mtp  # noqa
