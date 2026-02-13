10b0954ac66bc9d86b12ef6a05ddfb0f757d0d5d
MrZ20 <2609716663@qq.com>
2026-02-13 11:20:34 +0800

    modify
    Signed-off-by: MrZ20 <2609716663@qq.com>

--- tests/e2e/310p/singlecard/test_dense_model_singlecard.py
+++ tests/e2e/310p/singlecard/test_dense_model_singlecard.py
@@ -24,12 +24,13 @@ def test_qwen3_dense_tp1_fp16():
     ]
     max_tokens = 5
     with VllmRunner(
-            "Qwen/Qwen3-8B",
-            tensor_parallel_size=1,
-            enforce_eager=True,
-            dtype="float16"
+        "Qwen/Qwen3-8B",
+        tensor_parallel_size=1,
+        enforce_eager=True,
+        dtype="float16",
     ) as vllm_model:
         vllm_model.generate_greedy(example_prompts, max_tokens)
+
 
 def test_qwen3_dense_tp1_w8a8():
     example_prompts = [
@@ -37,10 +38,10 @@ def test_qwen3_dense_tp1_w8a8():
     ]
     max_tokens = 5
     with VllmRunner(
-            "vllm-ascend/Qwen3-8B-W8A8",
-            tensor_parallel_size=1,
-            enforce_eager=True,
-            dtype="float16",
-            quantization="ascend"
+        "vllm-ascend/Qwen3-8B-W8A8",
+        tensor_parallel_size=1,
+        enforce_eager=True,
+        dtype="float16",
+        quantization="ascend",
     ) as vllm_model:
         vllm_model.generate_greedy(example_prompts, max_tokens)
--- tests/e2e/multicard/2-cards/test_external_launcher.py
+++ tests/e2e/multicard/2-cards/test_external_launcher.py
@@ -29,6 +29,7 @@ from unittest.mock import patch
 import pytest
 import torch_npu
 from modelscope import snapshot_download  # type: ignore
+
 from tests.e2e.conftest import wait_until_npu_memory_free
 
 MODELS = ["Qwen/Qwen3-0.6B"]
@@ -39,9 +40,7 @@ DEVICE_NAME = torch_npu.npu.get_device_name(0)[:10]
 @pytest.mark.parametrize("model", MODELS)
 @patch.dict(os.environ, {"HCCL_BUFFSIZE": "500"})
 def test_qwen3_external_launcher(model):
-    script = Path(
-        __file__
-    ).parent.parent.parent.parent.parent / "examples" / "offline_external_launcher.py"
+    script = Path(__file__).parent.parent.parent.parent.parent / "examples" / "offline_external_launcher.py"
     env = os.environ.copy()
     # TODO: Change to 2 when ci machine has 4 cards
     cmd = [
@@ -68,7 +67,7 @@ def test_qwen3_external_launcher(model):
         stderr=subprocess.STDOUT,
         timeout=600,
     )
-    output = proc.stdout.decode(errors='ignore')
+    output = proc.stdout.decode(errors="ignore")
 
     print(output)
 
@@ -80,16 +79,24 @@ def test_qwen3_external_launcher(model):
 
 @pytest.mark.parametrize("model", MOE_MODELS)
 def test_qwen3_moe_external_launcher_ep_tp2(model):
-    script = Path(
-        __file__
-    ).parent.parent.parent.parent.parent / "examples" / "offline_external_launcher.py"
+    script = Path(__file__).parent.parent.parent.parent.parent / "examples" / "offline_external_launcher.py"
     env = os.environ.copy()
     # TODO: Change to 2 when ci machine has 4 cards
     cmd = [
         sys.executable,
-        str(script), "--model", model, "--tp-size", "2", "--node-size", "1",
-        "--node-rank", "0", "--proc-per-node", "2", "--trust-remote-code",
-        "--enable-expert-parallel"
+        str(script),
+        "--model",
+        model,
+        "--tp-size",
+        "2",
+        "--node-size",
+        "1",
+        "--node-rank",
+        "0",
+        "--proc-per-node",
+        "2",
+        "--trust-remote-code",
+        "--enable-expert-parallel",
     ]
 
     print(f"Running subprocess: {' '.join(cmd)}")
@@ -100,7 +107,7 @@ def test_qwen3_moe_external_launcher_ep_tp2(model):
         stderr=subprocess.STDOUT,
         timeout=600,
     )
-    output = proc.stdout.decode(errors='ignore')
+    output = proc.stdout.decode(errors="ignore")
 
     print(output)
 
@@ -112,9 +119,7 @@ def test_qwen3_moe_external_launcher_ep_tp2(model):
 @patch.dict(os.environ, {"VLLM_ASCEND_ENABLE_NZ": "0"})
 @wait_until_npu_memory_free()
 def test_qwen3_external_launcher_with_sleepmode():
-    script = Path(
-        __file__
-    ).parent.parent.parent.parent.parent / "examples" / "offline_external_launcher.py"
+    script = Path(__file__).parent.parent.parent.parent.parent / "examples" / "offline_external_launcher.py"
     env = os.environ.copy()
     # TODO: Change to 2 when ci machine has 4 cards
     cmd = [
@@ -146,7 +151,7 @@ def test_qwen3_external_launcher_with_sleepmode():
         stderr=subprocess.STDOUT,
         timeout=300,
     )
-    output = proc.stdout.decode(errors='ignore')
+    output = proc.stdout.decode(errors="ignore")
 
     print(output)
 
@@ -157,9 +162,7 @@ def test_qwen3_external_launcher_with_sleepmode():
 
 @patch.dict(os.environ, {"VLLM_ASCEND_ENABLE_NZ": "0"})
 def test_qwen3_external_launcher_with_sleepmode_level2():
-    script = Path(
-        __file__
-    ).parent.parent.parent.parent.parent / "examples" / "offline_external_launcher.py"
+    script = Path(__file__).parent.parent.parent.parent.parent / "examples" / "offline_external_launcher.py"
     env = os.environ.copy()
     model_path = snapshot_download("Qwen/Qwen3-8B")
     # TODO: Add moe model test
@@ -194,7 +197,7 @@ def test_qwen3_external_launcher_with_sleepmode_level2():
         stderr=subprocess.STDOUT,
         timeout=300,
     )
-    output = proc.stdout.decode(errors='ignore')
+    output = proc.stdout.decode(errors="ignore")
 
     print(output)
 
@@ -208,14 +211,9 @@ def test_qwen3_external_launcher_with_sleepmode_level2():
     reason="This test is only for Ascend910B devices.",
 )
 @pytest.mark.parametrize("model", MODELS)
-@patch.dict(os.environ, {
-    "VLLM_ASCEND_ENABLE_MATMUL_ALLREDUCE": "1",
-    "HCCL_BUFFSIZE": "500"
-})
+@patch.dict(os.environ, {"VLLM_ASCEND_ENABLE_MATMUL_ALLREDUCE": "1", "HCCL_BUFFSIZE": "500"})
 def test_qwen3_external_launcher_with_matmul_allreduce(model):
-    script = Path(
-        __file__
-    ).parent.parent.parent.parent.parent / "examples" / "offline_external_launcher.py"
+    script = Path(__file__).parent.parent.parent.parent.parent / "examples" / "offline_external_launcher.py"
     env = os.environ.copy()
     cmd = [
         sys.executable,
@@ -234,7 +232,7 @@ def test_qwen3_external_launcher_with_matmul_allreduce(model):
         timeout=600,
     )
 
-    output = proc.stdout.decode(errors='ignore')
+    output = proc.stdout.decode(errors="ignore")
     print(output)
 
     assert "Generated text:" in output
