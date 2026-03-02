8e6a59136ad581ec50b9db36ef415fa6b37fd188
tanhaoan333 <tanhaoan@huawei.com>
2026-03-02 16:17:42 +0800

    Update patch_qwen3_omni_moe_thinker.py
    Signed-off-by: tanhaoan333 <tanhaoan@huawei.com>

--- vllm_ascend/patch/worker/patch_qwen3_omni_moe_thinker.py
+++ vllm_ascend/patch/worker/patch_qwen3_omni_moe_thinker.py
@@ -34,21 +34,6 @@ from transformers.utils import auto_docstring
 from vllm.model_executor.models.qwen3_omni_moe_thinker import Qwen3OmniMoeThinkerForConditionalGeneration
 from vllm.model_executor.models.utils import WeightsMapper
 
-Qwen3OmniMoeThinkerForConditionalGeneration.hf_to_vllm_mapper = WeightsMapper(
-    orig_to_new_prefix={
-        "thinker.lm_head.": "language_model.lm_head.",
-        "thinker.model.": "language_model.model.",
-        "thinker.": "",
-        "lm_head.": "language_model.lm_head.",
-        "model.": "language_model.model.",
-    }
-)
-
-Qwen3OmniMoeThinkerForConditionalGeneration.packed_modules_mapping = {
-    "qkv_proj": ["q_proj", "k_proj", "v_proj"],
-    "gate_up_proj": ["gate_proj", "up_proj"],
-}
-
 
 class NPUQwen3OmniMoeAudioAttention(nn.Module):
     def __init__(self, config, quant_config=None, prefix=""):
