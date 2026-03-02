9e46580876f6212ba68ffaa6a815dee583689fa4
tanhaoan333 <tanhaoan@huawei.com>
2026-03-02 16:41:25 +0800

    Update modelslim_config.py
    Signed-off-by: tanhaoan333 <tanhaoan@huawei.com>

--- vllm_ascend/quantization/modelslim_config.py
+++ vllm_ascend/quantization/modelslim_config.py
@@ -203,7 +203,7 @@ packed_modules_model_mapping: dict[str, dict[str, list[str]]] = {
             "gate_proj",
             "up_proj",
         ],
-        experts": ["experts.0.gate_proj", "experts.0.up_proj", "experts.0.down_proj"],
+        "experts": ["experts.0.gate_proj", "experts.0.up_proj", "experts.0.down_proj"],
     },
 }
 
