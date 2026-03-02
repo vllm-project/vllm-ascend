deefaa5db35bea54530954056306fbffb6123cf6
tanhaoan333 <tanhaoan@huawei.com>
2026-03-02 16:23:03 +0800

    Update modelslim_config.py
    Signed-off-by: tanhaoan333 <tanhaoan@huawei.com>

--- vllm_ascend/quantization/modelslim_config.py
+++ vllm_ascend/quantization/modelslim_config.py
@@ -63,7 +63,8 @@ QUANT_MODEL_PREFIX_MAPPINGS: dict[str, dict[str, str]] = {
     "kimi_k25": {
         "mm_projector.linear_1": "mm_projector.proj.0",
         "mm_projector.linear_2": "mm_projector.proj.2",
-    },"qwen3_omni_moe_thinker": {
+    },
+    "qwen3_omni_moe_thinker": {
         "thinker.lm_head.": "language_model.lm_head.",
         "thinker.model.": "language_model.model.",
         "thinker.": "",
@@ -203,8 +204,7 @@ packed_modules_model_mapping: dict[str, dict[str, list[str]]] = {
             "gate_proj",
             "up_proj",
         ],
-        "experts":
-        ["experts.0.gate_proj", "experts.0.up_proj", "experts.0.down_proj"],
+        "experts":["experts.0.gate_proj", "experts.0.up_proj", "experts.0.down_proj"],
     },
 }
 
