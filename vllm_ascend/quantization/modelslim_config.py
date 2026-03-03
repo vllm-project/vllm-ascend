5ed45996a13164c5dc757ea3752012cc1913d6b4
tanhaoan333 <tanhaoan@huawei.com>
2026-03-03 18:10:43 +0800

    Update modelslim_config.py
    Signed-off-by: tanhaoan333 <tanhaoan@huawei.com>

--- vllm_ascend/quantization/modelslim_config.py
+++ vllm_ascend/quantization/modelslim_config.py
@@ -71,13 +71,6 @@ QUANT_MODEL_PREFIX_MAPPINGS: dict[str, dict[str, str]] = {
         "lm_head.": "language_model.lm_head.",
         "model.": "language_model.model.",
     },
-    "qwen3_omni_moe_thinker": {
-        "thinker.lm_head.": "language_model.lm_head.",
-        "thinker.model.": "language_model.model.",
-        "thinker.": "",
-        "lm_head.": "language_model.lm_head.",
-        "model.": "language_model.model.",
-    },
     "qwen2_5_omni_thinker": {
         "thinker.lm_head.": "language_model.lm_head.",
         "thinker.model.": "language_model.model.",
