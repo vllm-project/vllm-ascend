cc315bf5dfe9de9df777998f9e59ac799e6f3bd0
tanhaoan333 <tanhaoan@huawei.com>
2026-03-04 20:32:42 +0800

    Create patch_qwen3_omni_moe_thinker.py
    Signed-off-by: tanhaoan333 <tanhaoan@huawei.com>

---
+++ vllm_ascend/patch/worker/patch_qwen3_omni_moe_thinker.py
@@ -0,0 +1,39 @@
+#
+# Copyright (c) 2025 Huawei Technologies Co., Ltd. All Rights Reserved.
+# This file is a part of the vllm-ascend project.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from vllm.model_executor.models.utils import WeightsMapper
+
+
+def _patch_qwen3_omni_thinker_mapper() -> None:
+    try:
+        from vllm.model_executor.models.qwen3_omni_moe_thinker import (
+            Qwen3OmniMoeThinkerForConditionalGeneration
+        )
+    except Exception:
+        return
+    
+    extra_mapper = WeightsMapper(
+        orig_to_new_prefix={
+            "lm_head.": "language_model.lm_head.",
+            "model.": "language_model.model.",
+        }
+    )
+    Qwen3OmniMoeThinkerForConditionalGeneration.hf_to_vllm_mapper = (
+        Qwen3OmniMoeThinkerForConditionalGeneration.hf_to_vllm_mapper | extra_mapper
+    )
+
+
+_patch_qwen3_omni_thinker_mapper()
