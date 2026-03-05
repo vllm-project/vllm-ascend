36f5ef3cd4040e3ac9bc78f3071bce0d148d7e13
aipaes <82140963+aipaes@users.noreply.github.com>
2026-03-05 16:54:03 +0800

    Clean up formatting in utils.py
    Remove unnecessary blank line in utils.py
    
    Signed-off-by: aipaes <82140963+aipaes@users.noreply.github.com>

--- vllm_ascend/utils.py
+++ vllm_ascend/utils.py
@@ -1169,7 +1169,7 @@ def check_gdn_layer(vllm_config) -> bool:
         return False
 
     hf_config = model_config.hf_config
-    
+
     # Use `or []` to prevent errors when layer_types is None
     layer_types = getattr(hf_config, "layer_types", None) or []
     if "linear_attention" in layer_types:
