da5d52b4b39e43da193bb9e929db491a4b287837
SunnyLee219 <3294305115@qq.com>
2026-03-05 16:33:39 +0800

    Fix Lint
    Signed-off-by: SunnyLee219 <3294305115@qq.com>

--- vllm_ascend/patch/worker/patch_qwen3_next.py
+++ vllm_ascend/patch/worker/patch_qwen3_next.py
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # from collections.abc import Iterable
+# mypy: ignore-errors
 
 import torch
 import torch_npu
