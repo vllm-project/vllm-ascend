fix top9 precision

vllm_ascend/patch/worker/patch_deepseekv3.py
@@ -124,8 +124,8 @@ class AscendDeepseekV2MoE(DeepseekV2MoE, nn.Module):
             scoring_func=config.scoring_func,
             # we do scaling outside, set factor to 1.0 to avoid double mul
             # aiter applies routed_scaling_factor internally
-            routed_scaling_factor=1.0
-            if not mix_placement else self.routed_scaling_factor,
+            routed_scaling_factor=1.0,
+            # if not mix_placement else self.routed_scaling_factor,
             e_score_correction_bias=self.gate.e_score_correction_bias \
             if self.gate is not None else None,
             enable_eplb=self.enable_eplb,