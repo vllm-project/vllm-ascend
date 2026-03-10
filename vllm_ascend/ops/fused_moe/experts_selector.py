fix top9 precision for dsv2

vllm_ascend/ops/fused_moe/experts_selector.py
@@ -100,7 +100,7 @@ def select_experts(hidden_states: torch.Tensor,
             global_num_experts=global_num_experts,
         )
     if mix_placement:
-        shared_expert_routing_factor = 0.4
+        shared_expert_routing_factor = 1.0/routed_scaling_factor
         batch_size = topk_ids.shape[0]
         pad_shared_expert_ids = torch.arange(
             num_logical_experts,

vllm_ascend/patch/worker/patch_deepseekv3.py
@@ -91,7 +91,7 @@ class AscendDeepseekV2MoE(DeepseekV2MoE, nn.Module):
         ascend_config = get_ascend_config()
         mix_placement = getattr(ascend_config, "mix_placement", False)
         self.mix_placement = mix_placement
-        if (config.n_shared_experts is None or mix_placement):
+        if config.n_shared_experts is None or mix_placement:
             self.shared_experts = None
         else:
             intermediate_size = config.moe_intermediate_size * config.n_shared_experts
@@ -124,8 +124,8 @@ class AscendDeepseekV2MoE(DeepseekV2MoE, nn.Module):
             scoring_func=config.scoring_func,
             # we do scaling outside, set factor to 1.0 to avoid double mul
             # aiter applies routed_scaling_factor internally
-            routed_scaling_factor=1.0,
-            # if not mix_placement else self.routed_scaling_factor,
+            routed_scaling_factor=1.0
+            if not mix_placement else self.routed_scaling_factor,
             e_score_correction_bias=self.gate.e_score_correction_bias \
             if self.gate is not None else None,
             enable_eplb=self.enable_eplb,