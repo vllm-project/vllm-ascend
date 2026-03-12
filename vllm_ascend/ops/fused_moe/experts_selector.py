40a729ec51b141bc86620cbbecb811b377cfd9dc
xuzewei28 <xuzewei2@h-partners.com>
2026-03-12 15:05:25 +0800

    precmt fix
    Signed-off-by: xuzewei28 <xuzewei2@h-partners.com>

--- vllm_ascend/ops/fused_moe/experts_selector.py
+++ vllm_ascend/ops/fused_moe/experts_selector.py
@@ -34,7 +34,7 @@ def select_experts(
     routed_scaling_factor=1.0,
     e_score_correction_bias: torch.Tensor | None = None,
     indices_type: torch.dtype | None = None,
-    mix_placement: Optional[bool] = False,
+    mix_placement: bool = False,
     num_logical_experts: int = -1,
     num_shared_experts: int = 0,
     global_num_experts: int = -1,
@@ -105,20 +105,19 @@ def select_experts(
         shared_expert_routing_factor = 0.4
         batch_size = topk_ids.shape[0]
         pad_shared_expert_ids = torch.arange(
-            num_logical_experts,
-            num_logical_experts + num_shared_experts,
-            dtype=topk_ids.dtype,
-            device=topk_ids.device).repeat(batch_size, 1)
+            num_logical_experts, num_logical_experts + num_shared_experts, dtype=topk_ids.dtype, device=topk_ids.device
+        ).repeat(batch_size, 1)
 
         pad_shared_expert_weights = torch.full(
             (topk_weights.shape[0], num_shared_experts),
             shared_expert_routing_factor,
             dtype=topk_weights.dtype,
-            device=topk_weights.device)
+            device=topk_weights.device,
+        )
 
         topk_ids = torch.cat([topk_ids, pad_shared_expert_ids], dim=1)
-        topk_weights = torch.cat([topk_weights, pad_shared_expert_weights],
-                                 dim=1)
+        topk_weights = torch.cat([topk_weights, pad_shared_expert_weights], dim=1)
+
     return topk_weights, topk_ids
 
 
