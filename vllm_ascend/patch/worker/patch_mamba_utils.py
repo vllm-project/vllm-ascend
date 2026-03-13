0cfa290623a5c25530ed4a6cf513b740cbf4d882
Angazenn <supperccell@163.com>
2026-03-13 16:55:51 +0800

    modify
    Signed-off-by: Angazenn <supperccell@163.com>

--- vllm_ascend/patch/worker/patch_mamba_utils.py
+++ vllm_ascend/patch/worker/patch_mamba_utils.py
@@ -2,32 +2,8 @@
 
 
 import vllm
-from vllm.triton_utils import tl, triton
 
-
-@triton.jit
-def batch_memcpy_kernel(src_ptrs, dst_ptrs, sizes, BLOCK_SIZE: tl.constexpr):
-    pid = tl.program_id(0)
-
-    src_ptr = tl.load(src_ptrs + pid)
-    dst_ptr = tl.load(dst_ptrs + pid)
-    size = tl.load(sizes + pid)
-
-    # We need to mv pointer_type cast outside the loop.
-    # Otherwise it causes potential bugs.
-    src_ptr = src_ptr.to(tl.pointer_type(tl.uint8))
-    dst_ptr = dst_ptr.to(tl.pointer_type(tl.uint8))
-
-    offsets = tl.arange(0, BLOCK_SIZE)
-    for i in range(0, size, BLOCK_SIZE):
-        mask = (i + offsets) < size
-
-        curr_src_ptr = src_ptr + i + offsets
-        curr_dst_ptr = dst_ptr + i + offsets
-
-        # cache_modifier=".cg" bypasses L1 cache for streaming data.
-        data = tl.load(curr_src_ptr, mask=mask, cache_modifier=".cg")
-        tl.store(curr_dst_ptr, data, mask=mask, cache_modifier=".cg")
+from vllm_ascend.ops.triton.batch_memcpy import batch_memcpy_kernel
 
 
 def batch_memcpy(src_ptrs, dst_ptrs, sizes):
