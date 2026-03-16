e08f713168f3b178bb81613f9bb0fb55cd911775
QiuChunshuo <qiuchunshuo@huawei.com>
2026-03-16 10:09:24 +0800

    bugfix(MC2): refactor the comm group of MC2 to be compatible with PP
    Signed-off-by: QiuChunshuo <qiuchunshuo@huawei.com>

--- vllm_ascend/distributed/parallel_state.py
+++ vllm_ascend/distributed/parallel_state.py
@@ -38,19 +38,18 @@ def init_ascend_model_parallel(
     global_tp_size = parallel_config.tensor_parallel_size
     global_dp_size = parallel_config.data_parallel_size
     global_pp_size = parallel_config.pipeline_parallel_size
+    global_pcp_size = parallel_config.prefill_context_parallel_size
 
     # The layout of all ranks: ExternalDP * EP
     # ExternalDP is the data parallel group that is not part of the model,
     # every dp rank can generate independently (in verl integration).
-    all_ranks = torch.arange(world_size).reshape(
-        -1, global_dp_size * parallel_config.prefill_context_parallel_size * global_tp_size
-    )
+    all_ranks = torch.arange(world_size).reshape(-1, global_dp_size * global_pcp_size * global_tp_size)
     # TODO: all_ranks should be the same as vllm_all_ranks, all_ranks needs to be removed in the future.
     vllm_all_ranks = torch.arange(world_size).reshape(
         -1,
         global_dp_size,
         global_pp_size,
-        parallel_config.prefill_context_parallel_size,
+        global_pcp_size,
         global_tp_size,
     )
 
@@ -59,7 +58,6 @@ def init_ascend_model_parallel(
     global _P_TP
     assert _P_TP is None, "distributed prefill tensor parallel group is already initialized"
     prefill_tensor_model_parallel_size = pd_tp_ratio
-    pcp_size = parallel_config.prefill_context_parallel_size
     # divide alltoall groups
     if pd_head_ratio > 1 and get_current_vllm_config().kv_transfer_config.is_kv_producer:
         num_head_replica = get_ascend_config().num_head_replica
@@ -68,13 +66,13 @@ def init_ascend_model_parallel(
             group_ranks = all_ranks.view(-1, prefill_tensor_model_parallel_size).unbind(0)
         else:
             group_ranks = all_ranks.clone().view(
-                global_dp_size * pcp_size, -1, num_head_replica
+                global_dp_size * global_pcp_size, -1, num_head_replica
             )  # [DP_size, num_head, num_head_replica]
             group_ranks = group_ranks.permute(0, 2, 1)
             group_ranks = group_ranks.reshape(-1, group_ranks.size(-1))  # [DP_size * num_head_replica, num_head]
             alltoall_group_size = group_ranks.size(-1) // remote_tp_size
             group_ranks = group_ranks.unsqueeze(-1).view(
-                global_dp_size * pcp_size, num_head_replica, -1, alltoall_group_size
+                global_dp_size * global_pcp_size, num_head_replica, -1, alltoall_group_size
             )  # [DP_size, num_head_replica, num_alltoall_group, alltoall_group_size]
             group_ranks = group_ranks.reshape(-1, alltoall_group_size).unbind(0)
         group_ranks = [x.tolist() for x in group_ranks]
@@ -83,7 +81,14 @@ def init_ascend_model_parallel(
         _P_TP = init_model_parallel_group(group_ranks, get_world_group().local_rank, backend, group_name=f"p_tp_{num}")
 
     global _MC2
-    group_ranks = all_ranks.unbind(0)
+    group_ranks = (
+        vllm_all_ranks.transpose(1, 2)
+        .reshape(
+            -1,
+            global_dp_size * global_pcp_size * global_tp_size,
+        )
+        .unbind(0)
+    )
     group_ranks = [x.tolist() for x in group_ranks]
 
     _MC2 = init_model_parallel_group(group_ranks, get_world_group().local_rank, backend, group_name="mc2")
