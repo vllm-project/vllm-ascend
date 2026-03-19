2d1d0c10041df8506ed46f78c82aa8ecdec3ce18
KlyzhenkoVadim <vadimklyzhenko@gmail.com>
2026-03-19 19:59:38 +0300

    fixbug with eagle and draft proposers
    Signed-off-by: KlyzhenkoVadim <vadimklyzhenko@gmail.com>

--- vllm_ascend/spec_decode/draft_proposer.py
+++ vllm_ascend/spec_decode/draft_proposer.py
@@ -5,6 +5,7 @@ from vllm.v1.spec_decode.draft_model import DraftModelProposer
 
 from vllm_ascend.spec_decode.eagle_proposer import AscendSpecDecodeBaseProposer
 
+logger = init_logger(__name__)
 
 class AscendDraftModelProposer(AscendSpecDecodeBaseProposer, DraftModelProposer):
     def __init__(
