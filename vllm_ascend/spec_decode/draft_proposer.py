0795b9c87a00d646fc6f56540aee595af831dd30
KlyzhenkoVadim <vadimklyzhenko@gmail.com>
2026-03-19 20:07:31 +0300

    fixbug with eagle and draft proposers
    Signed-off-by: KlyzhenkoVadim <vadimklyzhenko@gmail.com>

--- vllm_ascend/spec_decode/draft_proposer.py
+++ vllm_ascend/spec_decode/draft_proposer.py
@@ -1,11 +1,8 @@
 import torch
 from vllm.config import VllmConfig
-from vllm.logger import init_logger
 from vllm.v1.spec_decode.draft_model import DraftModelProposer
 
 from vllm_ascend.spec_decode.eagle_proposer import AscendSpecDecodeBaseProposer
-
-logger = init_logger(__name__)
 
 
 class AscendDraftModelProposer(AscendSpecDecodeBaseProposer, DraftModelProposer):
