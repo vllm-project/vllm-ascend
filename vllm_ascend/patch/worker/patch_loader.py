bc0e6cfeeaa5a7960123b9c5f205720fad255051
Shangwei-Li <lishangwei@mail.ustc.edu.cn>
2026-03-20 14:31:22 +0800

    Refactor weight transpose for RL.
    Signed-off-by: Shangwei-Li <lishangwei@mail.ustc.edu.cn>

---
+++ vllm_ascend/patch/worker/patch_loader.py
@@ -0,0 +1,20 @@
+import vllm
+from vllm.model_executor.models.utils import AutoWeightsLoader
+
+class AutoWeightsLoaderWithTrans(AutoWeightsLoader):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # self._transposed_params = []
+
+    def _preprocess(self):
+        for k, v in self.module.named_parameters():
+            if getattr(v, "transposed", False):
+                setattr(v, "transposed", False)
+                v.data = v.data.transpose(1, 2)
+                assert not v.is_contiguous()
+
+    def load_weights(self, model_path: str):
+        self._preprocess()
+        super().load_weights(model_path)
+
+vllm.model_executor.models.utils.AutoWeightsLoader = AutoWeightsLoaderWithTrans
