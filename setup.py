bd025de0680101f2394d27e2bd7af708944e97d0
yuhongming-2026 <hongming@isrc.iscas.ac.cn>
2026-03-23 19:54:21 +0800

    Remove unnecessary comment lines in setup.py
    Signed-off-by: yuhongming-2026 <hongming@isrc.iscas.ac.cn>

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@
 # limitations under the License.
 # This file is a part of the vllm-ascend project.
 # Adapted from https://github.com/vllm-project/vllm/blob/main/setup.py
-# 
+#  
 
 import importlib.util
 import logging
