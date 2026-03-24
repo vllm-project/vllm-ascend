8cddff03834b880a6b1a9fc72e8a95a1c01efab7
LookAround <lixushi@huawei.com>
2026-03-24 16:15:32 +0800

    [Bugfix] fix lint problem
    Signed-off-by: LookAround <lixushi@huawei.com>

--- vllm_ascend/attention/attention_v1.py
+++ vllm_ascend/attention/attention_v1.py
@@ -208,12 +208,6 @@ class AscendAttentionMetadataBuilder(AttentionMetadataBuilder[AscendMetadata]):
     Handles attention mask generation and metadata preparation for
     Ascend FlashAttention backend.
     """
-
-    # Does this backend/builder reorder the batch?
-    # If not, set this to None. Otherwise set it to the query
-    # length that will be pulled into the front of the batch.
-    reorder_batch_threshold: int = 1
-
     def __init__(
         self,
         kv_cache_spec: AttentionSpec,
