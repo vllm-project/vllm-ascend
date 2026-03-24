d5bb154bda730f6d4d6c4a625923a228cb09307b
Wangbei25 <wangbei41@huawei.com>
2026-03-24 19:13:31 +0800

    Update utils.py
    Signed-off-by: Wangbei25 <wangbei41@huawei.com>

--- vllm_ascend/utils.py
+++ vllm_ascend/utils.py
@@ -704,7 +704,7 @@ def register_ascend_pluggable_layers(vllm_config: VllmConfig | None = None):
         return
 
     from vllm.model_executor.custom_op import PluggableLayer
-    
+
     from vllm_ascend.ops.qwen2_decoder import AscendCustomQwen2Decoder
     from vllm_ascend.ops.rel_pos_attention import AscendRelPosAttention
 
