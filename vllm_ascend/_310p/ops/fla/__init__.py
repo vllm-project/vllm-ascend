ad96ee049474c952296e0fb3b27b4023d51e138b
Tflowers-0129 <2906339855@qq.com>
2026-03-25 09:14:52 +0800

    cleancode
    Signed-off-by: Tflowers-0129 <2906339855@qq.com>

--- vllm_ascend/_310p/ops/fla/__init__.py
+++ vllm_ascend/_310p/ops/fla/__init__.py
@@ -1,6 +1,6 @@
+from .chunk_gated_delta_rule import chunk_gated_delta_rule_pytorch
 from .fused_gdn_gating import fused_gdn_gating_pytorch
 from .fused_recurrent_gated_delta_rule import fused_recurrent_gated_delta_rule_pytorch
-from .chunk_gated_delta_rule import chunk_gated_delta_rule_pytorch
 
 __all__ = [
     "fused_gdn_gating_pytorch",
