222b1fa1faa78902888b84b189ac2f39c0628bfd
Your Name <you@example.com>
2026-03-28 03:37:35 +0800

    Fix rule
    Signed-off-by: Your Name <you@example.com>

--- vllm_ascend/platform.py
+++ vllm_ascend/platform.py
@@ -519,9 +519,9 @@ class NPUPlatform(Platform):
             )
 
         if model_config is not None:
-            if model_config.hf_config is None:
+            if model_config.model is None:
                 return
-            if "Qwen" in model_config.hf_config.architectures[0]:
+            if "Qwen" in model_config.model:
                 if model_config.get_num_attention_heads(vllm_config.parallel_config) == model_config.get_num_kv_heads(
                     vllm_config.parallel_config
                 ) and model_config.get_head_size() not in [64, 128, 192]:
