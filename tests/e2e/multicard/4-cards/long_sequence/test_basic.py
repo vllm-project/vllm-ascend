ce3ae5f0a5171d042e300eaf6262e1438e42b245
Yang Yuxi <907276627@qq.com>
2026-03-28 10:52:19 +0800

    add sp&dcp test
    Signed-off-by: Yang Yuxi <907276627@qq.com>

--- tests/e2e/multicard/4-cards/long_sequence/test_basic.py
+++ tests/e2e/multicard/4-cards/long_sequence/test_basic.py
@@ -226,7 +226,7 @@ def test_dcp_basic():
         "The capital of France is", "Hello, my name is Tom, I am",
         "The president of United States is", "AI future is"
     ]
-    model = "vllm-ascend/Qwen3-30B-A3B-W8A8"
+    model = "deepseek-ai/DeepSeek-V2-Lite-Chat"
     sampling_params = SamplingParams(max_tokens=32, temperature=0.0)
     with VllmRunner(model,
                     enforce_eager=True,
@@ -235,9 +235,11 @@ def test_dcp_basic():
                     prefill_context_parallel_size=1,
                     decode_context_parallel_size=2,
                     max_num_batched_tokens=1024,
+                    enable_expert_parallel=True,
                     block_size=128,
                     compilation_config={"pass_config": {"enable_sp": True}}) as runner:
         runner.model.generate(prompts, sampling_params)
+
 
 @wait_until_npu_memory_free()
 def test_dcp_full_graph():
