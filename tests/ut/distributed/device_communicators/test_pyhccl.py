8f114da1a14116a1deb28ff9d2f9a5ae02c47c26
hfadzxy <starmoon_zhang@163.com>
2026-04-02 17:14:23 +0800

    revert 7881
    Signed-off-by: hfadzxy <starmoon_zhang@163.com>

--- tests/ut/distributed/device_communicators/test_pyhccl.py
+++ tests/ut/distributed/device_communicators/test_pyhccl.py
@@ -6,7 +6,7 @@ from vllm.distributed.utils import StatelessProcessGroup
 from tests.ut.base import TestBase
 from vllm_ascend.distributed.device_communicators.pyhccl import \
     PyHcclCommunicator
-from vllm_ascend.utils import ACL_FORMAT_FRACTAL_NZ
+from vllm_ascend.utils import ACL_FORMAT_FRACTAL_NZ, vllm_version_is
 
 
 class MockHcclLib:
@@ -46,9 +46,15 @@ class TestPyHcclCommunicator(TestBase):
     @patch("vllm_ascend.utils.current_stream",
            return_value=MagicMock(npu_stream=5678))
     def test_stateless_group(self, *_):
-        group = StatelessProcessGroup(rank=3,
-                                    world_size=4,
-                                    store=None)
+        if vllm_version_is("0.18.0"):
+            group = StatelessProcessGroup(rank=3,
+                                        world_size=4,
+                                        store=None,
+                                        socket=None)
+        else:
+            group = StatelessProcessGroup(rank=3,
+                                        world_size=4,
+                                        store=None)
 
         comm = PyHcclCommunicator(group=group, device=3)
 
