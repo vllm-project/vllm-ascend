35c8baee598a0b2310fd7d94a1ae03faa2ed7ff7
lhp-deep <liuhaopeng1@huawei.com>
2026-04-15 10:11:16 +0800

    [Optimize][model_runner_v2]:optimize the performance of the _min_p_kernel
    Signed-off-by: lhp-deep <liuhaopeng1@huawei.com>

--- vllm_ascend/patch/worker/patch_v2/patch_triton.py
+++ vllm_ascend/patch/worker/patch_v2/patch_triton.py
@@ -5,8 +5,8 @@ from vllm.v1.worker.gpu.spec_decode.eagle import speculator
 
 from vllm_ascend.worker.v2.input_batch import post_update
 from vllm_ascend.worker.v2.sample.bad_words import apply_bad_words
-from vllm_ascend.worker.v2.sample.logprob import compute_topk_logprobs
 from vllm_ascend.worker.v2.sample.gumbel import gumbel_sample
+from vllm_ascend.worker.v2.sample.logprob import compute_topk_logprobs
 from vllm_ascend.worker.v2.sample.min_p import apply_min_p
 from vllm_ascend.worker.v2.sample.penalties import apply_penalties, bincount
 
