2dc3380151a6721c301cc8f70f9054da9baa8e1e
Zetong Li <48438720+slippersss@users.noreply.github.com>
2026-04-17 17:19:52 +0800

    [BugFix] Patch kv-init-related logic for model_runner_v2 (#8247)
    ### What this PR does / why we need it?
    This PR aims to patch `_allocate_kv_cache` and `_reshape_kv_cache` for
    model_runner_v2. Since the different organization of kv cache tensors,
    it is non-trivial to reuse related code in vllm. This PR simply copies
    them from model_runner_v1 and deletes branches about sfa/hybrid, because
    they may be supported later with refactoring.
    
    At the same time, add cos/sin setting and updating back since currently
    mla still needs them. Now it should be ok to run dsv31 in
    model_runner_v2 in basic configuration. Related e2e is also added.
    
    Additionally, update README for recording gaps with vllm.
    
    BTW, ut is on the way.
    
    RFC: #5208
    
    ### Does this PR introduce _any_ user-facing change?
    N/A
    
    ### How was this patch tested?
    by ci
    
    - vLLM version: 
    - vLLM main: https://github.com/vllm-project/vllm/commit/v0.19.0
    
    Signed-off-by: Zetong Li <slippersss@126.com>

--- vllm_ascend/worker/v2/model_runner.py
+++ vllm_ascend/worker/v2/model_runner.py
@@ -45,6 +45,7 @@ from vllm_ascend.ascend_forward_context import (
     set_mc2_mask,
     set_mc2_tokens_capacity,
 )
+from vllm_ascend.ops.rotary_embedding import set_cos_and_sin, update_cos_sin
 from vllm_ascend.utils import set_weight_prefetch_method
 from vllm_ascend.worker.v2.aclgraph_utils import ModelAclGraphManager
 from vllm_ascend.worker.v2.attn_utils import build_attn_state
@@ -130,6 +131,8 @@ class NPUModelRunner(GPUModelRunner):
         # set _WEIGHT_PREFETCH_METHOD, _mc2_tokens_capacity and _reserved_mc2_mask which
         # is necessary for weight_prfetching function, and MoE communication optimization.
         set_weight_prefetch_method(self.ascend_config.weight_prefetch_config)
+        # TODO: remove set_cos_and_sin (together with update_cos_sin) when mla can properly handle cos/sin internally
+        set_cos_and_sin(vllm_config, self.max_num_reqs, self.decode_query_len, self.dtype, self.device)
         set_mc2_tokens_capacity(vllm_config, self.max_num_reqs, self.decode_query_len)
         set_mc2_mask(vllm_config, self.device)
 
@@ -347,6 +350,10 @@ class NPUModelRunner(GPUModelRunner):
             attn_state=attn_state,
             extra_kwargs=extra_kwargs,
         )
+
+        # For mla/sfa, update cos/sin. Here is for execute_model.
+        update_cos_sin(self.input_batch.positions)
+
         return self.input_batch
 
     def postprocess(
