daee26534f9ce7b74821df3bc97f8c4604912178
TX <tianxiang.xia1@huawei.com>
2026-04-17 15:39:42 +0000

    fix: update quarot copyright year
    Signed-off-by: TX <tianxiang.xia1@huawei.com>

--- tools/quarot/eval_wikimqa_speed_f1.py
+++ tools/quarot/eval_wikimqa_speed_f1.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Copyright (c) 2025 Huawei Technologies Co., Ltd. All Rights Reserved.
+# Copyright (c) 2026 Huawei Technologies Co., Ltd. All Rights Reserved.
 # This file is a part of the vllm-ascend project.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
--- vllm_ascend/quantization/quarot_kv_cache.py
+++ vllm_ascend/quantization/quarot_kv_cache.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2025 Huawei Technologies Co., Ltd. All Rights Reserved.
+# Copyright (c) 2026 Huawei Technologies Co., Ltd. All Rights Reserved.
 # This file is a part of the vllm-ascend project.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
