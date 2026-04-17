8b7780005ccf67ae5439d582ccb5d9a0d40731a1
nwpu-zxr <zhouxuerong2@huawei.com>
2026-04-17 11:53:45 +0800

    fix pd on qwen3.5
    Signed-off-by: nwpu-zxr <zhouxuerong2@huawei.com>

--- vllm_ascend/patch/worker/patch_qwen3_next.py
+++ vllm_ascend/patch/worker/patch_qwen3_next.py
@@ -87,7 +87,7 @@ class AscendQwen3Next_GatedDeltaNet(Qwen3NextGatedDeltaNet):
         # ============================================================
         # Part 3: Output Projection
         # ============================================================
-        maybe_save_kv_layer_to_connector("", [])
+        maybe_save_kv_layer_to_connector(self.prefix, [])
         z_shape_og = z.shape
         # Reshape input data into 2D tensor
         core_attn_out = core_attn_out.reshape(-1, core_attn_out.shape[-1])
