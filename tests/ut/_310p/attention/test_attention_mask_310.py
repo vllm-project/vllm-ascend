f10f6e0dff628d679a683336a77c3cc2e58caefa
liuhaiyang27 <liuhaiyang27@huawei.com>
2026-04-21 15:02:16 +0800

    [bugfix] Fix the input parameter error of atten_mask in the npu_fused_infer_attention_score_v2
    Signed-off-by: liuhaiyang27 <liuhaiyang27@huawei.com>

--- tests/ut/_310p/attention/test_attention_mask_310.py
+++ tests/ut/_310p/attention/test_attention_mask_310.py
@@ -40,7 +40,6 @@ class TestAttentionMaskBuilder310(TestBase):
         self.assertEqual(attn_mask.shape, (1, self.max_seqlen // 16, self.max_seqlen, 16))
         self.assertEqual(attn_mask[0][-1][0][-1], torch.tensor(float("-inf"), dtype=torch.float16))
 
-
     @patch("torch_npu.npu_format_cast")
     def test_get_splitfuse_attn_mask_310(self, mock_format_cast):
         mock_format_cast.side_effect = lambda x, y: x
