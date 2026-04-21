bccffc1f39a2d6ae113dd70f33dee88f063e0443
weiguihua2 <weiguihua2@huawei.com>
2026-04-21 16:21:40 +0800

    fix(pcp): correct logits metadata handling
    Signed-off-by: weiguihua2 <weiguihua2@huawei.com>

--- vllm_ascend/worker/pcp_utils.py
+++ vllm_ascend/worker/pcp_utils.py
@@ -513,22 +513,18 @@ class PCPManager:
         tokens_original: list[int] | None = None,
     ):
         if not self.pcp_use_hybrid_attn or tokens_original is None:
-            num_logits_reqs = min(num_reqs, cu_num_tokens.shape[0])
-            num_dycp_reqs = min(self.num_dycp_reqs, num_logits_reqs)
-            cu_num_tokens_np = torch.from_numpy(cu_num_tokens[:num_logits_reqs])
             logits_indices = (
-                cu_num_tokens_np * self.pcp_world_size
-                - self.num_pcp_pads_cpu_tensor[:num_logits_reqs]
+                torch.from_numpy(cu_num_tokens) * self.pcp_world_size
+                - self.num_pcp_pads_cpu_tensor[: self.num_reqs]
                 - 1
             )
-            if 0 < num_dycp_reqs < num_logits_reqs:
-                dycp_tokens = cu_num_tokens_np[num_dycp_reqs - 1]
-                logits_indices[num_dycp_reqs:num_logits_reqs] = (
-                    dycp_tokens * self.pcp_world_size
-                    + cu_num_tokens_np[num_dycp_reqs:num_logits_reqs]
-                    - dycp_tokens
-                    - 1
-                )
+            cu_num_tokens_tensor = torch.from_numpy(cu_num_tokens)
+            logits_indices = cu_num_tokens_tensor * self.pcp_world_size - self.num_pcp_pads_cpu_tensor[: self.num_reqs] - 1
+            if self.num_dycp_reqs == 0:
+                logits_indices[self.num_dycp_reqs: self.num_reqs] = cu_num_tokens_tensor - 1
+            else:
+                logits_indices[self.num_dycp_reqs: self.num_reqs] = cu_num_tokens_tensor[
+                    self.num_dycp_reqs - 1] * self.pcp_world_size + (cu_num_tokens_tensor[self.num_dycp_reqs:] - cu_num_tokens_tensor[self.num_dycp_reqs - 1]) - 1
         else:
             tokens_original_tensor = torch.tensor(tokens_original, dtype=torch.int32)
             num_prefill_reqs = (tokens_original_tensor > self.decode_threshold).sum().item()
