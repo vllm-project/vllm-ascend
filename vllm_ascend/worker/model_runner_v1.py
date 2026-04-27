c9cc16f1fe5ebd0b90821fbe687e41b6f5e3dd42
chenxi-hh <chen464822955@163.com>
2026-04-27 20:15:32 +0800

    [Performance] Asynchronous Scheduling Issuance Bubble Optimization
    Signed-off-by: chenxi-hh <chen464822955@163.com>

--- vllm_ascend/worker/model_runner_v1.py
+++ vllm_ascend/worker/model_runner_v1.py
@@ -1287,7 +1287,7 @@ class NPUModelRunner(GPUModelRunner):
 
         if self.use_async_spec_decode:
             # Stash for GPU-side correction in _prepare_inputs.
-            self.valid_sampled_token_count_gpu = valid_sampled_tokens_count
+            self.valid_sampled_token_count_gpu = valid_sampled_tokens_count # type: ignore[no-redef]
         self.input_batch.prev_sampled_token_ids = next_token_ids.unsqueeze(1)
 
     # TODO: Once the PCP features are complete, it will fully inherit the classes from the VLLM community.
