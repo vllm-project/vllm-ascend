b28a84016fcf2072ba1c4ddf24ca458eff8a801d
wangyongjun <wangyongjun7@huawei.com>
2026-05-07 22:15:50 +0800

    310p support pooling model
    Signed-off-by: wangyongjun <wangyongjun7@huawei.com>

--- tests/e2e/310p/singlecard/pooling/test_classification.py
+++ tests/e2e/310p/singlecard/pooling/test_classification.py
@@ -6,7 +6,6 @@ from tests.e2e.conftest import HfRunner, VllmRunner
 
 
 def test_qwen_pooling_classify_correctness() -> None:
-
     model_name = snapshot_download("Howeee/Qwen2.5-1.5B-apeach")
 
     prompts = [
