ef9b5cc05c99769d916592f539015578fb6c1d24
wangyongjun <wangyongjun7@huawei.com>
2026-05-07 22:08:56 +0800

    310p support pooling model
    Signed-off-by: wangyongjun <wangyongjun7@huawei.com>

--- tests/e2e/310p/singlecard/pooling/test_scoring.py
+++ tests/e2e/310p/singlecard/pooling/test_scoring.py
@@ -39,11 +39,7 @@ def test_cross_encoder_score_1_to_1(model_name):
     with HfRunner(model_name, dtype=DTYPE, is_cross_encoder=True) as hf_model:
         hf_outputs = hf_model.predict([text_pair]).tolist()
 
-    with VllmRunner(model_name,
-                    runner="pooling",
-                    dtype=DTYPE,
-                    max_model_len=1024,
-                    enforce_eager=True) as vllm_model:
+    with VllmRunner(model_name, runner="pooling", dtype=DTYPE, max_model_len=1024, enforce_eager=True) as vllm_model:
         vllm_outputs = vllm_model.score(text_pair[0], text_pair[1])
 
     assert len(vllm_outputs) == 1
@@ -100,9 +96,11 @@ def test_embedding_score_1_to_1(emb_model_name):
 
     with HfRunner(emb_model_name, dtype=DTYPE, is_sentence_transformer=True) as hf_model:
         hf_embeddings = hf_model.encode(text_pair)
-        hf_outputs = [F.cosine_similarity(*map(torch.tensor, hf_embeddings), dim=0)]
+        hf_outputs = [F.cosine_similarity(*map(torch.tensor, pair), dim=0) for pair in hf_embeddings]
 
-    with VllmRunner(emb_model_name, runner="pooling", dtype=DTYPE, max_model_len=1024, enforce_eager=True) as vllm_model:
+    with VllmRunner(
+           emb_model_name, runner="pooling", dtype=DTYPE, max_model_len=1024, enforce_eager=True
+    ) as vllm_model:
         vllm_outputs = vllm_model.score(text_pair[0], text_pair[1])
 
     assert len(vllm_outputs) == 1
@@ -119,9 +117,11 @@ def test_embedding_score_1_to_N(emb_model_name):
 
     with HfRunner(emb_model_name, dtype=DTYPE, is_sentence_transformer=True) as hf_model:
         hf_embeddings = [hf_model.encode(text_pair) for text_pair in text_pairs]
-        hf_outputs = [ F.cosine_similarity(*map(torch.tensor, pair), dim=0) for pair in hf_embeddings]
+        hf_outputs = [F.cosine_similarity(*map(torch.tensor, pair), dim=0) for pair in hf_embeddings]
 
-    with VllmRunner(emb_model_name, runner="pooling", dtype=DTYPE, max_model_len=1024, enforce_eager=True) as vllm_model:
+    with VllmRunner(
+           emb_model_name, runner="pooling", dtype=DTYPE, max_model_len=1024, enforce_eager=True
+    ) as vllm_model:
         vllm_outputs = vllm_model.score(TEXTS_1[0], TEXTS_2)
 
     assert len(vllm_outputs) == 2
@@ -141,11 +141,9 @@ def test_embedding_score_N_to_N(emb_model_name):
         hf_embeddings = [hf_model.encode(text_pair) for text_pair in text_pairs]
         hf_outputs = [F.cosine_similarity(*map(torch.tensor, pair), dim=0) for pair in hf_embeddings]
 
-    with VllmRunner(emb_model_name,
-                    runner="pooling",
-                    dtype=DTYPE,
-                    max_model_len=1024,
-                    enforce_eager=True) as vllm_model:
+    with VllmRunner(
+           emb_model_name, runner="pooling", dtype=DTYPE, max_model_len=1024, enforce_eager=True
+    ) as vllm_model:
         vllm_outputs = vllm_model.score(TEXTS_1, TEXTS_2)
 
     assert len(vllm_outputs) == 2
