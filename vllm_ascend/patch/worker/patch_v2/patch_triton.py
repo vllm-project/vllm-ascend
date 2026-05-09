35dc6a77f56e4de733ef82cb099669f352261340
wxsIcey <1790571317@qq.com>
2026-05-09 13:53:36 +0000

    fix
    Signed-off-by: wxsIcey <1790571317@qq.com>

--- vllm_ascend/patch/worker/patch_v2/patch_triton.py
+++ vllm_ascend/patch/worker/patch_v2/patch_triton.py
@@ -3,15 +3,13 @@ from vllm.v1.worker.gpu.sample import bad_words, gumbel, logprob, penalties, pro
 from vllm.v1.worker.gpu.spec_decode import probabilistic_rejection_sampler_utils, rejection_sampler
 from vllm.v1.worker.gpu.spec_decode.eagle import speculator
 
+from vllm_ascend.utils import vllm_version_is
 from vllm_ascend.worker.v2.input_batch import post_update
 from vllm_ascend.worker.v2.sample.bad_words import apply_bad_words
 from vllm_ascend.worker.v2.sample.gumbel import apply_temperature, gumbel_sample
 from vllm_ascend.worker.v2.sample.logprob import compute_token_logprobs, compute_topk_logprobs
 from vllm_ascend.worker.v2.sample.min_p import apply_min_p
 from vllm_ascend.worker.v2.sample.penalties import apply_penalties, bincount
-from vllm_ascend.worker.v2.spec_decode.probabilistic_rejection_sampler_utils import (
-    probabilistic_rejection_sample as npu_probabilistic_rejection_sample,
-)
 
 penalties.apply_penalties = apply_penalties
 # because sampler.py and speculator.py are imported before this patch, they must be overridden
@@ -29,5 +27,10 @@ gumbel.apply_temperature = apply_temperature
 states.apply_temperature = apply_temperature
 logprob.compute_token_logprobs = compute_token_logprobs
 
-probabilistic_rejection_sampler_utils.probabilistic_rejection_sample = npu_probabilistic_rejection_sample
-rejection_sampler.probabilistic_rejection_sample = npu_probabilistic_rejection_sample
+if not vllm_version_is("0.20.1"):
+    from vllm_ascend.worker.v2.spec_decode.probabilistic_rejection_sampler_utils import (
+        probabilistic_rejection_sample as npu_probabilistic_rejection_sample,
+    )
+
+    probabilistic_rejection_sampler_utils.probabilistic_rejection_sample = npu_probabilistic_rejection_sample
+    rejection_sampler.probabilistic_rejection_sample = npu_probabilistic_rejection_sample
