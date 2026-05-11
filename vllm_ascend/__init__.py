563b386c8c17a8e568cdc11140d1335a1ae5e8bf
Pai <416932041@qq.com>
2026-05-11 14:16:53 +0800

    fix
    Signed-off-by: Pai <416932041@qq.com>

--- vllm_ascend/__init__.py
+++ vllm_ascend/__init__.py
@@ -44,6 +44,7 @@ def register():
 def register_connector():
     _ensure_global_patch()
 
+    from vllm_ascend.distributed.ec_transfer import register_ec_connector
     from vllm_ascend.distributed.kv_transfer import register_connector
 
     register_connector()
