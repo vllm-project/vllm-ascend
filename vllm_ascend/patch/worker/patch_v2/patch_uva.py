ad5f135f8253e50d5e3d5e1c7fc7d699934704f6
ChenBoxun <chenboxun1@huawei.com>
2026-05-11 20:37:33 +0800

    [fix]problem of single line too long
    Signed-off-by: ChenBoxun <chenboxun1@huawei.com>

--- vllm_ascend/patch/worker/patch_v2/patch_uva.py
+++ vllm_ascend/patch/worker/patch_v2/patch_uva.py
@@ -102,17 +102,19 @@ class UvaBufferWrapper:
         if key in os.environ:
             value = os.environ[key]
             if not "pinned_mem_register:True" in value:
-                raise RuntimeError(
+                erro_info = (
                     f"UVA is not available, because environment_param {key} "
                     f"lack value 'pinned_mem_register:True', "
                     f"try to add os.environ['PYTORCH_NPU_ALLOC_CONF'] = 'pinned_mem_register:True'"
                 )
+                raise RuntimeError(erro_info)
             self.cpu: torch.Tensor = torch.zeros(size, dtype=dtype, device="cpu", pin_memory=True)
             if not self.cpu.is_pinned():
-                raise RuntimeError(
+                erro_info = (
                     "UVA is not available, because cpu tensor is not pinned, "
                     "try to use .pin_memory() to cpu_tensor wantted to use uva feature"
                 )
+                raise RuntimeError(erro_info)
             self.np = self.cpu.numpy()
             self.uva: torch.Tensor = self.cpu
         else:
