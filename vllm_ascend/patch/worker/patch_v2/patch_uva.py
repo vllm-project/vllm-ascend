e2d8f140bf7b0f65ec0272366b09f73a2033730a
ChenBoxun <chenboxun1@huawei.com>
2026-05-11 10:42:46 +0800

    [fix]problem of single line too long
    Signed-off-by: ChenBoxun <chenboxun1@huawei.com>

--- vllm_ascend/patch/worker/patch_v2/patch_uva.py
+++ vllm_ascend/patch/worker/patch_v2/patch_uva.py
@@ -107,14 +107,14 @@ class UvaBufferWrapper:
                     f"lack value 'pinned_mem_register:True', "
                     f"try to add os.environ['PYTORCH_NPU_ALLOC_CONF'] = 'pinned_mem_register:True'"
                 )
-                self.cpu: torch.Tensor = torch.zeros(size, dtype=dtype, device="cpu", pin_memory=True)
-                if not self.cpu.is_pinned():
-                    raise RuntimeError(
-                        "UVA is not available, because cpu tensor is not pinned, "
-                        "try to use .pin_memory() to cpu_tensor wantted to use uva feature"
-                    )
-                self.np = self.cpu.numpy()
-                self.uva: torch.Tensor = self.cpu
+            self.cpu: torch.Tensor = torch.zeros(size, dtype=dtype, device="cpu", pin_memory=True)
+            if not self.cpu.is_pinned():
+                raise RuntimeError(
+                    "UVA is not available, because cpu tensor is not pinned, "
+                    "try to use .pin_memory() to cpu_tensor wantted to use uva feature"
+                )
+            self.np = self.cpu.numpy()
+            self.uva: torch.Tensor = self.cpu
         else:
             self._cpu: torch.Tensor = torch.zeros(size, dtype=dtype, device="cpu", pin_memory=True)
             self._np = self._cpu.numpy()
