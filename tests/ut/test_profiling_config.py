c15ba644c5bc9623b05025cb6089f8769aa38c35
chenshi <chenshi5012@163.com>
2026-05-12 20:17:23 +0800

    Update test_profiling_config.py
    Signed-off-by: chenshi <chenshi5012@163.com>

--- tests/ut/test_profiling_config.py
+++ tests/ut/test_profiling_config.py
@@ -11,6 +11,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # This file is a part of the vllm-ascend project.
+#
 
 import ast
 import re
