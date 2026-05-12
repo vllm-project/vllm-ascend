95ee7daa78701f4e9d3977f23a00678734a33a64
chenshi <chenshi5012@163.com>
2026-05-12 20:17:05 +0800

    Update test_profiling_config.py
    Signed-off-by: chenshi <chenshi5012@163.com>

--- tests/ut/test_profiling_config.py
+++ tests/ut/test_profiling_config.py
@@ -1,5 +1,3 @@
-# Copyright (c) 2026 Huawei Technologies Co., Ltd. All Rights Reserved.
-# This file is a part of the vllm-ascend project.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +10,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
+# This file is a part of the vllm-ascend project.
+
 import ast
 import re
 from pathlib import Path
