a4cee58053b3224d19ad3d217ded25632952d345
wangli <wangli858794774@gmail.com>
2026-05-13 11:18:07 +0800

    fix patch
    Signed-off-by: wangli <wangli858794774@gmail.com>

--- vllm_ascend/patch/worker/patch_deepseek_mtp.py
+++ vllm_ascend/patch/worker/patch_deepseek_mtp.py
@@ -15,7 +15,7 @@ def get_spec_layer_idx_from_weight_name(config: DeepseekV2Config | DeepseekV3Con
         layer_idx = config.num_hidden_layers
         for i in range(config.num_nextn_predict_layers):
             if (
-                weight_name.startswith(f"models.layers.{layer_idx + i}.")
+                weight_name.startswith(f"model.layers.{layer_idx + i}.")
                 or weight_name.startswith(MTP_ROT_WEIGHT_NAME)
                 or weight_name.startswith(f"layers.{layer_idx + i}.")
             ):
@@ -29,7 +29,7 @@ def get_spec_layer_idx_from_weight_name_020(
     if hasattr(config, "num_nextn_predict_layers") and config.num_nextn_predict_layers > 0:
         layer_idx = config.num_hidden_layers
         for i in range(config.num_nextn_predict_layers):
-            if weight_name.startswith(f"models.layers.{layer_idx + i}.") or weight_name.startswith(MTP_ROT_WEIGHT_NAME):
+            if weight_name.startswith(f"model.layers.{layer_idx + i}.") or weight_name.startswith(MTP_ROT_WEIGHT_NAME):
                 return layer_idx + i
     return None
 
