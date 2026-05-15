38c96d387df4ab205f7f06b8bfdf291f12f410df
orangeBoxun <143923935+orangeBoxun@users.noreply.github.com>
2026-05-15 11:28:28 +0800

    reforce CI to run again
    Signed-off-by: orangeBoxun <143923935+orangeBoxun@users.noreply.github.com>

--- vllm_ascend/worker/v2/sample/logprob.py
+++ vllm_ascend/worker/v2/sample/logprob.py
@@ -15,6 +15,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# 
 # This file is a part of the vllm-ascend project.
 
 import torch
