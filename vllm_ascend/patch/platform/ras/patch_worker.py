787d271e523598e4a151a7f982b3a2948fdf025d
luwenbo <luwenbo10@huawei.com>
2026-05-19 20:21:33 +0800

    Add patch_worker

---
+++ vllm_ascend/patch/platform/ras/patch_worker.py
@@ -0,0 +1,83 @@
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: Copyright contributors to the vLLM project
+import multiprocessing
+import os
+import pickle
+import queue
+import signal
+import threading
+import time
+import traceback
+import weakref
+from collections import deque
+from collections.abc import Callable, Sequence
+from concurrent.futures import Future, InvalidStateError
+from contextlib import suppress
+from dataclasses import dataclass
+from enum import Enum, auto
+from functools import cached_property, partial
+from multiprocessing.connection import Connection
+from multiprocessing.process import BaseProcess
+from multiprocessing.synchronize import Lock as LockType
+from threading import Thread
+from typing import Any, cast
+
+import cloudpickle
+import torch
+
+import vllm.envs as envs
+from vllm.config import VllmConfig
+from vllm.distributed import destroy_distributed_environment, destroy_model_parallel
+from vllm.distributed.device_communicators.shm_broadcast import Handle, MessageQueue
+from vllm.distributed.kv_transfer.kv_connector.utils import KVOutputAggregator
+from vllm.distributed.parallel_state import (
+    get_dcp_group,
+    get_dp_group,
+    get_ep_group,
+    get_inner_dp_world_group,
+    get_pcp_group,
+    get_pp_group,
+    get_tp_group,
+    model_parallel_is_initialized,
+)
+from vllm.envs import enable_envs_cache
+from vllm.logger import init_logger
+from vllm.platforms import current_platform
+from vllm.tracing import instrument, maybe_init_worker_tracer
+from vllm.utils.network_utils import (
+    get_distributed_init_method,
+    get_ip,
+    get_loopback_ip,
+    get_open_port,
+)
+from vllm.utils.system_utils import (
+    _maybe_force_spawn,
+    decorate_logs,
+    get_mp_context,
+    set_process_title,
+)
+from vllm.v1.core.sched.output import GrammarOutput, SchedulerOutput
+from vllm.v1.executor.abstract import Executor, FailureCallback
+from vllm.v1.outputs import AsyncModelRunnerOutput, DraftTokenIds, ModelRunnerOutput
+from vllm.v1.worker.worker_base import WorkerWrapperBase
+
+def enqueue_output(self, output: Any):
+    """Prepares output from the worker and enqueues it to the
+    worker_response_mq. If the output is an Exception, it is
+    converted to a FAILURE response.
+    """
+    logger.info("进入了patch代码的逻辑~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    if isinstance(output, AsyncModelRunnerOutput):
+        try:
+            output = output.get_output()
+        except Exception as e:
+            logger.error("[WorkerDecorator] Enqueue_output detected exception")
+            output = e
+    if isinstance(output, Exception):
+        result = (WorkerProc.ResponseStatus.FAILURE, str(output))
+    else:
+        result = (WorkerProc.ResponseStatus.SUCCESS, output)
+    if (response_mq := self.worker_response_mq) is not None:
+        response_mq.enqueue(result)
+
+vllm.v1.executor.multiproc_executor.MultiprocExecutor.enqueue_output = enqueue_output
