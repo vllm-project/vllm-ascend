1f69aebc4b56b08385e1381ce10c5b9dfae382cf
tiankaijin <tiankaijin1@h-partners.com>
2026-05-20 11:41:49 +0800

    delete file

--- vllm_ascend/recovery/worker_recovery_monitor.py
+++
@@ -1,6 +0,0 @@
-# TODO: implement worker recovery monitor
-class RecoveryMonitor:
-    def __init__(self, engine_index: int, worker_count: int) -> None:
-        self.engine_index = engine_index
-        self.worker_count = worker_count
-        self.step_results = {}
