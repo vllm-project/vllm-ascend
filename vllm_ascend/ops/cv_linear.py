7499f707f8a4d1e13e4c88973e515250bf78443b
jiangpeng36 <jiangpeng36@huawei.com>
2026-05-22 18:57:59 +0800

    rebase mlalog

--- vllm_ascend/ops/cv_linear.py
+++ vllm_ascend/ops/cv_linear.py
@@ -121,6 +121,7 @@ class CVLinearWrapper:
                 output_dtype=self.linear.weight_scale.dtype,
             )
 
+
             if need_unsqz:
                 output = output.unsqueeze(dim=1)
             return output
