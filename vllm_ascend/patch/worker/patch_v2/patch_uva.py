012c04009e87360ee9ef383ab0efa92c1f15a2e5
ChenBoxun <chenboxun1@huawei.com>
2026-05-25 14:49:55 +0800

    [fix]problem of python format(mypy)
    Signed-off-by: ChenBoxun <chenboxun1@huawei.com>

--- vllm_ascend/patch/worker/patch_v2/patch_uva.py
+++ vllm_ascend/patch/worker/patch_v2/patch_uva.py
@@ -99,6 +99,9 @@ class UvaBufferWrapper:
 
     def __init__(self, size: int | Sequence[int], dtype: torch.dtype):
         key = "PYTORCH_NPU_ALLOC_CONF"
+        self._cpu: torch.Tensor
+        self._np: np.ndarray
+        self._uva: torch.Tensor
         if key in os.environ:
             value = os.environ[key]
             if "pinned_mem_register:True" not in value:
