58f14a6b7b4bf74c22fbd1cd5cc1c0f396ce4317
ChenBoxun <chenboxun1@huawei.com>
2026-05-25 14:56:36 +0800

    [fix]problem of python format(mypy)
    Signed-off-by: ChenBoxun <chenboxun1@huawei.com>

--- vllm_ascend/patch/worker/patch_v2/patch_uva.py
+++ vllm_ascend/patch/worker/patch_v2/patch_uva.py
@@ -105,13 +105,13 @@ class UvaBufferWrapper:
         if key in os.environ:
             value = os.environ[key]
             if "pinned_mem_register:True" not in value:
-                # error_info = (
-                #     f"UVA is not available, because environment_param {key} "
-                #     f"lack value 'pinned_mem_register:True', "
-                #     f"try to add os.environ['PYTORCH_NPU_ALLOC_CONF'] = 'pinned_mem_register:True'"
-                # )
-                raise RuntimeError("error")
-            self._cpu: torch.Tensor = torch.zeros(size, dtype=dtype, device="cpu", pin_memory=True)
+                error_info = (
+                    f"UVA is not available, because environment_param {key} "
+                    f"lack value 'pinned_mem_register:True', "
+                    f"try to add os.environ['PYTORCH_NPU_ALLOC_CONF'] = 'pinned_mem_register:True'"
+                )
+                raise RuntimeError(error_info)
+            self._cpu = torch.zeros(size, dtype=dtype, device="cpu", pin_memory=True)
             if not self._cpu.is_pinned():
                 error_info = (
                     "UVA is not available, because cpu tensor is not pinned, "
@@ -119,11 +119,11 @@ class UvaBufferWrapper:
                 )
                 raise RuntimeError(error_info)
             self._np = self._cpu.numpy()
-            self._uva: torch.Tensor = self._cpu
+            self._uva = self._cpu
         else:
-            self._cpu: torch.Tensor = torch.zeros(size, dtype=dtype, device="cpu", pin_memory=True)
+            self._cpu = torch.zeros(size, dtype=dtype, device="cpu", pin_memory=True)
             self._np = self._cpu.numpy()
-            self._uva: torch.Tensor = torch.zeros_like(self._cpu, device="npu")
+            self._uva = torch.zeros_like(self._cpu, device="npu")
             self._modified_indices: set[int] = set()
 
     def _mark_cpu_modified(self, key: int):
