a38788d966943cc379f6eab93f96fc0a1e4161b6
Chen zhichao <chenzhichao33@h-partners.com>
2026-05-26 11:58:21 +0800

    update dockerfile and e2e for py3.12
    Signed-off-by: Chen zhichao <chenzhichao33@h-partners.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -79,7 +79,7 @@ myst_substitutions = {
     "pip_vllm_ascend_version": "0.19.1rc1",
     "pip_vllm_version": "0.19.1",
     # CANN image tag
-    "cann_image_tag": "9.0.0-910b-ubuntu22.04-py3.11",
+    "cann_image_tag": "9.0.0-910b-ubuntu22.04-py3.12",
     # vLLM commit hash for main branch
     "main_vllm_commit": "1ac10f159a09897baada01b14b6a0dd6442aefd6",
     # vLLM tag for main branch
--- tests/e2e/nightly/multi_node/scripts/run.sh
+++ tests/e2e/nightly/multi_node/scripts/run.sh
@@ -119,18 +119,18 @@ install_extra_components() {
     chmod +x ./CANN-custom_ops-sfa-linux.aarch64.run
     ./CANN-custom_ops-sfa-linux.aarch64.run --quiet
     
-    if ! wget -q https://vllm-ascend.obs.cn-north-4.myhuaweicloud.com/vllm-ascend/a3/custom_ops-1.0-cp311-cp311-linux_aarch64.whl; then
+    if ! wget -q https://vllm-ascend.obs.cn-north-4.myhuaweicloud.com/vllm-ascend/a3/custom_ops-1.0-cp312-cp312-linux_aarch64.whl; then
         echo "Failed to download custom_ops wheel"
         return 1
     fi
-    pip install custom_ops-1.0-cp311-cp311-linux_aarch64.whl
+    pip install custom_ops-1.0-cp312-cp312-linux_aarch64.whl
     
     export ASCEND_CUSTOM_OPP_PATH="/usr/local/Ascend/ascend-toolkit/latest/opp/vendors/customize${ASCEND_CUSTOM_OPP_PATH:+:${ASCEND_CUSTOM_OPP_PATH}}"
     export LD_LIBRARY_PATH="/usr/local/Ascend/ascend-toolkit/latest/opp/vendors/customize/op_api/lib/${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}"
     source /usr/local/Ascend/ascend-toolkit/set_env.sh
     
     rm -f CANN-custom_ops-sfa-linux.aarch64.run \
-          custom_ops-1.0-cp311-cp311-linux_aarch64.whl
+          custom_ops-1.0-cp312-cp312-linux_aarch64.whl
     echo "====> Extra components installation completed"
 }
 
