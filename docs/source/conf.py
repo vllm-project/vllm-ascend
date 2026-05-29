1bda41f6f6ed74e65c4dd9166cf38929a9450718
zhangxinyuehfad <starmoon_zhang@163.com>
2026-05-29 11:05:30 +0000

    main2main: step-2 absorb upstream 05c50c721e..284e6f543d (torch stable ABI migration for merge_attn_states, mamba, sampler)
    Signed-off-by: zhangxinyuehfad <starmoon_zhang@163.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -81,7 +81,7 @@ myst_substitutions = {
     # CANN image tag
     "cann_image_tag": "9.0.0-910b-ubuntu22.04-py3.11",
     # vLLM commit hash for main branch
-    "main_vllm_commit": "05c50c721e07035dd36169fa8bb2825a7b952555",
+    "main_vllm_commit": "284e6f543d462016fc80c055ccbf088832c63129",
     # vLLM tag for main branch
     "main_vllm_tag": "v0.20.2",
     # Python version for main branch
