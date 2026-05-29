6e88d78da64cdc636eddc238d3aaa32c67ab485a
Qingsong Zhang <1640410765@qq.com>
2026-05-29 11:06:16 +0800

    main2main adapt
    Signed-off-by: Qingsong Zhang <1640410765@qq.com>

--- vllm_ascend/worker/model_runner_v1.py
+++ vllm_ascend/worker/model_runner_v1.py
@@ -2003,7 +2003,7 @@ class NPUModelRunner(GPUModelRunner):
             ) as kv_connector_output,
         ):
             if self.cache_config.mamba_cache_mode == "align":
-                mamba_utils.do_mamba_copy_block(self._get_mamba_copy_bufs())
+                mamba_utils.do_mamba_copy_block(preprocess_bufs)
             hidden_states = self._model_forward(
                 num_tokens_padded, input_ids, positions, intermediate_tensors, inputs_embeds, **model_kwargs
             )
