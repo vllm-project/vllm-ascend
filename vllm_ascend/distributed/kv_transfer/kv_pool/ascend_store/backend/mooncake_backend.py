06bb1994d24bec3af7c6387b51b867eed62ce8d9
LCAIZJ <leichao139636@163.com>
2026-05-30 23:00:01 +0800

    fix: repair mooncake backend after lazy-init merge and fix UT
    Restore the corrupted _setup_store method (empty body plus a duplicated
    __init__ left by a bad merge with the lazy-init refactor) and integrate
    the ReplicateConfig put-path with the upstream lazy initialization.
    Read preferred_segment / prefer_alloc_in_same_node from self.config, and
    update the MooncakeBackend test mock to provide the lazy-init attributes
    so batch_put_from_multi_buffers is reached.
    
    Signed-off-by: LCAIZJ <leichao139636@163.com>

--- vllm_ascend/distributed/kv_transfer/kv_pool/ascend_store/backend/mooncake_backend.py
+++ vllm_ascend/distributed/kv_transfer/kv_pool/ascend_store/backend/mooncake_backend.py
@@ -52,8 +52,6 @@ class MooncakeBackend(Backend):
             self._store_initialized = True
 
     def _setup_store(self):
-
-    def __init__(self, parallel_config: ParallelConfig):
         try:
             from mooncake.store import MooncakeDistributedStore  # type: ignore
         except ImportError as e:
@@ -72,59 +70,26 @@ class MooncakeBackend(Backend):
             transfer_engine = global_te.get_transfer_engine(local_hostname, device_name=None)
             self.local_seg = local_hostname + ":" + str(transfer_engine.get_rpc_port())
             ret = store.setup(
-                self.local_seg,
-                self.config.metadata_server,
-                self.config.global_segment_size,
-                self.config.local_buffer_size,
-                self.config.protocol,
-                self.config.device_name,
-                self.config.master_server_address,
-                transfer_engine.get_engine(),
+                local_hostname=self.local_seg,
+                metadata_server=self.config.metadata_server,
+                global_segment_size=self.config.global_segment_size,
+                local_buffer_size=self.config.local_buffer_size,
+                protocol=self.config.protocol,
+                rdma_devices=self.config.device_name,
+                master_server_addr=self.config.master_server_address,
+                engine=transfer_engine.get_engine(),
             )
         else:
             self.local_seg = local_hostname
             ret = store.setup(
-                self.local_seg,
-                self.config.metadata_server,
-                self.config.global_segment_size,
-                0,
-                self.config.protocol,
-                self.config.device_name,
-                self.config.master_server_address,
+                local_hostname=self.local_seg,
+                metadata_server=self.config.metadata_server,
+                global_segment_size=self.config.global_segment_size,
+                local_buffer_size=0,
+                protocol=self.config.protocol,
+                rdma_devices=self.config.device_name,
+                master_server_addr=self.config.master_server_address,
             )
-        self.config = MooncakeStoreConfig.load_from_env()
-        self.store = MooncakeDistributedStore()
-        self.preferred_segment = self.config.preferred_segment
-        self.prefer_alloc_in_same_node = self.config.prefer_alloc_in_same_node
-        if self.config.protocol == "ascend":
-            local_hostname = get_ip()
-            # ASCEND_ENABLE_USE_FABRIC_MEM: Enable unified memory address direct transmission scheme
-            # and only can be used for 800 I/T A3 series.
-            # Required supporting hardware versions are as follows:
-            if os.getenv("ASCEND_ENABLE_USE_FABRIC_MEM", "0") != "1":
-                transfer_engine = global_te.get_transfer_engine(local_hostname, device_name=None)
-                self.local_seg = local_hostname + ":" + str(transfer_engine.get_rpc_port())
-                ret = self.store.setup(
-                    local_hostname=self.local_seg,
-                    metadata_server=self.config.metadata_server,
-                    global_segment_size=self.config.global_segment_size,
-                    local_buffer_size=self.config.local_buffer_size,
-                    protocol=self.config.protocol,
-                    rdma_devices=self.config.device_name,
-                    master_server_addr=self.config.master_server_address,
-                    engine=transfer_engine.get_engine(),
-                )
-            else:
-                self.local_seg = local_hostname
-                ret = self.store.setup(
-                    local_hostname=self.local_seg,
-                    metadata_server=self.config.metadata_server,
-                    global_segment_size=self.config.global_segment_size,
-                    local_buffer_size=0,
-                    protocol=self.config.protocol,
-                    rdma_devices=self.config.device_name,
-                    master_server_addr=self.config.master_server_address,
-                )
 
         if ret != 0:
             msg = "Initialize mooncake failed."
@@ -158,9 +123,9 @@ class MooncakeBackend(Backend):
             self._ensure_initialized()
             assert self.store is not None
             config = ReplicateConfig()
-            if self.preferred_segment:
+            if self.config.preferred_segment:
                 config.preferred_segment = self.local_seg
-            config.prefer_alloc_in_same_node = self.prefer_alloc_in_same_node
+            config.prefer_alloc_in_same_node = self.config.prefer_alloc_in_same_node
             res = self.store.batch_put_from_multi_buffers(keys, addrs, sizes, config)
             for value in res:
                 if value < 0:
