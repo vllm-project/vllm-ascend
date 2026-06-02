3451d39ab5f53e0d992382c3667926a3dabfb08c
Pz1116 <zpbzpb123123@gmail.com>
2026-06-02 22:51:56 +0800

    [BugFix] Keep decode store init after real forward
    Signed-off-by: Pz1116 <zpbzpb123123@gmail.com>
    (cherry picked from commit c0722e737e905a83b770254f4f5310e275479a0a)

--- vllm_ascend/distributed/kv_transfer/kv_pool/ascend_store/ascend_store_connector.py
+++ vllm_ascend/distributed/kv_transfer/kv_pool/ascend_store/ascend_store_connector.py
@@ -99,7 +99,7 @@ class AscendStoreConnector(KVConnectorBase_V1, SupportsHMA):
         self._kv_cache_events: AscendStoreKVEvents | None = None
 
         self.sended_but_unfinished_reqs: set[str] = set()
-        self._current_step_needs_store_init = False
+        self._current_step_has_real_forward = False
 
         if role == KVConnectorRole.SCHEDULER:
             self.connector_scheduler = KVPoolScheduler(vllm_config, self.use_layerwise, kv_cache_config)
@@ -195,7 +195,7 @@ class AscendStoreConnector(KVConnectorBase_V1, SupportsHMA):
     def start_load_kv(self, forward_context: "ForwardContext", **kwargs) -> None:
         assert self.connector_worker is not None
         metadata = self._get_connector_metadata()
-        self._current_step_needs_store_init = forward_context is not None and bool(metadata.requests)
+        self._current_step_has_real_forward = forward_context is not None
         logger.debug(
             "KV pool connector start_load_kv metadata_requests=%d specs=%s",
             len(metadata.requests),
@@ -241,11 +241,11 @@ class AscendStoreConnector(KVConnectorBase_V1, SupportsHMA):
         """Get the finished recving and sending requests."""
         assert self.connector_worker is not None
         metadata = self._get_connector_metadata()
-        if self._current_step_needs_store_init:
+        if self._current_step_has_real_forward:
             try:
                 self.connector_worker.ensure_store_initialized()
             finally:
-                self._current_step_needs_store_init = False
+                self._current_step_has_real_forward = False
         done_sending, done_recving = self.connector_worker.get_finished(finished_req_ids, metadata)
         return done_sending, done_recving
 
