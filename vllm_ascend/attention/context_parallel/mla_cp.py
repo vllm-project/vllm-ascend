1ed8cef3e284b2b6f9f140946ac198d52ecdff22
Your Name <you@example.com>
2026-06-02 15:00:31 +0800

    fix mla
    Signed-off-by: Your Name <you@example.com>

--- vllm_ascend/attention/context_parallel/mla_cp.py
+++ vllm_ascend/attention/context_parallel/mla_cp.py
@@ -706,9 +706,7 @@ class AscendMlaCPImpl(AscendMLAImpl):
                     (num_decodes, num_heads, q_nope.shape[1], 1), dtype=torch.float, device=q_nope.device
                 )
             elif input_layout == "BNSD":
-                softmax_lse = torch.empty(
-                    (num_tokens, num_heads, 1, 1), dtype=torch.float, device=q_nope.device
-                )
+                softmax_lse = torch.empty((num_tokens, num_heads, 1, 1), dtype=torch.float, device=q_nope.device)
             else:
                 softmax_lse = torch.empty((num_tokens, num_heads, 1), dtype=torch.float, device=q_nope.device)
 
