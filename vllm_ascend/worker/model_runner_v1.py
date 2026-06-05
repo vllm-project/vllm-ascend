1a8842fc320b7a9785c07cafe04151e7d61018f7
Bai Yongbin <baiyongbin3@h-partners.com>
2026-06-05 14:18:07 +0800

    fix lint
    Signed-off-by: Bai Yongbin <baiyongbin3@h-partners.com>

--- vllm_ascend/worker/model_runner_v1.py
+++ vllm_ascend/worker/model_runner_v1.py
@@ -3407,7 +3407,6 @@ class NPUModelRunner(GPUModelRunner):
                     dummy_compute_logits=dummy_drafter_compute_logits,
                     in_graph_capturing=not force_attention,
                     is_profile=is_profile,
-                    profile_cpp=profile_cpp,
                 )
             if is_profile and self.dynamic_eplb:
                 target = self.model.language_model if hasattr(self.model, "language_model") else self.model
