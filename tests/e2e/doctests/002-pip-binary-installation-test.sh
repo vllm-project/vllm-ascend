9cf95a79957967628440b9e8d32f80e61d904da6
Co_duck <1256938997@qq.com>
2026-06-06 01:31:00 +0800

    Fix vLLM Ascend Test(main)(2)
    Signed-off-by: Co_duck <1256938997@qq.com>

--- tests/e2e/doctests/002-pip-binary-installation-test.sh
+++ tests/e2e/doctests/002-pip-binary-installation-test.sh
@@ -39,7 +39,7 @@ function install_binary_test() {
     config_pip_mirror
     create_vllm_venv
 
-    pip install docutils
+    pip install -r "${SCRIPT_DIR}/../../docs/requirements-docs.txt"
 
     PIP_VLLM_VERSION=$(get_version pip_vllm_version) || _err "Failed to get pip_vllm_version"
     VLLM_VERSION=$(get_version vllm_version) || _err "Failed to get vllm_version"
