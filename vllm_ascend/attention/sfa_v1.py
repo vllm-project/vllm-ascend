d8582154f74de23fbc2152be549e8a75f0c250b0
yyt <yangyit139@gmail.com>
2026-06-11 09:26:29 +0800

    fix(attention): isolate A5 sparse C8 DSA-CP path
    Keep the existing sparse C8 DSA-CP cache contract for non-A5 devices and route only A5 sparse C8 through the MXFP8 block-quantized KV cache path.
    
    Signed-off-by: yyt <yangyit139@gmail.com>

--- vllm_ascend/attention/sfa_v1.py
+++ vllm_ascend/attention/sfa_v1.py
@@ -479,6 +479,9 @@ class AscendSFAImpl(MLAAttentionImpl):
 
         # dsa c8
         self.use_sparse_c8_indexer = ascend_config.is_sparse_c8_layer(self.layer_name)
+        self.use_a5_sparse_c8_indexer = self.use_sparse_c8_indexer and (
+            get_ascend_device_type() == AscendDeviceType.A5
+        )
         if self.use_sparse_c8_indexer:
             if get_ascend_device_type() == AscendDeviceType.A5:
                 self.c8_k_cache_dtype = torch.float8_e4m3fn
@@ -870,7 +873,7 @@ class AscendSFAImpl(MLAAttentionImpl):
         cache_mode = "PA"
 
         if self.enable_dsa_cp:
-            if self.use_sparse_c8_indexer:
+            if self.use_a5_sparse_c8_indexer:
                 k_pe, k_nope, knope_scale = custom_kv_rmsnorm_rope(
                     kv_no_split,
                     self.kv_a_layernorm.weight,  # type: ignore[union-attr]
@@ -883,20 +886,21 @@ class AscendSFAImpl(MLAAttentionImpl):
                     cache_mode=cache_mode,
                     is_output_kv=True,
                 )
-            else:
-                _, _, k_pe, k_nope = torch_npu.npu_kv_rmsnorm_rope_cache(
-                    kv_no_split,
-                    self.kv_a_layernorm.weight,  # type: ignore[union-attr]
-                    cos,
-                    sin,
-                    slots.to(torch.int64),
-                    kv_cache[1],
-                    kv_cache[0],
-                    epsilon=self.kv_a_layernorm.variance_epsilon,  # type: ignore[union-attr]
-                    cache_mode=cache_mode,
-                    is_output_kv=True,
-                )
-                knope_scale = None
+                return k_pe, k_nope, knope_scale
+
+            _, _, k_pe, k_nope = torch_npu.npu_kv_rmsnorm_rope_cache(
+                kv_no_split,
+                self.kv_a_layernorm.weight,  # type: ignore[union-attr]
+                cos,
+                sin,
+                slots.to(torch.int64),
+                kv_cache[1],
+                kv_cache[0],
+                epsilon=self.kv_a_layernorm.variance_epsilon,  # type: ignore[union-attr]
+                cache_mode=cache_mode,
+                is_output_kv=True,
+            )
+            knope_scale = None
             return k_pe, k_nope, knope_scale
         else:
             torch_npu.npu_kv_rmsnorm_rope_cache(
@@ -1228,8 +1232,9 @@ class AscendSFAImpl(MLAAttentionImpl):
                         get_tp_group(),
                         async_op=async_op,
                     )
-                else:
+                elif self.use_a5_sparse_c8_indexer:
                     # due to different dtypes, we have to split commu pass
+                    assert knope_scale is not None
                     assert k_li_scale is not None
                     fused_kv_no_split, _ = all_gather_async(
                         torch.cat(
@@ -1237,6 +1242,30 @@ class AscendSFAImpl(MLAAttentionImpl):
                                 k_nope.view(-1, k_nope.shape[-1]),
                                 k_pe.view(-1, k_pe.shape[-1]),
                                 knope_scale.view(-1, knope_scale.shape[-1]),
+                            ],
+                            dim=1,
+                        ),
+                        get_tp_group(),
+                        async_op=async_op,
+                    )
+                    k_li, _ = all_gather_async(
+                        k_li,
+                        get_tp_group(),
+                        async_op=async_op,
+                    )
+                    k_li_scale, kv_ag_handle = all_gather_async(
+                        k_li_scale,
+                        get_tp_group(),
+                        async_op=async_op,
+                    )
+                else:
+                    # due to different dtypes, we have to split commu pass
+                    assert k_li_scale is not None
+                    fused_kv_no_split, _ = all_gather_async(
+                        torch.cat(
+                            [
+                                k_pe.view(-1, k_pe.shape[-1]),
+                                k_nope.view(-1, k_nope.shape[-1]),
                             ],
                             dim=1,
                         ),
@@ -1276,6 +1305,17 @@ class AscendSFAImpl(MLAAttentionImpl):
                         k_pe, k_nope, k_li = fused_kv_no_split.split(
                             [self.qk_rope_head_dim, self.kv_lora_rank, self.head_dim], dim=-1
                         )
+                    elif self.use_a5_sparse_c8_indexer:
+                        torch_npu.npu_scatter_nd_update_(
+                            kv_cache[0].view(-1, fused_kv_no_split.shape[-1]),
+                            slot_mapping[: attn_metadata.num_actual_tokens].view(-1, 1),
+                            fused_kv_no_split[: attn_metadata.num_actual_tokens],
+                        )
+                        k_pe = None
+                        k_nope = None
+                    else:
+                        k_pe, k_nope = fused_kv_no_split.split([self.qk_rope_head_dim, self.kv_lora_rank], dim=-1)
+                    if not self.use_a5_sparse_c8_indexer:
                         k_nope = k_nope.view(k_nope.shape[0], 1, -1)
                         k_pe = k_pe.view(k_pe.shape[0], 1, -1)
                         DeviceOperator.reshape_and_cache(
@@ -1284,12 +1324,6 @@ class AscendSFAImpl(MLAAttentionImpl):
                             key_cache=kv_cache[0],
                             value_cache=kv_cache[1],
                             slot_mapping=slot_mapping[: attn_metadata.num_actual_tokens],
-                        )
-                    else:
-                        torch_npu.npu_scatter_nd_update_(
-                            kv_cache[0].view(-1, fused_kv_no_split.shape[-1]),
-                            slot_mapping[: attn_metadata.num_actual_tokens].view(-1, 1),
-                            fused_kv_no_split[: attn_metadata.num_actual_tokens],
                         )
 
             k_li = self._get_full_kv(k_li, attn_metadata)
