d9fd9dbcf3f480a1b0048cc17ad67bb03cdf611b
F.Liu <1661888967@qq.com>
2026-06-16 10:10:17 +0800

    clean code
    Signed-off-by: F.Liu <1661888967@qq.com>

--- vllm_ascend/distributed/kv_transfer/kv_p2p/mooncake_connector.py
+++ vllm_ascend/distributed/kv_transfer/kv_p2p/mooncake_connector.py
@@ -84,7 +84,7 @@ class RemotePortInfo(TypedDict):
     host: str
 
 
-class MooncakeAgentMetadata(msgspec.Struct, omit_defaults=True, dict=True):  # type: ignore[call-arg]
+class MooncakeAgentMetadata(msgspec.Struct, omit_defaults=True, dict=True):
     engine_id: str
     te_rpc_port: int
     kv_group2layeridx: dict[int, tuple[dict[str, Any], list[int]]]
