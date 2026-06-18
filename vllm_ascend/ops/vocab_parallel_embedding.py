b785cf04de2af4d8f325ff21a2a8a82b2d92b7e2
Rui "Garry" Gao <garrygaogg@gmail.com>
2026-06-18 17:33:42 +0800

    fix pre-commit
    Signed-off-by: Rui "Garry" Gao <garrygaogg@gmail.com>

--- vllm_ascend/ops/vocab_parallel_embedding.py
+++ vllm_ascend/ops/vocab_parallel_embedding.py
@@ -161,8 +161,8 @@ class AscendVocabParallelEmbedding(VocabParallelEmbedding):
         return input_, ~vocab_mask
 
     def forward(self, input_):
+        # TODO: Remove this temporal fix after 0.23.0
         if input_.device != self.weight.device: 
-            # TODO: Remove this temporal fix after 0.23.0
             input_ = input_.to(self.weight.device)
         if self.forward_type == "embed_tp":
             return self._forward_embed_tp(input_)
