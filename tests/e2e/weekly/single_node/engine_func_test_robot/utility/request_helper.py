7be04ef49e09f38e40c61522cf9b5afb1546d766
chen <1636718796@qq.com>
2026-06-26 11:10:40 +0800

    [CI] mayi case
    Signed-off-by: chen-commits <1636718796@qq.com>
    Signed-off-by: chen <1636718796@qq.com>

--- tests/e2e/weekly/single_node/engine_func_test_robot/utility/request_helper.py
+++ tests/e2e/weekly/single_node/engine_func_test_robot/utility/request_helper.py
@@ -1,3 +1,3 @@
 def send_request(api_client, uri, request_body):
-    """发送请求，返回响应对象"""
+    """Send request and return response object"""
     return api_client.post(uri, json=request_body, headers={"Content-Type": "application/json"})
