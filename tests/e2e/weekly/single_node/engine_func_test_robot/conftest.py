c2b0bf88ba82ecf3ff6a378e63d2a600ae9e7649
chen <1636718796@qq.com>
2026-06-26 14:17:18 +0800

    [CI] mayi case
    Signed-off-by: chen-commits <1636718796@qq.com>
    Signed-off-by: chen <1636718796@qq.com>

--- tests/e2e/weekly/single_node/engine_func_test_robot/conftest.py
+++ tests/e2e/weekly/single_node/engine_func_test_robot/conftest.py
@@ -1,5 +1,4 @@
 import pytest
-from vllm.utils.network_utils import get_open_port
 
 from tests.e2e.conftest import RemoteOpenAIServer
 from tests.e2e.weekly.single_node.engine_func_test_robot.utility.http_client import (
@@ -8,11 +7,9 @@ from tests.e2e.weekly.single_node.engine_func_test_robot.utility.http_client imp
 
 env_dict: dict = {}
 
-server_model_name: str = "auto"
-
 server_args: list = [
     "--served-model-name",
-    server_model_name,
+    "auto",
     "--max-model-len",
     "65536",
     "--tensor-parallel-size",
@@ -34,11 +31,30 @@ server_args: list = [
 
 @pytest.fixture(scope="session")
 def api_client(request):
-    port = get_open_port()
-
     model = "Qwen/Qwen3-VL-30B-A3B-Instruct"
 
-    with (
-        RemoteOpenAIServer(model, server_args, server_port=port, env_dict=env_dict, auto_port=False) as server,
-    ):
+    with RemoteOpenAIServer(model, server_args, env_dict=env_dict, auto_port=True) as server:
         yield HTTPClient(base_url=server.url_root)
+
+
+def pytest_addoption(parser):
+    parser.addoption("--thinkTagOutput", action="store", type=str, required=False)
+    parser.addoption(
+        "--engineArchitecture",
+        action="store",
+        default="pd",
+        choices=["pd", "single"],
+    )
+    parser.addoption(
+        "--maxModelLength",
+        action="store",
+        default="128",
+    )
+    parser.addoption(
+        "--model",
+        action="store",
+        default="qwen",
+    )
+    parser.addoption("--imageNum", action="store", type=int, default=1)
+    parser.addoption("--videoNum", action="store", type=int, default=1)
+    parser.addoption("--audioNum", action="store", type=int, default=1)
