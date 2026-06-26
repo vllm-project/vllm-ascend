6dd3191443adb2441daa1b1dc48666f086745aad
lcfenglinwan <lcfenglin@qq.com>
2026-06-26 15:07:02 +0800

    tmp
    Signed-off-by: lcfenglinwan <lcfenglin@qq.com>

--- vllm_ascend/ops/fused_moe/token_dispatcher.py
+++ vllm_ascend/ops/fused_moe/token_dispatcher.py
@@ -29,7 +29,6 @@ from vllm.config import get_current_vllm_config
 from vllm.distributed.parallel_state import get_ep_group
 
 from vllm_ascend.ascend_config import get_ascend_config
-from vllm_ascend.device.device_op import DeviceOperator
 from vllm_ascend.distributed.parallel_state import get_mc2_group
 from vllm_ascend.ops.fused_moe.comm_utils import async_all_to_all, gather_from_sequence_parallel_region
 from vllm_ascend.ops.fused_moe.moe_runtime_args import (
@@ -573,13 +572,15 @@ class TokenDispatcherWithAll2AllV(MoETokenDispatcher[MoEAllToAllCombineMetadata]
         ) = self._preprocess(topk_ids)
         hidden_shape_before_permute = hidden_states.shape
 
-        permutated_local_input_tokens, reversed_local_input_permutation_mapping, _, _ = torch_npu.npu_moe_init_routing_v2(
-            hidden_states,
-            topk_ids,
-            active_num=num_out_tokens,
-            expert_num=self.num_experts,
-            expert_tokens_num_flag=True,
-            active_expert_range=[0, self.num_experts],
+        permutated_local_input_tokens, reversed_local_input_permutation_mapping, _, _ = (
+            torch_npu.npu_moe_init_routing_v2(
+                hidden_states,
+                topk_ids,
+                active_num=num_out_tokens,
+                expert_num=self.num_experts,
+                expert_tokens_num_flag=True,
+                active_expert_range=[0, self.num_experts],
+            )
         )
 
         return (
