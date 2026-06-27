d4caf01acd96f3bfb65be5be26fc57a524d51631
limuyuan <limuyuan3@huawei.com>
2026-06-27 11:01:53 +0800

    use npu_moe_init_routing_v2 to replace permute for all devices
    Signed-off-by: limuyuan <limuyuan3@huawei.com>

--- vllm_ascend/ops/fused_moe/token_dispatcher.py
+++ vllm_ascend/ops/fused_moe/token_dispatcher.py
@@ -651,41 +651,38 @@ class TokenDispatcherWithAll2AllV(MoETokenDispatcher[MoEAllToAllCombineMetadata]
             "global_input_tokens_local_experts_indices must be provided"
         )
 
-        # Handle MXFP case
-        if use_mxfp_quant:
-            experts_indices_2d_copy = global_input_tokens_local_experts_indices.reshape(
-                global_input_tokens_local_experts_indices.shape[0], 1
-            )
-            dynamic_scale_fp8_copy = dynamic_scale_after_all2all.view(torch.float8_e8m0fnu)
-
-            global_input_tokens, reversed_global_input_permutation_mapping, _, dynamic_scale_fp8_copy = (
-                torch_npu.npu_moe_init_routing_v2(
-                    global_input_tokens,
-                    experts_indices_2d_copy,
-                    scale=dynamic_scale_fp8_copy,
-                    active_num=experts_indices_2d_copy.shape[0],
-                    expert_num=self.num_local_experts,
-                    expert_tokens_num_type=1,
-                    expert_tokens_num_flag=True,
-                    active_expert_range=[0, self.num_local_experts],
-                )
-            )
-            dynamic_scale_after_all2all = dynamic_scale_fp8_copy.view(torch.uint8)
-
-            experts_indices_2d_copy.untyped_storage().resize_(0)
-            return global_input_tokens, dynamic_scale_after_all2all, reversed_global_input_permutation_mapping
-
-        # Handle quantized case
-        if with_quant:
-            dynamic_scale_after_all2all, _ = torch_npu.npu_moe_token_permute(
-                dynamic_scale_after_all2all.unsqueeze(-1), global_input_tokens_local_experts_indices
-            )
-            dynamic_scale_after_all2all = dynamic_scale_after_all2all.squeeze(-1)
-
-        # Non-quantized case
-        global_input_tokens, reversed_global_input_permutation_mapping = torch_npu.npu_moe_token_permute(
-            global_input_tokens, global_input_tokens_local_experts_indices
+        experts_indices_2d_copy = global_input_tokens_local_experts_indices.reshape(
+            global_input_tokens_local_experts_indices.shape[0], 1
         )
+
+        if use_mxfp_quant:
+            dynamic_scale_for_routing = dynamic_scale_after_all2all.view(torch.float8_e8m0fnu)
+        elif with_quant:
+            dynamic_scale_for_routing = dynamic_scale_after_all2all
+        else:
+            dynamic_scale_for_routing = None
+
+        global_input_tokens, reversed_global_input_permutation_mapping, _, routed_scale = (
+            torch_npu.npu_moe_init_routing_v2(
+                global_input_tokens,
+                experts_indices_2d_copy,
+                scale=dynamic_scale_for_routing,
+                active_num=experts_indices_2d_copy.shape[0],
+                expert_num=self.num_local_experts,
+                expert_tokens_num_type=1,
+                expert_tokens_num_flag=True,
+                active_expert_range=[0, self.num_local_experts],
+            )
+        )
+
+        if use_mxfp_quant:
+            dynamic_scale_after_all2all = routed_scale.view(torch.uint8)
+        elif with_quant:
+            dynamic_scale_after_all2all = routed_scale
+        else:
+            dynamic_scale_after_all2all = None
+
+        experts_indices_2d_copy.untyped_storage().resize_(0)
         return global_input_tokens, dynamic_scale_after_all2all, reversed_global_input_permutation_mapping
 
     def _combine_preprocess(
