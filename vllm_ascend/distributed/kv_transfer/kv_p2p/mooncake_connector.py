7a8533c7a22c9ee83cb8d843737495fcd95aed76
wangxiaoteng <wangxiaoteng@huawei.com>
2026-06-30 16:22:06 +0800

    add todo for lv layouts which have different num_dims but save num_heads
    Signed-off-by: wangxiaoteng <wangxiaoteng@huawei.com>

--- vllm_ascend/distributed/kv_transfer/kv_p2p/mooncake_connector.py
+++ vllm_ascend/distributed/kv_transfer/kv_p2p/mooncake_connector.py
@@ -1913,6 +1913,9 @@ class MooncakeConnectorWorker:
 
     @classmethod
     def _get_kv_transfer_spec_key(cls, spec: Any) -> tuple[str, int | None]:
+        # TODO: Extand this key with KV cache layout fields (for example num_dims) 
+        # if a future model has layers with the same number of kv heads but incompatiible
+        # cache shapes.
         return (type(spec).__name__, cls._get_spec_num_key_value_heads(spec))
 
     def _build_kv_group2layeridx(self) -> dict[int, tuple[dict[str, Any], list[int]]]:
