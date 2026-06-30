ecaf127f8b707731690c41158d9f6ffa64e4a70e
Erkong <Erkong@noreply.gitcode.com>
2026-06-30 11:04:34 +0000

    Add solve_tri AscendC custom op
    Signed-off-by: Erkong <Erkong@noreply.gitcode.com>

---
+++ csrc/attention/solve_tri/op_host/op_api/aclnn_solve_tri.h
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) 2025 Tianjin University, Ltd.
+ * BSD 3-Clause License.
+ */
+ #ifndef OP_API_INC_LEVEL0_OP_SOLVE_TRI_H
+ #define OP_API_INC_LEVEL0_OP_SOLVE_TRI_H
+
+ #include "opdev/op_executor.h"
+
+ namespace l0op {
+ const aclTensor* SolveTri(
+     const aclTensor *x,
+     const aclIntArray *cuSeqlensOptional,
+     const aclIntArray *chunkIndicesOptional,
+     const char *layout,
+     const aclTensor *xOut,
+     aclOpExecutor *executor);
+ }
+
+ #endif
