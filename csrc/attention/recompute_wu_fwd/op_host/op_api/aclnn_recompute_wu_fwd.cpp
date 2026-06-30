447919bdec875b3354e5e79d7a9542e04ef4d1ed
Erkong <Erkong@noreply.gitcode.com>
2026-06-30 13:28:44 +0000

    Add RecomputeWUFwd custom op
    Signed-off-by: Erkong <Erkong@noreply.gitcode.com>

---
+++ csrc/attention/recompute_wu_fwd/op_host/op_api/aclnn_recompute_wu_fwd.cpp
@@ -0,0 +1,170 @@
+/**
+ * Copyright (c) 2025 Tianjin University, Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * the BSD 3-Clause License (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ */
+#include "aclnn_recompute_wu_fwd.h"
+#include "recompute_wu_fwd.h"
+#include <dlfcn.h>
+#include <new>
+
+#include "aclnn_kernels/transdata.h"
+#include "aclnn_kernels/contiguous.h"
+#include "acl/acl.h"
+#include "aclnn/aclnn_base.h"
+#include "aclnn_kernels/common/op_error_check.h"
+#include "opdev/common_types.h"
+#include "opdev/data_type_utils.h"
+#include "opdev/format_utils.h"
+#include "opdev/op_dfx.h"
+#include "opdev/op_executor.h"
+#include "opdev/op_log.h"
+#include "opdev/platform.h"
+#include "opdev/shape_utils.h"
+#include "opdev/tensor_view_utils.h"
+#include "opdev/make_op_executor.h"
+
+
+using namespace op;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct RecomputeWUFwdParams {
+    const aclTensor *k = nullptr;
+    const aclTensor *v = nullptr;
+    const aclTensor *beta = nullptr;
+    const aclTensor *a = nullptr;
+    const aclTensor *g = nullptr;
+    const aclTensor *gk = nullptr;
+    const aclIntArray *cuSeqlensOptional = nullptr;
+    const aclIntArray *chunkIndicesOptional = nullptr;
+    int64_t chunkSize = 64;
+    const aclTensor *wOut = nullptr;
+    const aclTensor *uOut = nullptr;
+};
+
+static aclnnStatus CheckNotNull(RecomputeWUFwdParams params)
+{
+    CHECK_COND(params.k != nullptr, ACLNN_ERR_PARAM_NULLPTR, "k must not be nullptr.");
+    CHECK_COND(params.v != nullptr, ACLNN_ERR_PARAM_NULLPTR, "v must not be nullptr.");
+    CHECK_COND(params.beta != nullptr, ACLNN_ERR_PARAM_NULLPTR, "beta must not be nullptr.");
+    CHECK_COND(params.a != nullptr, ACLNN_ERR_PARAM_NULLPTR, "a must not be nullptr.");
+    CHECK_COND(params.g != nullptr, ACLNN_ERR_PARAM_NULLPTR, "g must not be nullptr.");
+    CHECK_COND(params.gk == nullptr, ACLNN_ERR_PARAM_NULLPTR, "gk must be nullptr.");
+
+    CHECK_COND(params.wOut != nullptr, ACLNN_ERR_PARAM_NULLPTR, "wOut must not be nullptr.");
+    CHECK_COND(params.uOut != nullptr, ACLNN_ERR_PARAM_NULLPTR, "uOut must not be nullptr.");
+    return ACLNN_SUCCESS;
+}
+
+static aclnnStatus CheckFormat(RecomputeWUFwdParams params)
+{
+    return ACLNN_SUCCESS;
+}
+
+static aclnnStatus CheckShape(RecomputeWUFwdParams params)
+{
+    return ACLNN_SUCCESS;
+}
+
+static aclnnStatus DataContiguous(const aclTensor *&tensor, aclOpExecutor *executor)
+{
+    tensor = l0op::Contiguous(tensor, executor);
+    CHECK_RET(tensor != nullptr, ACLNN_ERR_INNER_NULLPTR);
+    return ACLNN_SUCCESS;
+}
+
+static aclnnStatus ParamsDataContiguous(RecomputeWUFwdParams &params, aclOpExecutor *executorPtr)
+{
+    CHECK_COND(DataContiguous(params.k, executorPtr) == ACLNN_SUCCESS, ACLNN_ERR_PARAM_INVALID,
+               "Contiguous k failed.");
+    CHECK_COND(DataContiguous(params.v, executorPtr) == ACLNN_SUCCESS, ACLNN_ERR_PARAM_INVALID,
+               "Contiguous v failed.");
+    CHECK_COND(DataContiguous(params.beta, executorPtr) == ACLNN_SUCCESS, ACLNN_ERR_PARAM_INVALID,
+               "Contiguous beta failed.");
+    CHECK_COND(DataContiguous(params.a, executorPtr) == ACLNN_SUCCESS, ACLNN_ERR_PARAM_INVALID,
+               "Contiguous a failed.");
+    CHECK_COND(DataContiguous(params.g, executorPtr) == ACLNN_SUCCESS, ACLNN_ERR_PARAM_INVALID,
+               "Contiguous g failed.");
+
+    return ACLNN_SUCCESS;
+}
+
+static aclnnStatus CheckDtype(RecomputeWUFwdParams params)
+{
+    return ACLNN_SUCCESS;
+}
+
+static aclnnStatus CheckParams(RecomputeWUFwdParams params)
+{
+    CHECK_RET(CheckNotNull(params) == ACLNN_SUCCESS, ACLNN_ERR_PARAM_INVALID);
+    CHECK_RET(CheckFormat(params) == ACLNN_SUCCESS, ACLNN_ERR_PARAM_INVALID);
+    CHECK_RET(CheckShape(params) == ACLNN_SUCCESS, ACLNN_ERR_PARAM_INVALID);
+    CHECK_RET(CheckDtype(params) == ACLNN_SUCCESS, ACLNN_ERR_PARAM_INVALID);
+    return ACLNN_SUCCESS;
+}
+
+aclnnStatus aclnnRecomputeWUFwdGetWorkspaceSize(
+    const aclTensor *k,
+    const aclTensor *v,
+    const aclTensor *beta,
+    const aclTensor *a,
+    const aclTensor *g,
+    const aclTensor *gk,
+    const aclIntArray *cuSeqlensOptional,
+    const aclIntArray *chunkIndicesOptional,
+    int64_t chunkSize,
+    const aclTensor *wOut,
+    const aclTensor *uOut,
+    uint64_t *workspaceSize,
+    aclOpExecutor **executor)
+{
+    RecomputeWUFwdParams params{k, v, beta, a, g, gk,cuSeqlensOptional, chunkIndicesOptional, chunkSize, wOut, uOut};
+    // Standard syntax, Check parameters.
+    L2_DFX_PHASE_1(aclnnRecomputeWUFwd, DFX_IN(k, v, beta, a, g, gk, cuSeqlensOptional, chunkIndicesOptional),
+                   DFX_OUT(wOut, uOut));
+    // 固定写法，创建OpExecutor
+    auto uniqueExecutor = CREATE_EXECUTOR();
+    CHECK_RET(uniqueExecutor.get() != nullptr, ACLNN_ERR_INNER_CREATE_EXECUTOR);
+    auto executorPtr = uniqueExecutor.get();
+    // 固定写法，参数检查
+    auto ret = CheckParams(params);
+    CHECK_RET(ret == ACLNN_SUCCESS, ACLNN_ERR_PARAM_INVALID);
+    CHECK_COND(ParamsDataContiguous(params, executorPtr) == ACLNN_SUCCESS, ACLNN_ERR_PARAM_INVALID,
+               "ParamsDataContiguous failed.");
+    auto result = l0op::RecomputeWUFwd(params.k, params.v, params.beta, params.a, params.g, params.cuSeqlensOptional, params.chunkIndicesOptional, params.chunkSize, params.wOut, params.uOut, executorPtr);
+    CHECK_RET(result[0] != nullptr, ACLNN_ERR_PARAM_NULLPTR);
+    CHECK_RET(result[1] != nullptr, ACLNN_ERR_PARAM_NULLPTR);
+
+    // If the output tensor is non-contiguous, convert the calculated contiguous tensor to non-contiguous.
+    auto viewCopyResult = l0op::ViewCopy(result[0], params.wOut, executorPtr);
+    CHECK_RET(viewCopyResult != nullptr, ACLNN_ERR_INNER_NULLPTR);
+
+    viewCopyResult = l0op::ViewCopy(result[1], params.uOut, executorPtr);
+    CHECK_RET(viewCopyResult != nullptr, ACLNN_ERR_INNER_NULLPTR);
+
+
+    // Standard syntax, get the size of workspace needed during computation.
+    *workspaceSize = uniqueExecutor->GetWorkspaceSize();
+    uniqueExecutor.ReleaseTo(executor);
+    return ACLNN_SUCCESS;
+}
+
+
+aclnnStatus aclnnRecomputeWUFwd(void *workspace, uint64_t workspaceSize, aclOpExecutor *executor, aclrtStream stream)
+{
+    L2_DFX_PHASE_2(aclnnRecomputeWUFwd);
+    CHECK_COND(CommonOpExecutorRun(workspace, workspaceSize, executor, stream) == ACLNN_SUCCESS, ACLNN_ERR_INNER,
+               "This is an error in RecomputeWUFwd launch aicore.");
+    return ACLNN_SUCCESS;
+}
+
+
+#ifdef __cplusplus
+}
+#endif
---
+++ csrc/attention/recompute_wu_fwd/op_host/op_tiling/recompute_wu_fwd_tiling.h
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2025 Tianjin University, Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * the BSD 3-Clause License (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+/*!
+ * \file recompute_wu_fwd_tiling.h
+ * \brief Compatibility include for recompute_wu_fwd tiling data.
+ */
+
+#pragma once
+
+#include "../../op_kernel/recompute_wu_fwd_struct.h"
+
+namespace optiling {
+
+using GDN::RecomputeWUFwdTilingData;
+
+struct RecomputeWUFwdCompileInfo {};
+
+} // namespace optiling
