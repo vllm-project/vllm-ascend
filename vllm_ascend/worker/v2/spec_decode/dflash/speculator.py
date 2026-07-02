868ea6c4881b18fc66a502d8e1d05f383ac39155
wxsIcey <1790571317@qq.com>
2026-07-02 08:16:39 +0000

    fix
    Signed-off-by: wxsIcey <1790571317@qq.com>

--- vllm_ascend/worker/v2/spec_decode/dflash/speculator.py
+++ vllm_ascend/worker/v2/spec_decode/dflash/speculator.py
@@ -5,7 +5,7 @@
 from typing import Any
 
 import torch
-import vllm.v1.worker.gpu.spec_decode.dflash.speculator as dflash_speculator
+import vllm.v1.worker.gpu.spec_decode.dflash.speculator as vllm_dflash_speculator
 from vllm.config import VllmConfig
 from vllm.triton_utils import tl, triton
 from vllm.v1.worker.gpu.input_batch import InputBatch
@@ -188,4 +188,4 @@ def _prepare_dflash_inputs_kernel_ascend(
             tl.store(out_query_slot_mapping_ptr + i, PAD_SLOT_ID)
 
 
-dflash_speculator._prepare_dflash_inputs_kernel = _prepare_dflash_inputs_kernel_ascend
+vllm_dflash_speculator._prepare_dflash_inputs_kernel = _prepare_dflash_inputs_kernel_ascend
