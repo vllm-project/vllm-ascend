cbf559af097a1800fd926483d427b8d7243851df
hulida <hulida@huawei.com>
2026-07-09 16:04:29 +0800

    afd-bugfix

--- vllm_ascend/worker/npu_ffn_model_runner_v1.py
+++ vllm_ascend/worker/npu_ffn_model_runner_v1.py
@@ -196,7 +196,7 @@ class NPUFFNModelRunner(NPUModelRunner, GPUFFNModelRunner):
         #     # Capture mode: capture a single graph based on dp_metadata_list.
         #     self._capture_model(dp_metadata_list=dp_metadata_list)
         logger.info("FFN warmup, dp_metadata_list=%s", dp_metadata_list)
-        self._warmup_model(dp_metadata_list=dp_metadata_list)
+        # self._warmup_model(dp_metadata_list=dp_metadata_list)
         logger.info("FFN warmup completed, dp_metadata_list=%s", dp_metadata_list)
         dp_metadata_list, _, _ = (self.connector.recv_dp_metadata_list())
         self._capture_model(dp_metadata_list=dp_metadata_list)
