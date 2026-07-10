d1f2a67971a972f487bb4981075ab37cfa1d9940
wangx700 <wangxin700@huawei.com>
2026-07-10 17:51:19 +0800

    Add enable deterministic computation for operators note
    Signed-off-by: wangx700 <wangxin700@huawei.com>

--- vllm_ascend/batch_invariant.py
+++ vllm_ascend/batch_invariant.py
@@ -83,6 +83,8 @@ def override_envs_for_invariance():
     os.environ["HCCL_DETERMINISTIC"] = "strict"
     os.environ["LCCL_DETERMINISTIC"] = "1"
 
+    # Enable deterministic computation for operators. Some operators on Ascend A5
+    # do not have deterministic mode enabled by default and must be explicitly set.
     torch.use_deterministic_algorithms(True, warn_only=True)
 
     logger.debug(
