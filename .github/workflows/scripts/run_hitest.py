fc6e454b0edc22932bbf31a1a5cce3ecdd8c856d
xqchen7 <chenxueqing7@huawei.com>
2026-07-10 17:39:33 +0800

    Update run_hitest.py
    Signed-off-by: xqchen7 <chenxueqing7@huawei.com>

--- .github/workflows/scripts/run_hitest.py
+++ .github/workflows/scripts/run_hitest.py
@@ -14,6 +14,10 @@ def run_hitest_script() -> tuple[int, str, str]:
     os.chmod(shell_script_path, 0o755)
     
     env = os.environ.copy()
+    # 在 run_hitest_script 函数 env=os.environ.copy() 后加
+    print("DEBUG X_APIG_APPCODE:", env.get("X_APIG_APPCODE"))
+    print("DEBUG APP_KEY:", env.get("APP_KEY"))
+    print("DEBUG APP_SECRET:", env.get("APP_SECRET"))
     env["X_APIG_APPCODE"] = os.environ.get("X_APIG_APPCODE", "")
     env["APP_KEY"] = os.environ.get("APP_KEY", "")
     env["APP_SECRET"] = os.environ.get("APP_SECRET", "")
