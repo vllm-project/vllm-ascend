416e13f63a272b3124c42e9c095263da0cc87288
xqchen7 <chenxueqing7@huawei.com>
2026-07-10 16:04:00 +0800

    Update hitest.sh
    Signed-off-by: xqchen7 <chenxueqing7@huawei.com>

--- .github/workflows/scripts/hitest.sh
+++ .github/workflows/scripts/hitest.sh
@@ -40,10 +40,13 @@ if [ -z "${MR_THIRD_ID}" ]; then
 fi
 echo "Using current PR number as MR_THIRD_ID: ${MR_THIRD_ID}"
 
-env:
-  X_APIG_APPCODE: ${{ secrets.AUROGON_APPCODE }}
-  APP_KEY: ${{ secrets.AUROGON_APPKEY }}
-  APP_SECRET: ${{ secrets.AUROGON_APPSECRET }}
+# env:
+#   X_APIG_APPCODE: ${{ secrets.AUROGON_APPCODE }}
+#   APP_KEY: ${{ secrets.AUROGON_APPKEY }}
+#   APP_SECRET: ${{ secrets.AUROGON_APPSECRET }}
+X_APIG_APPCODE=${{ secrets.AUROGON_APPCODE }}
+APP_KEY=${{ secrets.AUROGON_APPKEY }}
+APP_SECRET=${{ secrets.AUROGON_APPSECRET }}
 
 # Api1 save mr with retry
 MAX_RETRY=3
