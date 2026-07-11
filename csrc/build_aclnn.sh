fa58667526e4f9e2b295acc195e89f942294b6b5
ZT-AIA <1028681969@qq.com>
2026-07-11 20:06:16 +0800

    storckv aicpu
    Signed-off-by: ZT-AIA <1028681969@qq.com>

--- csrc/build_aclnn.sh
+++ csrc/build_aclnn.sh
@@ -222,6 +222,7 @@ elif [[ "$SOC_VERSION" =~ ^ascend950 ]]; then
         "chunk_fwd_o"
         "chunk_gated_delta_rule_fwd_h"
         "store_kv_block"
+        "store_kv_block_metadata"
     )
 
     CUSTOM_OPS=$(IFS=';'; echo "${CUSTOM_OPS_ARRAY[*]}")
