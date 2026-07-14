36bf0479b637a6665e3b2f89dd9f210535ee706e
Rui "Garry" Gao <garrygaogg@gmail.com>
2026-07-14 09:34:54 +0800

    fix pre-commit
    Signed-off-by: Rui "Garry" Gao <garrygaogg@gmail.com>

--- vllm_ascend/patch/worker/patch_qwen3_5.py
+++ vllm_ascend/patch/worker/patch_qwen3_5.py
@@ -22,7 +22,7 @@ from vllm.distributed import get_tensor_model_parallel_world_size
 from vllm.distributed.parallel_state import get_pp_group
 from vllm.model_executor.layers.mamba.gdn.qwen_gdn_linear_attn import QwenGatedDeltaNetAttention as _GDNBaseCls
 from vllm.model_executor.models.qwen3_5 import (
-    Qwen3_5DecoderLayer
+    Qwen3_5DecoderLayer,
     Qwen3_5ForConditionalGeneration,
     Qwen3_5MoeForConditionalGeneration,
 )
