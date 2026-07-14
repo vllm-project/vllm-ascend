db3a7d2a1578c7b24b9fbdcbdd71c63d0ce75f98
YangShuai52 <shuaiyang047@163.com>
2026-07-14 14:15:51 +0800

    [v0.23.0][BugFix][310P] clean code
    Signed-off-by: YangShuai52 <shuaiyang047@163.com>

--- vllm_ascend/_310p/worker_310p.py
+++ vllm_ascend/_310p/worker_310p.py
@@ -108,9 +108,7 @@ class NPUWorker310(NPUWorker):
 
         if is_rc_device():
             vm = psutil.virtual_memory()
-            self.available_kv_cache_memory_bytes = (
-                self.requested_memory - (vm.total - vm.available)
-            ) // 2
+            self.available_kv_cache_memory_bytes = (self.requested_memory - (vm.total - vm.available)) // 2
         else:
             self.available_kv_cache_memory_bytes = (
                 self.requested_memory - profile_result.non_kv_cache_memory - non_torch_memory_cleared_by_empty_cache
