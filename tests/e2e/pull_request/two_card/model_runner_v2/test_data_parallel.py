3710de85b133f36a9fe5df633677b885dc0eccfb
zouzy <zouzongyu@huawei.com>
2026-07-14 16:49:35 +0800

    fix full_graph_mode for mrv2
    Signed-off-by: zouzy <zouzongyu@huawei.com>

--- tests/e2e/pull_request/two_card/model_runner_v2/test_data_parallel.py
+++ tests/e2e/pull_request/two_card/model_runner_v2/test_data_parallel.py
@@ -17,7 +17,7 @@
 """
 Data parallel inference with Model Runner V2.
 
-Run `pytest tests/e2e/pull_request/two_card/model_runner_v2/test_data_parallel.py`.
+Run `pytest -sv tests/e2e/pull_request/two_card/model_runner_v2/test_data_parallel.py`.
 """
 
 import os
@@ -54,8 +54,42 @@ def test_dsv2_w8a8_dp_eager_mode():
         tensor_parallel_size=1,
         enable_expert_parallel=True,
         max_model_len=1024,
-        enforce_eager=True,  # Note: currently graph mode is not supported for dp in model runner v2
+        enforce_eager=True,
         quantization="ascend",
         distributed_executor_backend="mp",
+    ) as vllm_model:
+        vllm_model.generate(example_prompts, sampling_params=sampling_params)
+
+
+@pytest.mark.parametrize(
+    "compilation_config",
+    [
+        pytest.param(
+            {"cudagraph_mode": "FULL_DECODE_ONLY", "cudagraph_capture_sizes": [4, 8]},
+            id="full_decode_only",
+        ),
+        pytest.param({}, id="default_full_and_piecewise"),
+    ],
+)
+@patch.dict(os.environ, {"VLLM_USE_V2_MODEL_RUNNER": "1"})
+@wait_until_npu_memory_free(target_free_percentage=0.7)
+def test_dsv2_w8a8_dp_graph_mode(compilation_config: dict):
+    example_prompts = [
+        "Hello, my name is",
+        "The president of the United States is",
+        "The capital of France is",
+        "The future of AI is",
+    ]
+    max_tokens = 20
+    sampling_params = SamplingParams(max_tokens=max_tokens, temperature=0.6, top_p=0.95)
+    with DPVllmRunner(
+        MODELS[0],
+        data_parallel_size=2,
+        tensor_parallel_size=1,
+        enable_expert_parallel=True,
+        max_model_len=1024,
+        quantization="ascend",
+        distributed_executor_backend="mp",
+        compilation_config=compilation_config,
     ) as vllm_model:
         vllm_model.generate(example_prompts, sampling_params=sampling_params)
