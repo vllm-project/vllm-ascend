6237acff1bfb49b4b20844d8ccc9ccdabf3317a6
Hongyu Shi <sakura.iostream@gmail.com>
2026-07-16 14:46:17 +0800

    [Del] delete msa_triton_indexer.py make them in one file
    Signed-off-by: Hongyu Shi <sakura.iostream@gmail.com>

--- vllm_ascend/attention/msa_m3.py
+++ vllm_ascend/attention/msa_m3.py
@@ -46,8 +46,6 @@ from vllm.v1.kv_cache_interface import (
 from vllm_ascend.attention.msa_m3_npu import minimax_m3_sparse_attn
 from vllm_ascend.attention.msa_m3_triton import (
     minimax_m3_sparse_attn_decode,
-)
-from vllm_ascend.attention.msa_m3_triton_indexer import (
     minimax_m3_index_decode,
     minimax_m3_index_score,
     minimax_m3_index_topk,
