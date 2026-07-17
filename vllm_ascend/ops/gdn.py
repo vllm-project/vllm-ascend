d1cd46ad12689de95ed376b37c2eeac7d1d3d334
shenzhao <shenzhao9@huawei.com>
2026-07-17 09:55:48 +0800

    [BugFix] Adapt FLA imports after upstream move
    Signed-off-by: shenzhao <shenzhao9@huawei.com>

--- vllm_ascend/ops/gdn.py
+++ vllm_ascend/ops/gdn.py
@@ -15,11 +15,12 @@
 # limitations under the License.
 #
 
+from typing import TYPE_CHECKING
+
 import torch
 from einops import rearrange
 from vllm.distributed import get_pcp_group
 from vllm.forward_context import get_forward_context
-from vllm.model_executor.layers.fla.ops.l2norm import l2norm_fwd
 from vllm.model_executor.layers.mamba.gdn.base import GatedDeltaNetAttention
 from vllm.model_executor.layers.mamba.mamba_utils import MambaStateShapeCalculator
 from vllm.triton_utils import triton
@@ -35,6 +36,13 @@ from vllm_ascend.ops.triton.fla.fused_qkvzba_split_reshape import fused_qkvzba_s
 from vllm_ascend.ops.triton.fla.utils import clear_ssm_states
 from vllm_ascend.ops.triton.mamba.causal_conv1d import extract_last_width
 from vllm_ascend.utils import vllm_version_is
+
+if TYPE_CHECKING:
+    from vllm.third_party.flash_linear_attention.ops.l2norm import l2norm_fwd
+elif vllm_version_is("0.24.0"):
+    from vllm.model_executor.layers.fla.ops.l2norm import l2norm_fwd
+else:
+    from vllm.third_party.flash_linear_attention.ops.l2norm import l2norm_fwd
 
 
 class AscendGatedDeltaNetAttention(GatedDeltaNetAttention):
