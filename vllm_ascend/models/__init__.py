d42f4e0d11f1b00f1d4bb507dd02ab56e2eaa592
unknown <katrina.cxy@gmail.com>
2026-07-17 16:20:08 +0800

    fix ops not found
    Signed-off-by: unknown <katrina.cxy@gmail.com>

--- vllm_ascend/models/__init__.py
+++ vllm_ascend/models/__init__.py
@@ -2,6 +2,8 @@ from vllm import ModelRegistry
 
 
 def register_model():
+    import vllm_ascend.patch.worker.patch_minimax_m3  # noqa: F401
+
     ModelRegistry.register_model(
         "DeepseekV4ForCausalLM",
         "vllm_ascend.models.deepseek_v4:AscendDeepseekV4ForCausalLM",
