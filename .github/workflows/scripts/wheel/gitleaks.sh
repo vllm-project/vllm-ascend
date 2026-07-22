b504bafaf15e2db681049b5c52254cc55f1a8f71
xqchen7 <chenxueqing7@huawei.com>
2026-07-22 14:28:36 +0800

    Delete .github/workflows/scripts/wheel/gitleaks.sh
    Signed-off-by: xqchen7 <chenxueqing7@huawei.com>

--- .github/workflows/scripts/wheel/gitleaks.sh
+++
@@ -1,4 +0,0 @@
-wget https://github.com/gitleaks/gitleaks/releases/download/v8.21.0/gitleaks_8.21.0_linux_x86_64.tar.gz
-tar -xf gitleaks_8.21.0_linux_x86_64.tar.gz
-mv gitleaks /usr/local/bin/
-chmod +x /usr/local/bin/gitleaks
