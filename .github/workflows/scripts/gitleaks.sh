3f2e87176ee44f34cd545e761631a9352364d317
xqchen7 <chenxueqing7@huawei.com>
2026-07-22 14:29:08 +0800

    Create gitleaks.sh
    Signed-off-by: xqchen7 <chenxueqing7@huawei.com>

---
+++ .github/workflows/scripts/gitleaks.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -x
+wget https://github.com/gitleaks/gitleaks/releases/download/v8.21.0/gitleaks_8.21.0_linux_x86_64.tar.gz
+tar -xf gitleaks_8.21.0_linux_x86_64.tar.gz
+mv gitleaks /usr/local/bin/
+chmod +x /usr/local/bin/gitleaks
