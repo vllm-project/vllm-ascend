1f0bb055fc542fdb5e3bbde2ef9f1b6e7767fd33
chenzeyu <2978509328@qq.com>
2026-07-22 18:05:48 +0800

    chore: add trivial comment marker for pre_12019

--- format.sh
+++ format.sh
@@ -17,6 +17,7 @@
 # limitations under the License.
 # This file is a part of the vllm-ascend project.
 # Adapted from https://github.com/vllm-project/vllm/tree/main/tools
+# pre_12019 branch marker
 #
 
 check_command() {
