098ea21f7b49d20a493c818c53d6ffc021e36f90
wangshiqi2026 <wangshiqi@isrc.iscas.ac.cn>
2026-07-23 09:54:50 +0000

    fix: remove legacy _V2_MODEL_RUNNER_SUPPORTED check aligned with vllm 0.24+
    Signed-off-by: wangshiqi2026 <wangshiqi@isrc.iscas.ac.cn>

--- vllm_ascend/patch/worker/__init__.py
+++ vllm_ascend/patch/worker/__init__.py
@@ -73,8 +73,8 @@ import vllm_ascend.patch.worker.patch_v2.patch_eagle_speculator  # noqa
 import vllm_ascend.patch.worker.patch_v2.patch_dflash_speculator  # noqa
 
 # only patch routed experts capture in main2main.
-if _V2_MODEL_RUNNER_SUPPORTED:
-    import vllm_ascend.patch.worker.patch_routed_experts_capture  # noqa
+
+import vllm_ascend.patch.worker.patch_routed_experts_capture  # noqa
 import vllm_ascend.patch.worker.patch_qwen3vl  # noqa
 import vllm_ascend.patch.worker.patch_bailing_moe_linear  # type: ignore # noqa
 import vllm_ascend.patch.worker.patch_glm4v  # noqa
