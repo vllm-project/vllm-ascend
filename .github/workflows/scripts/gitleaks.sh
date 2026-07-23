0ccb5b1d4244ea1125045ebd427464f4d13c320b
xqchen7 <chenxueqing7@huawei.com>
2026-07-23 10:21:22 +0800

    Update gitleaks.sh
    Signed-off-by: xqchen7 <chenxueqing7@huawei.com>

--- .github/workflows/scripts/gitleaks.sh
+++ .github/workflows/scripts/gitleaks.sh
@@ -2,31 +2,16 @@
 set -eo pipefail
 set -x
 
-GITLEAKS_VERSION="v8.21.0"
 BIN_NAME="./gitleaks"
 
 if [ -x "${BIN_NAME}" ]; then
     echo "gitleaks binary exists, skip download"
 else
-    ARCH=$(uname -m)
-    if [[ "${ARCH}" == "x86_64" ]]; then
-        PKG="gitleaks_${GITLEAKS_VERSION#v}_linux_amd64.tar.gz"
-    elif [[ "${ARCH}" == "aarch64" ]]; then
-        PKG="gitleaks_${GITLEAKS_VERSION#v}_linux_arm64.tar.gz"
-    else
-        echo "::error::Unsupported arch: ${ARCH}"
-        exit 1
-    fi
-
-    URL="https://github.com/gitleaks/gitleaks/releases/download/${GITLEAKS_VERSION}/${PKG}"
-    wget -q "${URL}"
-    tar -xf "${PKG}" gitleaks
-    chmod +x ./gitleaks
+    wget --no-host-directories -c --no-check-certificate https://pytorch-package.obs.cn-north-4.myhuaweicloud.com/pta-codecheck/gitleaks
+    chmod +x gitleaks
 fi
 
 ./gitleaks detect \
     --verbose \
     --redact \
     --config=pre-commit/.gitleaks.toml
-
-rm -f gitleaks_*.tar.gz
