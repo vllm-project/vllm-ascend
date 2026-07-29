4f6232dd54009b75dd59adc42855818f2c45463c
joiandjoe <garancemr@icloud.com>
2026-07-29 14:41:31 +0800

    fix: add num_input_tokens to v2/model_states/default/AscendModelState/prepare_attn()/build_attn_metadata()
    Signed-off-by: joiandjoe <garancemr@icloud.com>
    Signed-off-by: Your Name <you@example.com>
    Signed-off-by: jayQH <qinhuaisheng@h-partners.com>
    Signed-off-by: Your Name <you@example.com>
    Signed-off-by: Your <you@example.com>

--- vllm_ascend/worker/v2/model_states/default.py
+++ vllm_ascend/worker/v2/model_states/default.py
@@ -76,6 +76,7 @@ class AscendModelState(DefaultModelState):
             dcp_local_seq_lens=input_batch.dcp_local_seq_lens,
             # extra attributes for ascend npus.
             seq_lens_np=input_batch.seq_lens_np,
+            num_input_tokens=input_batch.num_tokens_after_padding,
             positions=input_batch.positions,
             attn_state=input_batch.attn_state,
             for_cudagraph_capture=for_capture,
