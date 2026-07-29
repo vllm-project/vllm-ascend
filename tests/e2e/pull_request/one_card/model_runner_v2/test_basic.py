16d731efe8d1b4221cdde72d63b5c96f3b9ab72c
zouzy <zouzongyu@huawei.com>
2026-07-29 22:38:41 +0800

    bugfix for makedummy in inputbatch
    Signed-off-by: zouzy <zouzongyu@huawei.com>

--- tests/e2e/pull_request/one_card/model_runner_v2/test_basic.py
+++ tests/e2e/pull_request/one_card/model_runner_v2/test_basic.py
@@ -205,6 +205,7 @@ def test_dflash_spec_decoding(
     ],
 )
 @patch.dict(os.environ, {"VLLM_USE_V2_MODEL_RUNNER": "1"})
+@wait_until_npu_memory_free(target_free_percentage=0.8)
 def test_dspark_spec_decoding(
     model: str,
     dspark_model: str,
@@ -252,6 +253,7 @@ def test_dspark_spec_decoding(
 @pytest.mark.parametrize("max_tokens", [32])
 @pytest.mark.parametrize("enforce_eager", [True])
 @patch.dict(os.environ, {"VLLM_USE_V2_MODEL_RUNNER": "1"})
+@wait_until_npu_memory_free(target_free_percentage=0.8)
 def test_mtp_spec_decoding(
     model: str,
     max_tokens: int,
@@ -300,6 +302,7 @@ def test_mtp_spec_decoding(
     ],
 )
 @patch.dict(os.environ, {"VLLM_USE_V2_MODEL_RUNNER": "1"})
+@wait_until_npu_memory_free(target_free_percentage=0.8)
 def test_qwen3_dense_graph_mode(
     model: str,
     max_tokens: int,
