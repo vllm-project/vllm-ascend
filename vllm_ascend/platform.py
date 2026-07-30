c65580740b976f6acf0eec5f037e5436744d6149
liyishi <1252651434@qq.com>
2026-07-30 19:32:00 +0800

    fix
    Signed-off-by: liyishi <1252651434@qq.com>

--- vllm_ascend/platform.py
+++ vllm_ascend/platform.py
@@ -132,7 +132,7 @@ class NPUPlatform(Platform):
     ray_device_key: str = "NPU"
     device_control_env_var: str = "ASCEND_RT_VISIBLE_DEVICES"
     ray_noset_device_env_vars: list[str] = [
-        "RAY_EXPERIMENTAL_NOSET_ASCEND_RT_VISIBLE_DEVICES",
+        # "RAY_EXPERIMENTAL_NOSET_ASCEND_RT_VISIBLE_DEVICES",
         # Also include CUDA_VISIBLE_DEVICES for Ray versions that detect
         # NPU actors via the CUDA resource path and would otherwise set
         # CUDA_VISIBLE_DEVICES="" on NPU workers (num_gpus=0).
