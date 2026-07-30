845d47b952e856cecd231735db70e008a96ad690
Pz1116 <zpbzpb123123@gmail.com>
2026-07-30 18:48:10 +0800

    fix(kv_pool): align AscendStore request hash granularity
    Signed-off-by: Pz1116 <zpbzpb123123@gmail.com>
    (cherry picked from commit 75b7a379049fd453664fdf4af8de91e2f298bb99)
    Signed-off-by: lijiaqi139 <lijiaqi139@huawei.com>
    Signed-off-by: jiaqi-lee <15316070896@163.com>

--- vllm_ascend/patch/platform/patch_kv_cache_utils.py
+++ vllm_ascend/patch/platform/patch_kv_cache_utils.py
@@ -53,7 +53,8 @@ def _ascend_resolve_kv_cache_block_sizes(
         # multiplied by the CP factors for proper alignment.
         group_block_sizes = [g.kv_cache_spec.block_size for g in groups]
         scheduler_block_size = math.lcm(*group_block_sizes) * dcp
-        if not cache_config.enable_prefix_caching:
+        connector_enabled = vllm_config.kv_transfer_config is not None
+        if not (cache_config.enable_prefix_caching or connector_enabled):
             return scheduler_block_size, scheduler_block_size
         hash_block_size = math.gcd(*group_block_sizes)
         return scheduler_block_size, hash_block_size
