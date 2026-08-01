1b21fa6d1ea8573d065a77699a7030d6e09451ff
weiguihua2 <weiguihua2@huawei.com>
2026-08-01 12:13:53 +0800

    [Feature]dsv4 dsa_cp support dspark
    Signed-off-by: weiguihua2 <weiguihua2@huawei.com>

--- tests/e2e/pull_request/four_card/spec_decode/test_dspark_deepseekv4.py
+++ tests/e2e/pull_request/four_card/spec_decode/test_dspark_deepseekv4.py
@@ -35,10 +35,26 @@ os.environ["VLLM_WORKER_MULTIPROC_METHOD"] = "spawn"
 
 
 @pytest.mark.parametrize("model_name", MODELS)
+@pytest.mark.parametrize(
+    ("golden", "num_speculative_tokens", "additional_config"),
+    [
+        pytest.param(
+            [0.88, 0.74, 0.58, 0.49, 0.40], 5, {"enable_flashcomm1": False, "enable_dsa_cp": False},
+            id="dspark",
+        ),
+        pytest.param(
+            [0.88, 0.74, 0.58, 0.49, 0.40, 0.30, 0.18], 7, {"enable_flashcomm1": True, "enable_dsa_cp": True},
+            id="dsa-cp-dspark",
+        ),
+    ],
+)
 @patch.dict(os.environ, {"HCCL_BUFFSIZE": "1024"})
-def test_deepseek_v4_dspark_acceptance_tp4(model_name):
-    golden = [0.88, 0.74, 0.58, 0.49, 0.40]
-
+def test_deepseek_v4_dspark_acceptance_tp4(
+    model_name,
+    golden,
+    num_speculative_tokens,
+    additional_config,
+):
     example_prompts = [
         "Hello, my name is",
         "The president of the United States is",
@@ -56,67 +72,20 @@ def test_deepseek_v4_dspark_acceptance_tp4(model_name):
         disable_log_stats=False,
         speculative_config={
             "method": "dspark",
-            "num_speculative_tokens": 5,
+            "num_speculative_tokens": num_speculative_tokens,
             "enforce_eager": True,
         },
-        compilation_config=CompilationConfig(cudagraph_mode="FULL_DECODE_ONLY", cudagraph_capture_sizes=[6, 18]),
+        compilation_config=CompilationConfig(
+            cudagraph_mode="FULL_DECODE_ONLY",
+            cudagraph_capture_sizes=[6, 8, 16, 18],
+        ),
+        additional_config=additional_config,
     ) as spec_vllm_model:
         _ = spec_vllm_model.generate_greedy(example_prompts, max_tokens)
         metrics = spec_vllm_model.model.get_metrics()
 
     num_drafts = 0
-    num_accepted_tokens_per_pos = [0] * 5
-    for metric in metrics:
-        if metric.name == "vllm:spec_decode_num_drafts":
-            assert isinstance(metric, Counter)
-            num_drafts += metric.value
-        elif metric.name == "vllm:spec_decode_num_accepted_tokens_per_pos":
-            assert isinstance(metric, Vector)
-            for pos in range(len(metric.values)):
-                num_accepted_tokens_per_pos[pos] += metric.values[pos]
-
-    acceptance_per_pos = [num_accepted_tokens / num_drafts for num_accepted_tokens in num_accepted_tokens_per_pos]
-
-    match = all((a >= b) or (b - a < 0.03) for a, b in zip(acceptance_per_pos, golden))
-    assert match, (
-        f"acceptance_per_pos {acceptance_per_pos} is not greater than golden {golden} (num_drafts={num_drafts})"
-    )
-    cleanup_dist_env_and_memory()
-
-
-@pytest.mark.parametrize("model_name", MODELS)
-@patch.dict(os.environ, {"HCCL_BUFFSIZE": "1024"})
-def test_deepseek_v4_dsa_cp_dspark_acceptance_tp4(model_name):
-    golden = [0.88, 0.74, 0.58, 0.49, 0.40, 0.30, 0.18]
-
-    example_prompts = [
-        "Hello, my name is",
-        "The president of the United States is",
-        "The capital of France is",
-        "The future of AI is",
-    ]
-
-    max_tokens = 1024
-
-    with VllmRunner(
-        model_name,
-        tensor_parallel_size=4,
-        max_model_len=4096,
-        enable_expert_parallel=True,
-        disable_log_stats=False,
-        speculative_config={
-            "method": "dspark",
-            "num_speculative_tokens": 7,
-            "enforce_eager": True,
-        },
-        additional_config={"enable_flashcomm1": True, "enable_dsa_cp": True},
-        compilation_config=CompilationConfig(cudagraph_mode="FULL_DECODE_ONLY", cudagraph_capture_sizes=[8, 16]),
-    ) as spec_vllm_model:
-        _ = spec_vllm_model.generate_greedy(example_prompts, max_tokens)
-        metrics = spec_vllm_model.model.get_metrics()
-
-    num_drafts = 0
-    num_accepted_tokens_per_pos = [0] * 7
+    num_accepted_tokens_per_pos = [0] * num_speculative_tokens
     for metric in metrics:
         if metric.name == "vllm:spec_decode_num_drafts":
             assert isinstance(metric, Counter)
