8b68eb80ff7cbe1299ef0da1269b6dfffe0d15df
shiqiangA <shiqiang15@huawei.com>
2026-08-05 15:58:59 +0800

    [Test] trigger coverage recommendation for moe_mlp
    Signed-off-by: shiqiangA <shiqiang15@huawei.com>

--- vllm_ascend/ops/fused_moe/moe_mlp.py
+++ vllm_ascend/ops/fused_moe/moe_mlp.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # This file is a part of the vllm-ascend project.
+# trigger coverage recommendation test
 
 
 import torch
