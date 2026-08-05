b50a1b653a292cbedeb5033217e84d4fd63839e8
xqchen7 <chenxueqing7@huawei.com>
2026-08-05 11:35:12 +0800

    Update select_tests.py
    Signed-off-by: xqchen7 <chenxueqing7@huawei.com>

--- .github/workflows/scripts/select_tests.py
+++ .github/workflows/scripts/select_tests.py
@@ -744,7 +744,6 @@ def main():
         action="store_true",
         help="Run tests for all configured modules regardless of changed files",
     )
-  
     parser.add_argument(
         "--modules",
         type=str,
