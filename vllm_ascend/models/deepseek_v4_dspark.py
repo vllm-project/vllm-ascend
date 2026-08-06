29dc512851e32857d7a4c79ce061b2e45af22d33
lilinsiman <lilinsiman@gmail.com>
2026-08-06 16:15:18 +0800

    add note
    Signed-off-by: lilinsiman <lilinsiman@gmail.com>

--- vllm_ascend/models/deepseek_v4_dspark.py
+++ vllm_ascend/models/deepseek_v4_dspark.py
@@ -276,6 +276,7 @@ class DSparkDeepseekV4ForCausalLM(nn.Module, DeepseekV2MixtureOfExperts):
         assert vllm_config.speculative_config is not None
         self.config = vllm_config.speculative_config.draft_model_config.hf_config
 
+        # check if quant config exist
         from vllm_ascend.patch.worker.patch_draft_quarot import get_rotation_path
         self.rotation_path = get_rotation_path(vllm_config) if vllm_config.quant_config is not None else None
 
