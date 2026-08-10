5c6c49650d75e6fd4370910f2df5f10b38091a2f
lizy124 <1950471827@qq.com>
2026-08-10 17:34:15 +0800

    Fix imports after backend refactor
    Signed-off-by: lizy124 <1950471827@qq.com>

--- vllm_ascend/attention/context_parallel/dsa_cp.py
+++ vllm_ascend/attention/context_parallel/dsa_cp.py
@@ -26,7 +26,6 @@ from vllm_ascend.attention.utils import (
 )
 from vllm_ascend.core.kv_cache_interface import AscendMLAAttentionSpec
 from vllm_ascend.device.device_op import DeviceOperator
-from vllm_ascend.distributed.utils import all_gather_async
 from vllm_ascend.distributed.kv_transfer.kv_pool.ascend_store.attention_fence import record_attention_compute_start
 from vllm_ascend.ops.linear import AscendUnquantizedLinearMethod
 from vllm_ascend.ops.rope_dsv4 import RopeDataProxy, get_cos_and_sin_dsa, get_full_cos_and_sin_dsa
