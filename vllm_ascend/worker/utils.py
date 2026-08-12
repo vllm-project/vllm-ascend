d3fb8948af4c5d9ed78316e83e4745dcb64c2113
mxc <meixianchang@huawei.com>
2026-08-12 15:59:27 +0800

    modify copy
    Signed-off-by: mxc <meixianchang@huawei.com>

--- vllm_ascend/worker/utils.py
+++ vllm_ascend/worker/utils.py
@@ -13,9 +13,9 @@ from vllm_ascend.ops.triton.triton_utils import get_vectorcore_num
 
 
 def copy_snapshot_to_gpu(buffer: CpuGpuBuffer) -> torch.Tensor:
-    """Copy a pinned CPU snapshot so later host writes do not race the copy."""
-    cpu_snapshot = buffer.cpu.clone().pin_memory()
-    return buffer.gpu.copy_(cpu_snapshot, non_blocking=True)
+    """Copy a CPU snapshot so later host writes do not race the copy."""
+    cpu_snapshot = buffer.cpu.clone()
+    return buffer.gpu.copy_(cpu_snapshot, non_blocking=False)
 
 
 @triton.jit
