92165ebd59c3751444511f14bbb440047fb7bc84
root <root@vllm-ascend.local>
2026-08-13 12:18:50 +0000

    rebase
    Signed-off-by: root <root@vllm-ascend.local>

--- vllm_ascend/worker/v2/spec_decode/dspark/speculator.py
+++ vllm_ascend/worker/v2/spec_decode/dspark/speculator.py
@@ -27,7 +27,6 @@ from vllm.v1.worker.gpu.spec_decode.dspark.speculator import (
     DSparkSpeculator,
 )
 
-from vllm_ascend.utils import vllm_version_is
 from vllm_ascend.worker.v2.attn_utils import (
     build_attn_metadata_wrapper,
     build_draft_attn_metadata_factory,
