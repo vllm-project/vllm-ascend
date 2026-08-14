08a0ad2af1f9400c6faf713a7f57f7c6a8c83e83
Liam <ml646@duke.edu>
2026-08-14 14:27:15 +0800

    [MRV2][BugFix][Kernel] Support apply_thinking_budget on Ascend
    Signed-off-by: Liam <ml646@duke.edu>

--- vllm_ascend/patch/worker/patch_v2/patch_triton.py
+++ vllm_ascend/patch/worker/patch_v2/patch_triton.py
@@ -1,7 +1,16 @@
 from vllm.triton_utils import triton
 from vllm.v1.worker.gpu import structured_outputs
 from vllm.v1.worker.gpu.metrics import logits as metrics_logits
-from vllm.v1.worker.gpu.sample import bad_words, gumbel, logprob, penalties, prompt_logprob, sampler, states
+from vllm.v1.worker.gpu.sample import (
+    bad_words,
+    gumbel,
+    logprob,
+    penalties,
+    prompt_logprob,
+    sampler,
+    states,
+    thinking_budget,
+)
 from vllm.v1.worker.gpu.spec_decode import rejection_sampler, rejection_sampler_utils
 from vllm.v1.worker.gpu.spec_decode.dflash import speculator as dflash_speculator
 from vllm.v1.worker.gpu.spec_decode.eagle import speculator
@@ -11,6 +20,10 @@ from vllm_ascend.worker.v2.sample.gumbel import apply_temperature, gumbel_sample
 from vllm_ascend.worker.v2.sample.logprob import compute_token_logprobs, compute_topk_logprobs
 from vllm_ascend.worker.v2.sample.min_p import apply_min_p
 from vllm_ascend.worker.v2.sample.penalties import apply_penalties, bincount
+from vllm_ascend.worker.v2.sample.thinking_budget import (
+    _load_effective_token_ascend,
+    _update_committed_marker_cache_kernel_ascend,
+)
 from vllm_ascend.worker.v2.spec_decode.dflash.speculator import _prepare_dflash_inputs_kernel_ascend
 from vllm_ascend.worker.v2.spec_decode.rejection_sampler_utils import (
     rejection_sample as npu_rejection_sample,
@@ -35,3 +48,6 @@ rejection_sampler_utils.rejection_sample = npu_rejection_sample
 rejection_sampler.rejection_sample = npu_rejection_sample
 dflash_speculator._prepare_dflash_inputs_kernel = _prepare_dflash_inputs_kernel_ascend
 metrics_logits.libdevice = triton.language.extra.cann.libdevice
+thinking_budget._load_effective_token = _load_effective_token_ascend
+# remove _update_committed_marker_cache_kernel patch when TA upgrade to 3.6
+thinking_budget._update_committed_marker_cache_kernel = _update_committed_marker_cache_kernel_ascend
