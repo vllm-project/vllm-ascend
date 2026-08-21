b8b909fc685f5e4e50fba5c0da885e9453211976
周天扬 <zhoutianyang@huawei.com>
2026-08-21 20:06:57 +0800

    update problem

---
+++ scrips/start-docker.sh
@@ -0,0 +1,24 @@
+IMAGES_ID=$1
+NAME=$2
+if [ $# -ne 2 ]; then
+    echo "error: need one argument describing your container name."
+    exit 1
+fi
+docker run --name ${NAME} -it -d --net=host --shm-size=500g \
+    --privileged=true \
+    -w /home \
+    --device=/dev/davinci_manager \
+    --device=/dev/hisi_hdc \
+    --device=/dev/devmm_svm \
+    --entrypoint=bash \
+    -v /usr/local/Ascend/driver:/usr/local/Ascend/driver \
+    -v /usr/local/dcmi:/usr/local/dcmi \
+    -v /usr/local/bin/npu-smi:/usr/local/bin/npu-smi \
+    -v /etc/ascend_install.info:/etc/ascend_install.info \
+    -v /usr/local/sbin:/usr/local/sbin \
+    -v /home:/home \
+    -v /data:/data \
+    -v /tmp:/tmp \
+    -v /mnt:/mnt \
+    -v /usr/share/zoneinfo/Asia/Shanghai:/etc/localtime \
+    ${IMAGES_ID}
