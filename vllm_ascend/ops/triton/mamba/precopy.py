a88d3fcfd0cd24da37aae3fd7ba83dc808211c5d
Lethobenthos20 <3205604419@qq.com>
2026-08-24 14:10:13 +0800

    apply pre-commit formatting
    Signed-off-by: Lethobenthos20 <3205604419@qq.com>

--- vllm_ascend/ops/triton/mamba/precopy.py
+++ vllm_ascend/ops/triton/mamba/precopy.py
@@ -52,29 +52,18 @@ def _copy_mamba_state_block(
         dim_rows = tl.load(state_dim_row_count_ptr + state_idx)
         row_stride = tl.load(state_dim_row_stride_ptr + state_idx)
 
-        per_row_bytes = (
-            (conv_width - token_bias).to(tl.int64) * state_elem_size
-        )
+        per_row_bytes = (conv_width - token_bias).to(tl.int64) * state_elem_size
 
         bias_bytes = token_bias.to(tl.int64) * state_elem_size
 
-        src_block_addr = (
-            state_base_addr
-            + src_block_id * state_block_stride
-        )
+        src_block_addr = state_base_addr + src_block_id * state_block_stride
 
         offsets = tl.arange(0, COPY_BLOCK_SIZE)
 
         for row in range(0, dim_rows):
-            row_src = (
-                src_block_addr
-                + row * row_stride
-                + bias_bytes
-            ).to(tl.pointer_type(tl.uint8))
+            row_src = (src_block_addr + row * row_stride + bias_bytes).to(tl.pointer_type(tl.uint8))
 
-            row_dst = (
-                dst_addr + row * row_stride
-            ).to(tl.pointer_type(tl.uint8))
+            row_dst = (dst_addr + row * row_stride).to(tl.pointer_type(tl.uint8))
 
             for offset in range(0, per_row_bytes, COPY_BLOCK_SIZE):
                 mask = offset + offsets < per_row_bytes
@@ -93,27 +82,13 @@ def _copy_mamba_state_block(
         return
 
     if is_conv_state:
-        src_block_id = tl.load(
-            block_table_base + src_col
-        ).to(tl.int64)
+        src_block_id = tl.load(block_table_base + src_col).to(tl.int64)
 
-        src_offset = (
-            token_bias.to(tl.int64)
-            * state_inner_size
-            * state_elem_size
-        )
+        src_offset = token_bias.to(tl.int64) * state_inner_size * state_elem_size
 
-        src_addr = (
-            state_base_addr
-            + src_block_id * state_block_stride
-            + src_offset
-        )
+        src_addr = state_base_addr + src_block_id * state_block_stride + src_offset
 
-        copy_size = (
-            (conv_width - token_bias).to(tl.int64)
-            * state_inner_size
-            * state_elem_size
-        )
+        copy_size = (conv_width - token_bias).to(tl.int64) * state_inner_size * state_elem_size
 
         offsets = tl.arange(0, COPY_BLOCK_SIZE)
 
@@ -136,14 +111,9 @@ def _copy_mamba_state_block(
 
         return
 
-    actual_src_block_id = tl.load(
-        block_table_base + src_col + token_bias
-    ).to(tl.int64)
+    actual_src_block_id = tl.load(block_table_base + src_col + token_bias).to(tl.int64)
 
-    src_addr = (
-        state_base_addr
-        + actual_src_block_id * state_block_stride
-    )
+    src_addr = state_base_addr + actual_src_block_id * state_block_stride
 
     copy_size = state_inner_size * state_elem_size
 
@@ -173,13 +143,9 @@ def _copy_mamba_state_block(
 
     tail_offsets = tl.arange(0, 8)
 
-    tail_src = (
-        src_addr + tail_start
-    ).to(tl.pointer_type(tl.uint8))
+    tail_src = (src_addr + tail_start).to(tl.pointer_type(tl.uint8))
 
-    tail_dst = (
-        dst_addr + tail_start
-    ).to(tl.pointer_type(tl.uint8))
+    tail_dst = (dst_addr + tail_start).to(tl.pointer_type(tl.uint8))
 
     tail_mask = tail_offsets < tail_bytes
 
@@ -223,9 +189,7 @@ def precopy_mamba_align_fused_kernel(
         return
 
     if HAS_IDX_MAPPING:
-        req_idx = tl.load(
-            idx_mapping_ptr + batch_idx
-        )
+        req_idx = tl.load(idx_mapping_ptr + batch_idx)
 
         if req_idx < 0:
             return
@@ -234,16 +198,12 @@ def precopy_mamba_align_fused_kernel(
 
     src_col = tl.load(src_col_ptr + req_idx)
 
-    dst_col = tl.load(
-        mamba_state_idx_ptr + req_idx
-    )
+    dst_col = tl.load(mamba_state_idx_ptr + req_idx)
 
     if src_col < 0 or src_col == dst_col:
         return
 
-    token_bias = tl.load(
-        token_bias_ptr + req_idx
-    )
+    token_bias = tl.load(token_bias_ptr + req_idx)
 
     _copy_mamba_state_block(
         state_idx,
