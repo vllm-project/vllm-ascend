d83dd21f58edac577be0f9ba33c3ae3576f81156
kk_1999 <97623903+kk-ss1999@users.noreply.github.com>
2026-08-25 21:34:37 +0800

    test(config): drop removed transpose KV env behavior case
    Signed-off-by: kk_1999 <97623903+kk-ss1999@users.noreply.github.com>

--- tests/ut/test_ascend_config.py
+++ tests/ut/test_ascend_config.py
@@ -508,15 +508,6 @@ class TestAscendConfig(TestBase):
         self.assertTrue(ascend_config.msmonitor_use_daemon)
 
     @_clean_up_ascend_config
-    @patch("vllm_ascend.platform.NPUPlatform.check_and_update_config")
-    def test_transpose_kv_cache_by_block_ignores_removed_env(self, mock_fix_incompatible_config):
-        test_vllm_config = VllmConfig()
-        with patch.dict(os.environ, {"VLLM_ASCEND_FUSION_OP_TRANSPOSE_KV_CACHE_BY_BLOCK": "0"}):
-            ascend_config = init_ascend_config(test_vllm_config)
-
-        self.assertTrue(ascend_config.enable_transpose_kv_cache_by_block)
-
-    @_clean_up_ascend_config
     @patch("vllm_ascend.ascend_config.logger.warning")
     def test_flashcomm_config_warns(self, mock_warning):
         test_vllm_config = VllmConfig()
