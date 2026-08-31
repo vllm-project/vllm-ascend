4072946091931bb5fc73e8c95453a3a2825e2f0e
LostFox11 <wangziyue17@huawei.com>
2026-08-31 16:26:58 +0800

    revert: remove Eagle draft config integration
    Revert the integration of vllm-ascend PR 15354 and restore the previous shared draft parallel configuration.
    
    Signed-off-by: LostFox11 <wangziyue17@huawei.com>

--- vllm_ascend/worker/v2/spec_decode/autoregressive/speculator.py
+++ vllm_ascend/worker/v2/spec_decode/autoregressive/speculator.py
@@ -100,6 +100,8 @@ class AscendAutoRegressiveSpeculator(AutoRegressiveSpeculator):
         parallel_config = replace(
             self.vllm_config.parallel_config,
             pipeline_parallel_size=1,
+            enable_expert_parallel=False,
+            enable_eplb=False,
         )
         return replace(
             self.vllm_config,
