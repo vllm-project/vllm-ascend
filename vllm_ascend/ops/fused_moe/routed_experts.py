63a0c3caa869c1bc9a0ff6a9764484f582f3861c
Spicy-Stick <873805887@qq.com>
2026-09-03 14:17:49 +0800

    fix eplb v2 with unquant
    Signed-off-by: Spicy-Stick <873805887@qq.com>

--- vllm_ascend/ops/fused_moe/routed_experts.py
+++ vllm_ascend/ops/fused_moe/routed_experts.py
@@ -66,10 +66,12 @@ class AscendUnquantizedFusedMoEMethod(UnquantizedFusedMoEMethod):
     @staticmethod
     def get_eplb_weight_views(layer) -> list[torch.Tensor]:
         weights = [layer.w13_weight, layer.w2_weight]
-        if layer.w13_bias is not None:
-            weights.append(layer.w13_bias)
-        if layer.w2_bias is not None:
-            weights.append(layer.w2_bias)
+        w13_bias = getattr(layer, "w13_bias", None)
+        w2_bias = getattr(layer, "w2_bias", None)
+        if w13_bias is not None:
+            weights.append(w13_bias)
+        if w2_bias is not None:
+            weights.append(w2_bias)
         return weights
 
     def process_weights_after_loading(self, layer):
