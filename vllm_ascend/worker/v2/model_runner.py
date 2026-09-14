bb3a17a999a3fdab567ff4472a34750adb521e69
wxsIcey <1790571317@qq.com>
2026-09-14 06:40:20 +0000

    fix bug
    Signed-off-by: wxsIcey <1790571317@qq.com>

--- vllm_ascend/worker/v2/model_runner.py
+++ vllm_ascend/worker/v2/model_runner.py
@@ -410,7 +410,7 @@ class NPUModelRunner(GPUModelRunner):
         # Some attention backends like FA3 require query_start_loc to be non-decreasing.
         query_start_loc_np[num_reqs + 1 :] = num_tokens
 
-        if self.use_fia and batch_desc.cg_mode == CUDAGraphMode.FULL and not adaptive_verification_active:
+        if self.use_fia and batch_desc.cg_mode == CUDAGraphMode.FULL and not adaptive_verification_manager:
             # This is only required for vllm-ascend.
             query_start_loc_np, num_reqs_padded = self._pad_query_start_loc_for_fia(
                 num_tokens_after_padding,
@@ -435,7 +435,7 @@ class NPUModelRunner(GPUModelRunner):
                 query_start_loc_np[: num_reqs + 1] = query_start_loc[: num_reqs + 1].cpu().numpy()
                 query_start_loc_np[num_reqs + 1 :] = int(query_start_loc_np[num_reqs])
 
-        if self.use_fia and adaptive_verification_active:
+        if self.use_fia and adaptive_verification_manager:
             if batch_desc.cg_mode == CUDAGraphMode.FULL:
                 query_start_loc_np, num_reqs_padded = self._pad_adaptive_query_start_loc_for_fia(
                     num_tokens_after_padding,
