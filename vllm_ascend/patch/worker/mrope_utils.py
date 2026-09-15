54a3190bda4902d1123e745c7d76df73bee2f106
Yujian Guo <guoyujian8@hotmail.com>
2026-09-15 17:04:11 +0800

    [Ops][MRoPE] Apply validated inline cos/sin implementation to vllm_ascend/patch/worker/mrope_utils.py
    Signed-off-by: Yujian Guo <guoyujian8@hotmail.com>

--- vllm_ascend/patch/worker/mrope_utils.py
+++ vllm_ascend/patch/worker/mrope_utils.py
@@ -1,12 +1,6 @@
 import torch
 
-from vllm_ascend import envs as ascend_envs
 from vllm_ascend.ops.rotary_embedding import AscendMRotaryEmbedding
-
-
-def fused_mrope_cos_sin_enabled() -> bool:
-    """Return whether MRoPE cos/sin is produced by the fused operator."""
-    return ascend_envs.VLLM_ASCEND_MROPE_INLINE_COS_SIN
 
 
 def get_rotary_inv_freq(rotary_emb: AscendMRotaryEmbedding, device: torch.device) -> torch.Tensor:
