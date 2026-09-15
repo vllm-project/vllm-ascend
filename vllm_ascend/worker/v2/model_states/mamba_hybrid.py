0d7655b7f1a859cd93cc5347e8128b97e0bdd632
windshado <1776438044@qq.com>
2026-09-15 10:42:46 +0800

    update mamba_hybrid
    Signed-off-by: windshado <1776438044@qq.com>

--- vllm_ascend/worker/v2/model_states/mamba_hybrid.py
+++ vllm_ascend/worker/v2/model_states/mamba_hybrid.py
@@ -28,6 +28,7 @@ from vllm.v1.worker.gpu.model_states.mamba_hybrid import (
 )
 from vllm.v1.worker.utils import AttentionGroup
 
+from vllm_ascend.utils import vllm_version_is
 from vllm_ascend.worker.v2.attn_utils import build_attn_metadata
 from vllm_ascend.worker.v2.input_batch import AscendInputBatch
 from vllm_ascend.worker.v2.model_states.default import AscendModelState
@@ -41,8 +42,9 @@ class AscendMambaHybridModelState(MambaHybridModelState, AscendModelState):
     base so cooperative ``super()`` calls retain the Ascend model-state MRO.
     """
 
-    _mamba_group_ids: list[int]
-    _mamba_spec: MambaSpec | None
+    if vllm_version_is("0.28.0"):
+        _mamba_group_ids: list[int]
+        _mamba_spec: MambaSpec | None
 
     def _get_mamba_group_info(self, kv_cache_config: KVCacheConfig) -> tuple[list[int], MambaSpec]:
         if self._mamba_spec is None:
