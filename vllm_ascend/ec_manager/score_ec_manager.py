0a7903654b7178d140e27039416265ef79b6aa4f
joeqth <joe_qth110@163.com>
2026-09-17 20:10:56 +0800

    style: drop stray whitespace line in the cost estimator
    Signed-off-by: joeqth <joe_qth110@163.com>

--- vllm_ascend/ec_manager/score_ec_manager.py
+++ vllm_ascend/ec_manager/score_ec_manager.py
@@ -119,7 +119,6 @@ class VisionEncoderCostEstimator:
         self.hidden_size = hidden_size
         self.feedforward = feedforward
         self.alpha = 4 * hidden_size + 5 * attn_heads
-        
         # TODO: there may be more kinds of compute ways
         model_type = getattr(hf_config, "model_type", None)
         if model_type == "qwen3_5":
