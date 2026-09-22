8a110bccdae61f9e54a5096f1c607666689fafac
Yao Liu <liuyao0929@gmail.com>
2026-09-22 16:17:53 +0800

    fix(xlite): declare DSA index mask on model config
    Signed-off-by: Yao Liu <liuyao0929@gmail.com>

--- vllm_ascend/xlite/utils.py
+++ vllm_ascend/xlite/utils.py
@@ -155,6 +155,8 @@ class XModel(AttributeSetterMixin, Model):
 class XModelConfig(AttributeSetterMixin, ModelConfig):
     """:mod:`xlite._C.ModelConfig` subclass with safe attribute setting for better backwards compatibility."""
 
+    index_full_mask: list[bool]
+
 
 @dataclass
 class AttnMetadataRouter:
