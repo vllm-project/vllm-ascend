4f5b7e3f332d08b22107e869f89c103d63a083c6
tangzhengzhi <t00511855@china.huawei.com>
2026-09-22 16:31:42 +0800

    EP disaggregation enable mooncake store backend

--- vllm_ascend/distributed/ec_transfer/e_mooncake_backend.py
+++ vllm_ascend/distributed/ec_transfer/e_mooncake_backend.py
@@ -54,4 +54,3 @@ class EMooncakeBackend(MooncakeBackend):
         ret = self.store.register_buffer(addr, length)
         if ret != 0:
             raise RuntimeError(f"Failed to register buffer for Mooncake Store, error code: {ret}")
-			
