f9b2451dfc36bddae5faec85e970f9e82e35af96
weiguihua2 <984323595@qq.com>
2026-09-23 14:15:13 +0800

    fix(dcp): restore attention mask buffer type narrowing
    Signed-off-by: weiguihua2 <984323595@qq.com>

--- vllm_ascend/worker/dcp_utils.py
+++ vllm_ascend/worker/dcp_utils.py
@@ -736,6 +736,7 @@ class DCPManager:
         )
 
         if self.speculative_config and not self.vllm_config.model_config.use_mla:
+            assert self.dcp_mtp_attn_mask is not None
             if self.num_decode_reqs > 0:
                 decode_scheduled = num_scheduled_tokens[: self.num_decode_reqs]
                 if fixed_decode_seq_lens_cpu is not None:
