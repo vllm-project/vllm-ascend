4fefabc4a115679441b3bc980e9f0472d22f70e9
xqchen7 <chenxueqing7@huawei.com>
2026-09-23 14:42:53 +0800

    op

--- csrc/moe/chunk_fwd_o/op_kernel/arch20/compat_310p.h
+++ csrc/moe/chunk_fwd_o/op_kernel/arch20/compat_310p.h
@@ -8,6 +8,8 @@
 // Dummy bfloat16_t only needed on 310P (dav_m200) where the compiler
 // doesn't provide a native bf16 type. On 910B/910C the compiler's
 // __clang_cce_types.h already typedefs bfloat16_t from __bf16.
+// CANN9.2: ascend310p also get native bfloat16_t, skip dummy definition
+#if defined(__CCE_AICORE__) && (__CCE_AICORE__ == 200) && !defined(__bfloat16_t_defined) && !defined(__clang_cce_types_h)
 #if defined(__CCE_AICORE__) && (__CCE_AICORE__ == 200) && !defined(__bfloat16_t_defined)
 #define __bfloat16_t_defined
 #define __COMPAT_310P_ACTIVE__
