a8d8ed95043490f7e27fbcf42d25dcdfacc2638b
xqchen7 <chenxueqing7@huawei.com>
2026-09-23 10:20:35 +0800

    Change git and pip mirror configurations
    Updated git and pip configurations to use Huawei Cloud mirrors.
    
    Signed-off-by: xqchen7 <chenxueqing7@huawei.com>

--- tests/e2e/nightly/multi_node/scripts/run.sh
+++ tests/e2e/nightly/multi_node/scripts/run.sh
@@ -141,10 +141,14 @@ check_npu_info() {
 
 check_and_config() {
     echo "====> Configure mirrors and git proxy"
-    git config --global url."https://ghfast.top/https://github.com/".insteadOf "https://github.com/"
-    pip config set global.index-url https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple
+    git config --global url."https://shturl.cc//https://github.com/".insteadOf "https://github.com/"
+    pip config set global.index-url https://mirrors.huaweicloud.com/repository/pypi/simple/
+    pip config set global.trusted-host mirrors.huaweicloud.com
     export PIP_EXTRA_INDEX_URL="https://mirrors.huaweicloud.com/ascend/repos/pypi"
+    pip config set global.trusted-host mirrors.huaweicloud.com
+    pip config set global.timeout 120
 }
+
 
 install_extra_components() {
     echo "====> Installing extra components for DeepSeek-v3.2-exp-bf16"
