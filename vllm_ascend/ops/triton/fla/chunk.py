ba4a5df9aa3bb569430af38bf3c666654e62073f
MikeJiang <1819484473@qq.com>
2026-09-23 11:34:41 +0800

    [Perf] Eliminate redundant transposition
    Signed-off-by: MikeJiang <1819484473@qq.com>

--- vllm_ascend/ops/triton/fla/chunk.py
+++ vllm_ascend/ops/triton/fla/chunk.py
@@ -77,13 +77,13 @@ def chunk_gated_delta_rule_fwd(
         chunk_indices_bt=chunk_indices_chunk64,
         output_dtype=k.dtype,
     )
-    g_tanspose = g.transpose(1, 2).contiguous()
+    g_transpose = g.transpose(1, 2).contiguous()
     w, u = recompute_w_u_fwd(
         k=k,
         v=v,
         beta=beta,
         A=A,
-        g_cumsum=g_tanspose,
+        g_cumsum=g_transpose,
         cu_seqlens=cu_seqlens,
         chunk_indices=chunk_indices_chunk64,
     )
@@ -116,7 +116,7 @@ def chunk_gated_delta_rule_fwd(
         k_ascendc,
         w_ascendc,
         u_ascendc,
-        g=g_tanspose,
+        g=g_transpose,
         gk=None,
         initial_state=initial_state_kern,
         output_final_state=True,
@@ -145,7 +145,7 @@ def chunk_gated_delta_rule_fwd(
             k=k,
             w=w,
             u=u,
-            g=g_tanspose,
+            g=g_transpose,
             cu_seqlens=cu_seqlens,
             chunk_indices=chunk_indices_chunk64,
             chunk_offsets=chunk_offsets_chunk64,
@@ -184,7 +184,7 @@ def chunk_gated_delta_rule_fwd(
                 k=k,
                 w=w,
                 u=u,
-                g=g_tanspose,
+                g=g_transpose,
                 initial_state=rerun_initial_state,
                 output_final_state=True,
                 cu_seqlens=cu_seqlens,
@@ -200,7 +200,7 @@ def chunk_gated_delta_rule_fwd(
         v_new,
         h,
         scale,
-        g=g_tanspose,
+        g=g_transpose,
         g_gamma=None,
         cu_seqlens=cu_seqlens_host,
         chunk_indices=chunk_indices_chunk64_host,
