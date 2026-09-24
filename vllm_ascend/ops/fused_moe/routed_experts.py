fe0abf7335d1f740ec2adef55325dac75e9a8a62
liyishi <1252651434@qq.com>
2026-09-24 12:09:39 +0800

    fix moe
    Signed-off-by: liyishi <1252651434@qq.com>

--- vllm_ascend/ops/fused_moe/routed_experts.py
+++ vllm_ascend/ops/fused_moe/routed_experts.py
@@ -42,7 +42,7 @@ from vllm_ascend.ops.fused_moe.force_eplb import get_force_eplb_topk
 from vllm_ascend.ops.fused_moe.moe_comm_method import (
     AllGatherCommImpl,
     FusedExpertsResult,
-    activate_moe_comm_method, get_moe_comm_method,
+    get_moe_comm_method,
 )
 from vllm_ascend.ops.fused_moe.moe_utils import get_moe_num_logical_experts
 from vllm_ascend.quantization.quant_type import QuantType
@@ -657,10 +657,6 @@ class AscendRoutedExperts(RoutedExperts):  # type: ignore[no-redef]
         if lora_context is not None:
             sync_lora_context(self.quant_method, lora_context)
 
-        # moe_comm_method = activate_moe_comm_method(
-        #     _EXTRA_CTX.moe_comm_type, self.moe_config, _EXTRA_CTX.moe_comm_method
-        # )
-        # assert moe_comm_method is not None
         moe_comm_method = get_moe_comm_method(_EXTRA_CTX.moe_comm_type, self.moe_config)
         _EXTRA_CTX.moe_comm_method = moe_comm_method
         prepare_output = moe_comm_method.prepare(
