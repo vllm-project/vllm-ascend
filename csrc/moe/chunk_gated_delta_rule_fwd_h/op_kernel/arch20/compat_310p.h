79d45b0e91647ed0643daf3adf57dc3f52fa8e2f
xqchen7 <chenxueqing7@huawei.com>
2026-09-24 11:32:05 +0800

    Update compat_310p.h
    Signed-off-by: xqchen7 <chenxueqing7@huawei.com>

--- csrc/moe/chunk_gated_delta_rule_fwd_h/op_kernel/arch20/compat_310p.h
+++ csrc/moe/chunk_gated_delta_rule_fwd_h/op_kernel/arch20/compat_310p.h
@@ -9,7 +9,7 @@
 // doesn't provide a native bf16 type. On 910B/910C the compiler's
 // __clang_cce_types.h already typedefs bfloat16_t from __bf16.
 #if defined(__CCE_AICORE__) && (__CCE_AICORE__ == 200) && !defined(__bfloat16_t_defined)
-#define __bfloat16_t_defined
+#defined(__clang_cce_types_h)
 #define __COMPAT_310P_ACTIVE__
 struct bfloat16_t {
     uint16_t val;
